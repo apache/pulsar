b6f061a7d42324f35f0247aa49bfb62b27607cf9
Rajan <dhabalia.me@gmail.com>
2017-02-17 21:06:49 -0800

    Derive direct memory used count from netty counter (#223)
    * Derive direct memory used count from netty counter
    
    * Fix: avoid initial direct-memory assertion with 0

--- pulsar-broker/src/test/java/com/yahoo/pulsar/broker/stats/BookieClientsStatsGeneratorTest.java
+++ pulsar-broker/src/test/java/com/yahoo/pulsar/broker/stats/BookieClientsStatsGeneratorTest.java
@@ -26,6 +26,10 @@ import org.testng.annotations.Test;
 
 import com.yahoo.pulsar.broker.service.BrokerTestBase;
 import com.yahoo.pulsar.broker.stats.BookieClientStatsGenerator;
+import com.yahoo.pulsar.broker.stats.metrics.JvmMetrics;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
 
 /**
  */
@@ -48,5 +52,34 @@ public class BookieClientsStatsGeneratorTest extends BrokerTestBase {
         // should not generate any NPE or other exceptions..
         Map<String, Map<String, PendingBookieOpsStats>> stats = BookieClientStatsGenerator.generate(super.getPulsar());
         assertEquals((boolean) stats.isEmpty(), true);
+    }
+    
+    @Test
+    public void testJvmDirectMemoryUsedMetric() throws Exception {
+        PooledByteBufAllocator allocator = new PooledByteBufAllocator( //
+                true, // preferDirect
+                0, // nHeapArenas,
+                1, // nDirectArena
+                8192, // pageSize
+                11, // maxOrder
+                64, // tinyCacheSize
+                32, // smallCacheSize
+                8 // normalCacheSize
+        );
+        int allocateMemory = 17777216;
+        long directMemory1 = JvmMetrics.getJvmDirectMemoryUsed();
+        ByteBuf buf2 = allocator.directBuffer(allocateMemory, allocateMemory);
+        long directMemory2 = JvmMetrics.getJvmDirectMemoryUsed();
+        assertEquals(directMemory2, directMemory1 + allocateMemory);
+        ByteBuf buf3 = allocator.directBuffer(allocateMemory, allocateMemory);
+        long directMemory3 = JvmMetrics.getJvmDirectMemoryUsed();
+        assertEquals(directMemory3, directMemory2 + allocateMemory);
+        buf3.release();
+        directMemory3 = JvmMetrics.getJvmDirectMemoryUsed();
+        assertEquals(directMemory3, directMemory2);
+        buf2.release();
+        directMemory2 = JvmMetrics.getJvmDirectMemoryUsed();
+        assertEquals(directMemory2, directMemory1);
+        
     }
 }
