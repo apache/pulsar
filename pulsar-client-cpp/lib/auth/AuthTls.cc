a8284825802bdad9c0a8201f8b5f325de8dce37d
Yuki Shiga <yushiga@yahoo-corp.jp>
2017-02-25 03:28:41 +0900

    Add TLS authentication end to end tests (#224)
    * Make C++ tls auth api same as Java
    
    * Add tls auth test for Java client
    
    * Add tls auth test for C++ client

--- pulsar-client-cpp/lib/auth/AuthTls.cc
+++ pulsar-client-cpp/lib/auth/AuthTls.cc
@@ -18,47 +18,47 @@
 
 namespace pulsar {
     AuthDataTls::AuthDataTls(ParamMap& params) {
-        tlsCertificates_ = params["tlsCertificates"];
-        tlsPrivateKey_ = params["tlsPrivateKey"];
+        tlsCertificates_ = params["tlsCertFile"];
+        tlsPrivateKey_ = params["tlsKeyFile"];
     }
-    
+
     AuthDataTls::~AuthDataTls() {
-        
+
     }
-    
+
     bool AuthDataTls::hasDataForTls() {
         return true;
     }
-    
+
     std::string AuthDataTls::getTlsCertificates() {
         return tlsCertificates_;
     }
-    
+
     std::string AuthDataTls::getTlsPrivateKey() {
         return tlsPrivateKey_;
     }
-    
+
     AuthTls::AuthTls(AuthenticationDataPtr& authDataTls) {
         authDataTls_ = authDataTls;
     }
-    
+
     AuthTls::~AuthTls() {
     }
-    
+
     AuthenticationPtr AuthTls::create(ParamMap& params) {
         AuthenticationDataPtr authDataTls = AuthenticationDataPtr(new AuthDataTls(params));
         return AuthenticationPtr(new AuthTls(authDataTls));
     }
-    
+
     const std::string AuthTls::getAuthMethodName() const {
         return "tls";
     }
-    
+
     Result AuthTls::getAuthData(AuthenticationDataPtr& authDataContent) const {
         authDataContent = authDataTls_;
         return ResultOk;
     }
-    
+
     extern "C" Authentication* create(ParamMap& params) {
         AuthenticationDataPtr authDataTls = AuthenticationDataPtr(new AuthDataTls(params));
         return new AuthTls(authDataTls);
