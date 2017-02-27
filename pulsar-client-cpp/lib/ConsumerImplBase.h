ae381b4cb956b60b57e3d45a1a5b5f84ddaaec34
jai1 <jai1@users.noreply.github.com>
2017-02-27 09:52:13 -0800

    Get ConsumerStats using Binary Protocol (#216)

--- pulsar-client-cpp/lib/ConsumerImplBase.h
+++ pulsar-client-cpp/lib/ConsumerImplBase.h
@@ -47,6 +47,7 @@ public:
     virtual void redeliverUnacknowledgedMessages() = 0;
     virtual const std::string& getName() const = 0;
     virtual int getNumOfPrefetchedMessages() const = 0;
+    virtual Result getConsumerStats(BrokerConsumerStats& brokerConsumerStats, int partitionIndex = -1) = 0;
 };
 }
 #endif //PULSAR_CONSUMER_IMPL_BASE_HEADER
