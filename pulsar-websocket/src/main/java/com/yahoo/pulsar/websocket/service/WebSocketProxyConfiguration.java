3b99f3047e3ec5332804ce3a82b6f99b8a33e4fe
Yuki Shiga <yushiga@yahoo-corp.jp>
2017-04-20 04:42:26 +0900

    Use binary protocol lookup for connection between WebSocket proxy and broker (#363)

--- pulsar-websocket/src/main/java/com/yahoo/pulsar/websocket/service/WebSocketProxyConfiguration.java
+++ pulsar-websocket/src/main/java/com/yahoo/pulsar/websocket/service/WebSocketProxyConfiguration.java
@@ -31,6 +31,8 @@ public class WebSocketProxyConfiguration implements PulsarConfiguration {
     // Pulsar cluster url to connect to broker (optional if globalZookeeperServers present)
     private String serviceUrl;
     private String serviceUrlTls;
+    private String brokerServiceUrl;
+    private String brokerServiceUrlTls;
 
     // Global Zookeeper quorum connection string
     private String globalZookeeperServers;
@@ -94,6 +96,22 @@ public class WebSocketProxyConfiguration implements PulsarConfiguration {
 
     public void setServiceUrlTls(String serviceUrlTls) {
         this.serviceUrlTls = serviceUrlTls;
+    }
+
+    public String getBrokerServiceUrl() {
+        return brokerServiceUrl;
+    }
+
+    public void setBrokerServiceUrl(String brokerServiceUrl) {
+        this.brokerServiceUrl = brokerServiceUrl;
+    }
+
+    public String getBrokerServiceUrlTls() {
+        return brokerServiceUrlTls;
+    }
+
+    public void setBrokerServiceUrlTls(String brokerServiceUrlTls) {
+        this.brokerServiceUrlTls = brokerServiceUrlTls;
     }
 
     public String getGlobalZookeeperServers() {
