26963dc73be053ba3958ea31a314986682d5deba
Rajan <dhabalia.me@gmail.com>
2017-04-21 13:54:27 -0700

    BrokerService should not remove failed topic from map with the same thread which adds in map to prevent deadlock (#375)

--- pulsar-broker/src/main/java/com/yahoo/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/com/yahoo/pulsar/broker/service/BrokerService.java
@@ -471,6 +471,7 @@ public class BrokerService implements Closeable, ZooKeeperCacheListener<Policies
             // namespace is being unloaded
             String msg = String.format("Namespace is being unloaded, cannot add topic %s", topic);
             log.warn(msg);
+            pulsar.getExecutor().submit(() -> topics.remove(topic, topicFuture));
             topicFuture.completeExceptionally(new ServiceUnitNotReadyException(msg));
             return;
         }
@@ -514,7 +515,9 @@ public class BrokerService implements Closeable, ZooKeeperCacheListener<Policies
 
         }).exceptionally((exception) -> {
             log.warn("[{}] Failed to get topic configuration: {}", topic, exception.getMessage(), exception);
-            topics.remove(topic, topicFuture);
+            // remove topic from topics-map in different thread to avoid possible deadlock if
+            // createPersistentTopic-thread only tries to handle this future-result
+            pulsar.getExecutor().submit(() -> topics.remove(topic, topicFuture));
             topicFuture.completeExceptionally(exception);
             return null;
         });
