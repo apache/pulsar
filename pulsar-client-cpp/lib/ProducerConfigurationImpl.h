c763b3cabf3d75e499f501c045f11dc3546d4207
jai1 <jai1@yahoo-inc.com>
2017-06-01 17:17:28 +0000

    Moving pending queue size changes to a different commit

--- pulsar-client-cpp/lib/ProducerConfigurationImpl.h
+++ pulsar-client-cpp/lib/ProducerConfigurationImpl.h
@@ -36,7 +36,7 @@ struct ProducerConfigurationImpl {
     ProducerConfigurationImpl()
             : sendTimeoutMs(30000),
               compressionType(CompressionNone),
-              maxPendingMessages(30000),
+              maxPendingMessages(1000),
               routingMode(ProducerConfiguration::UseSinglePartition),
               blockIfQueueFull(false),
               batchingEnabled(false),
