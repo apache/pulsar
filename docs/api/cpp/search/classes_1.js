84038f2f4bfc2ccb1c969e83e72c7d08eaa2ef8e
Luc Perkins <lucperkins@gmail.com>
2017-06-22 22:20:21 -0700

    re-add api_docs build command to make build

---
+++ docs/api/cpp/search/classes_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['batchmessageid',['BatchMessageId',['../classpulsar_1_1_batch_message_id.html',1,'pulsar']]],
+  ['brokerconsumerstats',['BrokerConsumerStats',['../classpulsar_1_1_broker_consumer_stats.html',1,'pulsar']]]
+];
