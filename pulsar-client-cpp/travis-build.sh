cb8bbb5e271035a116228783ba77e012d1a1618f
jai1 <jai1@users.noreply.github.com>
2017-06-27 14:53:58 -0700

    C++ Client - Changed compiler to default version (4.8.4) (#529)

--- pulsar-client-cpp/travis-build.sh
+++ pulsar-client-cpp/travis-build.sh
@@ -58,10 +58,10 @@ exec_cmd() {
 
 if [ "$3" = "all" -o "$3" = "dep" ]; then
   # Install dependant packages
-  exec_cmd "apt-get update && apt-get install -y cmake gcc-4.4 cpp-4.4 gcc-4.4-base libssl-dev libcurl4-openssl-dev liblog4cxx10-dev libprotobuf-dev libboost1.55-all-dev libgtest-dev libxml2-utils g++-4.4 libjsoncpp-dev";
+  exec_cmd "apt-get update && apt-get install -y cmake libssl-dev libcurl4-openssl-dev liblog4cxx10-dev libprotobuf-dev libboost1.55-all-dev libgtest-dev libxml2-utils libjsoncpp-dev";
   if [ ! -f "$1/libgtest.a" ]; then
     echo "Not Found: $1/libgtest.a"
-    exec_cmd "pushd /usr/src/gtest && CC=gcc-4.4 CXX=g++-4.4 cmake . && make && cp libgtest.a $1/ && popd";
+    exec_cmd "pushd /usr/src/gtest && cmake . && make && cp libgtest.a $1/ && popd";
   fi
   if [ ! -d "$1/protobuf-2.6.1/" ]; then
     echo "Not Found: $1/protobuf-2.6.1/"
@@ -77,7 +77,7 @@ if [ "$3" = "all" -o "$3" = "compile" ]; then
   export PATH=$PATH:$1/
   # Compile and run unit tests
   pushd $2/pulsar-client-cpp
-  CC=gcc-4.4 CXX=g++-4.4 cmake . && make
+  cmake . && make
   if [ $? -ne 0 ]; then
     echo "Failed to compile CPP client library"
     exit 1
