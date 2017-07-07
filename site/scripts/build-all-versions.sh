379c8ba9348baeab0a1f41128864380e2574828b
Luc Perkins <lucperkins@gmail.com>
2017-07-06 17:18:45 -0700

    change site build script

--- site/scripts/build-all-versions.sh
+++ site/scripts/build-all-versions.sh
@@ -6,3 +6,12 @@ for version in $(cat VERSIONS); do
   bundle _${BUNDLER_VERSION}_ exec jekyll build \
     --config _config.yml,docs/$version/_config.version.yml
 done
+
+git checkout asf-site
+
+(
+  cd $(git rev-parse --show-toplevel)
+  mv generated-site/* .
+)
+
+git checkout master
