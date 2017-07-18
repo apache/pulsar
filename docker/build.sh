c21e08fb684ce76b7e70266b7d94bb8466814d0f
Matteo Merli <mmerli@apache.org>
2017-07-18 16:32:09 -0700

    Build and publish docker images (#575)

--- docker/build.sh
+++ docker/build.sh
@@ -18,14 +18,10 @@
 # under the License.
 #
 
-# Get the project version from Maven
-pushd .. > /dev/null
-MVN_VERSION=$(mvn -q \
-    -Dexec.executable="echo" \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-popd > /dev/null
+ROOT_DIR=$(git rev-parse --show-toplevel)
+cd $ROOT_DIR/docker
+
+MVN_VERSION=`./get-version.sh`
 
 echo "Pulsar version: ${MVN_VERSION}"
 
@@ -54,14 +50,14 @@ rm pulsar-${MVN_VERSION}-bin.tar.gz
 
 
 # Build pulsar-grafana image
-docker build -t pulsar-grafana grafana
+docker build -t pulsar-grafana:latest grafana
 if [ $? != 0 ]; then
     echo "Error: Failed to create Docker image for pulsar-grafana"
     exit 1
 fi
 
 # Build dashboard docker image
-docker build -t pulsar-dashboard ../dashboard
+docker build -t pulsar-dashboard:latest ../dashboard
 if [ $? != 0 ]; then
     echo "Error: Failed to create Docker image for pulsar-dashboard"
     exit 1
