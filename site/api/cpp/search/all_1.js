670c176b4d3a60f109c12c099b522316a9cb50b6
Matteo Merli <mmerli@apache.org>
2017-07-31 18:57:58 -0700

    Docker image to build website in Jenkins

--- site/api/cpp/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['batchmessageid',['BatchMessageId',['../classpulsar_1_1_batch_message_id.html',1,'pulsar']]],
-  ['brokerconsumerstats',['BrokerConsumerStats',['../classpulsar_1_1_broker_consumer_stats.html',1,'pulsar']]],
-  ['build',['build',['../classpulsar_1_1_message_builder.html#a096971441b85caead9602670d7b901d6',1,'pulsar::MessageBuilder']]]
-];
--- site/api/cpp/search/classes_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['batchmessageid',['BatchMessageId',['../classpulsar_1_1_batch_message_id.html',1,'pulsar']]],
-  ['brokerconsumerstats',['BrokerConsumerStats',['../classpulsar_1_1_broker_consumer_stats.html',1,'pulsar']]]
-];
