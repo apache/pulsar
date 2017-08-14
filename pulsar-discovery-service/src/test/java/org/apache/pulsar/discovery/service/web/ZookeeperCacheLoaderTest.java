c2c3fb3efbe99ef1da253b56fd3486804fbf2bab
Rajan Dhabalia <rdhabalia@apache.org>
2017-08-14 14:14:49 -0700

    fix list collection import (#688)

--- pulsar-discovery-service/src/test/java/org/apache/pulsar/discovery/service/web/ZookeeperCacheLoaderTest.java
+++ pulsar-discovery-service/src/test/java/org/apache/pulsar/discovery/service/web/ZookeeperCacheLoaderTest.java
@@ -34,7 +34,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.beust.jcommander.internal.Lists;
+import com.google.common.collect.Lists;
 
 public class ZookeeperCacheLoaderTest extends BaseZKStarterTest {
 
