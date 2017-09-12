3a775cf0538680b6128f8caa666e9733cff38f35
Rajan Dhabalia <rdhabalia@apache.org>
2017-09-12 12:01:45 -0700

    Add msg ack-hole metrics to internal-stats and broker-stats (#750)

--- pulsar-common/src/main/java/org/apache/pulsar/common/policies/data/PersistentTopicInternalStats.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/policies/data/PersistentTopicInternalStats.java
@@ -59,7 +59,9 @@ public class PersistentTopicInternalStats {
         public String individuallyDeletedMessages;
         public String lastLedgerSwitchTimestamp;
         public String state;
-
+        public long numberOfEntriesSinceFirstNotAckedMessage;
+        public int totalNonContiguousDeletedMessagesRange;
+        
         public Map<String, Long> properties;
     }
 
