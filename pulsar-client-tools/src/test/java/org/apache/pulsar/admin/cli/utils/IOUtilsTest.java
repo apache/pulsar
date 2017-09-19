f3e08ac45679e418c18b5dea431b95d3293f2235
Matteo Merli <mmerli@apache.org>
2017-09-19 11:30:15 -0700

    [#766] - Disabling IOUtilsTest.test5() for failures

--- pulsar-client-tools/src/test/java/org/apache/pulsar/admin/cli/utils/IOUtilsTest.java
+++ pulsar-client-tools/src/test/java/org/apache/pulsar/admin/cli/utils/IOUtilsTest.java
@@ -97,7 +97,9 @@ public class IOUtilsTest {
         }
     }
 
-    @Test(description = "Should go into infinte loop since j is an invalid response.")
+    // TODO: Disabled because it fails with maven surefire 2.20
+    // https://github.com/apache/incubator-pulsar/issues/766
+    @Test(enabled = false, description = "Should go into infinte loop since j is an invalid response.")
     public void test5() {
         try {
             String data = "j";
