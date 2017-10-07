64c81e9662f37c07434a69505538d999da97ee12
kyontan <kyontan@monora.me>
2017-10-08 00:57:03 +0900

    Add method to returns bytes type in Python client (#812)

--- pulsar-client-cpp/python/src/message.cc
+++ pulsar-client-cpp/python/src/message.cc
@@ -38,6 +38,10 @@ std::string Message_str(const Message& msg) {
     return ss.str();
 }
 
+boost::python::object Message_data(const Message& msg) {
+    return boost::python::object(boost::python::handle<>(PyBytes_FromStringAndSize((const char*)msg.getData(), msg.getLength())));
+}
+
 const BatchMessageId& Message_getMessageId(const Message& msg) {
     return static_cast<const BatchMessageId&>(msg.getMessageId());
 }
@@ -76,7 +80,7 @@ void export_message() {
 
     class_<Message>("Message")
             .def("properties", &Message::getProperties, return_value_policy<copy_const_reference>())
-            .def("data", &Message::getDataAsString)
+            .def("data", &Message_data)
             .def("length", &Message::getLength)
             .def("partition_key", &Message::getPartitionKey, return_value_policy<copy_const_reference>())
             .def("publish_timestamp", &Message::getPublishTimestamp)
