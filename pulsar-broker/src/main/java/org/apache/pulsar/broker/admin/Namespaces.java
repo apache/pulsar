8ba20df0f6afc7fc9dcf9f82f7af75efb875cfd9
Matteo Merli <mmerli@apache.org>
2017-10-24 16:34:31 -0700

    Allow to configure the default number of bundles for new namespaces

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/Namespaces.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/Namespaces.java
@@ -217,6 +217,9 @@ public class Namespaces extends AdminResource {
                 } else {
                     policies.bundles = validateBundlesData(initialBundles);
                 }
+            } else {
+                int defaultNumberOfBundles = config().getDefaultNumberOfNamespaceBundles();
+                policies.bundles = getBundles(defaultNumberOfBundles);
             }
 
             zkCreateOptimistic(path(POLICIES, property, cluster, namespace),
@@ -1170,7 +1173,7 @@ public class Namespaces extends AdminResource {
                     (persistence.getBookkeeperEnsemble() >= persistence.getBookkeeperWriteQuorum())
                             && (persistence.getBookkeeperWriteQuorum() >= persistence.getBookkeeperAckQuorum()),
                     "Bookkeeper Ensemble (%s) >= WriteQuorum (%s) >= AckQuoru (%s)", persistence.getBookkeeperEnsemble(),
-                    persistence.getBookkeeperWriteQuorum(), persistence.getBookkeeperAckQuorum());            
+                    persistence.getBookkeeperWriteQuorum(), persistence.getBookkeeperAckQuorum());
         }catch(NullPointerException | IllegalArgumentException e) {
             throw new RestException(Status.PRECONDITION_FAILED, e.getMessage());
         }
