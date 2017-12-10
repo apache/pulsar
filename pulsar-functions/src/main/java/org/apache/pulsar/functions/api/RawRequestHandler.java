40cda226b8f060a63965c53f67eb48068a808970
Sijie Guo <guosijie@gmail.com>
2017-12-10 13:47:44 -0800

    Only test pulsar-functions (#5)

--- pulsar-functions/src/main/java/org/apache/pulsar/functions/api/RawRequestHandler.java
+++ pulsar-functions/src/main/java/org/apache/pulsar/functions/api/RawRequestHandler.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,14 +7,20 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
+package org.apache.pulsar.functions.api;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * This is the more generic function interface compared to the RequestHandler.
@@ -23,11 +29,6 @@
  * deserialize this in any way they want before processing. Any output goes into
  * OutputStream.
  */
-package org.apache.pulsar.functions.api;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-
 public interface RawRequestHandler {
     /**
      * Process the input coming as an input stream. The process function
