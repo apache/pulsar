468c6e10d64472816a3bbd4a34eacd115ee58261
Sijie Guo <guosijie@gmail.com>
2017-12-10 13:47:44 -0800

    Only test pulsar-functions (#5)

--- pulsar-functions/src/main/java/org/apache/pulsar/functions/instance/ContextImpl.java
+++ pulsar-functions/src/main/java/org/apache/pulsar/functions/instance/ContextImpl.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,23 +7,23 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * This class implements the Context interface exposed to the user.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.pulsar.functions.instance;
 
 import org.apache.pulsar.functions.api.Context;
 import org.slf4j.Logger;
 
+/**
+ * This class implements the Context interface exposed to the user.
+ */
 class ContextImpl implements Context {
     private JavaInstanceConfig config;
     private Logger logger;
