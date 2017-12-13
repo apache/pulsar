cace6c8eee0896eb626c19e36c68dd138619068a
Luc Perkins <lucperkins@gmail.com>
2017-12-12 17:32:05 -0800

    advertise pulsar host public IPs

--- deployment/terraform-ansible/aws/output.tf
+++ deployment/terraform-ansible/aws/output.tf
@@ -1,7 +1,15 @@
-output "pulsar_connection_url" {
+output "dns_name" {
+  value = "${aws_elb.default.dns_name}"
+}
+
+output "pulsar_service_url" {
   value = "pulsar://${aws_elb.default.dns_name}:6650"
 }
 
-output "pulsar_ssh_hosts" {
-  value = "${aws_instance.pulsar.*.public_ip}"
+output "pulsar_web_url" {
+  value = "http://${aws_elb.default.dns_name}:8080"
+}
+
+output "pulsar_ssh_host" {
+  value = "${aws_instance.pulsar.0.public_ip}"
 }
