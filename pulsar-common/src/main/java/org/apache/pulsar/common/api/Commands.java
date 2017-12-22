4141b46535e7c8d411fa164d0c1bb84045dd78f4
Chris <chris@Chriss-MacBook-Pro.local>
2017-12-22 15:04:37 -0800

    Remove unused code block.

--- pulsar-common/src/main/java/org/apache/pulsar/common/api/Commands.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/api/Commands.java
@@ -22,11 +22,8 @@ import static org.apache.pulsar.checksum.utils.Crc32cChecksum.computeChecksum;
 import static org.apache.pulsar.checksum.utils.Crc32cChecksum.resumeChecksum;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.apache.pulsar.common.api.proto.PulsarApi;
 import org.apache.pulsar.common.api.proto.PulsarApi.AuthMethod;
@@ -403,22 +400,6 @@ public class Commands {
         closeProducer.recycle();
         return res;
     }
-
-    /*public static ByteBuf newProducer(String topic, long producerId, long requestId, String producerName) {
-        CommandProducer.Builder producerBuilder = CommandProducer.newBuilder();
-        producerBuilder.setTopic(topic);
-        producerBuilder.setProducerId(producerId);
-        producerBuilder.setRequestId(requestId);
-        if (producerName != null) {
-            producerBuilder.setProducerName(producerName);
-        }
-
-        CommandProducer producer = producerBuilder.build();
-        ByteBuf res = serializeWithSize(BaseCommand.newBuilder().setType(Type.PRODUCER).setProducer(producer));
-        producerBuilder.recycle();
-        producer.recycle();
-        return res;
-    }*/
 
     public static ByteBuf newProducer(String topic, long producerId, long requestId, String producerName,
         Map<String, String> metadata) {
