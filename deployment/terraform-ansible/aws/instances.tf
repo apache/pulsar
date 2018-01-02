3ed91c026a57ea55acb2fb728b4b27f736936cdc
Luc Perkins <lucperkins@gmail.com>
2018-01-02 00:49:10 -0800

    Provide an Ansible playbook for AWS with documentation (#920)

---
+++ deployment/terraform-ansible/aws/instances.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "zookeeper" {
+  ami                    = "${var.aws_ami}"
+  instance_type          = "${var.instance_types["zookeeper"]}"
+  key_name               = "${aws_key_pair.default.id}"
+  subnet_id              = "${aws_subnet.default.id}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  count                  = "${var.num_zookeeper_nodes}"
+
+  tags {
+    Name = "zookeeper-${count.index + 1}"
+  }
+}
+
+resource "aws_instance" "pulsar" {
+  ami                         = "${var.aws_ami}"
+  instance_type               = "${var.instance_types["pulsar"]}"
+  key_name                    = "${aws_key_pair.default.id}"
+  subnet_id                   = "${aws_subnet.default.id}"
+  vpc_security_group_ids      = ["${aws_security_group.default.id}"]
+  count                       = "${var.num_pulsar_brokers}"
+
+  tags {
+    Name = "pulsar-${count.index + 1}"
+  }
+
+  associate_public_ip_address = true
+}
