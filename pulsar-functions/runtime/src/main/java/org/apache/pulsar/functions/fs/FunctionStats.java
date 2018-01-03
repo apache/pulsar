43b96010cf7319238d407c15b0a8be4439a5f4c7
Sanjeev Kulkarni <sanjeevrk@gmail.com>
2018-01-03 14:20:16 -0800

    Moved FunctionStats to fs since it does not belong to api (#64)

--- pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/fs/FunctionStats.java
+++ pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/fs/FunctionStats.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pulsar.functions.api;
+package org.apache.pulsar.functions.fs;
 
 import java.io.Serializable;
 
