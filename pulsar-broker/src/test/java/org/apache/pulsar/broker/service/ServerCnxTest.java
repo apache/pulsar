a96bcbf873dcf214ca15b0551473746152f2ef27
Ivan Kelly <ivank@apache.org>
2018-01-04 09:21:03 +0100

    Acknowledgement with properties for RawReader (#1006)
    * Acknowledgement with properties for RawReader
    
    This will be used by the compactor to update the topic with the
    compacted ledger. There will be a cursor/subscription owned by the
    compactor which keeps track of where we have compacted until, and the
    ID of the ledger into which the compacted data has been written.
    
    This change exposes the cursor's String->Long properties map which is
    already available for Producer sequence tracking. This is only exposed
    through the RawReader, which is part of the broker module. Clients
    will not be able to use it.
    
    * Review comments from @sijie
    
    * Updated protobuf for recycler

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ServerCnxTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ServerCnxTest.java
@@ -37,6 +37,7 @@ import static org.testng.Assert.assertTrue;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
@@ -1175,7 +1176,7 @@ public class ServerCnxTest {
         PositionImpl pos = new PositionImpl(0, 0);
 
         clientCommand = Commands.newAck(1 /* consumer id */, pos.getLedgerId(), pos.getEntryId(), AckType.Individual,
-                null);
+                                        null, Collections.emptyMap());
         channel.writeInbound(clientCommand);
 
         // verify nothing is sent out on the wire after ack
