6e8d14e843badd8086f0dd35aa73d72576675803
Sijie Guo <guosijie@gmail.com>
2018-01-25 15:23:23 -0800

    localrun uses broker admin url and remove empty module (#160)

--- pulsar-functions/run-publish-example.sh
+++ pulsar-functions/run-publish-example.sh
@@ -17,9 +17,9 @@
 # under the License.
 #
 
-bin/pulsar-functions functions localrun \
+bin/pulsar-functions --admin-url http://localhost:8080 functions localrun \
     --function-config conf/example.yml \
     --source-topics persistent://sample/standalone/ns1/test_src \
     --input-serde-classnames org.apache.pulsar.functions.api.utils.Utf8StringSerDe \
     --function-classname org.apache.pulsar.functions.api.examples.LoggingFunction \
-    --jar `pwd`/api-examples/target/pulsar-functions-api-examples.jar
+    --jar `pwd`/java-examples/target/pulsar-functions-api-examples.jar
