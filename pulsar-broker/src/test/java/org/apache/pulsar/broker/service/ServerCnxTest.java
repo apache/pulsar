8d04a42e4cb0e862f0346ead98c228b881cfef53
Ivan Kelly <ivank@apache.org>
2018-02-19 18:12:00 +0100

    Read from compacted topic ledger if available and enabled (#1231)
    * Read from compacted topic ledger if available and enabled
    
    If a topic has been compacted and the client has enabled reads from
    compacted topics, try to read from the compacted ledger if the cursor
    position lands before or within the range of message IDs in the
    compacted topic ledger. If the cursor position lands after the message
    IDs, in the compacted topic ledger, read from the cursor as normal.
    
    * fixup mocks in tests

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ServerCnxTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ServerCnxTest.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.broker.service;
 
+import static org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest.createMockBookKeeper;
 import static org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest.createMockZooKeeper;
 import static org.apache.pulsar.broker.cache.ConfigurationCacheService.POLICIES;
 import static org.mockito.Matchers.any;
@@ -153,6 +154,7 @@ public class ServerCnxTest {
 
         ZooKeeper mockZk = createMockZooKeeper();
         doReturn(mockZk).when(pulsar).getZkClient();
+        doReturn(createMockBookKeeper(mockZk)).when(pulsar).getBookKeeperClient();
 
         configCacheService = mock(ConfigurationCacheService.class);
         @SuppressWarnings("unchecked")
