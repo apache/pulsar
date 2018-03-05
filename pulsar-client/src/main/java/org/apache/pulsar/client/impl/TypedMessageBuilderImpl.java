8e7171e3786db84608907dac91d7f61de82e3efe
Matteo Merli <mmerli@apache.org>
2018-03-05 15:34:45 -0800

    Introduced TypedMessageBuilder

---
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/TypedMessageBuilderImpl.java
@@ -0,0 +1,119 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.client.impl;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.base.Preconditions;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.client.api.SchemaSerializationException;
+import org.apache.pulsar.client.api.TypedMessageBuilder;
+import org.apache.pulsar.common.api.proto.PulsarApi.KeyValue;
+import org.apache.pulsar.common.api.proto.PulsarApi.MessageMetadata;
+
+public class TypedMessageBuilderImpl<T> implements TypedMessageBuilder<T> {
+    private static final ByteBuffer EMPTY_CONTENT = ByteBuffer.allocate(0);
+
+    private final ProducerBase<T> producer;
+    private final MessageMetadata.Builder msgMetadataBuilder = MessageMetadata.newBuilder();
+    private final Schema<T> schema;
+    private ByteBuffer content;
+
+    public TypedMessageBuilderImpl(ProducerBase producer, Schema schema) {
+        this.producer = producer;
+        this.schema = schema;
+        this.content = EMPTY_CONTENT;
+    }
+
+    @Override
+    public MessageId send() throws PulsarClientException {
+        return producer.send((Message<T>) MessageImpl.create(msgMetadataBuilder, content, schema));
+    }
+
+    @Override
+    public CompletableFuture<MessageId> sendAsync() {
+        return producer.internalSendAsync((Message<T>) MessageImpl.create(msgMetadataBuilder, content, schema));
+    }
+
+    @Override
+    public TypedMessageBuilder<T> key(String key) {
+        msgMetadataBuilder.setPartitionKey(key);
+        return this;
+    }
+
+    @Override
+    public TypedMessageBuilder<T> value(T value) throws SchemaSerializationException {
+        this.content = ByteBuffer.wrap(schema.encode(value));
+        return this;
+    }
+
+    @Override
+    public TypedMessageBuilder<T> property(String name, String value) {
+        msgMetadataBuilder.addProperties(KeyValue.newBuilder().setKey(name).setValue(value).build());
+        return this;
+    }
+
+    @Override
+    public TypedMessageBuilder<T> properties(Map<String, String> properties) {
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            msgMetadataBuilder
+                    .addProperties(KeyValue.newBuilder().setKey(entry.getKey()).setValue(entry.getValue()).build());
+        }
+
+        return this;
+    }
+
+    @Override
+    public TypedMessageBuilder<T> eventTime(long timestamp) {
+        checkArgument(timestamp > 0, "Invalid timestamp : '%s'", timestamp);
+        msgMetadataBuilder.setEventTime(timestamp);
+        return this;
+    }
+
+    @Override
+    public TypedMessageBuilder<T> sequenceId(long sequenceId) {
+        checkArgument(sequenceId >= 0);
+        msgMetadataBuilder.setSequenceId(sequenceId);
+        return this;
+    }
+
+    @Override
+    public TypedMessageBuilder<T> replicationClusters(List<String> clusters) {
+        Preconditions.checkNotNull(clusters);
+        msgMetadataBuilder.clearReplicateTo();
+        msgMetadataBuilder.addAllReplicateTo(clusters);
+        return this;
+    }
+
+    @Override
+    public TypedMessageBuilder<T> disableReplication() {
+        msgMetadataBuilder.clearReplicateTo();
+        msgMetadataBuilder.addReplicateTo("__local__");
+        return this;
+    }
+}
