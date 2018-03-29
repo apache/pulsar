d05b5a5d382a3cd82c2ee9290363ec901b454056
jai1 <jai1@apache.org>
2018-03-29 14:26:33 -0700

    Pulsar Proxy - Added Prometheus metrics for throttled connections and lookups.

--- pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyConnectionThrottlingTest.java
+++ pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyConnectionThrottlingTest.java
@@ -74,6 +74,7 @@ public class ProxyConnectionThrottlingTest extends MockedPulsarServiceBaseTest {
         PulsarClient client2 = PulsarClient.builder().serviceUrl("pulsar://localhost:" + proxyConfig.getServicePort())
                 .build();
         Producer<byte[]> producer2;
+        Assert.assertEquals(ProxyConnection.rejectedConnections.get(), 0.0d);
         try {
             producer2 = client2.newProducer().topic("persistent://sample/test/local/producer-topic-1").create();
             producer2.send("Message 1".getBytes());
@@ -81,6 +82,7 @@ public class ProxyConnectionThrottlingTest extends MockedPulsarServiceBaseTest {
         } catch (Exception ex) {
             // OK
         }
+        Assert.assertEquals(ProxyConnection.rejectedConnections.get(), 1.0d);
     }
     
     private static final Logger LOG = LoggerFactory.getLogger(ProxyConnectionThrottlingTest.class);
