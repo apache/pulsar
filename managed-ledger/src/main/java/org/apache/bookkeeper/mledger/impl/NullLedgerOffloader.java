0dec2864d02b171ab214e5cebd45c26e28adb485
Ivan Kelly <ivank@apache.org>
2018-04-11 12:16:09 +0200

    LedgerOffloader interface for ManagedLedger (#1542)
    An interface which can be used for offloading ledgers to longterm
    storage.
    
    Master issue #1511

---
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/NullLedgerOffloader.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.bookkeeper.mledger.impl;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.bookkeeper.client.api.ReadHandle;
+import org.apache.bookkeeper.mledger.LedgerOffloader;
+
+/**
+ * Null implementation that throws an error on any invokation.
+ */
+public class NullLedgerOffloader implements LedgerOffloader {
+    public static NullLedgerOffloader INSTANCE = new NullLedgerOffloader();
+
+    @Override
+    public CompletableFuture<byte[]> offload(ReadHandle ledger,
+                                             Map<String, String> extraMetadata) {
+        CompletableFuture<byte[]> promise = new CompletableFuture<>();
+        promise.completeExceptionally(new UnsupportedOperationException());
+        return promise;
+    }
+
+    @Override
+    public CompletableFuture<ReadHandle> readOffloaded(long ledgerId, byte[] offloadContext) {
+        CompletableFuture<ReadHandle> promise = new CompletableFuture<>();
+        promise.completeExceptionally(new UnsupportedOperationException());
+        return promise;
+    }
+
+    @Override
+    public CompletableFuture<Void> deleteOffloaded(long ledgerId, byte[] offloadContext) {
+        CompletableFuture<Void> promise = new CompletableFuture<>();
+        promise.completeExceptionally(new UnsupportedOperationException());
+        return promise;
+    }
+}
