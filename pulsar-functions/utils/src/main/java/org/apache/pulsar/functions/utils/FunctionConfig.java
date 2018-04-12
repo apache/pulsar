072c4a163e0fa6a4393b5c4c612322abe09ca5e6
Jerry Peng <jerry.boyang.peng@gmail.com>
2018-04-11 18:16:19 -0700

    moving FunctionConfig to different module

--- pulsar-functions/utils/src/main/java/org/apache/pulsar/functions/utils/FunctionConfig.java
+++ pulsar-functions/utils/src/main/java/org/apache/pulsar/functions/utils/FunctionConfig.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pulsar.functions.api;
+package org.apache.pulsar.functions.utils;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
