235dd429891b5d4bb556a0ff991a57bbf26d633b
Jerry Peng <jerry.boyang.peng@gmail.com>
2018-04-19 22:29:20 -0700

    adding pull source interface

---
+++ pulsar-connect/core/src/main/java/org/apache/pulsar/connect/core/Source.java
@@ -0,0 +1,38 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.connect.core;
+
+import java.util.Map;
+
+public interface Source<T> extends AutoCloseable {
+    /**
+     * Open connector with configuration
+     *
+     * @param config initialization config
+     * @throws Exception IO type exceptions when opening a connector
+     */
+    void open(final Map<String, String> config) throws Exception;
+
+    /**
+     * Reads the next message from source
+     * @return next message from source
+     * @throws Exception
+     */
+     T read() throws Exception;
+}
