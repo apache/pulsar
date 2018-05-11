21d891e5fe4758a678db2bbf819b0e7a2e4c15ca
Ivan Kelly <ivank@apache.org>
2018-05-11 15:02:35 +0200

    Make s3mock use anon credentials to avoid going to S3
    Otherwise it uses the default chain which contacts S3. This is
    expensive and time consuming.

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/s3offload/S3TestBase.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/s3offload/S3TestBase.java
@@ -18,6 +18,8 @@
  */
 package org.apache.pulsar.broker.s3offload;
 
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -46,6 +48,7 @@ public class S3TestBase {
         } else {
             s3client = AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(new EndpointConfiguration(s3endpoint, "foobar"))
+                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
                 .withPathStyleAccessEnabled(true).build();
         }
 
