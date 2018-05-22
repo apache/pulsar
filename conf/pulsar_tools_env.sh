792ec0bf093f07505fc30984a0cac8142ead81e8
Sanjeev Kulkarni <sanjeev@streaml.io>
2018-05-22 11:46:13 -0700

    Took feedback

--- conf/pulsar_tools_env.sh
+++ conf/pulsar_tools_env.sh
@@ -42,9 +42,7 @@
 # PULSAR_GLOBAL_ZK_CONF=
 
 # Extra options to be passed to the jvm
-if [ -z "$PULSAR_MEM" ]; then
-    PULSAR_MEM=" -Xmx256m -XX:MaxDirectMemorySize=256m"
-fi
+PULSAR_MEM=${PULSAR_MEM:-"-Xmx256m -XX:MaxDirectMemorySize=256m"}
 
 # Garbage collection options
 PULSAR_GC=" -client "
