aca24e785c63a269670a9484c26d3480612ebb0b
Ali Ahmed <alahmed.se@gmail.com>
2018-06-05 16:39:53 -0700

    Add pulsar python client osx vagrant templates

---
+++ dev/pulsar-client-osx/osx-10.12-python2.7/getfile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+gitTag="${gitTag:-master}"
+pythonVer="PYTHON2"
+
+rm -rf *.whl
+sed s/#TAG#/$gitTag/g build.sh.template > build.sh.template2
+sed s/#PYTHONVER#/$pythonVer/g build.sh.template2 > build.sh
+vagrant up --provision
+vagrant scp :/Users/vagrant/incubator-pulsar/pulsar-client-cpp/python/dist/*.whl .
+vagrant halt -f
