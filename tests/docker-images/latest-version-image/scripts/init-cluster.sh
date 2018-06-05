eeb9db4ca2382dde93cd3befbf7229347ed5531a
Ivan Kelly <ivank@apache.org>
2018-06-05 20:03:00 +0200

    Use consistent naming for cluster envvar in integ test (#1909)
    Previously we were using cluster in some places, and clusterName in
    others. Settles on clusterName, as that's what the config files
    expects. Also changes the znode wait to be cluster specific.

--- tests/docker-images/latest-version-image/scripts/init-cluster.sh
+++ tests/docker-images/latest-version-image/scripts/init-cluster.sh
@@ -20,7 +20,7 @@
 
 set -x
 
-ZNODE="/initialized"
+ZNODE="/initialized-$clusterName"
 
 bin/watch-znode.py -z $zkServers -p / -w
 
@@ -28,7 +28,7 @@ bin/watch-znode.py -z $zkServers -p $ZNODE -e
 if [ $? != 0 ]; then
     echo Initializing cluster
     bin/apply-config-from-env.py conf/bookkeeper.conf &&
-        bin/pulsar initialize-cluster-metadata --cluster $cluster --zookeeper $zkServers \
+        bin/pulsar initialize-cluster-metadata --cluster $clusterName --zookeeper $zkServers \
                    --configuration-store $configurationStore --web-service-url http://$pulsarNode:8080/ \
                    --broker-service-url pulsar://$pulsarNode:6650/ &&
         bin/watch-znode.py -z $zkServers -p $ZNODE -c
