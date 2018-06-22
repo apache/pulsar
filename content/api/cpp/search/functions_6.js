695f6452a6ee42daefd9b081298dbaad0dca5bcb
jenkins <builds@apache.org>
2018-06-22 18:56:06 +0000

    Updated site at revision ed5a8ba

--- content/api/cpp/search/functions_6.js
+++ content/api/cpp/search/functions_6.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['hasmessageavailable',['hasMessageAvailable',['../classpulsar_1_1_reader.html#a7f751b63fba4d4734ff79da590f9fd56',1,'pulsar::Reader']]],
+  ['hasmessageavailableasync',['hasMessageAvailableAsync',['../classpulsar_1_1_reader.html#a0c3755b32954d1aeb1b4ef60e4314c1d',1,'pulsar::Reader']]],
   ['hasproperty',['hasProperty',['../classpulsar_1_1_message.html#a9a829dd199557ff87494690753abf9b7',1,'pulsar::Message']]]
 ];
