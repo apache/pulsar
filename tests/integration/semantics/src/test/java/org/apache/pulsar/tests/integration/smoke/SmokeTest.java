5d7386a4adf3f97100422a922c55cd3b5d389bc9
Ali Ahmed <alahmed.se@gmail.com>
2018-07-05 18:53:42 -0700

    Move Smoke Test and Cli test to semantic package (#2097)
    Me are moving single test projects to unified location , eventually semantic will be renamed to a more general name handle , this is due to migration from arquillian to testcontainers.

--- tests/integration/semantics/src/test/java/org/apache/pulsar/tests/integration/smoke/SmokeTest.java
+++ tests/integration/semantics/src/test/java/org/apache/pulsar/tests/integration/smoke/SmokeTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pulsar.tests.integration;
+package org.apache.pulsar.tests.integration.smoke;
 
 import lombok.Cleanup;
 import org.apache.pulsar.client.api.Consumer;
