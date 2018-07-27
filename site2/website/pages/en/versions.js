fd1b4593d9f993a3450a151e8aa7d1ce671f215e
Sijie Guo <guosijie@gmail.com>
2018-07-27 10:31:39 -0700

    [website] Add versions page (#2247)
    * Update release notes title
    
    * [website] Add versions page
    
     ### Motivation
    
    We need to link back to the documentation for the old versions.
    
     ### Changes
    
    Update `versions` page to include links back to old versions.
    
     ### NOTES
    
    This change doesn't include cutting a version for 2.1.0 release.
    This will be done after 2.1.0 release is cut.

--- site2/website/pages/en/versions.js
+++ site2/website/pages/en/versions.js
@@ -8,90 +8,99 @@ const GridBlock = CompLibrary.GridBlock;
 const CWD = process.cwd();
 
 const siteConfig = require(`${CWD}/siteConfig.js`);
-//const versions = require(CWD + '/versions.json');
+// versions post docusaurus
+// const versions = require(`${CWD}/versions.json`);
+// versions pre docusaurus
+const oldversions = require(`${CWD}/oldversions.json`);
 
-/*
-class Versions extends React.Component {
-  render() {
-    const latestVersion = versions[0];
+function Versions(props) {
+    const latestStableVersion = oldversions[0];
+    const repoUrl = `https://github.com/${siteConfig.organizationName}/${
+      siteConfig.projectName
+    }`;
     return (
-      <div className="docMainWrapper wrapper">
-        <Container className="mainContainer versionsContainer">
-          <div className="post">
-            <header className="postHeader">
-              <h2>{siteConfig.title + ' Versions'}</h2>
-            </header>
-            <p>New versions of this project are released every so often.</p>
-            <h3 id="latest">Current version (Stable)</h3>
-            <table className="versions">
-              <tbody>
-                <tr>
-                  <th>{latestVersion}</th>
-                  <td>
-                    <a href={''}>Documentation</a>
-                  </td>
-                  <td>
-                    <a href={''}>Release Notes</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <p>
-              This is the version that is configured automatically when you
-              first install this project.
-            </p>
-            <h3 id="rc">Pre-release versions</h3>
-            <table className="versions">
-              <tbody>
-                <tr>
-                  <th>master</th>
-                  <td>
-                    <a href={''}>Documentation</a>
-                  </td>
-                  <td>
-                    <a href={''}>Release Notes</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <p>Other text describing this section.</p>
-            <h3 id="archive">Past Versions</h3>
-            <table className="versions">
-              <tbody>
-                {versions.map(
-                  version =>
-                    version !== latestVersion && (
-                      <tr>
-                        <th>{version}</th>
-                        <td>
-                          <a href={''}>Documentation</a>
-                        </td>
-                        <td>
-                          <a href={''}>Release Notes</a>
-                        </td>
-                      </tr>
-                    )
-                )}
-              </tbody>
-            </table>
-            <p>
-              You can find past versions of this project{' '}
-              <a href="https://github.com/"> on GitHub </a>.
-            </p>
-          </div>
-        </Container>
-      </div>
-    );
-  }
+    <div className="pageContainer">
+      <Container className="mainContainer documentContainer postContainer">
+        <div className="post">
+          <header className="postHeader">
+            <h1>{siteConfig.title} Versions</h1>
+          </header>
+          <h3 id="latest">Latest Stable Version</h3>
+          <p>Latest stable release of Apache Pulsar.</p>
+          <table className="versions">
+            <tbody>
+              <tr>
+                <th>{latestStableVersion}</th>
+                <td>
+                  <a
+                    href={`${siteConfig.url}/docs/v${latestStableVersion}/getting-started/LocalCluster/`}>
+                    Documentation
+                  </a>
+                </td>
+                <td>
+                  <a href={`${siteConfig.baseUrl}release-notes#${latestStableVersion}`}>
+                    Release Notes
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <h3 id="rc">Latest Version</h3>
+          Here you can find the latest documentation and unreleased code.
+          <table className="versions">
+            <tbody>
+              <tr>
+                <th>master</th>
+                <td>
+                  <a
+                    href={`${siteConfig.baseUrl}docs/standalone`}>
+                    Documentation
+                  </a>
+                </td>
+                <td>
+                  <a href={repoUrl}>Source Code</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <h3 id="archive">Past Versions</h3>
+          <p>
+            Here you can find documentation for previous versions of Apache Pulsar.
+          </p>
+          <table className="versions">
+            <tbody>
+              {oldversions.map(
+                version =>
+                  version !== latestStableVersion && (
+                    <tr key={version}>
+                      <th>{version}</th>
+                      <td>
+                        <a
+                          href={`${siteConfig.url}/docs/v${version}/getting-started/LocalCluster/`}>
+                          Documentation
+                        </a>
+                      </td>
+                      <td>
+                        <a href={`${siteConfig.baseUrl}/release-notes#${version}`}>
+                          Release Notes
+                        </a>
+                      </td>
+                    </tr>
+                  )
+              )}
+            </tbody>
+          </table>
+          <p>
+            You can find past versions of this project on{' '}
+            <a href={`${repoUrl}/releases`}>GitHub</a> or download from{' '}
+            <a href={`${siteConfig.baseUrl}download`}>Apache</a>.
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
 }
-*/
 
-class Versions extends React.Component {
-  render() {
-    return (
-      <h3>versions</h3>
-    );
-  }
-}
+Versions.title = 'Versions';
 
 module.exports = Versions;
