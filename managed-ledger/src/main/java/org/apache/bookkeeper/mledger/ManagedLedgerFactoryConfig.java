ef37890331197ee4f40da9fa787ed5241a55cb6a
Matteo Merli <mmerli@apache.org>
2018-08-22 10:33:49 -0700

    Allow to configure number of IO and managed ledger threads (#2412)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/ManagedLedgerFactoryConfig.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/ManagedLedgerFactoryConfig.java
@@ -18,42 +18,26 @@
  */
 package org.apache.bookkeeper.mledger;
 
+import lombok.Data;
+
 /**
  * Configuration for a {@link ManagedLedgerFactory}.
  */
+@Data
 public class ManagedLedgerFactoryConfig {
     private static final long MB = 1024 * 1024;
 
     private long maxCacheSize = 128 * MB;
+
+    /**
+     * The cache eviction watermark is the percentage of the cache size to reach when removing entries from the cache.
+     */
     private double cacheEvictionWatermark = 0.90;
+
+    private int numManagedLedgerWorkerThreads = Runtime.getRuntime().availableProcessors();
+    private int numManagedLedgerSchedulerThreads = Runtime.getRuntime().availableProcessors();
 
     public long getMaxCacheSize() {
         return maxCacheSize;
     }
-
-    /**
-     *
-     * @param maxCacheSize
-     * @return
-     */
-    public ManagedLedgerFactoryConfig setMaxCacheSize(long maxCacheSize) {
-        this.maxCacheSize = maxCacheSize;
-        return this;
-    }
-
-    public double getCacheEvictionWatermark() {
-        return cacheEvictionWatermark;
-    }
-
-    /**
-     * The cache eviction watermark is the percentage of the cache size to reach when removing entries from the cache.
-     *
-     * @param cacheEvictionWatermark
-     * @return
-     */
-    public ManagedLedgerFactoryConfig setCacheEvictionWatermark(double cacheEvictionWatermark) {
-        this.cacheEvictionWatermark = cacheEvictionWatermark;
-        return this;
-    }
-
 }
