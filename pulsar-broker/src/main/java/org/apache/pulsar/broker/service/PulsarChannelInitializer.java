92b809784f631bc6b72cc1116761e8fac02ef41c
Ivan Kelly <ivank@apache.org>
2018-08-28 12:21:53 +0000

    Copy ByteBufPair buffers when using with SSL (#2401)
    The netty SSL handler uses a coalescing buffer queue, which modifies
    the buffers used to queue the writes so that SSL_write can be given
    larger chunks, thereby increasing the 'goodput'.
    
    If we pass in a retained duplicate as we have been doing until now,
    then later clients will be passed junk, as SSL will have modified cached
    entry buffers.
    
    This patch introduces a copying ByteBufPair encoder, which is only
    used with SSL connections.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/PulsarChannelInitializer.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/PulsarChannelInitializer.java
@@ -52,9 +52,11 @@ public class PulsarChannelInitializer extends ChannelInitializer<SocketChannel> 
         if (enableTLS) {
             SslContext sslCtx = SecurityUtility.createNettySslContextForServer(serviceConfig.isTlsAllowInsecureConnection(), serviceConfig.getTlsTrustCertsFilePath(), serviceConfig.getTlsCertificateFilePath(), serviceConfig.getTlsKeyFilePath());
             ch.pipeline().addLast(TLS_HANDLER, sslCtx.newHandler(ch.alloc()));
+            ch.pipeline().addLast("ByteBufPairEncoder", ByteBufPair.COPYING_ENCODER);
+        } else {
+            ch.pipeline().addLast("ByteBufPairEncoder", ByteBufPair.ENCODER);
         }
 
-        ch.pipeline().addLast("ByteBufPairEncoder", ByteBufPair.ENCODER);
         ch.pipeline().addLast("frameDecoder", new LengthFieldBasedFrameDecoder(PulsarDecoder.MaxFrameSize, 0, 4, 0, 4));
         ch.pipeline().addLast("handler", new ServerCnx(brokerService));
     }
