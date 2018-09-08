4f9f845ea9e86f8611b44487e0c83a684b326f98
David Kjerrumgaard <david@streaml.io>
2018-09-08 16:56:07 -0700

    Added ASF license and fixed integration tests

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/io/ElasticSearchSinkTester.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/io/ElasticSearchSinkTester.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pulsar.tests.integration.io;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -17,7 +35,6 @@ import org.testcontainers.containers.GenericContainer;
 
 public class ElasticSearchSinkTester extends SinkTester {
     
-    private static final String NAME = "ElasticSearch";
     private RestHighLevelClient elasticClient;
 
     public ElasticSearchSinkTester() {
@@ -29,7 +46,7 @@ public class ElasticSearchSinkTester extends SinkTester {
 
     @Override
     public void findSinkServiceContainer(Map<String, GenericContainer<?>> externalServices) {
-        GenericContainer<?> container = externalServices.get(NAME);
+        GenericContainer<?> container = externalServices.get(ElasticSearchContainer.NAME);
         checkState(container instanceof ElasticSearchContainer,
             "No ElasticSearch service found in the cluster");
     }
