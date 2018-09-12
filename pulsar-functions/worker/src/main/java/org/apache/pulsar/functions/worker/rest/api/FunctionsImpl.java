apache/pulsar

commit 02c47950eb9cc9664d53be4d435623b8507aa2fa

Fix Trigger Functinality (#2501)

pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/rest/api/FunctionsImpl.java
@@ -588,7 +588,7 @@ public class FunctionsImpl {
 
         FunctionMetaDataManager functionMetaDataManager = worker().getFunctionMetaDataManager();
         if (!functionMetaDataManager.containsFunction(tenant, namespace, functionName)) {
-            log.error("Function in getFunction does not exist @ /{}/{}/{}", tenant, namespace, functionName);
+            log.error("Function in trigger function does not exist @ /{}/{}/{}", tenant, namespace, functionName);
             return Response.status(Status.NOT_FOUND).type(MediaType.APPLICATION_JSON)
                     .entity(new ErrorData(String.format("Function %s doesn't exist", functionName))).build();
         }
@@ -599,15 +599,18 @@ public class FunctionsImpl {
         String inputTopicToWrite;
         if (topic != null) {
             inputTopicToWrite = topic;
-        } else if (functionMetaData.getFunctionDetails().getSource().getTopicsToSerDeClassNameMap().size() == 1) {
-            inputTopicToWrite = functionMetaData.getFunctionDetails().getSource().getTopicsToSerDeClassNameMap()
+        } else if (functionMetaData.getFunctionDetails().getSource().getInputSpecsCount() == 1) {
+            inputTopicToWrite = functionMetaData.getFunctionDetails().getSource().getInputSpecsMap()
                     .keySet().iterator().next();
         } else {
+            log.error("Function in trigger function has more than 1 input topics @ /{}/{}/{}", tenant, namespace, functionName);
             return Response.status(Status.BAD_REQUEST).build();
         }
-        if (functionMetaData.getFunctionDetails().getSource().getTopicsToSerDeClassNameMap() == null
-                || !functionMetaData.getFunctionDetails().getSource().getTopicsToSerDeClassNameMap()
+        if (functionMetaData.getFunctionDetails().getSource().getInputSpecsCount() == 0
+                || !functionMetaData.getFunctionDetails().getSource().getInputSpecsMap()
                         .containsKey(inputTopicToWrite)) {
+            log.error("Function in trigger function has unidentified topic @ /{}/{}/{} {}", tenant, namespace, functionName, inputTopicToWrite);
+
             return Response.status(Status.BAD_REQUEST).build();
         }
         String outputTopic = functionMetaData.getFunctionDetails().getSink().getTopic();

commit 14b1d695f762308c41e92abb5defb055af529f38

Fix BC issue in functions trigger function submitted by old CLI
*Motivation*

A BC issue was introduced by #2501. If a function is submitted by old CLI, which only containers topics to serde map.
The new function worker will fail trigger request because it can't find input specs.

```
17:28:32.105 [function-web-7-9] ERROR org.apache.pulsar.functions.worker.rest.api.FunctionsImpl - Function in trigger function has more than 1 input topics @ /public/default/ex
```

*Changes*

The functions worker should handle BC.

*Tests*

No tests are added here. Since it requires a BC testing framework to be able to submit a function to a new functions worker by old CLI.

Created https://github.com/apache/incubator-pulsar/issues/2569 for adding a BC testing framework to cover such BC issues.

pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/rest/api/FunctionsImpl.java
@@ -572,7 +572,6 @@ public class FunctionsImpl {
             return getUnavailableResponse();
         }
 
-        FunctionDetails functionDetails;
         // validate parameters
         try {
             validateTriggerRequestParams(tenant, namespace, functionName, topic, input, uploadedInputStream);
@@ -595,16 +594,23 @@ public class FunctionsImpl {
         String inputTopicToWrite;
         if (topic != null) {
             inputTopicToWrite = topic;
+        } else if (functionMetaData.getFunctionDetails().getSource().getTopicsToSerDeClassNameMap().size() == 1) {
+            // the function was submitted by an old CLI which only have topics-to-serde-class-name map.
+            inputTopicToWrite = functionMetaData.getFunctionDetails().getSource().getTopicsToSerDeClassNameMap()
+                    .keySet().iterator().next();
         } else if (functionMetaData.getFunctionDetails().getSource().getInputSpecsCount() == 1) {
+            // the function was submitted by a newer CLI which is using input specs
             inputTopicToWrite = functionMetaData.getFunctionDetails().getSource().getInputSpecsMap()
                     .keySet().iterator().next();
         } else {
             log.error("Function in trigger function has more than 1 input topics @ /{}/{}/{}", tenant, namespace, functionName);
             return Response.status(Status.BAD_REQUEST).build();
         }
-        if (functionMetaData.getFunctionDetails().getSource().getInputSpecsCount() == 0
-                || !functionMetaData.getFunctionDetails().getSource().getInputSpecsMap()
-                        .containsKey(inputTopicToWrite)) {
+        boolean topicIdentified =
+            (functionMetaData.getFunctionDetails().getSource().getInputSpecsCount() > 0
+                && functionMetaData.getFunctionDetails().getSource().getInputSpecsMap().containsKey(inputTopicToWrite))
+            || functionMetaData.getFunctionDetails().getSource().getTopicsToSerDeClassNameMap().containsKey(inputTopicToWrite);
+        if (!topicIdentified) {
             log.error("Function in trigger function has unidentified topic @ /{}/{}/{} {}", tenant, namespace, functionName, inputTopicToWrite);
 
             return Response.status(Status.BAD_REQUEST).build();