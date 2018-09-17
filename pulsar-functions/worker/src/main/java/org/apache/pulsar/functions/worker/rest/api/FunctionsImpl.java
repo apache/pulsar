a123380327fbe95eef1066c4c456020c19136e07
Sanjeev Kulkarni <sanjeevrk@gmail.com>
2018-09-17 01:03:06 -0700

    Preserve filename of the original function submission (#2588)
    ### Motivation
    
    When we submit a function, currently we don't note down the original filename that was uploaded from the client. For regular java functions this works fine. For python workers we manage by doing tricks wrt tricks like filename using classnames. However when we add things like wheel support, the filename encodes information that cannot be discarded away.
    This pr preserves that filename

--- pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/rest/api/FunctionsImpl.java
+++ pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/rest/api/FunctionsImpl.java
@@ -170,6 +170,9 @@ public class FunctionsImpl {
         } else {
             packageLocationMetaDataBuilder.setPackagePath(isPkgUrlProvided ? functionPkgUrl
                     : createPackagePath(tenant, namespace, functionName, fileDetail.getFileName()));
+            if (!isPkgUrlProvided) {
+                packageLocationMetaDataBuilder.setOriginalFileName(fileDetail.getFileName());
+            }
         }
 
         functionMetaDataBuilder.setPackageLocation(packageLocationMetaDataBuilder);
@@ -234,6 +237,9 @@ public class FunctionsImpl {
         } else {
             packageLocationMetaDataBuilder.setPackagePath(isPkgUrlProvided ? functionPkgUrl
                     : createPackagePath(tenant, namespace, functionName, fileDetail.getFileName()));
+            if (!isPkgUrlProvided) {
+                packageLocationMetaDataBuilder.setOriginalFileName(fileDetail.getFileName());
+            }
         }
 
         functionMetaDataBuilder.setPackageLocation(packageLocationMetaDataBuilder);
