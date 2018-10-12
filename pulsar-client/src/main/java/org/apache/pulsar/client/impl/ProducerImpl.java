6c8ac05e6340d5b2fcc08004dffa56821ad52de7
Jerry Peng <jerry.boyang.peng@gmail.com>
2018-10-11 19:01:22 -0500

    fix: unsafe cast of schema to JSONSchema

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ProducerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ProducerImpl.java
@@ -896,9 +896,11 @@ public class ProducerImpl<T> extends ProducerBase<T> implements TimerTask, Conne
                     // but now we have standardized on every schema to generate an Avro based schema
                     if (Commands.peerSupportJsonSchemaAvroFormat(cnx.getRemoteEndpointProtocolVersion())) {
                         schemaInfo = schema.getSchemaInfo();
-                    } else {
+                    } else if (schema instanceof JSONSchema){
                         JSONSchema jsonSchema = (JSONSchema) schema;
                         schemaInfo = jsonSchema.getBackwardsCompatibleJsonSchemaInfo();
+                    } else {
+                        schemaInfo = schema.getSchemaInfo();
                     }
                 } else if (schema.getSchemaInfo().getType() == SchemaType.BYTES) {
                     // don't set schema info for Schema.BYTES
