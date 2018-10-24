3db8b7392b1be22544117ea336dd46cc85c61ba0
Sanjeev Kulkarni <sanjeevrk@gmail.com>
2018-10-23 17:28:04 -0700

    Allow the ability to specify which artifactory to download dep from (#2824)
    * Allow the ability to specify which artifactory to download dep from
    
    * reverted earlier changes
    
    * Made python dependency repository a config of k8runtime
    
    * Fixed unittest
    
    * Added extra dependency
    
    * Fixed cmd lines

--- pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/runtime/KubernetesRuntime.java
+++ pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/runtime/KubernetesRuntime.java
@@ -105,6 +105,8 @@ class KubernetesRuntime implements Runtime {
                       String jobNamespace,
                       Map<String, String> customLabels,
                       Boolean installUserCodeDependencies,
+                      String pythonDependencyRepository,
+                      String pythonExtraDependencyRepository,
                       String pulsarDockerImageName,
                       String pulsarRootDir,
                       InstanceConfig instanceConfig,
@@ -129,7 +131,8 @@ class KubernetesRuntime implements Runtime {
         this.originalCodeFileName = pulsarRootDir + "/" + originalCodeFileName;
         this.pulsarAdminUrl = pulsarAdminUrl;
         this.processArgs = RuntimeUtils.composeArgs(instanceConfig, instanceFile, logDirectory, this.originalCodeFileName, pulsarServiceUrl, stateStorageServiceUrl,
-                authConfig, "$" + ENV_SHARD_ID, GRPC_PORT, -1l, "kubernetes_instance_log4j2.yml", installUserCodeDependencies);
+                authConfig, "$" + ENV_SHARD_ID, GRPC_PORT, -1l, "kubernetes_instance_log4j2.yml", installUserCodeDependencies,
+                pythonDependencyRepository, pythonExtraDependencyRepository);
         this.prometheusMetricsServerArgs = composePrometheusMetricsServerArgs(prometheusMetricsServerJarFile, expectedMetricsInterval);
         running = false;
         doChecks(instanceConfig.getFunctionDetails());
