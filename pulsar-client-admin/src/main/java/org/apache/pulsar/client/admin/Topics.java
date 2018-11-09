2fab7fe1be09cf938a9b64f9868a533c35f4eb20
Rajan Dhabalia <rdhabalia@apache.org>
2018-11-09 08:19:54 -0800

    Add partitioned-topic stats internal admin-api (#2958)
    * Add partitioned-topic stats internal admin-api
    
    * add class

--- pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/Topics.java
+++ pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/Topics.java
@@ -32,6 +32,7 @@ import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.MessageId;
 import org.apache.pulsar.common.partition.PartitionedTopicMetadata;
 import org.apache.pulsar.common.policies.data.AuthAction;
+import org.apache.pulsar.common.policies.data.PartitionedTopicInternalStats;
 import org.apache.pulsar.common.policies.data.PartitionedTopicStats;
 import org.apache.pulsar.common.policies.data.PersistentTopicInternalStats;
 import org.apache.pulsar.common.policies.data.TopicStats;
@@ -674,6 +675,26 @@ public interface Topics {
      * @return a future that can be used to track when the partitioned topic statistics are returned
      */
     CompletableFuture<PartitionedTopicStats> getPartitionedStatsAsync(String topic, boolean perPartition);
+
+    /**
+     * Get the stats for the partitioned topic
+     * 
+     * @param topic
+     * @param perPartition
+     * @return
+     * @throws PulsarAdminException
+     */
+    PartitionedTopicInternalStats getPartitionedInternalStats(String topic)
+            throws PulsarAdminException;
+
+    /**
+     * Get the stats-internal for the partitioned topic asynchronously
+     *
+     * @param topic
+     *            topic Name
+     * @return a future that can be used to track when the partitioned topic statistics are returned
+     */
+    CompletableFuture<PartitionedTopicInternalStats> getPartitionedInternalStatsAsync(String topic);
 
     /**
      * Delete a subscription.
