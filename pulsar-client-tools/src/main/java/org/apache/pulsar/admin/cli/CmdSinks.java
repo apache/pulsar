67879c45961a5de13c2a710722dbc1bbc167b564
Sanjeev Kulkarni <sanjeevkulkarni@Sanjeevs-MacBook-Pro-2.local>
2018-11-09 11:38:15 -0800

    Ignore the current class while going over variables

--- pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdSinks.java
+++ pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdSinks.java
@@ -168,6 +168,7 @@ public class CmdSinks extends CmdBase {
             localRunArgs.add(new Gson().toJson(sinkConfig));
             for (Field field : this.getClass().getDeclaredFields()) {
                 if (field.getName().startsWith("DEPRECATED")) continue;
+                if(field.getName().startsWith("this$0")) continue;
                 Object value = field.get(this);
                 if (value != null) {
                     localRunArgs.add("--" + field.getName());
--- pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdSources.java
+++ pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdSources.java
@@ -172,6 +172,7 @@ public class CmdSources extends CmdBase {
             localRunArgs.add(new Gson().toJson(sourceConfig));
             for (Field field : this.getClass().getDeclaredFields()) {
                 if (field.getName().startsWith("DEPRECATED")) continue;
+                if(field.getName().startsWith("this$0")) continue;
                 Object value = field.get(this);
                 if (value != null) {
                     localRunArgs.add("--" + field.getName());
