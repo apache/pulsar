593600ab02bd37d9b692564470e56005b65939d0
Jia Zhai <jiazhai@users.noreply.github.com>
2018-11-14 22:09:44 +0800

    default enable cpp client producer batch (#2949)
    Motivation
    In Cpp client producer batching is disabled by default, This is not align with java client, and use default value will cause benchmark has a low result.
    
    Modifications
    default enable cpp producer batch

--- pulsar-client-cpp/lib/ProducerConfigurationImpl.h
+++ pulsar-client-cpp/lib/ProducerConfigurationImpl.h
@@ -53,7 +53,7 @@ struct ProducerConfigurationImpl {
           routingMode(ProducerConfiguration::UseSinglePartition),
           hashingScheme(ProducerConfiguration::BoostHash),
           blockIfQueueFull(false),
-          batchingEnabled(false),
+          batchingEnabled(true),
           batchingMaxMessages(1000),
           batchingMaxAllowedSizeInBytes(128 * 1024),  // 128 KB
           batchingMaxPublishDelayMs(10),              // 10 milli seconds
