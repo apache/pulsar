325179d016b6a011c88435bd73a98fdb1c549674
tuteng <eguangning@gmail.com>
2018-12-31 13:34:16 +0800

    Add apache license

--- pulsar-io/canal/src/main/java/org/apache/pulsar/io/canal/CanalAbstractSource.java
+++ pulsar-io/canal/src/main/java/org/apache/pulsar/io/canal/CanalAbstractSource.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pulsar.io.canal;
 
 import com.alibaba.otter.canal.client.CanalConnector;
@@ -106,13 +124,10 @@ public abstract class CanalAbstractSource<V> extends PushSource<V> {
                 connector.subscribe();
                 while (running) {
                     Message message = connector.getWithoutAck(canalSourceConfig.getBatchSize());
-//                    printEntry(message.getEntries());
-                    // delete the setRaw in new version of canal-client
                     message.setRaw(false);
                     SimpleCanalConnector con = (SimpleCanalConnector) connector;
                     con.isLazyParseEntry();
                     List<FlatMessage> flatMessages = MessageUtils.messageConverter(message);
-//                    long batchId = message.getId();
                     long batchId = getMessageId(message);
                     int size = message.getEntries().size();
                     if (batchId == -1 || size == 0) {
