69316b4801e3a951b3f792bdd7de2501905806ff
Jia Zhai <jiazhai@users.noreply.github.com>
2019-01-02 19:10:51 +0800

    Improve documentation for message routing mode (#3263)
    Motivation
    User complain that the Routing modes documents is not align with code, and will cause some mis-understanding.
    
    Modifications
    make the documentation align with the code.

--- pulsar-client/src/main/java/org/apache/pulsar/client/api/MessageRoutingMode.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/api/MessageRoutingMode.java
@@ -26,12 +26,16 @@ package org.apache.pulsar.client.api;
  */
 public enum MessageRoutingMode {
     /**
-     * The producer will chose one single partition and publish all the messages into that partition.
+     * If no key is provided, The partitioned producer will randomly pick one single partition and publish all the messages into that partition.
+     * If a key is provided on the message, the partitioned producer will hash the key and assign message to a particular partition.
      */
     SinglePartition,
 
     /**
-     * Publish messages across all partitions in round-robin.
+     * If no key is provided, the producer will publish messages across all partitions in round-robin fashion to achieve maximum throughput.
+     * Please note that round-robin is not done per individual message but rather it's set to the same boundary of batching delay, to ensure batching is effective.
+     *
+     * While if a key is specified on the message, the partitioned producer will hash the key and assign message to a particular partition.
      */
     RoundRobinPartition,
 
