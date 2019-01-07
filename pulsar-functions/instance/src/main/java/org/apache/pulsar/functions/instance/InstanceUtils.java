5787ee5bdf81269e1a1ee39d3ba6f257a68d4c72
Boyang Jerry Peng <jerry.boyang.peng@gmail.com>
2019-01-07 14:01:46 -0800

    Clean up and correct properties to producer and consumers created by Functions/Sinks/Sources (#3315)
    * clean up and correct properties to producer and consumers created by functions
    
    * fix test
    
    * cleaning up comment
    
    * refactoring

--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/InstanceUtils.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/InstanceUtils.java
@@ -36,6 +36,9 @@ import org.apache.pulsar.functions.utils.Reflections;
 import net.jodah.typetools.TypeResolver;
 import org.apache.pulsar.functions.utils.Utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @UtilityClass
 public class InstanceUtils {
     public static SerDe<?> initializeSerDe(String serdeClassName, ClassLoader clsLoader, Class<?> typeArg,
@@ -102,5 +105,24 @@ public class InstanceUtils {
             return FUNCTION;
         }
         return SINK;
+    }
+
+    public static Map<String, String> getProperties(Utils.ComponentType componentType,
+                                                    String fullyQualifiedName, int instanceId) {
+        Map<String, String> properties = new HashMap<>();
+        switch (componentType) {
+            case FUNCTION:
+                properties.put("application", "pulsar-function");
+                break;
+            case SOURCE:
+                properties.put("application", "pulsar-source");
+                break;
+            case SINK:
+                properties.put("application", "pulsar-sink");
+                break;
+        }
+        properties.put("id", fullyQualifiedName);
+        properties.put("instance_id", String.valueOf(instanceId));
+        return properties;
     }
 }
