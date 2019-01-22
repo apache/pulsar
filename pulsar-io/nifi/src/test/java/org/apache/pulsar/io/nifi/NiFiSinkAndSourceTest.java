a0871cf61555672e575332126d5f7d9a78907425
ambition119 <1269223860@qq.com>
2019-01-22 15:28:08 +0800

    Support Pulsar to receive and send data from Apache NiFi

--- pulsar-io/nifi/src/test/java/org/apache/pulsar/io/nifi/NiFiSinkAndSourceTest.java
+++ pulsar-io/nifi/src/test/java/org/apache/pulsar/io/nifi/NiFiSinkAndSourceTest.java
@@ -45,7 +45,7 @@ public class NiFiSinkAndSourceTest {
     private String msg = "Data from Pulsar";
     private Map<String, Object> map;
 
-    @Test
+    @Test(enabled = false)
     public void TestSinkAndSource() throws Exception {
         map = new HashMap<>();
         map.put("url","http://localhost:8080/nifi");
@@ -76,4 +76,5 @@ public class NiFiSinkAndSourceTest {
         Assert.assertEquals(msg, new String(recordValue.getContent()));
         Assert.assertNotNull(recordValue.getAttributes());
     }
+
 }
