3422e8fcf0681934e51fcedeafda49c3bccefe14
Matteo Merli <mmerli@apache.org>
2019-02-01 09:29:47 -0800

    Copy both test scripts into /tmp

--- pulsar-client-cpp/run-unit-tests.sh
+++ pulsar-client-cpp/run-unit-tests.sh
@@ -54,7 +54,7 @@ if [ $RES -eq 0 ]; then
 
     # Running tests from a different directory to avoid importing directly
     # from the current dir, but rather using the installed wheel file
-    cp pulsar_test.py /tmp
+    cp *_test.py /tmp
     pushd /tmp
 
     python pulsar_test.py
