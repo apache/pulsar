3afb39f34a4ebed608f56f2db5c252e0859ee7c4
Matteo Merli <mmerli@apache.org>
2019-02-04 14:41:37 -0800

    Use testDupConsumersOnSharedModeNotThrowsExcOnUnsubscribe to avoid interference with other tests running in parallel

--- pulsar-client-cpp/tests/BasicEndToEndTest.cc
+++ pulsar-client-cpp/tests/BasicEndToEndTest.cc
@@ -2782,7 +2782,7 @@ TEST(BasicEndToEndTest, testDupConsumersOnSharedModeNotThrowsExcOnUnsubscribe) {
     ClientConfiguration config;
     Client client(lookupUrl);
     std::string subsName = "my-only-sub";
-    std::string topicName = "persistent://public/default/test-prevent-dup-consumers";
+    std::string topicName = "persistent://public/default/testDupConsumersOnSharedModeNotThrowsExcOnUnsubscribe";
     ConsumerConfiguration consumerConf;
     consumerConf.setConsumerType(ConsumerShared);
 
