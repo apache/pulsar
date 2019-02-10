fd42f45251101d9d2c3b1b6856308328067f20ac
Enrico Olivelli <eolivelli@gmail.com>
2019-02-10 18:00:02 +0100

    Update LedgerMetadataUtils.java

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/LedgerMetadataUtils.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/LedgerMetadataUtils.java
@@ -74,7 +74,7 @@ public final class LedgerMetadataUtils {
      *
      * @param compactedTopic reference to the compacted topic.
      * @param compactedToMessageId last mesasgeId.
-     * @return an immutable map which describes the cursor
+     * @return an immutable map which describes the compacted ledger
      */
     public static Map<String, byte[]> buildMetadataForCompactedLedger(String compactedTopic, byte[] compactedToMessageId) {
         return ImmutableMap.of(
