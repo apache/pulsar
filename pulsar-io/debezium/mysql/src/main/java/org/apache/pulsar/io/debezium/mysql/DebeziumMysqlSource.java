ccb771806a0fd8c4e0a7515f1eb5c7c6ba65a08c
Jia Zhai <zhaijia@apache.org>
2019-03-15 18:23:52 +0800

    change following sijie's comments

--- pulsar-io/debezium/mysql/src/main/java/org/apache/pulsar/io/debezium/mysql/DebeziumMysqlSource.java
+++ pulsar-io/debezium/mysql/src/main/java/org/apache/pulsar/io/debezium/mysql/DebeziumMysqlSource.java
@@ -34,42 +34,60 @@ import org.apache.pulsar.io.kafka.connect.PulsarKafkaWorkerConfig;
  */
 @Slf4j
 public class DebeziumMysqlSource extends KafkaConnectSource {
+    static private final String OFFSET_TOPIC_PREFIX = "mysql-offset-topic-";
+    static private final String HISTORY_TOPIC_PREFIX = "mysql-history-topic-";
 
-    static private String OFFSET_TOPIC_PREFIX = "mysql-offset-topic-";
-    static private String HISTORY_TOPIC_PREFIX = "mysql-history-topic-";
+    static private final String DEFAULT_TASK = "io.debezium.connector.mysql.MySqlConnectorTask";
+    static private final String DEFAULT_CONVERTER = "org.apache.kafka.connect.json.JsonConverter";
+    static private final String DEFAULT_HISTORY = "org.apache.pulsar.io.debezium.PulsarDatabaseHistory";
 
     private final String uuid = UUID.randomUUID().toString();
 
+    private static void throwExceptionIfConfigNotMatch(Map<String, Object> config,
+                                                       String key,
+                                                       String expect) throws IllegalArgumentException {
+        Object orig = config.get(key);
+        if (orig == null) {
+            config.put(key, expect);
+            return;
+        }
+
+        // throw exception if value not match
+        if (!orig.equals(expect)) {
+            throw new IllegalArgumentException("Expected " + expect + " but has " + orig);
+        }
+    }
+
+    private static void setConfigIfNull(Map<String, Object> config, String key, String value) {
+        Object orig = config.get(key);
+        if (orig == null) {
+            config.put(key, value);
+        }
+    }
 
     @Override
     public void open(Map<String, Object> config, SourceContext sourceContext) throws Exception {
-        config.put(
-            TaskConfig.TASK_CLASS_CONFIG,
-            "io.debezium.connector.mysql.MySqlConnectorTask");
-        config.put(
-            PulsarKafkaWorkerConfig.KEY_CONVERTER_CLASS_CONFIG,
-            "org.apache.kafka.connect.json.JsonConverter");
-        config.put(
-            PulsarKafkaWorkerConfig.VALUE_CONVERTER_CLASS_CONFIG,
-            "org.apache.kafka.connect.json.JsonConverter");
+        // connector task
+        throwExceptionIfConfigNotMatch(config, TaskConfig.TASK_CLASS_CONFIG, DEFAULT_TASK);
 
-        // database.history
-        config.put(
-            MySqlConnectorConfig.DATABASE_HISTORY.name(),
-            "org.apache.pulsar.io.debezium.PulsarDatabaseHistory");
-        // database.history.pulsar.topic
-        config.put(
-            PulsarDatabaseHistory.TOPIC.name(),
-            HISTORY_TOPIC_PREFIX + uuid);
-        // database.history.pulsar.service.url, this is set as the value of pulsar.service.url
-        config.put(
-            PulsarDatabaseHistory.SERVICE_URL.name(),
-            config.get(PulsarKafkaWorkerConfig.PULSAR_SERVICE_URL_CONFIG));
+        // key.converter
+        setConfigIfNull(config, PulsarKafkaWorkerConfig.KEY_CONVERTER_CLASS_CONFIG, DEFAULT_CONVERTER);
+        // value.converter
+        setConfigIfNull(config, PulsarKafkaWorkerConfig.VALUE_CONVERTER_CLASS_CONFIG, DEFAULT_CONVERTER);
 
-        // offset.storage.topic
-        config.put(
-            PulsarKafkaWorkerConfig.OFFSET_STORAGE_TOPIC_CONFIG,
-            OFFSET_TOPIC_PREFIX + uuid);
+        // database.history implementation class
+        setConfigIfNull(config, MySqlConnectorConfig.DATABASE_HISTORY.name(), DEFAULT_HISTORY);
+        // database.history.pulsar.topic: history topic name
+        setConfigIfNull(config, PulsarDatabaseHistory.TOPIC.name(), HISTORY_TOPIC_PREFIX + uuid);
+        // offset.storage.topic: offset topic name
+        setConfigIfNull(config, PulsarKafkaWorkerConfig.OFFSET_STORAGE_TOPIC_CONFIG, OFFSET_TOPIC_PREFIX + uuid);
+
+        String serviceUrl = (String) config.get(PulsarKafkaWorkerConfig.PULSAR_SERVICE_URL_CONFIG);
+        if (serviceUrl == null) {
+            throw new IllegalArgumentException("Pulsar service URL not provided.");
+        }
+        // database.history.pulsar.service.url, this is set as the value of pulsar.service.url if null.
+        setConfigIfNull(config, PulsarDatabaseHistory.SERVICE_URL.name(), serviceUrl);
 
         super.open(config, sourceContext);
     }
