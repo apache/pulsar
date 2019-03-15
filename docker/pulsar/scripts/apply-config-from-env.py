174001f098069f26f2203114a6f5a2c833079c3f
Sanjeev Kulkarni <sanjeev@streaml.io>
2019-03-14 20:53:20 -0700

    Took feedback into account

--- docker/pulsar/scripts/apply-config-from-env.py
+++ docker/pulsar/scripts/apply-config-from-env.py
@@ -34,7 +34,7 @@ if len(sys.argv) < 2:
 # Always apply env config to env scripts as well
 conf_files = ['conf/pulsar_env.sh', 'conf/bkenv.sh'] + sys.argv[1:]
 
-PF_ENV_PREFIX = 'INS_'
+PF_ENV_PREFIX = 'PULSAR_'
 
 
 for conf_filename in conf_files:
