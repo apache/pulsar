3bfc264ea2d7539eb80b183c01f347ece07f3e5c
Fangbin Sun <sunfangbin@gmail.com>
2019-03-27 01:22:52 +0800

    [Issue 3896] [pulsar-io] Fix NPE in ElasticSearchSink (#3899)
    * Fix NPE in ElasticSearchSink.
    
    * Init by an empty string

--- pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSink.java
+++ pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSink.java
@@ -100,7 +100,7 @@ public class ElasticSearchSink implements Sink<byte[]> {
     }
 
     public KeyValue<String, byte[]> extractKeyValue(Record<byte[]> record) {
-        String key = record.getKey().orElseGet(null);
+        String key = record.getKey().orElse("");
         return new KeyValue<>(key, record.getValue());
     }
 
