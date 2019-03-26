ee531dc104afd00c985e60926552a828e0475beb
丛搏 <237716289@qq.com>
2019-03-26 17:13:56 +0800

    Modify the judge condition

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -154,7 +154,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
     static <T> ConsumerImpl<T> newConsumerImpl(PulsarClientImpl client, String topic, ConsumerConfigurationData<T> conf,
                  ExecutorService listenerExecutor, int partitionIndex, CompletableFuture<Consumer<T>> subscribeFuture,
                  SubscriptionMode subscriptionMode, MessageId startMessageId, Schema<T> schema, ConsumerInterceptors<T> interceptors) {
-        if (schema.supportSchemaVersioning()) {
+        if (schema != null && schema.supportSchemaVersioning()) {
             Map<String, Schema> supportSchemaVersioningSchemaCache = client.getSupportSchemaVersioningSchemaCache();
             Schema<T> schemaFromCache = supportSchemaVersioningSchemaCache.get(topic);
             if (schemaFromCache == null) {
