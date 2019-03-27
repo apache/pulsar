e80027345caa101fe9847b208f02c8ddcf50f949
Sijie Guo <guosijie@gmail.com>
2019-03-26 22:18:11 -0700

    [schema] Batch message should propagate the schema version from single message (#3870)
    *Motivation*
    
    Schema version is set on single message's header. But it isn't propagated to
    the batch message's header. So when the consumer receives the message batch,
    it doesn't contain the schema version.
    
    *Modifications*
    
    When intializing a message batch, propagate the schema version from single message to batch message header.
    
    *Verify this change*
    
    Verified with an integration test. Need to write some unit tests for it.

--- pulsar-common/src/main/java/org/apache/pulsar/common/api/Commands.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/api/Commands.java
@@ -1012,6 +1012,9 @@ public class Commands {
         if (builder.hasReplicatedFrom()) {
             messageMetadata.setReplicatedFrom(builder.getReplicatedFrom());
         }
+        if (builder.hasSchemaVersion()) {
+            messageMetadata.setSchemaVersion(builder.getSchemaVersion());
+        }
         return builder.getSequenceId();
     }
 
