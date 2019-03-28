bda6a9cc8edb0aef7d969ce618fcfe212bdb425a
wpl <1269223860@qq.com>
2019-03-28 01:59:25 -0500

    [pulsar-client] add Date/Time/Timestamp schema (#3856)
    Fixes #3831

---
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/schema/DateSchema.java
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.client.impl.schema;
+
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.common.schema.SchemaInfo;
+import org.apache.pulsar.common.schema.SchemaType;
+
+import java.util.Date;
+
+/**
+ * A schema for `java.util.Date` or `java.sql.Date`.
+ */
+public class DateSchema implements Schema<Date> {
+   public static DateSchema of() {
+      return INSTANCE;
+   }
+
+   private static final DateSchema INSTANCE = new DateSchema();
+   private static final SchemaInfo SCHEMA_INFO = new SchemaInfo()
+         .setName("Date")
+         .setType(SchemaType.DATE)
+         .setSchema(new byte[0]);
+
+   @Override
+   public byte[] encode(Date message) {
+      if (null == message) {
+         return null;
+      }
+
+      Long date = message.getTime();
+      return LongSchema.of().encode(date);
+   }
+
+   @Override
+   public Date decode(byte[] bytes) {
+      if (null == bytes) {
+         return null;
+      }
+
+      Long decode = LongSchema.of().decode(bytes);
+      return new Date(decode);
+   }
+
+   @Override
+   public SchemaInfo getSchemaInfo() {
+      return SCHEMA_INFO;
+   }
+}
