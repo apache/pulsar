d9125902e6493c48decd6de6da8755530d0cbc02
Sanjeev Kulkarni <sanjeev@streaml.io>
2019-04-05 14:47:15 -0700

    Refactored for simplicity

--- pulsar-sql/presto-pulsar/src/main/java/org/apache/pulsar/sql/presto/StringSchemaHandler.java
+++ pulsar-sql/presto-pulsar/src/main/java/org/apache/pulsar/sql/presto/StringSchemaHandler.java
@@ -19,53 +19,17 @@
 package org.apache.pulsar.sql.presto;
 
 import io.airlift.log.Logger;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericDatumReader;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.BinaryDecoder;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.DecoderFactory;
-import org.apache.pulsar.shade.io.netty.buffer.ByteBuf;
-import org.apache.pulsar.shade.io.netty.buffer.ByteBufAllocator;
-import org.apache.pulsar.shade.io.netty.util.ReferenceCountUtil;
-import org.apache.pulsar.shade.io.netty.util.concurrent.FastThreadLocal;
-
-import java.io.IOException;
-import java.util.List;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-public class StringSchemaHandler implements SchemaHandler {
+public class StringSchemaHandler extends PrimitiveSchemaHandler {
     private static final Logger log = Logger.get(StringSchemaHandler.class);
-
-    private static final FastThreadLocal<byte[]> tmpBuffer = new FastThreadLocal<byte[]>() {
-        @Override
-        protected byte[] initialValue() {
-            return new byte[1024];
-        }
-    };
 
     public StringSchemaHandler() {
     }
 
     @Override
-    public Object deserialize(ByteBuf payload) {
-        // we need to convert a direct mem buffer into a byte[].
-        int size = payload.readableBytes();
-        byte[] buffer = tmpBuffer.get();
-        if (buffer.length < size) {
-            // If the thread-local buffer is not big enough, replace it with
-            // a bigger one
-            buffer = new byte[size * 2];
-            tmpBuffer.set(buffer);
-        }
-
-        payload.readBytes(buffer, 0, size);
-        return new String(tmpBuffer.get(), 0, size, UTF_8);
-    }
-
-    @Override
-    public Object extractField(int index, Object currentRecord) {
-        return currentRecord;
+    public Object deserialize(byte[] byteArray, int size) {
+        return new String(byteArray, 0, size, UTF_8);
     }
 }
