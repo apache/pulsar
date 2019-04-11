3350a8053c3e8eef89d0e16a6e966ae0f20ac754
丛搏 <237716289@qq.com>
2019-04-12 01:03:50 +0800

    return to the original condition of AutoConsumeSchema and AutoProduceBytesSchema

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/PulsarClientImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/PulsarClientImpl.java
@@ -306,7 +306,24 @@ public class PulsarClientImpl implements PulsarClient {
     }
 
     private <T> CompletableFuture<Consumer<T>> singleTopicSubscribeAsync(ConsumerConfigurationData<T> conf, Schema<T> schema, ConsumerInterceptors<T> interceptors) {
+        if (schema instanceof AutoConsumeSchema) {
+            AutoConsumeSchema autoConsumeSchema = (AutoConsumeSchema) schema;
+            return lookup.getSchema(TopicName.get(conf.getSingleTopic()))
+                    .thenCompose(schemaInfoOptional -> {
+                        if (schemaInfoOptional.isPresent() && schemaInfoOptional.get().getType() == SchemaType.AVRO) {
+                            GenericSchemaImpl genericSchema = GenericSchemaImpl.of(schemaInfoOptional.get());
+                            log.info("Auto detected schema for topic {} : {}",
+                                    conf.getSingleTopic(), new String(schemaInfoOptional.get().getSchema(), UTF_8));
+                            autoConsumeSchema.setSchema(genericSchema);
+                            return doSingleTopicSubscribeAsync(conf, schema, interceptors);
+                        } else {
+                            return FutureUtil.failedFuture(
+                                    new PulsarClientException.LookupException("Currently schema detection only works for topics with avro schemas"));
+                        }
+                    });
+        } else {
             return doSingleTopicSubscribeAsync(conf, schema, interceptors);
+        }
     }
 
     private <T> CompletableFuture<Consumer<T>> doSingleTopicSubscribeAsync(ConsumerConfigurationData<T> conf, Schema<T> schema, ConsumerInterceptors<T> interceptors) {
