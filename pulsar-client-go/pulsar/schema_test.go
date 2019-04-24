add3e09695bcba216740f4325634ea43eade3d74
xiaolong.ran <ranxiaolong716@gmail.com>
2019-04-24 17:28:59 +0800

    fix defer question
    Signed-off-by: xiaolong.ran <ranxiaolong716@gmail.com>

--- pulsar-client-go/pulsar/schema_test.go
+++ pulsar-client-go/pulsar/schema_test.go
@@ -65,6 +65,8 @@ func TestJsonSchema(t *testing.T) {
 	producer, err := client.CreateProducerWithSchema(ProducerOptions{
 		Topic: "jsonTopic",
 	}, jsonSchema)
+	assert.Nil(t, err)
+	defer producer.Close()
 	err = producer.Send(context.Background(), ProducerMessage{
 		Value: &testJson{
 			ID:   100,
@@ -74,14 +76,15 @@ func TestJsonSchema(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	properties := make(map[string]string)
-	properties["pulsar"]="hello"
+	properties["pulsar"] = "hello"
 	jsonSchemaWithProperties := NewJsonSchema(exampleSchemaDef, properties)
 	producer1, err := client.CreateProducerWithSchema(ProducerOptions{
 		Topic: "jsonTopic",
 	}, jsonSchemaWithProperties)
+	assert.Nil(t, err)
+	defer producer1.Close()
 	err = producer1.Send(context.Background(), ProducerMessage{
 		Value: &testJson{
 			ID:   100,
@@ -91,7 +94,6 @@ func TestJsonSchema(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer producer1.Close()
 
 	//create consumer
 	var s testJson
@@ -102,14 +104,13 @@ func TestJsonSchema(t *testing.T) {
 		SubscriptionName: "sub-2",
 	}, consumerJS)
 	assert.Nil(t, err)
+	defer consumer.Close()
 	msg, err := consumer.Receive(context.Background())
 	assert.Nil(t, err)
 	err = msg.GetValue(&s)
 	assert.Nil(t, err)
 	assert.Equal(t, s.ID, 100)
 	assert.Equal(t, s.Name, "pulsar")
-
-	defer consumer.Close()
 }
 
 func TestProtoSchema(t *testing.T) {
@@ -121,6 +122,8 @@ func TestProtoSchema(t *testing.T) {
 	producer, err := client.CreateProducerWithSchema(ProducerOptions{
 		Topic: "proto",
 	}, psProducer)
+	assert.Nil(t,err)
+	defer producer.Close()
 	if err := producer.Send(context.Background(), ProducerMessage{
 		Value: &pb.Test{
 			Num: 100,
@@ -138,6 +141,7 @@ func TestProtoSchema(t *testing.T) {
 		SubscriptionName: "sub-1",
 	}, psConsumer)
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	msg, err := consumer.Receive(context.Background())
 	assert.Nil(t, err)
@@ -145,7 +149,6 @@ func TestProtoSchema(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, unobj.Num, int32(100))
 	assert.Equal(t, unobj.Msf, "pulsar")
-	defer consumer.Close()
 }
 
 func TestAvroSchema(t *testing.T) {
@@ -158,6 +161,7 @@ func TestAvroSchema(t *testing.T) {
 		Topic: "avro-topic",
 	}, asProducer)
 	assert.Nil(t, err)
+	defer producer.Close()
 	if err := producer.Send(context.Background(), ProducerMessage{
 		Value: testAvro{
 			ID:   100,
@@ -176,6 +180,7 @@ func TestAvroSchema(t *testing.T) {
 		SubscriptionName: "sub-1",
 	}, asConsumer)
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	msg, err := consumer.Receive(context.Background())
 	assert.Nil(t, err)
@@ -183,7 +188,6 @@ func TestAvroSchema(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, unobj.ID, 100)
 	assert.Equal(t, unobj.Name, "pulsar")
-	defer consumer.Close()
 }
 
 func TestStringSchema(t *testing.T) {
@@ -195,12 +199,12 @@ func TestStringSchema(t *testing.T) {
 		Topic: "strTopic",
 	}, ssProducer)
 	assert.Nil(t, err)
+	defer producer.Close()
 	if err := producer.Send(context.Background(), ProducerMessage{
 		Value: "hello pulsar",
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	var res *string
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
@@ -208,13 +212,12 @@ func TestStringSchema(t *testing.T) {
 		SubscriptionName: "sub-2",
 	}, NewStringSchema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	msg, err := consumer.Receive(context.Background())
 	assert.Nil(t, err)
 	err = msg.GetValue(&res)
 	assert.Equal(t, *res, "hello pulsar")
-
-	defer consumer.Close()
 }
 
 func TestBytesSchema(t *testing.T) {
@@ -226,13 +229,13 @@ func TestBytesSchema(t *testing.T) {
 		Topic: "bytesTopic",
 	}, NewBytesSchema(nil))
 	assert.Nil(t, err)
+	defer producer.Close()
 	ctx := context.Background()
 	if err := producer.Send(ctx, ProducerMessage{
 		Value: bytes,
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	var res []byte
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
@@ -240,13 +243,12 @@ func TestBytesSchema(t *testing.T) {
 		SubscriptionName: "sub-2",
 	}, NewBytesSchema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	msg, err := consumer.Receive(context.Background())
 	assert.Nil(t, err)
 	err = msg.GetValue(&res)
 	assert.Equal(t, res, bytes)
-
-	defer consumer.Close()
 }
 
 func TestInt8Schema(t *testing.T) {
@@ -257,19 +259,20 @@ func TestInt8Schema(t *testing.T) {
 		Topic: "int8Topic1",
 	}, NewInt8Schema(nil))
 	assert.Nil(t, err)
+	defer producer.Close()
 	ctx := context.Background()
 	if err := producer.Send(ctx, ProducerMessage{
 		Value: int8(1),
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
 		Topic:            "int8Topic1",
 		SubscriptionName: "sub-2",
 	}, NewInt8Schema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	var res int8
 	msg, err := consumer.Receive(ctx)
@@ -277,8 +280,6 @@ func TestInt8Schema(t *testing.T) {
 	err = msg.GetValue(&res)
 	assert.Nil(t, err)
 	assert.Equal(t, res, int8(1))
-
-	defer consumer.Close()
 }
 
 func TestInt16Schema(t *testing.T) {
@@ -289,19 +290,20 @@ func TestInt16Schema(t *testing.T) {
 		Topic: "int16Topic",
 	}, NewInt16Schema(nil))
 	assert.Nil(t, err)
+	defer producer.Close()
 	ctx := context.Background()
 	if err := producer.Send(ctx, ProducerMessage{
 		Value: int16(1),
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
 		Topic:            "int16Topic",
 		SubscriptionName: "sub-2",
 	}, NewInt16Schema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	var res int16
 	msg, err := consumer.Receive(ctx)
@@ -309,7 +311,6 @@ func TestInt16Schema(t *testing.T) {
 	err = msg.GetValue(&res)
 	assert.Nil(t, err)
 	assert.Equal(t, res, int16(1))
-	defer consumer.Close()
 }
 
 func TestInt32Schema(t *testing.T) {
@@ -320,19 +321,20 @@ func TestInt32Schema(t *testing.T) {
 		Topic: "int32Topic1",
 	}, NewInt32Schema(nil))
 	assert.Nil(t, err)
+	defer producer.Close()
 	ctx := context.Background()
 	if err := producer.Send(ctx, ProducerMessage{
 		Value: int32(1),
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
 		Topic:            "int32Topic1",
 		SubscriptionName: "sub-2",
 	}, NewInt32Schema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	var res int32
 	msg, err := consumer.Receive(ctx)
@@ -340,7 +342,6 @@ func TestInt32Schema(t *testing.T) {
 	err = msg.GetValue(&res)
 	assert.Nil(t, err)
 	assert.Equal(t, res, int32(1))
-	defer consumer.Close()
 }
 
 func TestInt64Schema(t *testing.T) {
@@ -351,19 +352,20 @@ func TestInt64Schema(t *testing.T) {
 		Topic: "int64Topic",
 	}, NewInt64Schema(nil))
 	assert.Nil(t, err)
+	defer producer.Close()
 	ctx := context.Background()
 	if err := producer.Send(ctx, ProducerMessage{
 		Value: int64(1),
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
 		Topic:            "int64Topic",
 		SubscriptionName: "sub-2",
 	}, NewInt64Schema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	var res int64
 	msg, err := consumer.Receive(ctx)
@@ -371,7 +373,6 @@ func TestInt64Schema(t *testing.T) {
 	err = msg.GetValue(&res)
 	assert.Nil(t, err)
 	assert.Equal(t, res, int64(1))
-	defer consumer.Close()
 }
 
 func TestFloatSchema(t *testing.T) {
@@ -382,18 +383,19 @@ func TestFloatSchema(t *testing.T) {
 		Topic: "floatTopic",
 	}, NewFloatSchema(nil))
 	assert.Nil(t, err)
+	defer producer.Close()
 	if err := producer.Send(context.Background(), ProducerMessage{
 		Value: float32(1),
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
 		Topic:            "floatTopic",
 		SubscriptionName: "sub-2",
 	}, NewFloatSchema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	var res float32
 	msg, err := consumer.Receive(context.Background())
@@ -401,7 +403,6 @@ func TestFloatSchema(t *testing.T) {
 	err = msg.GetValue(&res)
 	assert.Nil(t, err)
 	assert.Equal(t, res, float32(1))
-	defer consumer.Close()
 }
 
 func TestDoubleSchema(t *testing.T) {
@@ -412,19 +413,20 @@ func TestDoubleSchema(t *testing.T) {
 		Topic: "doubleTopic",
 	}, NewDoubleSchema(nil))
 	assert.Nil(t, err)
+	defer producer.Close()
 	ctx := context.Background()
 	if err := producer.Send(ctx, ProducerMessage{
 		Value: float64(1),
 	}); err != nil {
 		log.Fatal(err)
 	}
-	defer producer.Close()
 
 	consumer, err := client.SubscribeWithSchema(ConsumerOptions{
 		Topic:            "doubleTopic",
 		SubscriptionName: "sub-2",
 	}, NewDoubleSchema(nil))
 	assert.Nil(t, err)
+	defer consumer.Close()
 
 	var res float64
 	msg, err := consumer.Receive(ctx)
@@ -432,5 +434,4 @@ func TestDoubleSchema(t *testing.T) {
 	err = msg.GetValue(&res)
 	assert.Nil(t, err)
 	assert.Equal(t, res, float64(1))
-	defer consumer.Close()
 }
