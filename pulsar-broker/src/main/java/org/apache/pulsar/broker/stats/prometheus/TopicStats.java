ce8cc89e5d2efc48ea080ebf11cc30766f7384c9
tuteng <eguangning@gmail.com>
2019-05-18 01:00:47 +0800

    Add double quotation marks for metrics with remote_cluster (#4295)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/prometheus/TopicStats.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/prometheus/TopicStats.java
@@ -196,7 +196,7 @@ class TopicStats {
             String topic,
             String name, String remoteCluster, double value) {
         stream.write(name).write("{cluster=\"").write(cluster).write("\",namespace=\"").write(namespace);
-        stream.write("\",topic=\"").write(topic).write("remote_cluster=\"").write(remoteCluster).write("\"} ");
+        stream.write("\",topic=\"").write(topic).write("\",remote_cluster=\"").write(remoteCluster).write("\"} ");
         stream.write(value).write(' ').write(System.currentTimeMillis()).write('\n');
     }
 }
