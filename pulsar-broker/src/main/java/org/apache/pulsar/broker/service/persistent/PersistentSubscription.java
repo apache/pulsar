f419535f0817e5e2798406989e96b345b1956361
lipenghui <penghui@apache.org>
2019-05-31 14:41:45 +0800

    Use Subscription.isIndividualAckMode check in subscription stats

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentSubscription.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentSubscription.java
@@ -670,7 +670,7 @@ public class PersistentSubscription implements Subscription {
                 subStats.activeConsumerName = activeConsumer.consumerName();
             }
         }
-        if (SubType.Shared.equals(subStats.type) || SubType.Key_Shared.equals(subStats.type)) {
+        if (Subscription.isIndividualAckMode(subStats.type)) {
             if (dispatcher instanceof PersistentDispatcherMultipleConsumers) {
                 PersistentDispatcherMultipleConsumers d = (PersistentDispatcherMultipleConsumers) dispatcher;
                 subStats.unackedMessages = d.getTotalUnackedMessages();
