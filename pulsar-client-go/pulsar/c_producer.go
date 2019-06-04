7d4d914434cecf1a6fa5f8fe51472c1c65086678
xiaolong.ran <ranxiaolong716@gmail.com>
2019-06-04 18:29:48 +0800

    Support key-shared subscribe mode for go client
    Signed-off-by: xiaolong.ran <ranxiaolong716@gmail.com>

--- pulsar-client-go/pulsar/c_producer.go
+++ pulsar-client-go/pulsar/c_producer.go
@@ -156,7 +156,7 @@ func createProducerAsync(client *client, schema Schema, options ProducerOptions,
 		C._pulsar_producer_configuration_set_message_router(conf, savePointer(&options.MessageRouter))
 	}
 
-	if options.Batching {
+	if !options.Batching {
 		C.pulsar_producer_configuration_set_batching_enabled(conf, cBool(options.Batching))
 	}
 
