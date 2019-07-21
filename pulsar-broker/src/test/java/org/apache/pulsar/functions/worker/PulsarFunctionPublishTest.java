719ce13983cf2962c7b39149f858852df534d9df
vzhikserg <vzhikserg@users.noreply.github.com>
2019-07-22 01:28:29 +0200

    Convert anonymous classes to lambda (#4703)
    * Convert anonymous functions to lambda
    
    * Replacing lambda with anonymous implementation, because lambda cannot be mocked
    
    (cherry picked from commit 84364ddc10394f7361083a3505b34f0dc5710316)

--- pulsar-broker/src/test/java/org/apache/pulsar/functions/worker/PulsarFunctionPublishTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/functions/worker/PulsarFunctionPublishTest.java
@@ -123,11 +123,7 @@ public class PulsarFunctionPublishTest {
 
         // delete all function temp files
         File dir = new File(System.getProperty("java.io.tmpdir"));
-        File[] foundFiles = dir.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.startsWith("function");
-            }
-        });
+        File[] foundFiles = dir.listFiles((ignoredDir, name) -> name.startsWith("function"));
 
         for (File file : foundFiles) {
             file.delete();
@@ -376,11 +372,7 @@ public class PulsarFunctionPublishTest {
 
         // make sure all temp files are deleted
         File dir = new File(System.getProperty("java.io.tmpdir"));
-        File[] foundFiles = dir.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.startsWith("function");
-            }
-        });
+        File[] foundFiles = dir.listFiles((dir1, name) -> name.startsWith("function"));
 
         Assert.assertEquals(foundFiles.length, 0, "Temporary files left over: " + Arrays.asList(foundFiles));
     }
