8e1d00ffabf3cf75ec69c504334934a3b4caa4af
Matteo Merli <mmerli@apache.org>
2019-07-22 15:03:26 -0700

    Added more blog posts in the resources page (#4774)
    * Added more blog posts in the resources page
    
    * Added 2 more posts

--- site2/website/data/resources.js
+++ site2/website/data/resources.js
@@ -23,6 +23,175 @@ module.exports = {
       forum_link: 'https://streaml.io/blog',
       title: 'Why Apache Pulsar? Part 2',
       link: 'https://streaml.io/blog/why-apache-pulsar-part-2'
+    },
+    {
+      forum: "Jack Vanlightly's blog",
+      forum_link: 'https://jack-vanlightly.com',
+      title: 'Understanding How Apache Pulsar Works',
+      link: 'https://jack-vanlightly.com/blog/2018/10/2/understanding-how-apache-pulsar-works'
+    },
+    {
+        forum: "Anuradha Prasanna's blog",
+        forum_link: 'https://medium.com/@anuradha.neo',
+        title: 'Kafka is not the best anymore - Meet Pulsar!',
+        link: 'https://medium.com/@anuradha.neo/kafka-is-not-the-best-anymore-meet-pulsar-9eb435c9fc0b'
+    },
+    {
+        forum: 'Kafkaesque blog',
+        forum_link: 'https://kafkaesque.io/news/',
+        title: '7 Reasons We Choose Apache Pulsar over Apache Kafka',
+        link: 'https://kafkaesque.io/7-reasons-we-choose-apache-pulsar-over-apache-kafka/'
+    },
+    {
+        forum: "Jack Vanlightly's blog",
+        forum_link: 'https://jack-vanlightly.com',
+        title: 'How to (not) lose messages on an Apache Pulsar Cluster',
+        link: 'https://jack-vanlightly.com/blog/2018/10/21/how-to-not-lose-messages-on-an-apache-pulsar-cluster'
+    },
+    {
+        forum: 'StreamNative blog',
+        forum_link: 'https://medium.com/streamnative/',
+        title: 'Apache Pulsar as One Unified Storage System for Real Time and Historical Analysis',
+        link: 'https://medium.com/streamnative/apache-pulsar-as-one-storage-455222c59017'
+    },
+    {
+        forum: 'StreamNative blog',
+        forum_link: 'https://medium.com/streamnative/',
+        title: "What’s New in Apache Pulsar 2.4.0",
+        link: 'https://medium.com/streamnative/whats-new-in-apache-pulsar-2-4-0-d646f6727642'
+    },
+    {
+        forum: "Penghui Li's blog",
+        forum_link: 'https://medium.com/@codelipenghui',
+        title: "Simplifying Zhaopin's Event Center Using Apache Pulsar",
+        link: 'https://medium.com/@codelipenghui/simplifying-zhaopins-event-center-with-apache-pulsar-9784b73bead1'
+    },
+    {
+        forum: "Jowanza Joseph's blog",
+        forum_link: "https://www.jowanza.com/blog",
+        title: 'Efficient Stream Processing With Pulsar Functions',
+        link: 'https://www.jowanza.com/blog/2019/3/9/efficient-stream-processing-with-pulsar-functions'
+    },
+    {
+        forum: "Jesse Anderson's blog",
+        forum_link: 'http://www.jesse-anderson.com/',
+        title: 'Creating Work Queues with Apache Kafka and Apache Pulsar',
+        link: 'http://www.jesse-anderson.com/2018/08/creating-work-queues-with-apache-kafka-and-apache-pulsar/'
+    },
+    {
+        forum: 'Streamlio blog',
+        forum_link: 'https://streaml.io/blog',
+        title: 'Building Data Driven Applications at STICORP using Apache Pulsar',
+        link: 'https://streaml.io/blog/building-data-driven-applications-with-apache-pulsar-at-sticorp'
+    },
+    {
+        forum: 'The Hut Group tech blog',
+        forum_link: 'https://medium.com/thg-tech-blog',
+        title: 'A feather in the caps',
+        link: 'https://medium.com/thg-tech-blog/a-feather-in-their-caps-4c4083cf9f46'
+    },
+    {
+        forum: "Karthikeyan Palanivelu's blog",
+        forum_link: 'https://medium.com/@pckeyan',
+        title: 'Apache Pulsar — One Cluster for the entire enterprise using Multi-tenancy',
+        link: 'https://medium.com/@pckeyan/apache-pulsar-one-cluster-for-the-entire-enterprise-using-multi-tenancy-c37e2ee350ef'
+    },
+    {
+        forum: "Capital One tech blog",
+        forum_link: "https://medium.com/capital-one-tech",
+        title: 'Apache Pulsar — A Gentle Introduction to Apache’s Newest Pub-Sub Messaging Platform',
+        link: 'https://medium.com/capital-one-tech/apache-pulsar-apaches-newest-pub-sub-messaging-platform-1c1ba1a6c673'
+    },
+    {
+        forum: "Karthikeyan Palanivelu's blog",
+        forum_link: 'https://medium.com/@pckeyan',
+        title: 'Apache Pulsar: Geo-replication',
+        link: 'https://medium.com/@pckeyan/apache-pulsar-geo-replication-ad4f0ca3224b'
+    },
+    {
+        forum: "Karthikeyan Palanivelu's blog",
+        forum_link: 'https://medium.com/@pckeyan',
+        title: 'Apache Pulsar: Geo-replication — Synchronous Replication : Hybrid Deployment Model',
+        link: 'https://medium.com/@pckeyan/apache-pulsar-geo-replication-synchronous-replication-hybrid-deployment-model-3282013e9ae5'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Pulsar topic compaction',
+        link: 'https://streaml.io/blog/pulsar-topic-compaction'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Tiered Storage in Apache Pulsar',
+        link: 'https://streaml.io/blog/tiered-storage-in-apache-pulsar'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Introducing Pulsar IO',
+        link: 'https://streaml.io/blog/introducing-pulsar-io'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Pulsar topic compaction',
+        link: 'https://streaml.io/blog/pulsar-topic-compaction'
+    },
+
+    {
+        forum: "Jesse Anderson's blog",
+        forum_link: 'https://www.jesse-anderson.com',
+        title: 'Reducing Operational Overhead with Pulsar Functions',
+        link: 'https://www.jesse-anderson.com/2019/05/reducing-operational-overhead-with-pulsar-functions/'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Querying Data Streams with Apache Pulsar SQL',
+        link: 'https://streaml.io/blog/querying-data-streams-with-apache-pulsar-sql'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Configuring Apache Pulsar Tiered Storage with Amazon S3',
+        link: 'https://streaml.io/blog/configuring-apache-pulsar-tiered-storage-with-amazon-s3'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Comparing LogDevice and Apache Pulsar',
+        link: 'https://streaml.io/blog/comparing-logdevice-and-apache-pulsar'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Pulsar’s Multi-Layer System Architecture',
+        link: 'https://streaml.io/blog/apache-pulsar-architecture-designing-for-streaming-performance-and-scalability'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Debugging Pulsar Functions in Java',
+        link: 'https://streaml.io/blog/debugging-pulsar-functions-in-java'
+    },
+    {
+        forum: "Streamlio blog",
+        forum_link: 'https://streaml.io/blog',
+        title: 'Sentiment Analysis of Tweets using Apache Pulsar',
+        link: 'https://streaml.io/blog/sentiment-analysis-of-tweets-using-apache-pulsar'
+    },
+    {
+        forum: 'Apache Flink blog',
+        forum_link: 'https://flink.apache.org/blog/',
+        title: 'When Flink & Pulsar Come Together',
+        link: 'https://flink.apache.org/2019/05/03/pulsar-flink.html'
+    },
+    {
+        forum: 'Debezium blog',
+        forum_link: 'https://debezium.io/blog',
+        title: 'Tutorial for Using Debezium Connectors With Apache Pulsar',
+        link: 'https://debezium.io/blog/2019/05/23/tutorial-using-debezium-connectors-with-apache-pulsar/'
     }
   ],
   presentations: [
