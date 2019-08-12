0391a9aa1cc9d86edf77a6aab42f9a27137992fd
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-08-12 16:39:55 +0800

    Fix license header

--- pulsar-transaction/buffer/src/main/java/org/apache/pulsar/transaction/buffer/impl/PersistentTransactionBufferProvider.java
+++ pulsar-transaction/buffer/src/main/java/org/apache/pulsar/transaction/buffer/impl/PersistentTransactionBufferProvider.java
@@ -15,9 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
-
 package org.apache.pulsar.transaction.buffer.impl;
 
 import java.util.concurrent.CompletableFuture;
