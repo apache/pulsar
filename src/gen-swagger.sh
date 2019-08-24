0847dddd739bcfbddf2008cf5b81f23e8c2a3eb9
tuteng <eguangning@gmail.com>
2019-08-24 12:15:18 +0800

    Generate swagger json for multi api

--- src/gen-swagger.sh
+++ src/gen-swagger.sh
@@ -22,18 +22,29 @@ PULSAR_PATH=$(git rev-parse --show-toplevel)
 
 cd $PULSAR_PATH
 
-git checkout branch-2.3
-
-echo "Generating swagger json file for 2.3 ..."
+git checkout v2.3.0
+echo "Generating swagger json file for 2.3.0 ..."
 mvn -am -pl pulsar-broker install -DskipTests -Pswagger
-echo "Swagger json file is generated for 2.3."
+echo "Swagger json file is generated for 2.3.0"
 
 cp pulsar-broker/target/docs/swagger*.json site2/website/static/swagger/2.3.0/
+
+git checkout v2.3.1
+echo "Generating swagger json file for 2.3.1 ..."
+mvn -am -pl pulsar-broker install -DskipTests -Pswagger
+echo "Swagger json file is generated for 2.3.1"
+
 cp pulsar-broker/target/docs/swagger*.json site2/website/static/swagger/2.3.1/
+
+git checkout v2.3.2
+echo "Generating swagger json file for 2.3.2 ..."
+mvn -am -pl pulsar-broker install -DskipTests -Pswagger
+echo "Swagger json file is generated for 2.3.2"
+
 cp pulsar-broker/target/docs/swagger*.json site2/website/static/swagger/2.3.2/
 
 
-git checkout branch-2.4
+git checkout v2.4.0
 echo "Generating swagger json file for 2.4.0 ..."
 mvn -am -pl pulsar-broker install -DskipTests -Pswagger
 echo "Swagger json file is generated for 2.4.0."
