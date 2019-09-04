54288f3260806684250b6b8df114cf4d435526b4
lipenghui <penghui@apache.org>
2019-09-04 11:12:20 +0800

    Fix storage size always 0 without subscription.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -1403,7 +1403,7 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
             stats.replication.put(replicator.getRemoteCluster(), replicatorStats);
         });
 
-        stats.storageSize = ledger.getEstimatedBacklogSize();
+        stats.storageSize = ledger.getTotalSize();
         stats.deduplicationStatus = messageDeduplication.getStatus().toString();
 
         return stats;
