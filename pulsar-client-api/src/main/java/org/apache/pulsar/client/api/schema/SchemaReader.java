629d04b08cbe099c4c128d72ea7ca4897455ca1e
congbo <39078850+congbobo184@users.noreply.github.com>
2019-09-19 20:25:52 +0800

    Modify the schema decode method can decode ByteBuf (#5123)

--- pulsar-client-api/src/main/java/org/apache/pulsar/client/api/schema/SchemaReader.java
+++ pulsar-client-api/src/main/java/org/apache/pulsar/client/api/schema/SchemaReader.java
@@ -18,9 +18,12 @@
  */
 package org.apache.pulsar.client.api.schema;
 
+import java.io.InputStream;
+
 /**
  * Deserialize messages from bytes.
  */
+
 public interface SchemaReader<T> {
 
     /**
@@ -29,5 +32,25 @@ public interface SchemaReader<T> {
      * @param bytes the data
      * @return the serialized object
      */
-    T read(byte[] bytes);
+    default T read(byte[] bytes) {
+        return read(bytes, 0, bytes.length);
+    }
+
+    /**
+     * serialize bytes convert pojo.
+     *
+     * @param bytes the data
+     * @param offset the byte[] initial position
+     * @param length the byte[] read length
+     * @return the serialized object
+     */
+    T read(byte[] bytes, int offset, int length);
+
+    /**
+     * serialize bytes convert pojo.
+     *
+     * @param inputStream the stream of message
+     * @return the serialized object
+     */
+    T read(InputStream inputStream);
 }
--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/schema/LongSchema.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/schema/LongSchema.java
@@ -18,7 +18,7 @@
  */
 package org.apache.pulsar.client.impl.schema;
 
-import org.apache.pulsar.client.api.Schema;
+import io.netty.buffer.ByteBuf;
 import org.apache.pulsar.client.api.SchemaSerializationException;
 import org.apache.pulsar.common.schema.SchemaInfo;
 import org.apache.pulsar.common.schema.SchemaType;
@@ -26,7 +26,7 @@ import org.apache.pulsar.common.schema.SchemaType;
 /**
  * A schema for `Long`.
  */
-public class LongSchema implements Schema<Long> {
+public class LongSchema extends AbstractSchema<Long> {
 
     public static LongSchema of() {
         return INSTANCE;
@@ -41,6 +41,13 @@ public class LongSchema implements Schema<Long> {
     @Override
     public void validate(byte[] message) {
         if (message.length != 8) {
+            throw new SchemaSerializationException("Size of data received by LongSchema is not 8");
+        }
+    }
+
+    @Override
+    public void validate(ByteBuf message) {
+        if (message.readableBytes() != 8) {
             throw new SchemaSerializationException("Size of data received by LongSchema is not 8");
         }
     }
@@ -74,6 +81,21 @@ public class LongSchema implements Schema<Long> {
             value <<= 8;
             value |= b & 0xFF;
         }
+        return value;
+    }
+
+    @Override
+    public Long decode(ByteBuf byteBuf) {
+        if (null == byteBuf) {
+            return null;
+        }
+        validate(byteBuf);
+        long value = 0L;
+        for (int i = 0; i < 8; i++) {
+            value <<= 8;
+            value |= byteBuf.getByte(i) & 0xFF;
+        }
+
         return value;
     }
 
