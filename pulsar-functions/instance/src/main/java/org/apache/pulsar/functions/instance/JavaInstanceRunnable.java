412d077d8d2b3a8afc8450366b62d0815c1c1d7a
xiaolong.ran <ranxiaolong716@gmail.com>
2019-10-10 19:39:10 +0800

    [functions][Issue:5350]Fix pulsar can't load the customized SerDe
    Signed-off-by: xiaolong.ran <ranxiaolong716@gmail.com>

--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstanceRunnable.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstanceRunnable.java
@@ -207,6 +207,8 @@ public class JavaInstanceRunnable implements AutoCloseable, Runnable {
 
         ContextImpl contextImpl = setupContext();
 
+        Thread.currentThread().setContextClassLoader(functionClassLoader);
+
         // start the output producer
         setupOutput(contextImpl);
         // start the input consumer
@@ -263,7 +265,6 @@ public class JavaInstanceRunnable implements AutoCloseable, Runnable {
                 stats.processTimeStart();
 
                 // process the message
-                Thread.currentThread().setContextClassLoader(functionClassLoader);
                 result = javaInstance.handleMessage(currentRecord, currentRecord.getValue());
                 Thread.currentThread().setContextClassLoader(instanceClassLoader);
 
