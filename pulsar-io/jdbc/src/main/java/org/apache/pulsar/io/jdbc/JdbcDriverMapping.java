8eb512d94db3e15e6737ae6f1434eddaa5f35f1b
黄大相 <localhost80@foxmail.com>
2019-11-13 14:15:53 +0800

    add jdbc driver

--- pulsar-io/jdbc/src/main/java/org/apache/pulsar/io/jdbc/JdbcDriverMapping.java
+++ pulsar-io/jdbc/src/main/java/org/apache/pulsar/io/jdbc/JdbcDriverMapping.java
@@ -25,35 +25,12 @@ import java.util.Map;
 public class JdbcDriverMapping {
     private static Map<String,String> DRIVER_MAPPING = new HashMap<>(32);
     static{
-        DRIVER_MAPPING.put("db2","com.ibm.db2.jcc.DB2Driver");
         DRIVER_MAPPING.put("postgresql","org.postgresql.Driver");
-        DRIVER_MAPPING.put("hsqldb","org.hsqldb.jdbcDriver");
-        DRIVER_MAPPING.put("sybase","com.sybase.jdbc.SybDriver");
         DRIVER_MAPPING.put("sqlserver","com.microsoft.jdbc.sqlserver.SQLServerDriver");
-        DRIVER_MAPPING.put("jtds","net.sourceforge.jtds.jdbc.Driver");
-        DRIVER_MAPPING.put("oracle","oracle.jdbc.OracleDriver");
-        DRIVER_MAPPING.put("AliOracle","com.alibaba.jdbc.AlibabaDriver");
         DRIVER_MAPPING.put("mysql","com.mysql.jdbc.Driver");
         DRIVER_MAPPING.put("mariadb","org.mariadb.jdbc.Driver");
-        DRIVER_MAPPING.put("derby","org.apache.derby.jdbc.EmbeddedDriver");
-        DRIVER_MAPPING.put("hive","org.apache.hive.jdbc.HiveDriver");
-        DRIVER_MAPPING.put("h2","org.h2.Driver");
-        DRIVER_MAPPING.put("dm","dm.jdbc.driver.DmDriver");
-        DRIVER_MAPPING.put("kingbase","com.kingbase.Driver");
-        DRIVER_MAPPING.put("gbase","com.gbase.jdbc.Driver");
-        DRIVER_MAPPING.put("xugu","com.xugu.cloudjdbc.Driver");
-        DRIVER_MAPPING.put("oceanbase","com.mysql.jdbc.Driver");
-        DRIVER_MAPPING.put("odps","com.aliyun.odps.jdbc.OdpsDriver");
-        DRIVER_MAPPING.put("teradata","com.teradata.jdbc.TeraDriver");
-        DRIVER_MAPPING.put("log4jdbc","net.sf.log4jdbc.DriverSpy");
-        DRIVER_MAPPING.put("phoenix","org.apache.phoenix.jdbc.PhoenixDriver");
-        DRIVER_MAPPING.put("edb","com.edb.Driver");
-        DRIVER_MAPPING.put("kylin","org.apache.kylin.jdbc.Driver");
         DRIVER_MAPPING.put("sqlite","org.sqlite.JDBC");
-        DRIVER_MAPPING.put("presto","com.facebook.presto.jdbc.PrestoDriver");
-        DRIVER_MAPPING.put("elastic_search","com.alibaba.xdriver.elastic.jdbc.ElasticDriver");
         DRIVER_MAPPING.put("clickhouse","ru.yandex.clickhouse.ClickHouseDriver");
-        DRIVER_MAPPING.put("presto","com.facebook.presto.jdbc.PrestoDriver");
     }
     public static String getDriverClass(String type){
         return DRIVER_MAPPING.get(type);
