1fae168c4fe1d8e3002868a1f5f8131ed3b29393
Boyang Jerry Peng <jerry.boyang.peng@gmail.com>
2019-11-13 03:18:47 -0800

    Fix: don't set interrupt flag again after catching interrupt exception in Pulsar Client (#5643)
    (cherry picked from commit b7b9926b243fbf16e582c29fb1c2b0abe7777785)

--- pulsar-client-api/src/main/java/org/apache/pulsar/client/api/PulsarClientException.java
+++ pulsar-client-api/src/main/java/org/apache/pulsar/client/api/PulsarClientException.java
@@ -232,7 +232,6 @@ public class PulsarClientException extends IOException {
         } else if (t instanceof RuntimeException) {
             throw (RuntimeException) t;
         }  else if (t instanceof InterruptedException) {
-            Thread.currentThread().interrupt();
             return new PulsarClientException(t);
         } else if (!(t instanceof ExecutionException)) {
             // Generic exception
