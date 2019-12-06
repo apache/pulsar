3b7e5137289af2a23f254227290a33143765fbdf
lipenghui <penghui@apache.org>
2019-12-06 18:08:12 +0800

    Fix cpp tests

--- pulsar-client-cpp/lib/ClientConnection.cc
+++ pulsar-client-cpp/lib/ClientConnection.cc
@@ -117,7 +117,6 @@ static Result getResult(ServerError serverError) {
 
         case InvalidTxnStatus:
             return ResultInvalidTxnStatusError;
-
     }
     // NOTE : Do not add default case in the switch above. In future if we get new cases for
     // ServerError and miss them in the switch above we would like to get notified. Adding
