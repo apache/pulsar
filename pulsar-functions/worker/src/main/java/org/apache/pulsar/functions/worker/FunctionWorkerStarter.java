61a2c0c2829780882ce47c8ffec881651802655e
黄大相 <localhost80@foxmail.com>
2019-12-12 21:38:46 +0800

    reset

--- pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/FunctionWorkerStarter.java
+++ pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/FunctionWorkerStarter.java
@@ -18,21 +18,12 @@
  */
 package org.apache.pulsar.functions.worker;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * A starter to start function worker.
@@ -42,8 +33,8 @@ public class FunctionWorkerStarter {
 
     private static class WorkerArguments {
         @Parameter(
-            names = { "-c", "--conf" },
-            description = "Configuration File for Function Worker")
+                names = { "-c", "--conf" },
+                description = "Configuration File for Function Worker")
         private String configFile;
 
         @Parameter(names = {"-h", "--help"}, description = "Show this help message")
@@ -73,7 +64,6 @@ public class FunctionWorkerStarter {
 
         final Worker worker = new Worker(workerConfig);
         try {
-            loadJdbcDriver(workerConfig.getConnectorsDirectory());
             worker.start();
         }catch(Exception e){
             log.error("Failed to start function worker", e);
@@ -84,59 +74,5 @@ public class FunctionWorkerStarter {
             log.info("Stopping function worker service ..");
             worker.stop();
         }));
-    }
-    private static void loadJdbcDriver(String connectorsDirectory) throws IOException {
-        Path path = Paths.get(connectorsDirectory).toAbsolutePath();
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, "*.jar")) {
-            for (Path archive : stream) {
-                loadJar(archive.toFile().getPath());
-            }
-        }
-    }
-    private static void loadJar(String jarPath) {
-        File jarFile = new File(jarPath);
-        if(jarFile.exists() == false){
-            return;
-        }
-        Method method = null;
-        boolean accessible = false;
-        try {
-            method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-            URLClassLoader classLoader = (URLClassLoader) getClassLoader();
-            accessible = method.isAccessible();
-            if (accessible){
-                method.setAccessible(true);
-            }
-            method.invoke(classLoader,jarFile.toURI().toURL());
-        } catch (Exception e) {
-            log.error("load jar error ..",e);
-        }finally {
-            if(method != null){
-                method.setAccessible(accessible);
-            }
-        }
-    }
-    private static ClassLoader getClassLoader() {
-        ClassLoader cl = null;
-        try {
-            cl = Thread.currentThread().getContextClassLoader();
-        }
-        catch (Throwable ex) {
-            // Cannot access thread context ClassLoader - falling back...
-        }
-        if (cl == null) {
-            // No thread context class loader -> use class loader of this class.
-            cl = FunctionWorkerStarter.class.getClassLoader();
-            if (cl == null) {
-                // getClassLoader() returning null indicates the bootstrap ClassLoader
-                try {
-                    cl = ClassLoader.getSystemClassLoader();
-                }
-                catch (Throwable ex) {
-                    // Cannot access system ClassLoader - oh well, maybe the caller can live with null...
-                }
-            }
-        }
-        return cl;
     }
 }
