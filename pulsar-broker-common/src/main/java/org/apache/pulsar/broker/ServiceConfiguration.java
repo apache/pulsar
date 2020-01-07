c83dd46dacc23f51475276e557598ca6ec8dc0b3
Penghui Li <penghui@apache.org>
2020-01-07 10:36:37 +0800

    Add interceptor definition

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
@@ -91,6 +91,8 @@ public class ServiceConfiguration implements PulsarConfiguration {
     private static final String CATEGORY_HTTP = "HTTP";
     @Category
     private static final String CATEGORY_TRANSACTION = "Transaction";
+    @Category
+    private static final String CATEGORY_INTERCEPTORS = "Interceptors";
 
     /***** --- pulsar configuration --- ****/
     @FieldContext(
@@ -1748,6 +1750,20 @@ public class ServiceConfiguration implements PulsarConfiguration {
                   + " used by the internal client to authenticate with Pulsar brokers"
     )
     private Set<String> brokerClientTlsProtocols = Sets.newTreeSet();
+
+    /**** --- Interceptor variables --- ****/
+
+    @FieldContext(
+        category = CATEGORY_INTERCEPTORS,
+        doc = "The directory to locate interceptors"
+    )
+    private String interceptorDirectory = "./interceptors";
+
+    @FieldContext(
+        category = CATEGORY_INTERCEPTORS,
+        doc = "List of interceptors to load, which is a list of interceptor names"
+    )
+    private Set<String> interceptors = Sets.newTreeSet();
 
     /**
      * @deprecated See {@link #getConfigurationStoreServers}
