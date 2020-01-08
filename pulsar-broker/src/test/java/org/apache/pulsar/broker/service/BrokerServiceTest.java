f3ca73f0677f282f2e3773a7ba3cbfc8c6d4c51d
lipenghui <penghui@apache.org>
2020-01-08 17:41:17 +0800

    Fix unit test (#6006)
    ### Motivation
    
    Since #5599 merged, it introduce some conflict code with master branch, maybe the reason is #5599 not rebase with master
    
    ### Verifying this change
    
    This is a test change
    
    (cherry picked from commit 275854ead37cd5bb39cac8b69cdbfdfd2487c533)

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/BrokerServiceTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/BrokerServiceTest.java
@@ -946,9 +946,9 @@ public class BrokerServiceTest extends BrokerTestBase {
                 .get(mlFactory);
         assertNotNull(ledgers.get(topicMlName));
 
-        org.apache.pulsar.broker.service.Producer prod = spy(topic.producers.values().get(0));
+        org.apache.pulsar.broker.service.Producer prod = (org.apache.pulsar.broker.service.Producer) spy(topic.producers.values().toArray()[0]);
         topic.producers.clear();
-        topic.producers.add(prod);
+        topic.producers.put(prod.getProducerName(), prod);
         CompletableFuture<Void> waitFuture = new CompletableFuture<Void>();
         doReturn(waitFuture).when(prod).disconnect();
         Set<NamespaceBundle> bundles = pulsar.getNamespaceService().getOwnedServiceUnits();
