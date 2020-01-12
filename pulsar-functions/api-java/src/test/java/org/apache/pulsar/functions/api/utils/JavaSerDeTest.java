b690e5137265bcf2a953b8db92ef69843cbda540
Sergii Zhevzhyk <vzhikserg@gmail.com>
2020-01-12 21:41:44 +0100

    Remove duplicated lombok annotations in the pulsar-functions modules

--- pulsar-functions/api-java/src/test/java/org/apache/pulsar/functions/api/utils/JavaSerDeTest.java
+++ pulsar-functions/api-java/src/test/java/org/apache/pulsar/functions/api/utils/JavaSerDeTest.java
@@ -23,8 +23,6 @@ import static org.testng.Assert.assertEquals;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 import org.testng.annotations.Test;
 
 /**
@@ -34,8 +32,6 @@ public class JavaSerDeTest {
 
     @Data
     @AllArgsConstructor
-    @EqualsAndHashCode
-    @ToString
     private static class TestObject implements Serializable {
 
         private int intField;
