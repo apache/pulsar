a72d3eb2416e25670215ac085492ba7109e35890
cbornet <cbornet@hotmail.com>
2020-01-26 01:26:03 +0100

    Clean up

--- pulsar-protocols/grpc/src/main/java/org/apache/pulsar/protocols/grpc/PulsarGrpcService.java
+++ pulsar-protocols/grpc/src/main/java/org/apache/pulsar/protocols/grpc/PulsarGrpcService.java
@@ -35,24 +35,6 @@ public class PulsarGrpcService extends PulsarGrpcServiceGrpc.PulsarGrpcServiceIm
     }
 
     @Override
-    public void hello(SimpleValue request, StreamObserver<SimpleValue> responseObserver) {
-        service.getTopic(request.getName(), true)
-            .whenComplete((topic, throwable) ->
-                {
-                    if (throwable == null) {
-                        String state = topic.map(topic1 -> topic1.getInternalStats().state).orElse("null");
-                        SimpleValue reply = SimpleValue.newBuilder().setName(state).build();
-                        responseObserver.onNext(reply);
-                        responseObserver.onCompleted();
-                    } else {
-                        responseObserver.onError(throwable);
-                    }
-                }
-            );
-
-    }
-
-    @Override
     public StreamObserver<PulsarApi.BaseCommand> produce(StreamObserver<PulsarApi.BaseCommand> responseObserver) {
         PulsarApi.CommandProducer cmdProducer = PRODUCER_PARAMS_CTX_KEY.get();
         final String topic = cmdProducer.getTopic();
