8003d08e5ca325867d2e825921f18ddda8d4e1d4
congbo <39078850+congbobo184@users.noreply.github.com>
2020-03-06 14:28:30 +0800

    Independent schema is set for each consumer generated by topic (#6356)
    ### Motivation
    
    Master Issue: #5454 
    
    When one Consumer subscribe multi topic, setSchemaInfoPorvider() will be covered by the consumer generated by the last topic.
    
    ### Modification
    clone schema for each consumer generated by topic.
    ### Verifying this change
    Add the schemaTest for it.

--- pulsar-broker/src/test/java/org/apache/pulsar/schema/Schemas.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/schema/Schemas.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pulsar.schema.compatibility;
+package org.apache.pulsar.schema;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
