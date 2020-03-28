503805e8481f612dd46dfed230b967b1a09db99d
linlin <315157973@qq.com>
2020-03-28 17:36:33 +0800

    fix #6619

--- pulsar-websocket/src/test/java/org/apache/pulsar/websocket/AbstractWebSocketHandlerTest.java
+++ pulsar-websocket/src/test/java/org/apache/pulsar/websocket/AbstractWebSocketHandlerTest.java
@@ -1,6 +1,23 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pulsar.websocket;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.pulsar.broker.authentication.AuthenticationDataSource;
 import org.apache.pulsar.common.naming.TopicName;
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
@@ -14,10 +31,7 @@ import java.io.IOException;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- * @author: feynmamnlin
- * @date: 2020/3/28 11:54 上午
- */
+
 public class AbstractWebSocketHandlerTest {
     @Mock
     private HttpServletRequest httpServletRequest;
@@ -31,7 +45,7 @@ public class AbstractWebSocketHandlerTest {
         String producerV2 = "/ws/v2/producer/persistent/my-property/my-ns/my-topic";
         String consumerV2 = "/ws/v2/consumer/persistent/my-property/my-ns/my-topic/my-subscription";
         String consumerLongTopicNameV2 = "/ws/v2/consumer/persistent/my-tenant/my-ns/some/topic/with/slashes/my-sub";
-        String readerV2 = "/ws/v2/reader/persistent/my-property/my-ns/my-topic";
+        String readerV2 = "/ws/v2/reader/persistent/my-property/my-ns/my-topic/ / /@!$#^&*( /)1 /_+、`，《》</>";
 
         httpServletRequest = mock(HttpServletRequest.class);
 
@@ -43,7 +57,7 @@ public class AbstractWebSocketHandlerTest {
         when(httpServletRequest.getRequestURI()).thenReturn(consumerV1);
         webSocketHandler = new WebSocketHandlerImpl(null, httpServletRequest, null);
         topicName = webSocketHandler.getTopic();
-        Assert.assertEquals("persistent://my-property/my-cluster/my-ns/my-topic/my-subscription", topicName.toString());
+        Assert.assertEquals("persistent://my-property/my-cluster/my-ns/my-topic", topicName.toString());
 
         when(httpServletRequest.getRequestURI()).thenReturn(readerV1);
         webSocketHandler = new WebSocketHandlerImpl(null, httpServletRequest, null);
@@ -58,17 +72,17 @@ public class AbstractWebSocketHandlerTest {
         when(httpServletRequest.getRequestURI()).thenReturn(consumerV2);
         webSocketHandler = new WebSocketHandlerImpl(null, httpServletRequest, null);
         topicName = webSocketHandler.getTopic();
-        Assert.assertEquals("persistent://my-property/my-ns/my-topic/my-subscription", topicName.toString());
+        Assert.assertEquals("persistent://my-property/my-ns/my-topic", topicName.toString());
 
         when(httpServletRequest.getRequestURI()).thenReturn(consumerLongTopicNameV2);
         webSocketHandler = new WebSocketHandlerImpl(null, httpServletRequest, null);
         topicName = webSocketHandler.getTopic();
-        Assert.assertEquals("persistent://my-tenant/my-ns/some/topic/with/slashes/my-sub", topicName.toString());
+        Assert.assertEquals("persistent://my-tenant/my-ns/some/topic/with/slashes", topicName.toString());
 
         when(httpServletRequest.getRequestURI()).thenReturn(readerV2);
         webSocketHandler = new WebSocketHandlerImpl(null, httpServletRequest, null);
         topicName = webSocketHandler.getTopic();
-        Assert.assertEquals("persistent://my-property/my-ns/my-topic", topicName.toString());
+        Assert.assertEquals("persistent://my-property/my-ns/my-topic/ / /@!$#^&*( /)1 /_+、`，《》</>", topicName.toString());
 
     }
 
