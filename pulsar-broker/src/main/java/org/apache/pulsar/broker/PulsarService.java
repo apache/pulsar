2ebdaf79b44c860da761e75c2022c3faac8c6b9f
rdhabalia <rdhabalia@apache.org>
2020-04-03 16:01:25 -0700

    [minor] avoid function call to get globalzk

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -637,7 +637,7 @@ public class PulsarService implements AutoCloseable {
             throw new PulsarServerException(e);
         }
 
-        this.configurationCacheService = new ConfigurationCacheService(getGlobalZkCache(), this.config.getClusterName());
+        this.configurationCacheService = new ConfigurationCacheService(globalZkCache, this.config.getClusterName());
         this.localZkCacheService = new LocalZooKeeperCacheService(getLocalZkCache(), this.configurationCacheService);
     }
 
