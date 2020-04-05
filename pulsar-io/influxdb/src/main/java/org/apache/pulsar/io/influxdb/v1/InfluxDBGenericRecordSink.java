b8e37ee90815c0db45b1c60454d174560773c5ad
Lin Yiming <roymatrix.lin@gmail.com>
2020-04-06 03:32:42 +0800

    ISSUE-6514 Add support for influxdb2 in pulsar-influxdb-sink(#6514) (#6601)
    Fixes #6514
    ### Motivation
    The latest InfluxDB release(v2) has changed their write path, 'org'、'bucket'、'token' are required properties to communicate with it; New InfluxDB client library is provided with different package name, from org.influxdb to com.influxdb
    This PR would add support to the latest InfluxDB 2.0 write API.
    
    ### Modifications
    
    Since the influxdb2 client library changes a lot, even the package name changed, so the new client library is added in this pull request, while the old one is kept, which means the implement of client v1 is kept unchanged, so no migration is needed for influxdbv1 users。
    
    The sink for influxdbv1 fails to decode  messages encoded in json, it supposes only avro. Support of json schema is added in this pull request.
    
    The sink for influxdbv1 requires  influxdb fields as schema fields, so users should define schemas for each kind of device, that is not convenient.  The new added sink for influxdbv2 redefines the message schema, and wraps influxdb 'fields' in one map field, just like 'tags' which is already defined as map in the previous sink for influxdbv1.
    
    ### Verifying this change
      - Unit tests are added for this sink for influxdbv2.

---
+++ pulsar-io/influxdb/src/main/java/org/apache/pulsar/io/influxdb/v1/InfluxDBGenericRecordSink.java
@@ -0,0 +1,95 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.io.influxdb.v1;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.client.api.SchemaSerializationException;
+import org.apache.pulsar.client.api.schema.Field;
+import org.apache.pulsar.client.api.schema.GenericRecord;
+import org.apache.pulsar.functions.api.Record;
+import org.influxdb.dto.Point;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * A Simple InfluxDB sink, which interprets input Record in generic record.
+ * In order to successfully parse and write points to InfluxDB, points must be in InfluxDB’s Line Protocol format.
+ * This class expects records from Pulsar to have a field named 'measurement', a field named 'tags' if necessary.
+ */
+@Slf4j
+public class InfluxDBGenericRecordSink extends InfluxDBAbstractSink<GenericRecord> {
+
+    private final Set<String> FIELDS_TO_SKIP = ImmutableSet.of("measurement", "tags");
+
+    @Override
+    protected Point buildPoint(Record<GenericRecord> message) throws Exception {
+        Map<String, String> tags;
+        Map<String, Object> fields = Maps.newHashMap();
+
+        GenericRecord record = message.getValue();
+
+        Object measurementField = getFiled(record, "measurement");
+        if (null == measurementField) {
+            throw new SchemaSerializationException("measurement is a required field.");
+        }
+
+        String measurement = measurementField.toString();
+
+        // Looking for tags
+        Object tagsField = getFiled(record, "tags");
+        if (null == tagsField) {
+            tags = ImmutableMap.of();
+        } else if (Map.class.isAssignableFrom(tagsField.getClass())) {
+            tags = ((Map<Object, Object>) tagsField).entrySet()
+                    .stream().collect(Collectors.toMap(
+                            entry -> entry.getKey().toString(),
+                            entry -> entry.getValue().toString())
+                    );
+        } else {
+            // Field 'tags' that is not of Map type will be ignored
+            tags = ImmutableMap.of();
+        }
+
+        // Just insert the current time millis
+        long timestamp = System.currentTimeMillis();
+
+        for (Field field : record.getFields()) {
+            String fieldName = field.getName();
+            if (FIELDS_TO_SKIP.contains(fieldName)) {
+                continue;
+            }
+            Object fieldValue = record.getField(field);
+            if (null != fieldValue) {
+                fields.put(fieldName, fieldValue);
+            }
+        }
+
+        Point.Builder builder = Point.measurement(measurement)
+                .time(timestamp, TimeUnit.MILLISECONDS)
+                .tag(tags)
+                .fields(fields);
+        return builder.build();
+    }
+}
