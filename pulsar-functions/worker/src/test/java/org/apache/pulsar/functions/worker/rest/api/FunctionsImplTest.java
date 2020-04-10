3e34a403eb7e6068d6645e72f3c30713e16b6cdd
xuesong172500 <54351417+xuesong172500@users.noreply.github.com>
2020-04-10 14:37:22 +0800

    Update FunctionsImplTest.java

--- pulsar-functions/worker/src/test/java/org/apache/pulsar/functions/worker/rest/api/FunctionsImplTest.java
+++ pulsar-functions/worker/src/test/java/org/apache/pulsar/functions/worker/rest/api/FunctionsImplTest.java
@@ -85,6 +85,10 @@ public class FunctionsImplTest {
     }
 
     private static final class TestFunction implements org.apache.pulsar.functions.api.Function<String, String> {
+        @Override
+        public void prepare(Context context) throws Exception {
+
+        }
 
         @Override
         public String process(String input, Context context) {
