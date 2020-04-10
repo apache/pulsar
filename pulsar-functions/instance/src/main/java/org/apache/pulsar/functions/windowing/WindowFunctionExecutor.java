e58cfe2658b189f7b0ffd07cef5a86f78207f366
xuesong172500 <54351417+xuesong172500@users.noreply.github.com>
2020-04-10 14:16:24 +0800

    Update WindowFunctionExecutor.java

--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/windowing/WindowFunctionExecutor.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/windowing/WindowFunctionExecutor.java
@@ -269,6 +269,10 @@ public class WindowFunctionExecutor<I, O> implements Function<I, O> {
     }
 
     @Override
+    public void prepare(Context context) throws Exception {
+    }
+    
+    @Override
     public O process(I input, Context context) throws Exception {
         if (!this.initialized) {
             initialize(context);
