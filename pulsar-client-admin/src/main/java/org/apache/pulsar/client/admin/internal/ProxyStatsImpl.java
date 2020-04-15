60d95670a01deab97408d219e3dbb976d7b7e7af
rdhabalia <rdhabalia@apache.org>
2020-04-14 22:23:44 -0700

    fix imports

--- pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/ProxyStatsImpl.java
+++ pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/ProxyStatsImpl.java
@@ -18,15 +18,15 @@
  */
 package org.apache.pulsar.client.admin.internal;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import javax.ws.rs.client.WebTarget;
 
 import org.apache.pulsar.client.admin.ProxyStats;
 import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.client.api.Authentication;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 /**
  * Pulsar Admin API client.
@@ -41,7 +41,7 @@ public class ProxyStatsImpl extends BaseResource implements ProxyStats {
         super(auth, readTimeoutMs);
         adminProxyStats = target.path("/proxy-stats");
     }
-    
+
     @Override
     public JsonArray getConnections() throws PulsarAdminException {
         try {
