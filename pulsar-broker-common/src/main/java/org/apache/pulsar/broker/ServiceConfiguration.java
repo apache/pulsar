74c668a35130b85cf67256ab10466dbc06f99e50
pheecian <pheecian@gmail.com>
2020-04-22 17:06:22 +0800

    Avoid prefetch too much data when offloading data to HDFS (#6717)
    Fixes #6692
    
    ### Motivation
    avoid prefetch too much data when offloading, which may lead to OOM;
    fix object not close issue, which is also mentioned by congbobo184 https://github.com/apache/pulsar/pull/6697
    
    *Explain here the context, and why you're making that change. What is the problem you're trying to solve.*
    
    ### Does this pull request potentially affect one of the following parts:
    
    *If `yes` was chosen, please highlight the changes*
    
      - Dependencies (does it add or upgrade a dependency): (no)
      - The public API: (no)
      - The schema: (no)
      - The default values of configurations: (no)
      - The wire protocol: (no)
      - The rest endpoints: (no)
      - The admin cli options: (no)
      - Anything that affects deployment: (no)
    
    ### Documentation
    
      - Does this pull request introduce a new feature? (no)
    (cherry picked from commit 514b6af7586633424739cfc3c6131b0d0afec9e4)

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
@@ -1431,6 +1431,12 @@ public class ServiceConfiguration implements PulsarConfiguration {
     )
     private int managedLedgerOffloadMaxThreads = 2;
 
+    @FieldContext(
+            category = CATEGORY_STORAGE_OFFLOADING,
+            doc = "Maximum prefetch rounds for ledger reading for offloading"
+    )
+    private int managedLedgerOffloadPrefetchRounds = 1;
+
     /**** --- Transaction config variables --- ****/
     @FieldContext(
             category = CATEGORY_TRANSACTION,
