028945a98bb4e974a54b007930b5147e76c0de47
lipenghui <penghui@apache.org>
2020-04-22 19:48:56 +0800

    Add topic compaction test for empty message. (#6784)

--- pulsar-broker/src/test/java/org/apache/pulsar/compaction/CompactedTopicTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/compaction/CompactedTopicTest.java
@@ -24,13 +24,18 @@ import com.google.common.collect.Sets;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 import lombok.Cleanup;
@@ -42,18 +47,41 @@ import org.apache.bookkeeper.mledger.impl.PositionImpl;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 import org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest;
+import org.apache.pulsar.client.admin.LongRunningProcessStatus;
+import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.apache.pulsar.client.api.BatcherBuilder;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.ProducerBuilder;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.RawMessage;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
 import org.apache.pulsar.client.impl.RawMessageImpl;
 import org.apache.pulsar.common.api.proto.PulsarApi.MessageIdData;
 import org.apache.pulsar.common.policies.data.ClusterData;
 import org.apache.pulsar.common.policies.data.TenantInfo;
+import org.apache.pulsar.common.util.FutureUtil;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CompactedTopicTest extends MockedPulsarServiceBaseTest {
     private final Random r = new Random(0);
+
+    @DataProvider(name = "batchEnabledProvider")
+    public Object[][] batchEnabledProvider() {
+        return new Object[][] {
+                { Boolean.FALSE },
+                { Boolean.TRUE }
+        };
+    }
 
     @BeforeMethod
     @Override
@@ -232,5 +260,61 @@ public class CompactedTopicTest extends MockedPulsarServiceBaseTest {
         bk.openLedger(newCompactedLedger.getId(),
                       Compactor.COMPACTED_TOPIC_LEDGER_DIGEST_TYPE,
                       Compactor.COMPACTED_TOPIC_LEDGER_PASSWORD).close();
+    }
+
+    @Test(dataProvider = "batchEnabledProvider")
+    public void testCompactWithEmptyMessage(boolean batchEnabled) throws Exception {
+        final String key = "1";
+        byte[] msgBytes = "".getBytes();
+        final String topic = "persistent://my-property/use/my-ns/testCompactWithEmptyMessage-" + UUID.randomUUID();
+        admin.topics().createPartitionedTopic(topic, 1);
+        final int messages = 10;
+
+        ProducerBuilder<byte[]> builder = pulsarClient.newProducer().topic(topic);
+        if (!batchEnabled) {
+            builder.enableBatching(false);
+        } else {
+            builder.batchingMaxMessages(messages / 2);
+        }
+        Producer<byte[]> producer = builder.create();
+
+        List<CompletableFuture<MessageId>> list = new ArrayList<>(messages);
+        for (int i = 0; i < messages; i++) {
+            list.add(producer.newMessage().keyBytes(key.getBytes(Charset.defaultCharset())).value(msgBytes).sendAsync());
+        }
+
+        FutureUtil.waitForAll(list).get();
+        admin.topics().triggerCompaction(topic);
+
+        boolean succeed = retryStrategically((test) -> {
+            try {
+                return LongRunningProcessStatus.Status.SUCCESS.equals(admin.topics().compactionStatus(topic).status);
+            } catch (PulsarAdminException e) {
+                return false;
+            }
+        }, 10, 200);
+
+        Assert.assertTrue(succeed);
+
+        // Send more messages and trigger compaction again.
+
+        list.clear();
+        for (int i = 0; i < messages; i++) {
+            list.add(producer.newMessage().key(key).value(msgBytes).sendAsync());
+        }
+
+        FutureUtil.waitForAll(list).get();
+        admin.topics().triggerCompaction(topic);
+
+        succeed = retryStrategically((test) -> {
+            try {
+                return LongRunningProcessStatus.Status.SUCCESS.equals(admin.topics().compactionStatus(topic).status);
+            } catch (PulsarAdminException e) {
+                return false;
+            }
+        }, 10, 200);
+        Assert.assertTrue(succeed);
+
+        producer.close();
     }
 }
