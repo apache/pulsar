fc1906174ad9be18e3771662452f09c739825d7e
penghui <penghui@apache.org>
2020-05-26 21:59:08 +0800

    Disable the topic level policy service and system topic by default.

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
@@ -677,13 +677,13 @@ public class ServiceConfiguration implements PulsarConfiguration {
     @FieldContext(
             category = CATEGORY_SERVER,
             doc = "Enable or disable system topic.")
-    private boolean systemTopicEnabled = true;
+    private boolean systemTopicEnabled = false;
 
     @FieldContext(
         category = CATEGORY_SERVER,
         doc = "Enable or disable topic level policies, topic level policies depends on the system topic, " +
                 "please enable the system topic first.")
-    private boolean topicLevelPoliciesEnabled = true;
+    private boolean topicLevelPoliciesEnabled = false;
 
     /***** --- TLS --- ****/
     @FieldContext(
