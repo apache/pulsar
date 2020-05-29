c281d95c1a9ad413d1eeb947a9da31b578be30d3
Jia Zhai <zhaijia@apache.org>
2020-05-30 05:39:40 +0800

    update asynchttpclient verison (#7087)
    * update asynchttpclient verison
    
    * add default config for new version of asynchttp
    
    * update license version
    
    * fix ut
    
    * add default config for asynchttp to avoid npe

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/HttpClient.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/HttpClient.java
@@ -21,6 +21,7 @@ package org.apache.pulsar.client.impl;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URL;
 import java.util.Map;
@@ -76,9 +77,11 @@ public class HttpClient implements Closeable {
         confBuilder.setUserAgent(String.format("Pulsar-Java-v%s", PulsarVersion.getVersion()));
         confBuilder.setKeepAliveStrategy(new DefaultKeepAliveStrategy() {
             @Override
-            public boolean keepAlive(Request ahcRequest, HttpRequest request, HttpResponse response) {
+            public boolean keepAlive(InetSocketAddress remoteAddress, Request ahcRequest,
+                                     HttpRequest request, HttpResponse response) {
                 // Close connection upon a server error or per HTTP spec
-                return (response.status().code() / 100 != 5) && super.keepAlive(ahcRequest, request, response);
+                return (response.status().code() / 100 != 5)
+                       && super.keepAlive(remoteAddress, ahcRequest, request, response);
             }
         });
 
