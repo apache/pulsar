b6254d92d9f68544463049a8d86d99cff8264640
Matteo Merli <mmerli@apache.org>
2020-05-31 09:17:05 -0700

    Avoid race while mocking compactor (#7102)
    There seems to be a race when mocking the compactor in
    MockedPulsarServiceBaseTest which yields errors like:
    ```
    Caused by: java.lang.ClassCastException: org.apache.pulsar.compaction.TwoPhaseCompactor$MockitoMock$1141048386 cannot be cast to org.apache.pulsar.broker.service.BrokerService
    ```
    
    This obviously causes tests to fail in strange and surprising
    ways. I've not been able to reproduce, but the issue seems to be with
    how we mock the compactor.
    
    We don't want to have to construct the whole compactor as we just want
    to spy on it, so we get the current compactor, wrap it in spy and tell
    the PulsarService instance to return it when getCompactor is called.
    
    However, we are doing this after we have already called
    PulsarService#start(). So there are threads already using the mock
    structures. The mock structures themselves are not thread safe, so
    modifying them while they are in use, is not a good idea.
    
    The fix is to finish mocking before invoking #start(). Do do this,
    I've broken out the Compactor construction method in PulsarService, so
    that alone can be mocked to wrap the object in a spy.
    
    Co-authored-by: Ivan Kelly <ikelly@splunk.com>

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/auth/MockedPulsarServiceBaseTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/auth/MockedPulsarServiceBaseTest.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.broker.auth;
 
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
@@ -246,9 +247,6 @@ public abstract class MockedPulsarServiceBaseTest {
         pulsar.start();
         conf.setAuthorizationEnabled(isAuthorizationEnabled);
 
-        Compactor spiedCompactor = spy(pulsar.getCompactor());
-        doReturn(spiedCompactor).when(pulsar).getCompactor();
-
         return pulsar;
     }
 
@@ -261,6 +259,10 @@ public abstract class MockedPulsarServiceBaseTest {
         doReturn(namespaceServiceSupplier).when(pulsar).getNamespaceServiceProvider();
 
         doReturn(sameThreadOrderedSafeExecutor).when(pulsar).getOrderedExecutor();
+
+        doAnswer((invocation) -> {
+                return spy(invocation.callRealMethod());
+            }).when(pulsar).newCompactor();
     }
 
     public TenantInfo createDefaultTenantInfo() throws PulsarAdminException {
