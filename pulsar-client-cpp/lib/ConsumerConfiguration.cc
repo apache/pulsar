a69863725eff8a8b011973c99cb42553682efca3
Jia Zhai <zhaijia@apache.org>
2020-06-05 10:45:51 +0800

    [cpp] make able to set acktimeout to 0 in consumerconfig (#7169)
    If user already set UnAckedMessagesTimeoutMs in consumer config, It is not able to set it back to 0. This change fix the issue.

--- pulsar-client-cpp/lib/ConsumerConfiguration.cc
+++ pulsar-client-cpp/lib/ConsumerConfiguration.cc
@@ -92,7 +92,7 @@ void ConsumerConfiguration::setConsumerName(const std::string& consumerName) {
 long ConsumerConfiguration::getUnAckedMessagesTimeoutMs() const { return impl_->unAckedMessagesTimeoutMs; }
 
 void ConsumerConfiguration::setUnAckedMessagesTimeoutMs(const uint64_t milliSeconds) {
-    if (milliSeconds < 10000) {
+    if (milliSeconds < 10000 && milliSeconds != 0) {
         throw "Consumer Config Exception: Unacknowledged message timeout should be greater than 10 seconds.";
     }
     impl_->unAckedMessagesTimeoutMs = milliSeconds;
