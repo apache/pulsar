fd8766e46f22329c831b9141a05d1a90b8e3146e
Sanjeev Kulkarni <sanjeevk@splunk.com>
2020-06-12 12:35:03 -0700

    Fix test

--- pulsar-broker/src/test/java/org/apache/pulsar/functions/worker/PulsarFunctionE2ESecurityTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/functions/worker/PulsarFunctionE2ESecurityTest.java
@@ -201,7 +201,9 @@ public class PulsarFunctionE2ESecurityTest {
         superUserAdmin.tenants().createTenant(TENANT2, propAdmin);
         superUserAdmin.namespaces().createNamespace( TENANT2 + "/" + NAMESPACE);
 
-        Thread.sleep(100);
+        while(!functionWorkerService.get().getMembershipManager().isLeader()) {
+            Thread.sleep(1000);
+        }
     }
 
     @AfterMethod
