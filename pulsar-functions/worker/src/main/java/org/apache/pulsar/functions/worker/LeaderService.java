8fc89c35e9752682a70540510447764639ed5fbe
Sanjeev Kulkarni <sanjeevk@splunk.com>
2020-06-20 17:00:17 -0700

    make the function sync

--- pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/LeaderService.java
+++ pulsar-functions/worker/src/main/java/org/apache/pulsar/functions/worker/LeaderService.java
@@ -127,7 +127,7 @@ public class LeaderService implements AutoCloseable, ConsumerEventListener {
         }
     }
 
-    public boolean isLeader() {
+    public synchronized boolean isLeader() {
         return isLeader.get();
     }
 
