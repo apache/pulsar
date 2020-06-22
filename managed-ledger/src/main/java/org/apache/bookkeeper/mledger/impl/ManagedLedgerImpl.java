1535de9e18ab118695c270ab594117d8c0752c80
Sijie Guo <sijie@apache.org>
2020-06-22 13:32:28 -0700

    Fix producer stucks on creating ledger timeout (#7319)
    * Fix producer stucks on creating ledger timeout
    
    *Motivation*
    
    The `ledgerCreated` flag is passed as ctx to the createLedger callback.
    The callback already had the logic on handling `ledgerCreated` flag. But we set the flag to `false`
    when timeout happens. It will trigger the following race condition:
    
    a) The createComplete callback is triggered when timeout. But the pending add requests are not error'd out.
    b) If the createComplete callback eventually completes, it will see `ledgerCreated` flag is set to true,
    so it will cause `checkAndCompleteLedgerOpTask` returns false and exist too early without processing the
    pending add requests.
    
    This race condition only happens when creating ledger times out.
    
    ```
        public synchronized void createComplete(int rc, final LedgerHandle lh, Object ctx) {
            if (log.isDebugEnabled()) {
                log.debug("[{}] createComplete rc={} ledger={}", name, rc, lh != null ? lh.getId() : -1);
            }
    
            if (checkAndCompleteLedgerOpTask(rc, lh, ctx)) {
                return;
            }
    ```
    
    *Modification*
    
    The timeout logic shouldn't modify the `ledgerCreated` context. It should let the callback itself to process
    the `ledgerCreated` context.
    
    * Change to use CAS
    
    (cherry picked from commit a34f6939f0aa639c37192ea0c9bc7b927a245664)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -3177,8 +3177,7 @@ public class ManagedLedgerImpl implements ManagedLedger, CreateCallback {
                 digestType, config.getPassword(), cb, ledgerCreated, finalMetadata);
         scheduledExecutor.schedule(() -> {
             if (!ledgerCreated.get()) {
-                ledgerCreated.set(true);
-                cb.createComplete(BKException.Code.TimeoutException, null, null);
+                cb.createComplete(BKException.Code.TimeoutException, null, ledgerCreated);
             }
         }, config.getMetadataOperationsTimeoutSeconds(), TimeUnit.SECONDS);
     }
@@ -3194,14 +3193,15 @@ public class ManagedLedgerImpl implements ManagedLedger, CreateCallback {
     protected boolean checkAndCompleteLedgerOpTask(int rc, LedgerHandle lh, Object ctx) {
         if (ctx instanceof AtomicBoolean) {
             // ledger-creation is already timed out and callback is already completed so, delete this ledger and return.
-            if (((AtomicBoolean) (ctx)).get()) {
+            if (((AtomicBoolean) (ctx)).compareAndSet(false, true)) {
+                return false;
+            } else {
                 if (rc == BKException.Code.OK) {
                     log.warn("[{}]-{} ledger creation timed-out, deleting ledger", this.name, lh.getId());
                     asyncDeleteLedger(lh.getId(), DEFAULT_LEDGER_DELETE_RETRIES);
                 }
                 return true;
             }
-            ((AtomicBoolean) ctx).set(true);
         }
         return false;
     }
