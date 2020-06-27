a4f98a2ca91227b482804ea2240a6927e178b631
Matteo Merli <mmerli@apache.org>
2020-06-27 10:34:55 -0700

    Fixed test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiSchemaTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiSchemaTest.java
@@ -21,6 +21,7 @@ package org.apache.pulsar.broker.admin;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static org.junit.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 import com.google.common.collect.Sets;
 import java.util.HashMap;
@@ -162,8 +163,9 @@ public class AdminApiSchemaTest extends MockedPulsarServiceBaseTest {
 
         try {
             admin.schemas().createSchema(topicName, foo1SchemaInfo);
+            fail("Should have failed");
         } catch (PulsarAdminException.ConflictException e) {
-            assertTrue(e.getMessage().contains("HTTP 409 Conflict"));
+            // Ok
         }
 
         namespace = "schematest/testnotfound";
@@ -171,8 +173,9 @@ public class AdminApiSchemaTest extends MockedPulsarServiceBaseTest {
 
         try {
             admin.schemas().createSchema(topicName, fooSchemaInfo);
+            fail("Should have failed");
         } catch (PulsarAdminException.NotFoundException e) {
-            assertTrue(e.getMessage().contains("HTTP 404 Not Found"));
+            // Ok
         }
     }
 
