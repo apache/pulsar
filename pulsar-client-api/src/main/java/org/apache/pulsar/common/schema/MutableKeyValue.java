11f3baa7f5dd4b9ef6eceb9fa3fcad5eb4cb239f
feynmanlin <feynmanlin@tencent.com>
2020-07-05 23:25:45 +0800

    add Javadoc comment

--- pulsar-client-api/src/main/java/org/apache/pulsar/common/schema/MutableKeyValue.java
+++ pulsar-client-api/src/main/java/org/apache/pulsar/common/schema/MutableKeyValue.java
@@ -24,6 +24,9 @@ import java.util.Objects;
 
 import org.apache.pulsar.client.api.Schema;
 
+/**
+ * A simple KeyValue class.key and value can be modified at any time
+ */
 public class MutableKeyValue<K, V> {
     private K key;
     private V value;
