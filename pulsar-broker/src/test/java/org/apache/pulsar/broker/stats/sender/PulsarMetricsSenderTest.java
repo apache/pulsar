bca84606bd8f09111d01b249fe242dd4f2ad5b91
Alexandre DUVAL - @kannarfr <kannarfr@gmail.com>
2020-07-06 16:27:46 +0200

    test POC: fix test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/sender/PulsarMetricsSenderTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/sender/PulsarMetricsSenderTest.java
@@ -1,55 +1,77 @@
 package org.apache.pulsar.broker.stats.sender;
 
-import org.apache.pulsar.broker.service.BrokerTestBase;
+import com.google.common.collect.Sets;
+import org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest;
 import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.Producer;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.common.naming.NamespaceName;
+import org.apache.pulsar.common.naming.TopicName;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class PulsarMetricsSenderTest extends BrokerTestBase {
+import java.util.concurrent.TimeUnit;
 
-    @BeforeMethod
-    @Override
-    protected void setup() throws Exception {
-        super.baseSetup();
+public class PulsarMetricsSenderTest extends MockedPulsarServiceBaseTest {
+
+    public PulsarMetricsSenderTest() {
+        super();
     }
 
-    @AfterMethod
+    @BeforeClass
     @Override
-    protected void cleanup() throws Exception {
-        super.internalCleanup();
+    public void setup() throws Exception {
+        conf.setClusterName("c1");
+        conf.setAuthorizationEnabled(false);
+        conf.setAuthorizationAllowWildcardsMatching(true);
+        conf.setSuperUserRoles(Sets.newHashSet("pulsar.super_user"));
+        conf.setMetricsSenderEnabled(true);
+        conf.setMetricsSenderDestinationTenant("metrics");
+        conf.setMetricsSenderIntervalInSeconds(3);
+        internalSetup();
+    }
+
+    @AfterClass
+    @Override
+    public void cleanup() throws Exception {
+        internalCleanup();
     }
 
     @Test
     public void testPerTopicStats() throws Exception {
         Producer<byte[]> p1 = pulsarClient.newProducer().topic("persistent://my-property/use/my-ns/my-topic1").create();
-        Producer<byte[]> p2 = pulsarClient.newProducer().topic("persistent://my-property/use/my-ns/my-topic2").create();
 
         Consumer<byte[]> c1 = pulsarClient.newConsumer()
                 .topic("persistent://my-property/use/my-ns/my-topic1")
                 .subscriptionName("test")
                 .subscribe();
 
-        Consumer<byte[]> c2 = pulsarClient.newConsumer()
-                .topic("persistent://my-property/use/my-ns/my-topic2")
-                .subscriptionName("test")
-                .subscribe();
-
-        final int messages = 10;
+        final int messages = 1;
 
         for (int i = 0; i < messages; i++) {
             String message = "my-message-" + i;
             p1.send(message.getBytes());
-            p2.send(message.getBytes());
         }
 
         for (int i = 0; i < messages; i++) {
             c1.acknowledge(c1.receive());
-            c2.acknowledge(c2.receive());
         }
 
-        //Consumer<String> cMetrics = pulsarClient.newConsumer().topic(this.conf.)
+        TopicName metricsTopic = TopicName.get(
+                "persistent",
+                NamespaceName.get(conf.getMetricsSenderDestinationTenant(), "brokers"),
+                this.pulsar.getAdvertisedAddress());
+
+        Consumer<String> cMetrics = pulsarClient.newConsumer(Schema.STRING)
+                .topic(metricsTopic.toString())
+                .subscriptionName("consumer-test")
+                .subscribe();
+
+        Message<String> s = cMetrics.receive(10, TimeUnit.SECONDS);
+
+        System.out.println(s.getValue());
     }
 
 }
