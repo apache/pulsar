ffab725547159f1cbaa8a0a2aac9c13ca44210b0
Sijie Guo <sijie@apache.org>
2020-07-15 01:43:31 -0700

    Add unit test for packed repeated long

--- pulsar-common/src/test/java/org/apache/pulsar/common/protocol/RepeatedLongNonPackedTest.java
+++ pulsar-common/src/test/java/org/apache/pulsar/common/protocol/RepeatedLongNonPackedTest.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pulsar.common.protocol;
 
 import static org.testng.Assert.assertEquals;
@@ -9,10 +27,10 @@ import org.apache.pulsar.common.util.protobuf.ByteBufCodedInputStream;
 import org.apache.pulsar.common.util.protobuf.ByteBufCodedOutputStream;
 import org.testng.annotations.Test;
 
-public class RepeatedLongTest {
+public class RepeatedLongNonPackedTest {
 
     @Test
-    public void testMessageIDAckSet() throws Exception {
+    public void testRepeatedLongPacked() throws Exception {
         MessageIdData messageIdData = MessageIdData.newBuilder()
             .setLedgerId(0L)
             .setEntryId(0L)
