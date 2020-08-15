c670835c0e7116f2152cae2a0c91e81b5de4dcb8
feynmanlin <feynmanlin@tencent.com>
2020-08-15 23:18:43 +0800

    fix unit test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/MaxUnackedMessagesTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/MaxUnackedMessagesTest.java
@@ -190,12 +190,13 @@ public class MaxUnackedMessagesTest extends ProducerConsumerBase {
 
     private void waitCacheInit(String topicName) throws Exception {
         for (int i = 0; i < 50; i++) {
+            //wait for server init
+            Thread.sleep(1000);
             try {
                 admin.topics().getMaxUnackedMessagesOnSubscription(topicName);
                 break;
             } catch (Exception e) {
                 //ignore
-                Thread.sleep(100);
             }
             if (i == 49) {
                 throw new RuntimeException("Waiting for cache initialization has timed out");
