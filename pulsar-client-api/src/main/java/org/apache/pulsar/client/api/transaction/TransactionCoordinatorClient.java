abb3fccc992f9c891c80f60c2d03bd69ef4da023
gaoran10 <gaoran_10@126.com>
2020-08-31 21:49:04 +0800

    fix

--- pulsar-client-api/src/main/java/org/apache/pulsar/client/api/transaction/TransactionCoordinatorClient.java
+++ pulsar-client-api/src/main/java/org/apache/pulsar/client/api/transaction/TransactionCoordinatorClient.java
@@ -125,8 +125,25 @@ public interface TransactionCoordinatorClient extends Closeable {
      */
     CompletableFuture<Void> addPublishPartitionToTxnAsync(TxnID txnID, List<String> partitions);
 
-    void addSubscriptionToTxn(TxnID txnID, String topic, String subscription) throws TransactionCoordinatorClientException;
+    /**
+     * Add ack subscription to txn.
+     *
+     * @param txnID transaction id
+     * @param topic topic name
+     * @param subscription subscription name
+     * @throws TransactionCoordinatorClientException
+     */
+    void addSubscriptionToTxn(TxnID txnID, String topic, String subscription)
+            throws TransactionCoordinatorClientException;
 
+    /**
+     * Add ack subscription to txn asynchronously.
+     *
+     * @param txnID transaction id
+     * @param topic topic name
+     * @param subscription subscription name
+     * @return
+     */
     CompletableFuture<Void> addSubscriptionToTxnAsync(TxnID txnID, String topic, String subscription);
 
     /**
