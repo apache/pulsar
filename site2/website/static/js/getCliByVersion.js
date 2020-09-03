6f1f1e1591e08d919949eb492e723521404d1c23
Guangning <guangning@apache.org>
2020-09-03 21:13:31 +0800

    [Admin cli]Fixed website admin cli link error (#7969)
    Motivation
    Website admin cli link error
    Modifications
    Update link version
    Verifying this change
    Local test

--- site2/website/static/js/getCliByVersion.js
+++ site2/website/static/js/getCliByVersion.js
@@ -27,6 +27,7 @@ function getCliByVersion(){
             return
         }
     } else {
+        version = parseInt(versions[0]) + "." + parseInt(versions[1]) + ".0"
         window.location.href = "http://pulsar.apache.org/tools/pulsar-admin/" + version + "-SNAPSHOT"
         return
     }
