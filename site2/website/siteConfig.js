be09150d8f298e8071174c296565bd2bc4a86727
Jinfeng Huang <hjf@streamnative.io>
2020-09-09 10:17:38 +0800

    update

--- site2/website/siteConfig.js
+++ site2/website/siteConfig.js
@@ -180,7 +180,7 @@ const siteConfig = {
 
   projectDescription: `
     Apache Pulsar is a cloud-native, distributed messaging and streaming platform originally
-    created at Yahoo! and now a top-level Apache Software Foundation
+    created at Yahoo! and now a top-level Apache Software Foundation project
   `,
 
   markdownPlugins: [
