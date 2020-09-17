1eb03a9c15125730f8d5315dae76504ef4d7e9be
丛搏 <237716289@qq.com>
2020-09-17 14:59:14 +0800

    Rebase

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionConsumeTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionConsumeTest.java
@@ -141,7 +141,7 @@ public class TransactionConsumeTest extends TransactionTestBase {
 
         Map<String, Integer> exclusiveBatchIndexMap = new HashMap<>();
         Map<String, Integer> sharedBatchIndexMap = new HashMap<>();
-        // receive transaction messages successfully after commitd
+        // receive transaction messages successfully after commit
         for (int i = 0; i < transactionMessageCnt; i++) {
             message = exclusiveConsumer.receive(5, TimeUnit.SECONDS);
             Assert.assertNotNull(message);
