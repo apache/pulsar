88201a39e09dadd22542dd2e527d13307d719540
Yunze Xu <xyzinfernity@163.com>
2020-10-23 22:02:33 +0800

    [C++] Fix message id error when subscribing a single partition (#8341)
    ### Motivation
    
    A consumer/reader can subscribe a single partition of a partitioned topic. However, the partition index was not set correctly. So the partition field of received messages' id is always -1.
    
    In addition, current `TopicName#getPartitionIndex` Java implementation doesn't handle some corner cases, like "xxx-partition-00" or "xxx-partition--1". For the backward compatibility, the C++ implementation will use the same implementation and note it in Java `TopicName`'s unit test.
    
    ### Modifications
    
    - Add `getPartitionIndex` method to `TopicName`;
    - Set the partition index when a `Consumer` was created from a `Client`;
    - Set the partition index when a `Reader` was created;
    - Add relative unit tests for above changes;
    - Note the "incorrect" behavior in Java `TopicName`'s unit test.
    
    ### Verifying this change
    
    This change added tests and can be verified as follows:
    
    - `TopicNameTest#testPartitionIndex`
    - `ConsumerTest#testPartitionIndex`
    - `ReaderTest#testPartitionIndex`
    
    * Add getPartitionIndex() method to TopicName
    
    * Set partition index when creating a ConsumerImpl from Client
    
    * Add test cases to Java TopicName to note the corner cases
    
    * Set partition index for Reader
    
    * Fix compile error

--- pulsar-common/src/test/java/org/apache/pulsar/common/naming/TopicNameTest.java
+++ pulsar-common/src/test/java/org/apache/pulsar/common/naming/TopicNameTest.java
@@ -199,6 +199,11 @@ public class TopicNameTest {
         assertEquals(topicName.getPartitionIndex(), -1);
 
         assertEquals(TopicName.getPartitionIndex("persistent://myprop/mycolo/myns/mytopic-partition-4"), 4);
+
+        // NOTE: Following behavior is not right actually, but for the backward compatibility, it shouldn't be changed
+        assertEquals(TopicName.getPartitionIndex("mytopic-partition--1"), 1);
+        assertEquals(TopicName.getPartitionIndex("mytopic-partition-00"), 0);
+        assertEquals(TopicName.getPartitionIndex("mytopic-partition-012"), 12);
     }
 
     @Test
