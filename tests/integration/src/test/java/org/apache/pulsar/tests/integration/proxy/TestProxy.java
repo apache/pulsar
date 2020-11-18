8a4864c421bace2d361aed34eb290cba7c85880b
penghui <penghui@apache.org>
2020-11-18 21:08:28 +0800

    Enable WebSocket on Pulsar Proxy.

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/proxy/TestProxy.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/proxy/TestProxy.java
@@ -20,7 +20,12 @@ package org.apache.pulsar.tests.integration.proxy;
 
 import static org.testng.Assert.assertEquals;
 
+import java.net.URI;
 import java.util.Collections;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import lombok.Cleanup;
 
@@ -34,6 +39,13 @@ import org.apache.pulsar.common.policies.data.TopicStats;
 import org.apache.pulsar.tests.integration.containers.ProxyContainer;
 import org.apache.pulsar.tests.integration.suites.PulsarTestSuite;
 import org.apache.pulsar.tests.integration.topologies.PulsarClusterSpec;
+import org.awaitility.Awaitility;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketListener;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import org.slf4j.Logger;
@@ -53,6 +65,7 @@ public class TestProxy extends PulsarTestSuite {
         proxyViaURL = new ProxyContainer(clusterName, "proxy-via-url")
             .withEnv("brokerServiceURL", "pulsar://pulsar-broker-0:6650")
             .withEnv("brokerWebServiceURL", "http://pulsar-broker-0:8080")
+            .withEnv("webSocketServiceEnabled", "true")
             .withEnv("clusterName", clusterName);
 
         specBuilder.externalService("proxy-via-url", proxyViaURL);
@@ -130,6 +143,73 @@ public class TestProxy extends PulsarTestSuite {
         for (int i = 0; i < 10; i++) {
             // Ensure we the command works even if re-directs happen with a request body
             admin.topics().createSubscription(topic, "test-" + i, MessageId.earliest);
+        }
+    }
+
+    @Test
+    public void testWebSocket() throws Exception {
+
+        final String tenant = "proxy-test-" + randomName(10);
+        final String namespace = tenant + "/ns1";
+
+        @Cleanup
+        PulsarAdmin admin = PulsarAdmin.builder()
+                .serviceHttpUrl(pulsarCluster.getPlainTextServiceUrl())
+                .build();
+
+        admin.tenants().createTenant(tenant,
+                new TenantInfo(Collections.emptySet(), Collections.singleton(pulsarCluster.getClusterName())));
+
+        admin.namespaces().createNamespace(namespace, Collections.singleton(pulsarCluster.getClusterName()));
+
+        HttpClient httpClient = new HttpClient();
+        WebSocketClient webSocketClient = new WebSocketClient(httpClient);
+        webSocketClient.start();
+        MyWebSocket myWebSocket = new MyWebSocket();
+        Future<Session> sessionFuture =  webSocketClient.connect(myWebSocket,
+                URI.create(pulsarCluster.getHttpServiceUrl().replaceFirst("http", "ws")
+                        + "/ws/v2/producer/persistent/" + namespace + "/my-topic"));
+        sessionFuture.get().getRemote().sendString("{\n" +
+                "  \"payload\": \"SGVsbG8gV29ybGQ=\",\n" +
+                "  \"properties\": {\"key1\": \"value1\", \"key2\": \"value2\"},\n" +
+                "  \"context\": \"1\"\n" +
+                "}");
+
+        Awaitility.await().atMost(3, TimeUnit.SECONDS).untilAsserted(() -> {
+            String response = myWebSocket.getResponse();
+            log.info(response);
+            Assert.assertTrue(response.contains("ok"));
+        });
+    }
+
+    @WebSocket
+    public static class MyWebSocket implements WebSocketListener {
+        Queue<String> incomingMessages = new ArrayBlockingQueue<>(10);
+        @Override
+        public void onWebSocketBinary(byte[] bytes, int i, int i1) {
+        }
+
+        @Override
+        public void onWebSocketText(String s) {
+            incomingMessages.add(s);
+        }
+
+        @Override
+        public void onWebSocketClose(int i, String s) {
+        }
+
+        @Override
+        public void onWebSocketConnect(Session session) {
+
+        }
+
+        @Override
+        public void onWebSocketError(Throwable throwable) {
+
+        }
+
+        public String getResponse() {
+            return incomingMessages.poll();
         }
     }
 }
