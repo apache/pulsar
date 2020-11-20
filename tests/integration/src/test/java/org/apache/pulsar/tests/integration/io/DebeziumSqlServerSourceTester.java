131c94fe4279cadec199d4c5182c1ef7b172e3c6
黄大相 <localhost80@foxmail.com>
2020-11-20 13:41:09 +0800

    fix integration test

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/io/DebeziumSqlServerSourceTester.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/io/DebeziumSqlServerSourceTester.java
@@ -76,11 +76,12 @@ public class DebeziumSqlServerSourceTester extends SourceTester<DebeziumSqlServe
 
     @Override
     public void prepareSource() throws Exception {
-        URL inventorySql = DebeziumSqlServerSourceTester.class.getResource("debezium-sqlserver-init-inventory.sql");
+        URL inventorySql = DebeziumSqlServerSourceTester.class.getClassLoader().getResource("debezium-sqlserver-init-inventory.sql");
         this.debeziumSqlServerContainer.execCmd("docker cp " + inventorySql + "  " + DebeziumSqlServerContainer.NAME + ":debezium-sqlserver-init/inventory.sql ");
         this.debeziumSqlServerContainer.execCmd("cat debezium-sqlserver-init/inventory.sql | docker exec -i " + DebeziumSqlServerContainer.NAME + " bash -c '/opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD'");
         log.info("debezium sqlserver server already contains preconfigured data.");
     }
+
 
     @Override
     public void prepareInsertEvent() throws Exception {
