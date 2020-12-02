3ec11746cce7531f3c680b640d58dfb820efbfd9
Andy Walker <walkeraj@gmail.com>
2020-12-02 00:28:38 -0500

    prefer table-driven tests

--- pulsar-function-go/pf/topicName_test.go
+++ pulsar-function-go/pf/topicName_test.go
@@ -26,67 +26,45 @@ import (
 )
 
 func TestParseTopicName(t *testing.T) {
-	topic, err := ParseTopicName("persistent://my-tenant/my-ns/my-topic")
-
-	assert.Nil(t, err)
-	assert.Equal(t, "persistent://my-tenant/my-ns/my-topic", topic.Name)
-	assert.Equal(t, "my-tenant/my-ns", topic.Namespace)
-	assert.Equal(t, -1, topic.Partition)
-
-	topic, err = ParseTopicName("my-topic")
-	assert.Nil(t, err)
-	assert.Equal(t, "persistent://public/default/my-topic", topic.Name)
-	assert.Equal(t, "public/default", topic.Namespace)
-	assert.Equal(t, -1, topic.Partition)
-
-	topic, err = ParseTopicName("my-tenant/my-namespace/my-topic")
-	assert.Nil(t, err)
-	assert.Equal(t, "persistent://my-tenant/my-namespace/my-topic", topic.Name)
-	assert.Equal(t, "my-tenant/my-namespace", topic.Namespace)
-	assert.Equal(t, -1, topic.Partition)
-
-	topic, err = ParseTopicName("non-persistent://my-tenant/my-namespace/my-topic")
-	assert.Nil(t, err)
-	assert.Equal(t, "non-persistent://my-tenant/my-namespace/my-topic", topic.Name)
-	assert.Equal(t, "my-tenant/my-namespace", topic.Namespace)
-	assert.Equal(t, -1, topic.Partition)
-
-	topic, err = ParseTopicName("my-topic-partition-5")
-	assert.Nil(t, err)
-	assert.Equal(t, "persistent://public/default/my-topic-partition-5", topic.Name)
-	assert.Equal(t, "public/default", topic.Namespace)
-	assert.Equal(t, 5, topic.Partition)
-
-	// V1 topic name
-	topic, err = ParseTopicName("persistent://my-tenant/my-cluster/my-ns/my-topic")
-	assert.Nil(t, err)
-	assert.Equal(t, "persistent://my-tenant/my-cluster/my-ns/my-topic", topic.Name)
-	assert.Equal(t, "my-tenant/my-cluster/my-ns", topic.Namespace)
-	assert.Equal(t, -1, topic.Partition)
-
-	topic, err = ParseTopicName("my-tenant/my-cluster/my-ns/my-topic")
-	assert.Nil(t, err)
-	assert.Equal(t, "persistent://my-tenant/my-cluster/my-ns/my-topic", topic.Name)
-	assert.Equal(t, "my-tenant/my-cluster/my-ns", topic.Namespace)
-	assert.Equal(t, -1, topic.Partition)
+	testCases := []struct {
+		in        string
+		name      string
+		namespace string
+		partition int
+	}{
+		{in: "persistent://my-tenant/my-ns/my-topic", name: "persistent://my-tenant/my-ns/my-topic", namespace: "my-tenant/my-ns", partition: -1},
+		{in: "my-topic", name: "persistent://public/default/my-topic", namespace: "public/default", partition: -1},
+		{in: "my-tenant/my-namespace/my-topic", name: "persistent://my-tenant/my-namespace/my-topic", namespace: "my-tenant/my-namespace", partition: -1},
+		{in: "non-persistent://my-tenant/my-namespace/my-topic", name: "non-persistent://my-tenant/my-namespace/my-topic", namespace: "my-tenant/my-namespace", partition: -1},
+		{in: "my-topic-partition-5", name: "persistent://public/default/my-topic-partition-5", namespace: "public/default", partition: 5},
+		// V1 topic name
+		{in: "persistent://my-tenant/my-cluster/my-ns/my-topic", name: "persistent://my-tenant/my-cluster/my-ns/my-topic", namespace: "my-tenant/my-cluster/my-ns", partition: -1},
+		{in: "my-tenant/my-cluster/my-ns/my-topic", name: "persistent://my-tenant/my-cluster/my-ns/my-topic", namespace: "my-tenant/my-cluster/my-ns", partition: -1},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.in, func(t *testing.T) {
+			topic, err := ParseTopicName(testCase.in)
+			assert.Nil(t, err)
+			assert.Equal(t, testCase.name, topic.Name)
+			assert.Equal(t, testCase.namespace, topic.Namespace)
+			assert.Equal(t, testCase.partition, topic.Partition)
+		})
+	}
 }
 
 func TestParseTopicNameErrors(t *testing.T) {
-	_, err := ParseTopicName("invalid://my-tenant/my-ns/my-topic")
-	assert.NotNil(t, err)
-
-	_, err = ParseTopicName("invalid://my-tenant/my-ns/my-topic-partition-xyz")
-	assert.NotNil(t, err)
-
-	_, err = ParseTopicName("my-tenant/my-ns/my-topic-partition-xyz/invalid")
-	assert.NotNil(t, err)
-
-	_, err = ParseTopicName("persistent://my-tenant")
-	assert.NotNil(t, err)
-
-	_, err = ParseTopicName("persistent://my-tenant/my-namespace")
-	assert.NotNil(t, err)
-
-	_, err = ParseTopicName("persistent://my-tenant/my-cluster/my-ns/my-topic-partition-xyz/invalid")
-	assert.NotNil(t, err)
+	testCases := []string{
+		"invalid://my-tenant/my-ns/my-topic",
+		"invalid://my-tenant/my-ns/my-topic-partition-xyz",
+		"my-tenant/my-ns/my-topic-partition-xyz/invalid",
+		"persistent://my-tenant",
+		"persistent://my-tenant/my-namespace",
+		"persistent://my-tenant/my-cluster/my-ns/my-topic-partition-xyz/invalid",
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase, func(t *testing.T) {
+			_, err := ParseTopicName(testCase)
+			assert.NotNil(t, err)
+		})
+	}
 }
