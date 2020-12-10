9f44920e41f726d62c1d5262d3457491f0c3c069
Aaron Robert <Robert.Aaron2017@outlook.com>
2020-12-10 21:01:14 +0800

    Fix bugs.

--- pulsar-client-auth-athenz/src/main/java/org/apache/pulsar/client/impl/auth/AuthenticationAthenz.java
+++ pulsar-client-auth-athenz/src/main/java/org/apache/pulsar/client/impl/auth/AuthenticationAthenz.java
@@ -26,6 +26,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.security.PrivateKey;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -57,6 +58,7 @@ public class AuthenticationAthenz implements Authentication, EncodedAuthenticati
     private String tenantDomain;
     private String tenantService;
     private String providerDomain;
+    private final Object providerDomainLock = new Object();
     private PrivateKey privateKey;
     private String keyId = "0";
     private String roleHeader = null;
@@ -87,7 +89,10 @@ public class AuthenticationAthenz implements Authentication, EncodedAuthenticati
             // the following would set up the API call that requests tokens from the server
             // that can only be used if they are 10 minutes from expiration and last twenty
             // four hours
-            RoleToken token = getZtsClient().getRoleToken(providerDomain, null, minValidity, maxValidity, false);
+            RoleToken token;
+            synchronized (providerDomainLock) {
+                token = getZtsClient().getRoleToken(providerDomain, null, minValidity, maxValidity, false);
+            }
             roleToken = token.getToken();
             cachedRoleTokenTimestamp = System.nanoTime();
             return new AuthenticationDataAthenz(roleToken, isNotBlank(roleHeader) ? roleHeader : ZTSClient.getHeader());
@@ -128,7 +133,9 @@ public class AuthenticationAthenz implements Authentication, EncodedAuthenticati
     private void setAuthParams(Map<String, String> authParams) {
         this.tenantDomain = authParams.get("tenantDomain");
         this.tenantService = authParams.get("tenantService");
-        this.providerDomain = authParams.get("providerDomain");
+        synchronized (providerDomainLock) {
+            this.providerDomain = authParams.get("providerDomain");
+        }
         // privateKeyPath is deprecated, this is for compatibility
         if (isBlank(authParams.get("privateKey")) && isNotBlank(authParams.get("privateKeyPath"))) {
             this.privateKey = loadPrivateKey(authParams.get("privateKeyPath"));
@@ -188,7 +195,7 @@ public class AuthenticationAthenz implements Authentication, EncodedAuthenticati
                 throw new IllegalArgumentException(
                         "Unsupported media type or encoding format: " + urlConnection.getContentType());
             }
-            String keyData = CharStreams.toString(new InputStreamReader((InputStream) urlConnection.getContent()));
+            String keyData = CharStreams.toString(new InputStreamReader((InputStream) urlConnection.getContent(), Charset.defaultCharset()));
             privateKey = Crypto.loadPrivateKey(keyData);
         } catch (URISyntaxException e) {
             throw new IllegalArgumentException("Invalid privateKey format", e);
