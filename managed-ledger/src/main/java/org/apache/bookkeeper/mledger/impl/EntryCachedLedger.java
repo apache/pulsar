1bd8ecec2e4c21d55d03184f2ec4a48c2afcd973
Renkai <gaelookair@gmail.com>
2020-12-10 17:31:42 +0800

    fix interrupt issues
    Signed-off-by: Renkai <gaelookair@gmail.com>

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/EntryCachedLedger.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/EntryCachedLedger.java
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.bookkeeper.mledger.impl;
 
 import java.util.concurrent.CompletableFuture;
