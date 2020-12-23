1f3e42ac37d91354422635a58259983c8b529156
rajan <rdhabalia@apache.org>
2020-12-22 21:54:34 -0800

    [pulsar-io] upgrade debezium version
    fix compilation

--- pulsar-io/debezium/core/src/main/java/org/apache/pulsar/io/debezium/PulsarDatabaseHistory.java
+++ pulsar-io/debezium/core/src/main/java/org/apache/pulsar/io/debezium/PulsarDatabaseHistory.java
@@ -257,4 +257,9 @@ public final class PulsarDatabaseHistory extends AbstractDatabaseHistory {
         }
         return "Pulsar topic";
     }
+
+    @Override
+    public boolean storageExists() {
+        return true;
+    }
 }
