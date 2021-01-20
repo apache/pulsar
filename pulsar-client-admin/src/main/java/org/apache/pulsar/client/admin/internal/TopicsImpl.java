54b083b43506e396f426c5361d1f8d19be95a5db
feynmanlin <feynmanlin@tencent.com>
2021-01-20 17:39:08 +0800

    Support get topic applied policy for message TTL (#9225)
    Master Issue: #9216
    
    ### Motivation
    Provide a way to get the applied policy of the topic. In Pulsar, the topic will apply the topic level policy if present or apply the namespace policy if present or use the broker level configuration. It needs to support get the applied policy for a topic that might use the namespace policy or topic policy or broker level default configuration.
    
    ### Modifications
    1 Now if there is no data at the namespace-level, the broker-level data will be returned by default, so I fix it
    2 Now if there is no data at the topic-level, the data at the namespace-level will be returned by default, so I fix it
    3 Add applied interface
    4 Since the initial value becomes null, the corresponding unit test is modified

--- pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/TopicsImpl.java
+++ pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/TopicsImpl.java
@@ -2001,10 +2001,16 @@ public class TopicsImpl extends BaseResource implements Topics {
     }
 
     @Override
-    public int getMessageTTL(String topic) throws PulsarAdminException {
+    public Integer getMessageTTL(String topic) throws PulsarAdminException {
+        return getMessageTTL(topic, false);
+    }
+
+    @Override
+    public Integer getMessageTTL(String topic, boolean applied) throws PulsarAdminException {
         try {
             TopicName topicName = validateTopic(topic);
             WebTarget path = topicPath(topicName, "messageTTL");
+            path = path.queryParam("applied", applied);
             return request(path).get(new GenericType<Integer>() {});
         } catch (Exception e) {
             throw getApiException(e);
