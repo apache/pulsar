1b3af699c94ff2b884c7dfcece7b428d280f239b
Matteo Merli <mmerli@apache.org>
2021-02-05 17:04:23 -0800

    [C++] Removed usages of boost::regex

--- pulsar-client-cpp/lib/auth/athenz/ZTSClient.cc
+++ pulsar-client-cpp/lib/auth/athenz/ZTSClient.cc
@@ -38,12 +38,17 @@
 #include <boost/property_tree/ptree.hpp>
 namespace ptree = boost::property_tree;
 
-#include <boost/regex.hpp>
 #include <boost/xpressive/xpressive.hpp>
 #include <boost/archive/iterators/base64_from_binary.hpp>
 #include <boost/archive/iterators/transform_width.hpp>
 
 #include <mutex>
+
+#ifdef PULSAR_USE_BOOST_REGEX
+#  include <boost/regex.hpp>
+#else
+#  include <regex>
+#endif
 
 DECLARE_LOG_OBJECT()
 
@@ -360,15 +365,27 @@ const std::string ZTSClient::getHeader() const { return roleHeader_; }
 PrivateKeyUri ZTSClient::parseUri(const char *uri) {
     PrivateKeyUri uriSt;
     // scheme mediatype[;base64] path file
-    static const boost::regex expression(
-        "^(\?:([^:/\?#]+):)(\?:([;/\\-\\w]*),)\?(/\?(\?:[^\?#/]*/)*)\?([^\?#]*)");
-    boost::cmatch groups;
-    if (boost::regex_match(uri, groups, expression)) {
+
+#ifdef PULSAR_USE_BOOST_REGEX
+   static const boost::regex expression(
+         "^(\?:([^:/\?#]+):)(\?:([;/\\-\\w]*),)\?(/\?(\?:[^\?#/]*/)*)\?([^\?#]*)");
+   boost::cmatch groups;
+   if (boost::regex_match(uri, groups, expression)) {
+       uriSt.scheme = groups.str(1);
+       uriSt.mediaTypeAndEncodingType = groups.str(2);
+       uriSt.data = groups.str(4);
+   }
+#else // !PULSAR_USE_BOOST_REGEX
+    static const std::regex expression(
+        R"(^(?:([A-Za-z]+):)(?:([/\w\-]+;\w+),([=\w]+))?(?:\/\/)?(\/[^?#]+)?)");
+    std::cmatch groups;
+    if (std::regex_match(uri, groups, expression)) {
         uriSt.scheme = groups.str(1);
         uriSt.mediaTypeAndEncodingType = groups.str(2);
-        uriSt.data = groups.str(4);
-        uriSt.path = groups.str(3) + groups.str(4);
+        uriSt.data = groups.str(3);
+        uriSt.path = groups.str(4);
     }
+#endif //PULSAR_USE_BOOST_REGEX
     return uriSt;
 }
 }  // namespace pulsar
