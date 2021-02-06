19dec2cb9c071f96202c1cc318894eb22d62b9cd
Rajan Dhabalia <rdhabalia@apache.org>
2021-02-05 19:36:51 -0800

    [pulsar-broker] MockZK: Handle zk-children watch notification (#9473)
    Co-authored-by: Matteo Merli <mmerli@apache.org>

--- testmocks/src/main/java/org/apache/zookeeper/MockZooKeeper.java
+++ testmocks/src/main/java/org/apache/zookeeper/MockZooKeeper.java
@@ -275,6 +275,8 @@ public class MockZooKeeper extends ZooKeeper {
                 cb.processResult(KeeperException.Code.NODEEXISTS.intValue(), path, ctx, null);
             } else if (!parent.isEmpty() && !tree.containsKey(parent)) {
                 mutex.unlock();
+                toNotifyParent.forEach(watcher -> watcher
+                        .process(new WatchedEvent(EventType.NodeChildrenChanged, KeeperState.SyncConnected, parent)));
                 cb.processResult(KeeperException.Code.NONODE.intValue(), path, ctx, null);
             } else {
                 tree.put(name, Pair.of(data, 0));
