0bf968a93dd2fa769f4c4a45a7b37573dd6a7b9b
congbo <congbobo184@github.com>
2021-02-08 09:21:47 +0800

    Fix check style

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -28,7 +28,6 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import io.netty.buffer.ByteBuf;
 import io.netty.util.concurrent.FastThreadLocal;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
