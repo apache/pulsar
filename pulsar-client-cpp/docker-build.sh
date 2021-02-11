4e059b22bad59217a49e46f1a74fc16879dd3440
Yunze Xu <xyzinfernity@163.com>
2021-02-11 15:32:09 +0800

    Fix docker-build.sh

--- pulsar-client-cpp/docker-build.sh
+++ pulsar-client-cpp/docker-build.sh
@@ -39,10 +39,10 @@ VOLUME_OPTION=${VOLUME_OPTION:-"-v $ROOT_DIR:/pulsar"}
 PYTHON_INCLUDE_DIR="\$(python3 -c \"from distutils.sysconfig import get_python_inc; print(get_python_inc())\")"
 PYTHON_LIBRARY="\$(python3 -c \"import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))\")"
 CMAKE_ARGS="-DPYTHON_INCLUDE_DIR=\"$PYTHON_INCLUDE_DIR\" -DPYTHON_LIBRARY=\"$PYTHON_LIBRARY\" $CMAKE_ARGS"
-COMMAND="cd /pulsar/pulsar-client-cpp
- && apt install -y python3-dev
- && cmake . $CMAKE_ARGS
- && make check-format
+COMMAND="cd /pulsar/pulsar-client-cpp \\
+ && apt install -y python3-dev \\
+ && cmake . $CMAKE_ARGS \\
+ && make check-format \\
  && make -j8"
 
 DOCKER_CMD="docker run -i ${VOLUME_OPTION} ${IMAGE}"
