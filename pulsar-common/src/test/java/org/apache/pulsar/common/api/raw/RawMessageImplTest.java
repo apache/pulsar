251f2ca11af7902bbe1365ee44a0d91b8e1fcda0
Simon <simon.xianming@ext.deposit-solution.com>
2021-02-24 10:15:29 +0800

    add License

--- pulsar-common/src/test/java/org/apache/pulsar/common/api/raw/RawMessageImplTest.java
+++ pulsar-common/src/test/java/org/apache/pulsar/common/api/raw/RawMessageImplTest.java
@@ -1,15 +1,27 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pulsar.common.api.raw;
 
-import io.netty.buffer.ByteBuf;
 import junit.framework.TestCase;
-import org.apache.pulsar.client.api.Schema;
-import org.apache.pulsar.common.api.proto.MessageMetadata;
 import org.apache.pulsar.common.api.proto.SingleMessageMetadata;
 
-import java.nio.ByteBuffer;
 import java.util.Map;
-
-import static org.testng.Assert.assertEquals;
 
 public class RawMessageImplTest extends TestCase {
 
