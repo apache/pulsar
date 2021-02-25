371b311eac93ac1cf5d5a3ceeeb36f7ae11538a1
ran <gaoran_10@126.com>
2021-02-25 16:49:53 +0800

    [Pulsar SQL] Fix Pulsar SQL query bytes schema data error (#9631)
    ### Motivation
    
    Currently, the Pulsar SQL query bytes schema data will cause an error.
    
    *Reproduce*
    1. produce bytes schema data.
    2. query data by the Pulsar SQL.
    3. the error log could be seen.
    
    *Error log*
    ```
    com.google.common.util.concurrent.UncheckedExecutionException: java.nio.BufferUnderflowException
    	at com.google.common.cache.LocalCache$Segment.get(LocalCache.java:2051)
    	at com.google.common.cache.LocalCache.get(LocalCache.java:3951)
    	at com.google.common.cache.LocalCache.getOrLoad(LocalCache.java:3974)
    	at com.google.common.cache.LocalCache$LocalLoadingCache.get(LocalCache.java:4935)
    	at org.apache.pulsar.sql.presto.PulsarSqlSchemaInfoProvider.getSchemaByVersion(PulsarSqlSchemaInfoProvider.java:76)
    	at org.apache.pulsar.sql.presto.PulsarRecordCursor.advanceNextPosition(PulsarRecordCursor.java:471)
    	at io.prestosql.spi.connector.RecordPageSource.getNextPage(RecordPageSource.java:90)
    	at io.prestosql.operator.TableScanOperator.getOutput(TableScanOperator.java:302)
    	at io.prestosql.operator.Driver.processInternal(Driver.java:379)
    	at io.prestosql.operator.Driver.lambda$processFor$8(Driver.java:283)
    	at io.prestosql.operator.Driver.tryWithLock(Driver.java:675)
    	at io.prestosql.operator.Driver.processFor(Driver.java:276)
    	at io.prestosql.execution.SqlTaskExecution$DriverSplitRunner.processFor(SqlTaskExecution.java:1075)
    	at io.prestosql.execution.executor.PrioritizedSplitRunner.process(PrioritizedSplitRunner.java:163)
    	at io.prestosql.execution.executor.TaskExecutor$TaskRunner.run(TaskExecutor.java:484)
    	at io.prestosql.$gen.Presto_332__testversion____20210219_094906_2.run(Unknown Source)
    	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
    	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
    	at java.lang.Thread.run(Thread.java:748)
    Caused by: java.nio.BufferUnderflowException
    	at java.nio.Buffer.nextGetIndex(Buffer.java:509)
    	at java.nio.HeapByteBuffer.getLong(HeapByteBuffer.java:415)
    	at org.apache.pulsar.sql.presto.PulsarSqlSchemaInfoProvider.loadSchema(PulsarSqlSchemaInfoProvider.java:106)
    	at org.apache.pulsar.sql.presto.PulsarSqlSchemaInfoProvider.access$000(PulsarSqlSchemaInfoProvider.java:49)
    	at org.apache.pulsar.sql.presto.PulsarSqlSchemaInfoProvider$1.load(PulsarSqlSchemaInfoProvider.java:61)
    	at org.apache.pulsar.sql.presto.PulsarSqlSchemaInfoProvider$1.load(PulsarSqlSchemaInfoProvider.java:58)
    	at com.google.common.cache.LocalCache$LoadingValueReference.loadFuture(LocalCache.java:3529)
    	at com.google.common.cache.LocalCache$Segment.loadSync(LocalCache.java:2278)
    	at com.google.common.cache.LocalCache$Segment.lockedGetOrLoad(LocalCache.java:2155)
    	at com.google.common.cache.LocalCache$Segment.get(LocalCache.java:2045)
    	... 18 more
    
    ```
    
    ### Modifications
    
    Add check for bytes schema, if the schema is bytes schema use the schema info of the bytes schema directly.
    
    ### Verifying this change
    
    Add a new integration test for different schemas.
    
    ### Does this pull request potentially affect one of the following parts:
    
    *If `yes` was chosen, please highlight the changes*
    
      - Dependencies (does it add or upgrade a dependency): (no)
      - The public API: (no)
      - The schema: (no)
      - The default values of configurations: (no)
      - The wire protocol: (no)
      - The rest endpoints: (no)
      - The admin cli options: (no)
      - Anything that affects deployment: (no)

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/presto/TestPrestoQueryTieredStorage.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/presto/TestPrestoQueryTieredStorage.java
@@ -19,6 +19,7 @@
 package org.apache.pulsar.tests.integration.presto;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +29,7 @@ import org.apache.pulsar.client.admin.PulsarAdmin;
 import org.apache.pulsar.client.api.Consumer;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.api.SubscriptionInitialPosition;
 import org.apache.pulsar.client.impl.MessageIdImpl;
 import org.apache.pulsar.client.impl.schema.JSONSchema;
@@ -43,7 +45,7 @@ import org.testng.annotations.Test;
 
 
 /**
- * Test presto query from tiered storage.
+ * Test presto query from tiered storage, the Pulsar SQL is cluster mode.
  */
 @Slf4j
 public class TestPrestoQueryTieredStorage extends TestPulsarSQLBase {
@@ -106,18 +108,21 @@ public class TestPrestoQueryTieredStorage extends TestPulsarSQLBase {
     public void testQueryTieredStorage1() throws Exception {
         TopicName topicName = TopicName.get(
                 TopicDomain.persistent.value(), TENANT, NAMESPACE, "stocks_ts_nons_" + randomName(5));
-        pulsarSQLBasicTest(topicName, false, false);
+        pulsarSQLBasicTest(topicName, false, false, JSONSchema.of(Stock.class));
     }
 
     @Test
     public void testQueryTieredStorage2() throws Exception {
         TopicName topicName = TopicName.get(
                 TopicDomain.persistent.value(), TENANT, NAMESPACE, "stocks_ts_ns_" + randomName(5));
-        pulsarSQLBasicTest(topicName, false, true);
+        pulsarSQLBasicTest(topicName, false, true, JSONSchema.of(Stock.class));
     }
 
     @Override
-    protected int prepareData(TopicName topicName, boolean isBatch, boolean useNsOffloadPolices) throws Exception {
+    protected int prepareData(TopicName topicName,
+                              boolean isBatch,
+                              boolean useNsOffloadPolices,
+                              Schema schema) throws Exception {
         @Cleanup
         PulsarClient pulsarClient = PulsarClient.builder()
                 .serviceUrl(pulsarCluster.getPlainTextServiceUrl())
@@ -203,6 +208,15 @@ public class TestPrestoQueryTieredStorage extends TestPulsarSQLBase {
             admin.topics().unload(topicName.toString());
         } catch (Exception e) {
             Assert.fail("Failed to deleteOffloadedDataFromBK.");
+        }
+    }
+
+    @Override
+    protected void validateContent(int messageNum, String[] contentArr, Schema schema) {
+        for (int i = 0; i < messageNum; ++i) {
+            assertThat(contentArr).contains("\"" + i + "\"");
+            assertThat(contentArr).contains("\"" + "STOCK_" + i + "\"");
+            assertThat(contentArr).contains("\"" + (100.0 + i * 10) + "\"");
         }
     }
 
