af40266719a2df80d7a24e9f22f349a9d4bbcd6c
Yong Zhang <zhangyong1025.zy@gmail.com>
2021-03-05 17:15:07 +0800

    Move the test to the another file

---
+++ pulsar-client-cpp/lib/TestUtil.h
@@ -0,0 +1,10 @@
+//
+// Created by yong on 2021/3/5.
+//
+
+#ifndef PULSAR_CPP_TESTUTIL_H
+#define PULSAR_CPP_TESTUTIL_H
+
+#define FRIEND_TEST(test_case_name, test_name) friend class test_case_name##_##test_name##_Test
+
+#endif  // PULSAR_CPP_TESTUTIL_H
