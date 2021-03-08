apache/pulsar

commit b8f3040265a79a2985948c406b798304e6d7f60a

[Tests] Cleanup test state after a failure, before retrying or proceeding to next methods (#9823)
- adds TestRetrySupport with the logic
- make MockedPulsarServiceBaseTest and ReplicatorTestBase extend TestRetrySupport
  so that all tests that extend these classes get the tests state resetting behavior

build/run_unit_group.sh
@@ -32,7 +32,7 @@ function broker_group_1() {
                                       -DtestReuseFork=true
 
   $MVN_TEST_COMMAND -pl pulsar-broker -Dinclude="org/apache/pulsar/broker/**/*.java" \
-                                      -Dexclude="org/apache/pulsar/broker/zookeeper/**/*.java,
+                                      -Dexclude="**/*$*,org/apache/pulsar/broker/zookeeper/**/*.java,
                                                  org/apache/pulsar/broker/loadbalance/**/*.java,
                                                  org/apache/pulsar/broker/service/**/*.java,
                                                  **/AdminApiOffloadTest.java"
@@ -63,7 +63,7 @@ function broker_group_2() {
   $MVN_TEST_COMMAND -pl pulsar-broker -Dinclude="org/apache/pulsar/broker/zookeeper/**/*.java,
                                                  org/apache/pulsar/broker/loadbalance/**/*.java,
                                                  org/apache/pulsar/broker/service/**/*.java" \
-                                      -Dexclude="**/ReplicatorTest.java,
+                                      -Dexclude="**/*$*,**/ReplicatorTest.java,
                                                  **/MessagePublishBufferThrottleTest.java,
                                                  **/TopicOwnerTest.java,
                                                  **/*StreamingDispatcher*Test.java,
@@ -80,7 +80,7 @@ function broker_client_api() {
                                       -DtestReuseFork=true
 
   $MVN_TEST_COMMAND -pl pulsar-broker -Dinclude="org/apache/pulsar/client/api/**/*.java" \
-                                      -Dexclude="**/DispatcherBlockConsumerTest.java,
+                                      -Dexclude="**/*$*,**/DispatcherBlockConsumerTest.java,
                                                  **/SimpleProducerConsumerTest.java"
 }
 
@@ -89,7 +89,7 @@ function broker_client_impl() {
 }
 
 function broker_client_other() {
-  $MVN_TEST_COMMAND -pl pulsar-broker -Dexclude="org/apache/pulsar/broker/**/*.java,
+  $MVN_TEST_COMMAND -pl pulsar-broker -Dexclude="**/*$*,org/apache/pulsar/broker/**/*.java,
                                                  org/apache/pulsar/client/**/*.java"
 }
 
@@ -119,7 +119,7 @@ function proxy() {
                                      -DtestReuseFork=true
 
   $MVN_TEST_COMMAND -pl pulsar-proxy -DtestForkCount=1 \
-                                     -Dexclude="**/ProxyRolesEnforcementTest.java,
+                                     -Dexclude="**/*$*,**/ProxyRolesEnforcementTest.java,
                                                 **/ProxyAuthenticationTest.java,
                                                 **/ProxyTest.java,
                                                 **/MessagePublishBufferThrottleTest.java" \
@@ -128,7 +128,7 @@ function proxy() {
 
 function other() {
   build/retry.sh mvn -B -ntp install -PbrokerSkipTest \
-                                     -Dexclude="org/apache/pulsar/proxy/**/*.java,
+                                     -Dexclude="**/*$*,org/apache/pulsar/proxy/**/*.java,
                                                 **/ManagedLedgerTest.java,
                                                 **/TestPulsarKeyValueSchemaHandler.java,
                                                 **/PrimitiveSchemaTest.java,

buildtools/src/main/java/org/apache/pulsar/tests/TestRetrySupport.java
@@ -1,0 +1,94 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.tests;
+
+import java.lang.reflect.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Adds support for resetting the internal state of the test
+ * by calling "cleanup" and "setup" methods before running a test method
+ * after a previous test method has failed.
+ *
+ * This is useful for making test retries to work on classes which use BeforeClass
+ * and AfterClass methods to setup a test environment that is shared across all test methods in the test
+ * class.
+ */
+public abstract class TestRetrySupport {
+    private static final Logger LOG = LoggerFactory.getLogger(TestRetrySupport.class);
+    private int currentSetupNumber;
+    private int failedSetupNumber = -1;
+    private int cleanedUpSetupNumber;
+
+    @BeforeMethod(groups = "setup")
+    public final void stateCheck(Method method) throws Exception {
+        // run cleanup and setup if the current setup number is the one where a failure happened
+        // this is to cleanup state before retrying
+        if (currentSetupNumber == failedSetupNumber
+                && cleanedUpSetupNumber != failedSetupNumber) {
+            LOG.info("Previous test run has failed before {}.{}, failedSetupNumber={}. Running cleanup and setup.",
+                    method.getDeclaringClass().getSimpleName(), method.getName(), failedSetupNumber);
+            try {
+                cleanup();
+            } catch (Exception e) {
+                LOG.error("Cleanup failed, ignoring this.", e);
+            }
+            setup();
+            LOG.info("State cleanup finished.");
+            failedSetupNumber = -1;
+        }
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public final void failureCheck(ITestResult testResult, Method method) {
+        // track the setup number where the failure happened
+        if (!testResult.isSuccess()) {
+            LOG.info("Detected test failure in test {}.{}, currentSetupNumber={}",
+                    method.getDeclaringClass().getSimpleName(), method.getName(),
+                    currentSetupNumber);
+            failedSetupNumber = currentSetupNumber;
+        }
+    }
+
+    /**
+     * This method should be called in the setup method of the concrete class.
+     *
+     * This increases an internal counter and resets the failure state which are used to determine
+     * whether cleanup is needed before a test method is called.
+     *
+     */
+    protected final void incrementSetupNumber() {
+        currentSetupNumber++;
+        failedSetupNumber = -1;
+        LOG.debug("currentSetupNumber={}", currentSetupNumber);
+    }
+
+    protected final void markCurrentSetupNumberCleaned() {
+        cleanedUpSetupNumber = currentSetupNumber;
+        LOG.debug("cleanedUpSetupNumber={}", cleanedUpSetupNumber);
+    }
+
+    protected abstract void setup() throws Exception;
+
+    protected abstract void cleanup() throws Exception;
+}

buildtools/src/test/java/org/apache/pulsar/tests/TestRetrySupportBeforeMethodRetryTest.java
@@ -1,0 +1,70 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.tests;
+
+import org.testng.Assert;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestRetrySupportBeforeMethodRetryTest extends TestRetrySupport {
+    public static class IllegalStateRetry implements IRetryAnalyzer {
+        @Override
+        public boolean retry(ITestResult result) {
+            return result.getThrowable().getClass() == IllegalStateException.class;
+        }
+    }
+
+    private int setupCallCount;
+    private int cleanupCallCount;
+    private int invocationCount;
+
+    @BeforeMethod
+    @Override
+    protected void setup() throws Exception {
+        setupCallCount++;
+        incrementSetupNumber();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    @Override
+    protected void cleanup() throws Exception {
+        cleanupCallCount++;
+        markCurrentSetupNumberCleaned();
+    }
+
+    @Test(retryAnalyzer = IllegalStateRetry.class)
+    void shouldNotDoAnythingWhenThereIsBeforeAndAfterMethod(ITestContext testContext) {
+        // get the number of times this method has been called
+        invocationCount++;
+        // setup method should have been called the same amount of times when TestRetrySupport
+        // is doing the handling. setup method has @BeforeClass annotation so that TestNG calls it
+        // only once
+        Assert.assertEquals(setupCallCount, invocationCount);
+        // cleanup method one less times
+        Assert.assertEquals(cleanupCallCount, invocationCount - 1);
+        // trigger a retry
+        if (invocationCount < 5) {
+            throw new IllegalStateException("Sample failure to trigger retry.");
+        }
+    }
+}

buildtools/src/test/java/org/apache/pulsar/tests/TestRetrySupportRetryTest.java
@@ -1,0 +1,71 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.tests;
+
+import java.lang.reflect.Method;
+import org.testng.Assert;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestRetrySupportRetryTest extends TestRetrySupport {
+    public static class IllegalStateRetry implements IRetryAnalyzer {
+        @Override
+        public boolean retry(ITestResult result) {
+            return result.getThrowable().getClass() == IllegalStateException.class;
+        }
+    }
+
+    private int setupCallCount;
+    private int cleanupCallCount;
+    private int invocationCount;
+
+    @BeforeClass
+    @Override
+    protected void setup() throws Exception {
+        setupCallCount++;
+        incrementSetupNumber();
+    }
+
+    @AfterClass(alwaysRun = true)
+    @Override
+    protected void cleanup() throws Exception {
+        cleanupCallCount++;
+        markCurrentSetupNumberCleaned();
+    }
+
+    @Test(retryAnalyzer = IllegalStateRetry.class)
+    void shouldCallSetupBeforeRetrying(ITestContext testContext, Method method) {
+        // get the number of times this method has been called
+        invocationCount++;
+        // setup method should have been called the same amount of times when TestRetrySupport
+        // is doing the handling. setup method has @BeforeClass annotation so that TestNG calls it
+        // only once
+        Assert.assertEquals(setupCallCount, invocationCount);
+        // cleanup method one less times
+        Assert.assertEquals(cleanupCallCount, invocationCount - 1);
+        // trigger a retry
+        if (invocationCount < 5) {
+            throw new IllegalStateException("Sample failure to trigger retry.");
+        }
+    }
+}

buildtools/src/test/java/org/apache/pulsar/tests/TestRetrySupportSuccessTest.java
@@ -1,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Tests that TestRetrySupport doesn't call setup & cleanup when tests are successful
+ */
+public class TestRetrySupportSuccessTest extends TestRetrySupport {
+    private int setupCallCount;
+    private int cleanupCallCount;
+
+    @BeforeClass
+    @Override
+    protected void setup() throws Exception {
+        setupCallCount++;
+        incrementSetupNumber();
+    }
+
+    @AfterClass(alwaysRun = true)
+    @Override
+    protected void cleanup() throws Exception {
+        cleanupCallCount++;
+        markCurrentSetupNumberCleaned();
+    }
+
+    @Test
+    void shouldCallSetupOnce1() {
+        Assert.assertEquals(setupCallCount, 1);
+        Assert.assertEquals(cleanupCallCount, 0);
+    }
+
+    @Test
+    void shouldCallSetupOnce2() {
+        Assert.assertEquals(setupCallCount, 1);
+        Assert.assertEquals(cleanupCallCount, 0);
+    }
+
+    @Test
+    void shouldCallSetupOnce3() {
+        Assert.assertEquals(setupCallCount, 1);
+        Assert.assertEquals(cleanupCallCount, 0);
+    }
+}

pulsar-broker/src/test/java/org/apache/pulsar/broker/auth/MockedPulsarServiceBaseTest.java
@@ -57,6 +57,7 @@ import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.common.policies.data.ClusterData;
 import org.apache.pulsar.common.policies.data.TenantInfo;
 import org.apache.pulsar.metadata.impl.ZKMetadataStore;
+import org.apache.pulsar.tests.TestRetrySupport;
 import org.apache.pulsar.zookeeper.ZooKeeperClientFactory;
 import org.apache.pulsar.zookeeper.ZookeeperClientFactoryImpl;
 import org.apache.zookeeper.CreateMode;
@@ -71,7 +72,7 @@ import org.slf4j.LoggerFactory;
  * Base class for all tests that need a Pulsar instance without a ZK and BK cluster.
  */
 @PowerMockIgnore(value = {"org.slf4j.*", "com.sun.org.apache.xerces.*" })
-public abstract class MockedPulsarServiceBaseTest {
+public abstract class MockedPulsarServiceBaseTest extends TestRetrySupport {
 
     protected final String DUMMY_VALUE = "DUMMY_VALUE";
     protected final String GLOBAL_DUMMY_VALUE = "GLOBAL_DUMMY_VALUE";
@@ -102,7 +103,9 @@ public abstract class MockedPulsarServiceBaseTest {
         this.conf = getDefaultConf();
     }
 
+
     protected final void internalSetup() throws Exception {
+        incrementSetupNumber();
         init();
         lookupUrl = new URI(brokerUrl.toString());
         if (isTcpLookup) {
@@ -175,6 +178,7 @@ public abstract class MockedPulsarServiceBaseTest {
     }
 
     protected final void internalCleanup() throws Exception {
+        markCurrentSetupNumberCleaned();
         // if init fails, some of these could be null, and if so would throw
         // an NPE in shutdown, obscuring the real error
         if (admin != null) {

pulsar-broker/src/test/java/org/apache/pulsar/broker/service/PeerReplicatorTest.java
@@ -50,14 +50,14 @@ public class PeerReplicatorTest extends ReplicatorTestBase {
 
     @Override
     @BeforeClass(timeOut = 300000)
-    void setup() throws Exception {
+    public void setup() throws Exception {
         super.setup();
     }
 
     @Override
     @AfterClass(alwaysRun = true, timeOut = 300000)
-    void shutdown() throws Exception {
-        super.shutdown();
+    public void cleanup() throws Exception {
+        super.cleanup();
     }
 
     @DataProvider(name = "lookupType")

pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ReplicatorGlobalNSTest.java
@@ -46,14 +46,14 @@ public class ReplicatorGlobalNSTest extends ReplicatorTestBase {
 
     @Override
     @BeforeClass(timeOut = 300000)
-    void setup() throws Exception {
+    public void setup() throws Exception {
         super.setup();
     }
 
     @Override
     @AfterClass(alwaysRun = true, timeOut = 300000)
-    void shutdown() throws Exception {
-        super.shutdown();
+    public void cleanup() throws Exception {
+        super.cleanup();
     }
 
     /**

commit 6b2c4a43115b9088a1df57004160e0cdac39e942

[Testing] Fix Maven Surefire default exclude
- Problem introduced in PR #9823
- In the shell script "**/*$*" gets replaced with "**/*" when using double quotes.
- Fix: use single quotes for the exclude parameter

build/run_unit_group.sh
@@ -32,10 +32,10 @@ function broker_group_1() {
                                       -DtestReuseFork=true
 
   $MVN_TEST_COMMAND -pl pulsar-broker -Dinclude="org/apache/pulsar/broker/**/*.java" \
-                                      -Dexclude="**/*$*,org/apache/pulsar/broker/zookeeper/**/*.java,
+                                      -Dexclude='**/*$*,org/apache/pulsar/broker/zookeeper/**/*.java,
                                                  org/apache/pulsar/broker/loadbalance/**/*.java,
                                                  org/apache/pulsar/broker/service/**/*.java,
-                                                 **/AdminApiOffloadTest.java"
+                                                 **/AdminApiOffloadTest.java'
 
 }
 
@@ -63,11 +63,11 @@ function broker_group_2() {
   $MVN_TEST_COMMAND -pl pulsar-broker -Dinclude="org/apache/pulsar/broker/zookeeper/**/*.java,
                                                  org/apache/pulsar/broker/loadbalance/**/*.java,
                                                  org/apache/pulsar/broker/service/**/*.java" \
-                                      -Dexclude="**/*$*,**/ReplicatorTest.java,
+                                      -Dexclude='**/*$*,**/ReplicatorTest.java,
                                                  **/MessagePublishBufferThrottleTest.java,
                                                  **/TopicOwnerTest.java,
                                                  **/*StreamingDispatcher*Test.java,
-                                                 **/AntiAffinityNamespaceGroupTest.java"
+                                                 **/AntiAffinityNamespaceGroupTest.java'
 }
 
 function broker_client_api() {
@@ -80,8 +80,8 @@ function broker_client_api() {
                                       -DtestReuseFork=true
 
   $MVN_TEST_COMMAND -pl pulsar-broker -Dinclude="org/apache/pulsar/client/api/**/*.java" \
-                                      -Dexclude="**/*$*,**/DispatcherBlockConsumerTest.java,
-                                                 **/SimpleProducerConsumerTest.java"
+                                      -Dexclude='**/*$*,**/DispatcherBlockConsumerTest.java,
+                                                 **/SimpleProducerConsumerTest.java'
 }
 
 function broker_client_impl() {
@@ -89,8 +89,8 @@ function broker_client_impl() {
 }
 
 function broker_client_other() {
-  $MVN_TEST_COMMAND -pl pulsar-broker -Dexclude="**/*$*,org/apache/pulsar/broker/**/*.java,
-                                                 org/apache/pulsar/client/**/*.java"
+  $MVN_TEST_COMMAND -pl pulsar-broker -Dexclude='**/*$*,org/apache/pulsar/broker/**/*.java,
+                                                 org/apache/pulsar/client/**/*.java'
 }
 
 function proxy() {
@@ -119,20 +119,20 @@ function proxy() {
                                      -DtestReuseFork=true
 
   $MVN_TEST_COMMAND -pl pulsar-proxy -DtestForkCount=1 \
-                                     -Dexclude="**/*$*,**/ProxyRolesEnforcementTest.java,
+                                     -Dexclude='**/*$*,**/ProxyRolesEnforcementTest.java,
                                                 **/ProxyAuthenticationTest.java,
                                                 **/ProxyTest.java,
-                                                **/MessagePublishBufferThrottleTest.java" \
+                                                **/MessagePublishBufferThrottleTest.java' \
                                      -DtestReuseFork=true
 }
 
 function other() {
   build/retry.sh mvn -B -ntp install -PbrokerSkipTest \
-                                     -Dexclude="**/*$*,org/apache/pulsar/proxy/**/*.java,
+                                     -Dexclude='**/*$*,org/apache/pulsar/proxy/**/*.java,
                                                 **/ManagedLedgerTest.java,
                                                 **/TestPulsarKeyValueSchemaHandler.java,
                                                 **/PrimitiveSchemaTest.java,
-                                                BlobStoreManagedLedgerOffloaderTest.java"
+                                                BlobStoreManagedLedgerOffloaderTest.java'
 
   $MVN_TEST_COMMAND -pl managed-ledger -Dinclude="**/ManagedLedgerTest.java" \
                                        -DtestForkCount=1 \