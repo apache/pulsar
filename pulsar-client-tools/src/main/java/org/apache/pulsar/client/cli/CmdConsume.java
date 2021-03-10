03ccb7c95618cc186de5f27ac83d78d4afb0dca7
Enrico Olivelli <eolivelli@apache.org>
2021-03-10 12:28:02 +0100

    fix import

--- pulsar-client-tools/src/main/java/org/apache/pulsar/client/cli/CmdConsume.java
+++ pulsar-client-tools/src/main/java/org/apache/pulsar/client/cli/CmdConsume.java
@@ -38,6 +38,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
