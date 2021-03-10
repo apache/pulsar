beec796e0ce318d5bd1ce72b2d4c8a7d8e5e088a
Enrico Olivelli <eolivelli@apache.org>
2021-03-10 12:16:34 +0100

    remove start import

--- pulsar-client-tools/src/main/java/org/apache/pulsar/client/cli/CmdConsume.java
+++ pulsar-client-tools/src/main/java/org/apache/pulsar/client/cli/CmdConsume.java
@@ -36,7 +36,10 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Pattern;
 
