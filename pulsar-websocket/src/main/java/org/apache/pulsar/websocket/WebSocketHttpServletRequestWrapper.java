686543e27aa9c6169aee572e4e9c8a411a4d1f27
gaoran10 <gaoran_10@126.com>
2021-03-12 03:02:14 +0800

    add the license header

--- pulsar-websocket/src/main/java/org/apache/pulsar/websocket/WebSocketHttpServletRequestWrapper.java
+++ pulsar-websocket/src/main/java/org/apache/pulsar/websocket/WebSocketHttpServletRequestWrapper.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pulsar.websocket;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,9 +37,8 @@ public class WebSocketHttpServletRequestWrapper extends HttpServletRequestWrappe
 
     @Override
     public String getHeader(String name) {
-        // The Javascript WebSocket client couldn't add the auth param to the request header,
-        // so add the auth param in the queryParameter, use the query param `token` value instead of
-        // the Authorization header if the Authorization header not exist.
+        // The browser javascript WebSocket client couldn't add the auth param to the request header, use the
+        // query param `token` to transport the auth token for the browser javascript WebSocket client.
         if (name.equals(HTTP_HEADER_NAME)
                 && !((UpgradeHttpServletRequest) this.getRequest()).getHeaders().containsKey(HTTP_HEADER_NAME)) {
             return getRequest().getParameter(TOKEN);
