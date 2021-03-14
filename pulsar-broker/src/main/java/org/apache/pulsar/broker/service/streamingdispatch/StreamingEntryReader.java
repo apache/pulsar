95c2a93705ea512e73602f83b88f4aa327b4a527
Marvin Cai <zxc@streamnative.io>
2021-03-14 00:34:26 -0800

    Try to ease the wait time for some of test case for StreamingEntryReader to reduce flakiness.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/streamingdispatch/StreamingEntryReader.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/streamingdispatch/StreamingEntryReader.java
@@ -74,7 +74,7 @@ public class StreamingEntryReader implements AsyncCallbacks.ReadEntryCallback, W
             1, TimeUnit.SECONDS, 0, TimeUnit.MILLISECONDS);
 
     /**
-     * Read entries in streaming way, that said instead reading with micro batch and send entries to consumer after all
+     * Read entries in streaming way, that said instead of reading with micro batch and send entries to consumer after all
      * entries in the batch are read from ledger, this method will fire numEntriesToRead requests to managedLedger
      * and send entry to consumer whenever it is read && all entries before it have been sent to consumer.
      * @param numEntriesToRead number of entry to read from ledger.
