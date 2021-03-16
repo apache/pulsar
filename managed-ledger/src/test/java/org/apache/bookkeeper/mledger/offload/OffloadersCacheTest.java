46a579d5c0428fa65ec6c710592cf4e907d07dd7
Michael Marshall <mikemarsh17@gmail.com>
2021-03-15 23:50:09 -0600

    Add license and remove unnecessary imports

--- managed-ledger/src/test/java/org/apache/bookkeeper/mledger/offload/OffloadersCacheTest.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/mledger/offload/OffloadersCacheTest.java
@@ -1,6 +1,23 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.bookkeeper.mledger.offload;
 
-import org.apache.bookkeeper.mledger.LedgerOffloader;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
@@ -9,16 +26,11 @@ import org.testng.IObjectFactory;
 import org.testng.annotations.ObjectFactory;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertSame;
 
 @PrepareForTest({OffloaderUtils.class})
-@PowerMockIgnore({"org.apache.logging.log4j.*", "org.apache.pulsar.common.nar.*", "java.io.*"})
+@PowerMockIgnore({"org.apache.logging.log4j.*", "org.apache.pulsar.common.nar.*"})
 public class OffloadersCacheTest {
 
     // Necessary to make PowerMockito.mockStatic work with TestNG.
@@ -47,5 +59,4 @@ public class OffloadersCacheTest {
 
         assertSame(offloaders2, expectedOffloaders, "The offloaders should be the mocked one.");
     }
-
 }
