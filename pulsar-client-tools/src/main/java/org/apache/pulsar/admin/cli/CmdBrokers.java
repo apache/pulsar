be79b07c7f84790c2e8a58ec7dd5197e87dc4ad7
guangning <guangning@apache.org>
2021-03-23 07:45:53 +0800

    format code, fixed space

--- pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdBrokers.java
+++ pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdBrokers.java
@@ -151,7 +151,7 @@ public class CmdBrokers extends CmdBase {
 
         @Override
         void run() throws Exception {
-            System.out.println( getAdmin().brokers().getVersion());
+            System.out.println(getAdmin().brokers().getVersion());
         }
     }
 
