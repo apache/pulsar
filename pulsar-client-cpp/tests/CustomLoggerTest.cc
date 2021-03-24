283369e77836d107daf44e2bc7f0e85845753186
Yunze Xu <xyzinfernity@163.com>
2021-03-24 18:24:16 +0800

    Add tests for SimpleLoggerFactory

--- pulsar-client-cpp/tests/CustomLoggerTest.cc
+++ pulsar-client-cpp/tests/CustomLoggerTest.cc
@@ -17,7 +17,7 @@
  * under the License.
  */
 #include <pulsar/Client.h>
-#include <pulsar/Logger.h>
+#include <pulsar/SimpleLoggerFactory.h>
 #include <LogUtils.h>
 #include <gtest/gtest.h>
 #include <thread>
@@ -66,4 +66,34 @@ TEST(CustomLoggerTest, testCustomLogger) {
     client.close();
     // custom logger didn't get any new lines
     ASSERT_EQ(logLines.size(), 2);
+}
+
+TEST(CustomLoggerTest, testSimpleLoggerFactory) {
+    std::unique_ptr<SimpleLoggerFactory> factory(new SimpleLoggerFactory);
+    std::unique_ptr<Logger> logger(factory->getLogger(__FILE__));
+    ASSERT_FALSE(logger->isEnabled(Logger::LEVEL_DEBUG));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_INFO));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_WARN));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_ERROR));
+
+    factory.reset(new SimpleLoggerFactory(Logger::LEVEL_DEBUG));
+    logger.reset(factory->getLogger(__FILE__));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_DEBUG));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_INFO));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_WARN));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_ERROR));
+
+    factory.reset(new SimpleLoggerFactory(Logger::LEVEL_WARN));
+    logger.reset(factory->getLogger(__FILE__));
+    ASSERT_FALSE(logger->isEnabled(Logger::LEVEL_DEBUG));
+    ASSERT_FALSE(logger->isEnabled(Logger::LEVEL_INFO));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_WARN));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_ERROR));
+
+    factory.reset(new SimpleLoggerFactory(Logger::LEVEL_ERROR));
+    logger.reset(factory->getLogger(__FILE__));
+    ASSERT_FALSE(logger->isEnabled(Logger::LEVEL_DEBUG));
+    ASSERT_FALSE(logger->isEnabled(Logger::LEVEL_INFO));
+    ASSERT_FALSE(logger->isEnabled(Logger::LEVEL_WARN));
+    ASSERT_TRUE(logger->isEnabled(Logger::LEVEL_ERROR));
 }
