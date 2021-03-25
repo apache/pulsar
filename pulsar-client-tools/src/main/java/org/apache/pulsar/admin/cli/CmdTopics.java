8f65af452ffbd32f097f2d9e6c00f861d3a77d51
feynmanlin <feynmanlin@tencent.com>
2021-03-25 13:07:37 +0800

    Support get applied compaction threshold

--- pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdTopics.java
+++ pulsar-client-tools/src/main/java/org/apache/pulsar/admin/cli/CmdTopics.java
@@ -37,6 +37,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -1739,10 +1740,13 @@ public class CmdTopics extends CmdBase {
         @Parameter(description = "persistent://tenant/namespace/topic", required = true)
         private java.util.List<String> params;
 
+        @Parameter(names = { "-ap", "--applied" }, description = "Get the applied policy of the topic")
+        private boolean applied = false;
+
         @Override
         void run() throws PulsarAdminException {
             String persistentTopic = validatePersistentTopic(params);
-            print(getAdmin().topics().getCompactionThreshold(persistentTopic));
+            print(getAdmin().topics().getCompactionThreshold(persistentTopic, applied));
         }
     }
 
