3c386242aada5edec140bedbed1a22bdcaf49e35
Lari Hotari <lari@hotari.net>
2021-04-27 09:16:22 +0300

    [Build] Fix the usage of core-modules and docker profiles (exclude main profile)
    - The current Pulsar build is designed in a way that "main" profile shouldn't be
      active when "core-modules" or "docker" profile is active.
      Using a core-modules profile requires explicitly disabling main profile with either
      "-main" or "!main" in the "-P" parameter.
      For example:
      "-Pcore-modules,-main" to activate core-modules
      "-Pdocker,-main" to activate the docker profile.

--- docker/build.sh
+++ docker/build.sh
@@ -21,5 +21,5 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
 cd $ROOT_DIR/docker
 
-mvn -f ../dashboard/pom.xml package -Pdocker
-mvn package -Pdocker
+mvn -f ../dashboard/pom.xml package -Pdocker,-main
+mvn package -Pdocker,-main
