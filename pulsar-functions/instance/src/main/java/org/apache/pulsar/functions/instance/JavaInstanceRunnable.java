9f79092ccc59d1128a147d68fbef3f5b662ae8b5
Abhilash Mandaliya <abhilashmandaliya@gmail.com>
2021-04-30 11:35:06 +0530

    removed unused import

--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstanceRunnable.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstanceRunnable.java
@@ -20,7 +20,6 @@
 package org.apache.pulsar.functions.instance;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.HashMap;
