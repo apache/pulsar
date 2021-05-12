c5b008ae770cafde7221968dbe5d5611f9f9ad44
Matteo Merli <mmerli@apache.org>
2021-05-11 21:03:53 -0700

    Fixed accessing potentially empty Optional field (#10542)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/namespace/NamespaceService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/namespace/NamespaceService.java
@@ -644,7 +644,7 @@ public class NamespaceService implements AutoCloseable {
 
         if (LOG.isDebugEnabled()) {
             LOG.debug("Broker not found for SLA Monitoring Namespace {}",
-                    candidateBroker + ":" + config.getWebServicePort().get());
+                    candidateBroker + ":" + config.getWebServicePort());
         }
         return false;
     }
