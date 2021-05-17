883e0d4c3af9dca749093a47c44899635517206b
Matteo Merli <mmerli@apache.org>
2021-05-17 14:03:07 -0700

    Fixed missed ZK caching when fetching list of namespaces for a tenant (#10594)
    * Fixed missed ZK caching when fetching list of namespaces for a tenant
    
    * Fixed generic object type

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/AdminResource.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/AdminResource.java
@@ -23,6 +23,7 @@ import static org.apache.pulsar.common.util.Codec.decode;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.ArrayList;
@@ -239,15 +240,23 @@ public abstract class AdminResource extends PulsarWebResource {
         List<String> namespaces = Lists.newArrayList();
 
         // this will return a cluster in v1 and a namespace in v2
-        for (String clusterOrNamespace : globalZk().getChildren(path(POLICIES, property), false)) {
+        for (String clusterOrNamespace : globalZkCache().getChildren(path(POLICIES, property))) {
             // Then get the list of namespaces
             try {
-                final List<String> children = globalZk().getChildren(path(POLICIES, property, clusterOrNamespace), false);
+                final Set<String> children = globalZkCache().getChildren(path(POLICIES, property, clusterOrNamespace));
                 if (children == null || children.isEmpty()) {
                     String namespace = NamespaceName.get(property, clusterOrNamespace).toString();
-                    // if the length is 0 then this is probably a leftover cluster from namespace created
-                    // with the v1 admin format (prop/cluster/ns) and then deleted, so no need to add it to the list
-                    if (globalZk().getData(path(POLICIES, namespace), false, null).length != 0) {
+                    Optional<Policies> znodeContent = globalZkCache().getData(path(POLICIES, namespace),
+                            (key, content) -> {
+                                try {
+                                    return ObjectMapperFactory.getThreadLocal().readValue(content, Policies.class);
+                                } catch (IOException e) {
+                                    return null;
+                                }
+                            });
+                    if (znodeContent.map(x -> x != null).orElse(false)) {
+                        // if the length is 0 then this is probably a leftover cluster from namespace created
+                        // with the v1 admin format (prop/cluster/ns) and then deleted, so no need to add it to the list
                         namespaces.add(namespace);
                     }
                 } else {
