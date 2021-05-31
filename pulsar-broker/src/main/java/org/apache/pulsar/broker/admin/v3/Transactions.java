432ebc5cccb80be3e13077b55f9779ac1f5954d1
congbo <congbobo184@github.com>
2021-05-31 15:16:04 +0800

    Fix some code style

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v3/Transactions.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v3/Transactions.java
@@ -20,7 +20,6 @@ package org.apache.pulsar.broker.admin.v3;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import javax.ws.rs.Consumes;
