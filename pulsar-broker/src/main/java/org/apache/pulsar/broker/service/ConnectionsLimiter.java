63cc873f26d9be263af99aac2fcf3af254b0c787
feynmanlin <feynmanlin@tencent.com>
2021-06-01 11:13:15 +0800

    code style

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ConnectionsLimiter.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ConnectionsLimiter.java
@@ -63,8 +63,8 @@ public class ConnectionsLimiter {
             if (CURRENT_CONNECTION_NUM.getValue() > maxConnections
                     || CONNECTIONS.get(ip).getValue() > maxConnectionPerIp) {
                 log.info("Reject connect request from {}, because reached the maximum number of connections, broker "
-                                + "connections:{}, IP connections: {}", remoteAddress, CURRENT_CONNECTION_NUM.getValue(),
-                        CONNECTIONS.get(ip).getValue());
+                                + "connections:{}, IP connections: {}",
+                        remoteAddress, CURRENT_CONNECTION_NUM.getValue(), CONNECTIONS.get(ip).getValue());
                 return false;
             }
         } catch (Exception e) {
