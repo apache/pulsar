01361760b97ee00deb1d829c7d26af33c866d5dc
Lari Hotari <lhotari@users.noreply.github.com>
2021-06-01 15:31:42 +0300

    [Broker] Replace illegal use of Prometheus client method CollectorRegistry.getSampleValue (#10764)
    Fixes #10654
    
    - The javadoc of CollectorRegistry.getSampleValue method states:
      "This is inefficient, and intended only for use in unittests.".
    
    - refactor DimensionStats to use a more efficient method where a snapshot of the metrics is created before
      individual quantile metric values are requested.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/DimensionStats.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/stats/DimensionStats.java
@@ -22,7 +22,11 @@ import static io.prometheus.client.CollectorRegistry.defaultRegistry;
 import io.prometheus.client.Collector;
 import io.prometheus.client.Summary;
 import io.prometheus.client.Summary.Builder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,10 +38,19 @@ public class DimensionStats {
     private final String dimensionSumLabel;
     private final String dimensionCountLabel;
     private final Summary summary;
-    private static final double[] QUANTILES = { 0.50, 0.75, 0.95, 0.99, 0.999, 0.9999 };
-    private static final String[] QUANTILE_LABEL = { "quantile" };
+    private static final double[] QUANTILES = {0.50, 0.75, 0.95, 0.99, 0.999, 0.9999};
+    private static final List<String> QUANTILE_LABEL = Collections.unmodifiableList(Arrays.asList("quantile"));
+    private static final List<List<String>> QUANTILE_LABEL_VALUES = Collections.unmodifiableList(
+            Arrays.stream(QUANTILES).mapToObj(Collector::doubleToGoString)
+                    .map(Collections::singletonList)
+                    .collect(Collectors.toList()));
+
 
     public DimensionStats(String name, long updateDurationInSec) {
+        this(name, updateDurationInSec, true);
+    }
+
+    DimensionStats(String name, long updateDurationInSec, boolean register) {
         this.name = name;
         this.dimensionSumLabel = name + "_sum";
         this.dimensionCountLabel = name + "_count";
@@ -46,11 +59,13 @@ public class DimensionStats {
             summaryBuilder.quantile(QUANTILES[i], 0.01);
         }
         this.summary = summaryBuilder.maxAgeSeconds(updateDurationInSec).create();
-        try {
-            defaultRegistry.register(summary);
-        } catch (IllegalArgumentException ie) {
-            // it only happens in test-cases when try to register summary multiple times in registry
-            log.warn("{} is already registred {}", name, ie.getMessage());
+        if (register) {
+            try {
+                defaultRegistry.register(summary);
+            } catch (IllegalArgumentException ie) {
+                // it only happens in test-cases when try to register summary multiple times in registry
+                log.warn("{} is already registred {}", name, ie.getMessage());
+            }
         }
     }
 
@@ -58,54 +73,83 @@ public class DimensionStats {
         summary.observe(unit.toMillis(latency));
     }
 
-    public double getMeanDimension() {
-        double sum = getDimensionSum();
-        double count = getDimensionCount();
-        if (!Double.isNaN(sum) && !Double.isNaN(count)) {
-            return sum / count;
+    public DimensionStatsSnapshot getSnapshot() {
+        return new DimensionStatsSnapshot(summary.collect());
+    }
+
+
+    public class DimensionStatsSnapshot {
+        private final List<Collector.MetricFamilySamples> samples;
+
+        public DimensionStatsSnapshot(List<Collector.MetricFamilySamples> samples) {
+            this.samples = samples;
         }
-        return 0;
-    }
 
-    public double getMedianDimension() {
-        return getQuantile(QUANTILES[0]);
-    }
+        public double getMeanDimension() {
+            double sum = getDimensionSum();
+            double count = getDimensionCount();
+            if (!Double.isNaN(sum) && !Double.isNaN(count)) {
+                return sum / count;
+            }
+            return 0;
+        }
 
-    public double getDimension75() {
-        return getQuantile(QUANTILES[1]);
-    }
+        public double getMedianDimension() {
+            return getQuantile(QUANTILE_LABEL_VALUES.get(0));
+        }
 
-    public double getDimension95() {
-        return getQuantile(QUANTILES[2]);
-    }
+        public double getDimension75() {
+            return getQuantile(QUANTILE_LABEL_VALUES.get(1));
+        }
 
-    public double getDimension99() {
-        return getQuantile(QUANTILES[3]);
-    }
+        public double getDimension95() {
+            return getQuantile(QUANTILE_LABEL_VALUES.get(2));
+        }
 
-    public double getDimension999() {
-        return getQuantile(QUANTILES[4]);
-    }
+        public double getDimension99() {
+            return getQuantile(QUANTILE_LABEL_VALUES.get(3));
+        }
 
-    public double getDimension9999() {
-        return getQuantile(QUANTILES[5]);
-    }
+        public double getDimension999() {
+            return getQuantile(QUANTILE_LABEL_VALUES.get(4));
+        }
 
-    public double getDimensionSum() {
-        return defaultRegistry.getSampleValue(dimensionSumLabel).doubleValue();
-    }
+        public double getDimension9999() {
+            return getQuantile(QUANTILE_LABEL_VALUES.get(5));
+        }
 
-    public double getDimensionCount() {
-        return defaultRegistry.getSampleValue(dimensionCountLabel).doubleValue();
+        public double getDimensionSum() {
+            return getSampleValue(dimensionSumLabel);
+        }
+
+        public double getDimensionCount() {
+            return getSampleValue(dimensionCountLabel);
+        }
+
+        private double getQuantile(List<String> labelValues) {
+            return getSampleValue(name, QUANTILE_LABEL, labelValues);
+        }
+
+        private double getSampleValue(String name) {
+            return getSampleValue(name, Collections.emptyList(), Collections.emptyList());
+        }
+
+        private double getSampleValue(String name, List<String> labelNames, List<String> labelValues) {
+            for (Collector.MetricFamilySamples metricFamilySamples : samples) {
+                for (Collector.MetricFamilySamples.Sample sample : metricFamilySamples.samples) {
+                    if (sample.name.equals(name)
+                            && sample.labelNames.equals(labelNames)
+                            && sample.labelValues.equals(labelValues)) {
+                        return sample.value;
+                    }
+                }
+            }
+            return 0;
+        }
     }
 
     public void reset() {
         summary.clear();
-    }
-
-    private double getQuantile(double q) {
-        return defaultRegistry.getSampleValue(name, QUANTILE_LABEL, new String[] { Collector.doubleToGoString(q) })
-                .doubleValue();
     }
 
     private static final Logger log = LoggerFactory.getLogger(DimensionStats.class);
