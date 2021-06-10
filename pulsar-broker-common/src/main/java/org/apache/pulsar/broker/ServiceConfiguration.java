8252f9db7f2f0382e19e4e715352ccb48a5c638a
Sijie Guo <sijie@apache.org>
2021-06-10 21:58:35 +0800

    Provide a flag to disable managed ledger metrics (#10885)
    (cherry picked from commit 3de089c3b54b7c35c5f18bcf5437c73a20ff4594)

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
@@ -1945,6 +1945,11 @@ public class ServiceConfiguration implements PulsarConfiguration {
     private boolean exposeProducerLevelMetricsInPrometheus = false;
     @FieldContext(
             category = CATEGORY_METRICS,
+            doc = "If true, export managed ledger metrics (aggregated by namespace)"
+    )
+    private boolean exposeManagedLedgerMetricsInPrometheus = true;
+    @FieldContext(
+            category = CATEGORY_METRICS,
             doc = "If true, export managed cursor metrics"
     )
     private boolean exposeManagedCursorMetricsInPrometheus = false;
