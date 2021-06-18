9c3e904ae56b660ca7b0f7b201effcc884654fd5
Aloys <lofterzhang@gmail.com>
2021-06-19 02:00:48 +0800

    fix parseMessageMetadata error cause by not skip broker entry metadata (#10968)
    Fixes #10967
    
    ### Motivation
    fix parseMessageMetadata error cause by not skip broker entry metadata 
    
    ### Modifications
    
    skip broker entry metadata if exist before parsing message metadata
    
    (cherry picked from commit 0774b5fddddc0c9fe9b7cc00ae40e43322690ef1)

--- pulsar-common/src/main/java/org/apache/pulsar/common/protocol/Commands.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/protocol/Commands.java
@@ -418,6 +418,9 @@ public class Commands {
     }
 
     public static void parseMessageMetadata(ByteBuf buffer, MessageMetadata msgMetadata) {
+        // initially reader-index may point to start of broker entry metadata :
+        // increment reader-index to start_of_headAndPayload to parse metadata
+        skipBrokerEntryMetadataIfExist(buffer);
         // initially reader-index may point to start_of_checksum : increment reader-index to start_of_metadata
         // to parse metadata
         skipChecksumIfPresent(buffer);
@@ -1667,7 +1670,6 @@ public class Commands {
         try {
             // save the reader index and restore after parsing
             int readerIdx = metadataAndPayload.readerIndex();
-            skipBrokerEntryMetadataIfExist(metadataAndPayload);
             MessageMetadata metadata = Commands.parseMessageMetadata(metadataAndPayload);
             metadataAndPayload.readerIndex(readerIdx);
 
@@ -1682,7 +1684,6 @@ public class Commands {
     public static byte[] peekStickyKey(ByteBuf metadataAndPayload, String topic, String subscription) {
         try {
             int readerIdx = metadataAndPayload.readerIndex();
-            skipBrokerEntryMetadataIfExist(metadataAndPayload);
             MessageMetadata metadata = Commands.parseMessageMetadata(metadataAndPayload);
             metadataAndPayload.readerIndex(readerIdx);
             if (metadata.hasOrderingKey()) {
