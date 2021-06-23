c66ba5fd9cd925412d75963eae2ea4a83040c0c0
Roel Rymenants <roelrymenants@users.noreply.github.com>
2021-06-23 15:20:37 +0200

    Added additional test coverage for loading ProducerConfigurationData wrt BatcherBuilderType

---
+++ pulsar-client/src/test/java/org/apache/pulsar/client/impl/conf/ProducerConfigurationDataTest.java
@@ -0,0 +1,67 @@
+package org.apache.pulsar.client.impl.conf;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.pulsar.client.api.BatcherBuilder;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertThrows;
+
+public class ProducerConfigurationDataTest {
+    private static final String PRODUCER_NAME = "test-producer";
+
+    private ProducerConfigurationData confData;
+
+    @BeforeTest
+    public void setUp() {
+        confData = new ProducerConfigurationData();
+        confData.setProducerName(PRODUCER_NAME);
+    }
+
+    @AfterTest
+    public void ensurePreserveOtherProperties() {
+        assertEquals(PRODUCER_NAME, confData.getProducerName());
+    }
+
+    @Test
+    public void testLoadBatcherBuilderDefaultCase() throws JsonProcessingException {
+        assertEquals(BatcherBuilder.DEFAULT, confData.getBatcherBuilder());
+    }
+
+    @Test
+    public void testLoadChangeBatcherBuilderTypeToKeyBased() {
+        confData.setBatcherBuilderType(ProducerConfigurationData.BatcherBuilderType.Default);
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("batcherBuilderType", "Key_Based");
+
+        confData = ConfigurationDataUtils.loadData(config, confData, ProducerConfigurationData.class);
+
+        assertEquals(BatcherBuilder.KEY_BASED, confData.getBatcherBuilder());
+    }
+
+    @Test
+    public void testLoadChangeBatcherBuilderTypeToDefault() {
+        confData.setBatcherBuilderType(ProducerConfigurationData.BatcherBuilderType.Key_Based);
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("batcherBuilderType", "Default");
+
+        confData = ConfigurationDataUtils.loadData(config, confData, ProducerConfigurationData.class);
+
+        assertEquals(BatcherBuilder.DEFAULT, confData.getBatcherBuilder());
+    }
+
+    @Test
+    public void testLoadBatcherBuilderTypeUnknown() throws JsonProcessingException {
+        Map<String, Object> config = new HashMap<>();
+        config.put("batcherBuilderType", "UNKNOWN");
+
+        assertThrows(RuntimeException.class, () -> ConfigurationDataUtils.loadData(config, confData, ProducerConfigurationData.class));
+    }
+}
