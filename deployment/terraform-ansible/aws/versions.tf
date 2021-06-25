a77920f4334bfe4880926d79e235f194a99e39ee
Koen Rutten <13603490+KoenR3@users.noreply.github.com>
2021-06-25 16:57:28 +0200

    Update versions.tf
    Unlock Terraform version

--- deployment/terraform-ansible/aws/versions.tf
+++ deployment/terraform-ansible/aws/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.3.0"
+      version = ">=3.3.0"
     }
     random = {
       source = "hashicorp/random"
