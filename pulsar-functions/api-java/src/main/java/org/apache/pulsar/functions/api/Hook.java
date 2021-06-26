ff26202fd2cf8cfc87d918fc6dd5412979b08598
chenlei <chenlei@cmss.chinamobile.com>
2021-06-26 20:39:01 +0800

    pulsar-function:Preload and release of external resources

---
+++ pulsar-functions/api-java/src/main/java/org/apache/pulsar/functions/api/Hook.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.functions.api;
+
+import org.apache.pulsar.common.classification.InterfaceAudience;
+
+/**
+ * An interface for hook
+ * Preload and release of external resources
+ */
+@InterfaceAudience.Public
+public interface Hook {
+
+    /**
+     * Pre-Process Function Hook
+     *
+     * @throws Exception
+     */
+    void preProcess(Context context) throws RuntimeException;
+
+
+    /**
+     * Post-Process Function Hook
+     *
+     * @throws Exception
+     */
+    void postProcess() throws RuntimeException;
+}
--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstance.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstance.java
@@ -32,6 +32,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.functions.api.Function;
+import org.apache.pulsar.functions.api.Hook;
 import org.apache.pulsar.functions.api.Record;
 
 /**
@@ -74,6 +75,24 @@ public class JavaInstance implements AutoCloseable {
         }
     }
 
+    public void setup() {
+        if (null != function && function instanceof Hook) {
+            try {
+                ((Hook) function).preProcess(context);
+            } catch (RuntimeException e) {
+                log.error("setup error:", e);
+                throw new RuntimeException("function preProcess occurred exception", e);
+            }
+        }
+        if (null != javaUtilFunction && javaUtilFunction instanceof Hook) {
+            try {
+                ((Hook) javaUtilFunction).preProcess(context);
+            } catch (RuntimeException e) {
+                log.error("setup error:", e);
+                throw new RuntimeException("javaUtilFunction preProcess occurred exception", e);
+            }
+        }
+    }
     @VisibleForTesting
     public JavaExecutionResult handleMessage(Record<?> record, Object input) {
         return handleMessage(record, input, (rec, result) -> {}, cause -> {});
@@ -160,7 +179,22 @@ public class JavaInstance implements AutoCloseable {
     @Override
     public void close() {
         context.close();
-        executor.shutdown();
+        if (null != function && function instanceof Hook) {
+            try {
+                ((Hook) function).postProcess();
+            } catch (RuntimeException e) {
+                log.error("function postProcess occurred exception", e);
+                throw new RuntimeException("function postProcess occurred exception", e);
+            }
+        }
+        if (null != javaUtilFunction && javaUtilFunction instanceof Hook) {
+            try {
+                ((Hook) javaUtilFunction).postProcess();
+            } catch (RuntimeException e) {
+                log.error("function postProcess occurred exception", e);
+                throw new RuntimeException("function postProcess occurred exception", e);
+            }
+        }
     }
 
     public Map<String, Double> getAndResetMetrics() {
