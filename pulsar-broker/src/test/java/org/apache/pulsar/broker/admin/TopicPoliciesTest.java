d816c8e54d61521b64ad8ce1b0103067f93ae06a
chenhang <chenhang@apache.org>
2021-06-28 23:40:02 +0800

    format code

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/TopicPoliciesTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/TopicPoliciesTest.java
@@ -77,7 +77,6 @@ import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
-import org.testng.util.TimeUtils;
 
 @Slf4j
 @Test(groups = "broker")
