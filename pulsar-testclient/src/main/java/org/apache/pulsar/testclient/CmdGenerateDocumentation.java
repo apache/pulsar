ad4acd7867afe6339bd883b8a9a8b8b97363f8ae
LiLi <urfreespace@gmail.com>
2021-07-01 19:38:40 +0800

    fix: pulsar-client and pulsar-perf cli doc command flag duplicated
    Signed-off-by: LiLi <urfreespace@gmail.com>

--- pulsar-testclient/src/main/java/org/apache/pulsar/testclient/CmdGenerateDocumentation.java
+++ pulsar-testclient/src/main/java/org/apache/pulsar/testclient/CmdGenerateDocumentation.java
@@ -109,17 +109,12 @@ public class CmdGenerateDocumentation {
         }
         sb.append("|Flag|Description|Default|\n");
         sb.append("|---|---|---|\n");
-        // IKey is an internal interface and cannot be accessed directly,
-        // so the type needs to be erased and force cast to a subclass
-        Map descriptionMap = cmd.getDescriptions();
-        descriptionMap.forEach((k, v) -> {
-            StringKey key = (StringKey) k;
-            ParameterDescription description = (ParameterDescription) v;
-            sb.append("| `").append(key.getName())
-                    .append("` | ").append(description.getDescription().replace("\n", " "))
-                    .append("|").append(description.getDefault()).append("|\n");
-
-        });
+        List<ParameterDescription> options = cmd.getParameters();
+        options.forEach((option) ->
+                sb.append("| `").append(option.getNames())
+                        .append("` | ").append(option.getDescription().replace("\n", " "))
+                        .append("|").append(option.getDefault()).append("|\n")
+        );
         System.out.println(sb.toString());
         return sb.toString();
     }
