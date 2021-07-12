c982284e2a096adee2c002c8d0ef78600a095f96
Anonymitaet <anonymitaet_hotmail.com>
2021-07-12 12:06:34 +0800

    [Doc + Website] Support Google Analytics

--- site2/website/brodocs/brodoc.js
+++ site2/website/brodocs/brodoc.js
@@ -182,6 +182,13 @@ function generateDoc(navContent, bodyContent, codeTabContent) {
 `<!DOCTYPE html>
 <html>
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NRV2PFT');</script>
+<!-- End Google Tag Manager -->
 <meta charset="UTF-8">
 <title>${config.title}</title>
 <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
@@ -192,6 +199,10 @@ function generateDoc(navContent, bodyContent, codeTabContent) {
 <link rel="stylesheet" href="stylesheet.css" type="text/css">
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRV2PFT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="sidebar-wrapper" class="side-nav side-bar-nav">${navContent}<br/><div class="copyright">${config.copyright}</div></div>
 <div id="wrapper">
 <div id="code-tabs-wrapper" class="code-tabs"><ul class="code-tab-list">${codeTabContent}</ul></div>
