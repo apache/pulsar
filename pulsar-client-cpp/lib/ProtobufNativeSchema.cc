c798eaf45db3bebb283a5e58121d1fb6583d0fe9
Yunze Xu <xyzinfernity@163.com>
2021-07-30 08:55:58 +0800

    [C++] Add padding characters to base64 encoded protobuf native schema (#11492)
    ### Motivation
    
    https://github.com/apache/pulsar/pull/11388 added support for protobuf native schema in C++ client. The implementation serializes a protobuf generated class' descriptor to a Base64 encoded string. However, it doesn't add the padding characters while the broker side requires the padding characters. Therefore, if the Base64 encoded string needs the padding characters, the broker will fail to deserialize:
    
    > com.fasterxml.jackson.databind.exc.InvalidFormatException: Cannot deserialize value of type `byte[]` from String "": Unexpected end of base64-encoded String: base64 variant 'MIME-NO-LINEFEEDS' expects padding (one or more '=' characters) at the end. This Base64Variant might have been incorrectly configured
    
    See https://en.wikipedia.org/wiki/Base64#Decoding_Base64_with_padding for the Base64 with padding.
    
    ### Modifications
    
    - Add the padding `=` characters if the Base64 encoded string's length is not a multiple of four.
    - Add a `PaddingDemo.proto` to test the above case to ensure that broker can validate the schema string.

--- pulsar-client-cpp/lib/ProtobufNativeSchema.cc
+++ pulsar-client-cpp/lib/ProtobufNativeSchema.cc
@@ -51,8 +51,15 @@ SchemaInfo createProtobufNativeSchema(const google::protobuf::Descriptor* descri
     std::vector<char> bytes(fileDescriptorSet.ByteSizeLong());
     fileDescriptorSet.SerializeToArray(bytes.data(), bytes.size());
 
+    std::string base64String{base64(bytes.data()), base64(bytes.data() + bytes.size())};
+    // Pulsar broker only supports decoding Base64 with padding so we need to add padding '=' here
+    const size_t numPadding = 4 - base64String.size() % 4;
+    for (size_t i = 0; i < numPadding; i++) {
+        base64String.push_back('=');
+    }
+
     const std::string schemaJson =
-        R"({"fileDescriptorSet":")" + std::string(base64(bytes.data()), base64(bytes.data() + bytes.size())) +
+        R"({"fileDescriptorSet":")" + base64String +
         R"(","rootMessageTypeName":")" + rootMessageTypeName +
         R"(","rootFileDescriptorName":")" + rootFileDescriptorName + R"("})";
 
