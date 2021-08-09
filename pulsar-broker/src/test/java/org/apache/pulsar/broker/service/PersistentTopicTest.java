a50fe878773a76ca974d1af6ea8b994a5df7f81a
congbo <39078850+congbobo184@users.noreply.github.com>
2021-08-09 09:49:13 +0800

    [Transaction] Fix delete sub then delete pending ack. (#11023)
    Fix delete sub then delete pending ack managedledger.

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/PersistentTopicTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/PersistentTopicTest.java
@@ -183,6 +183,12 @@ public class PersistentTopicTest extends MockedBookKeeperTestCase {
         doReturn(mlFactoryMock).when(pulsar).getManagedLedgerFactory();
         doReturn(mock(PulsarClientImpl.class)).when(pulsar).getClient();
 
+        doAnswer(invocation -> {
+            DeleteLedgerCallback deleteLedgerCallback = invocation.getArgument(1);
+            deleteLedgerCallback.deleteLedgerComplete(null);
+            return null;
+        }).when(mlFactoryMock).asyncDelete(any(), any(), any());
+
         ZooKeeper mockZk = createMockZooKeeper();
         doReturn(mockZk).when(pulsar).getZkClient();
         doReturn(createMockBookKeeper(executor))
