b507cf09c4d664b3ee5627a7913f7600faa97744
LiLi <urfreespace@gmail.com>
2021-08-20 09:46:48 +0800

    feat: partial docs migration
    Signed-off-by: LiLi <urfreespace@gmail.com>

--- site2/website-next/docusaurus.config.js
+++ site2/website-next/docusaurus.config.js
@@ -185,7 +185,7 @@ module.exports = {
       "@docusaurus/preset-classic",
       {
         docs: {
-          path: "../docs",
+          // path: "../docs",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
