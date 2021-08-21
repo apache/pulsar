c0ef593990ce8a7ea9ee6f1def880f71def3fc97
GuoJiwei <technoboy@apache.org>
2021-08-21 22:58:20 +0800

    Add compacted topic metrics for TopicStats in CLI (#11564)
    ### Motivation
    Add below metrics to help track potential flows or examine the overall condition of compacted topics .
    - lastCompactionRemovedEventCount : the removed event count of last compaction
    - lastCompactionSucceedTimestamp : the timestamp of last succeed compaction
    - lastCompactionFailedTimestamp : the timestamp of last failed compaction
    - lastCompactionDurationTimeInMills: the duration time of last compaction
    
    These 4 metrics will be displayed in topic stats CLI :
    ```
    ./pulsar-admin topics stats persistent://tenant/ns/topic
    ```
    
    ### Documentation
    This patch will add metrics in CLI , which would generate doc automatically.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -1324,12 +1324,16 @@ public class PulsarService implements AutoCloseable {
     // only public so mockito can mock it
     public Compactor newCompactor() throws PulsarServerException {
         return new TwoPhaseCompactor(this.getConfiguration(),
-                                     getClient(), getBookKeeperClient(),
-                                     getCompactorExecutor());
+                getClient(), getBookKeeperClient(),
+                getCompactorExecutor());
     }
 
     public synchronized Compactor getCompactor() throws PulsarServerException {
-        if (this.compactor == null) {
+        return getCompactor(true);
+    }
+
+    public synchronized Compactor getCompactor(boolean shouldInitialize) throws PulsarServerException {
+        if (this.compactor == null && shouldInitialize) {
             this.compactor = newCompactor();
         }
         return this.compactor;
@@ -1338,8 +1342,8 @@ public class PulsarService implements AutoCloseable {
     protected synchronized OrderedScheduler getOffloaderScheduler(OffloadPoliciesImpl offloadPolicies) {
         if (this.offloaderScheduler == null) {
             this.offloaderScheduler = OrderedScheduler.newSchedulerBuilder()
-                .numThreads(offloadPolicies.getManagedLedgerOffloadMaxThreads())
-                .name("offloader").build();
+                    .numThreads(offloadPolicies.getManagedLedgerOffloadMaxThreads())
+                    .name("offloader").build();
         }
         return this.offloaderScheduler;
     }
