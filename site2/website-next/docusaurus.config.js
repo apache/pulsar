1a0aff3614919ffec69eab5700a92bc7691d9322
LiLi <urfreespace@gmail.com>
2021-08-24 18:28:20 +0800

    chore: remove migration files for initial preview
    Signed-off-by: LiLi <urfreespace@gmail.com>

--- site2/website-next/docusaurus.config.js
+++ site2/website-next/docusaurus.config.js
@@ -1,174 +1,78 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-const linkifyRegex = require("./remark-linkify-regex");
-
-const url = "https://pulsar.incubator.apache.org";
-const javadocUrl = url + "/api";
-const restApiUrl = url + "/admin-rest-api";
-const functionsApiUrl = url + "/functions-rest-api";
-const sourceApiUrl = url + "/source-rest-api";
-const sinkApiUrl = url + "/sink-rest-api";
-const packagesApiUrl = url + "/packages-rest-api";
-const githubUrl = "https://github.com/apache/incubator-pulsar";
-const baseUrl = "/";
-
-const injectLinkParse = ([, prefix, , name, path]) => {
-  if (prefix == "javadoc") {
-    return {
-      link: javadocUrl + path,
-      text: name,
-    };
-  } else if (prefix == "github") {
-    return {
-      link: githubUrl + "/tree/master/" + path,
-      text: name,
-    };
-  } else if (prefix == "rest") {
-    return {
-      link: restApiUrl + "#" + path,
-      text: name,
-    };
-  } else if (prefix == "functions") {
-    return {
-      link: functionsApiUrl + "#" + path,
-      text: name,
-    };
-  } else if (prefix == "source") {
-    return {
-      link: sourceApiUrl + "#" + path,
-      text: name,
-    };
-  } else if (prefix == "sink") {
-    return {
-      link: sinkApiUrl + "#" + path,
-      text: name,
-    };
-  } else if (prefix == "packages") {
-    return {
-      link: packagesApiUrl + "#" + path,
-      text: name,
-    };
-  }
-
-  return {
-    link: path,
-    text: name,
-  };
-};
-
-const injectLinkParseForEndpoint = ([, info]) => {
-  const [method, path, suffix] = info.split("|");
-
-  const restPath = path.split("/");
-  const restApiVersion = restPath[2];
-  const restApiType = restPath[3];
-  let restBaseUrl = restApiUrl;
-  if (restApiType == "functions") {
-    restBaseUrl = functionsApiUrl;
-  } else if (restApiType == "source") {
-    restBaseUrl = sourceApiUrl;
-  } else if (restApiType == "sink") {
-    restBaseUrl = sinkApiUrl;
-  }
-  let restUrl = "";
-  if (suffix.indexOf("?version") >= 0) {
-    restUrl = suffix + "&apiVersion=" + restApiVersion;
-  } else {
-    restUrl = suffix + "version=master&apiVersion=" + restApiVersion;
-  }
-  return {
-    text: method + " " + path,
-    link: restBaseUrl + "#" + restUrl,
-  };
-};
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "My Site",
+      title: 'My Site',
       logo: {
-        alt: "My Site Logo",
-        src: "img/logo.svg",
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
       },
       items: [
         {
-          type: "doc",
-          docId: "intro",
-          position: "left",
-          label: "Tutorial",
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
         },
-        { to: "/blog", label: "Blog", position: "left" },
+        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right",
-        },
-        {
-          label: "Version",
-          to: "docs",
-          position: "right",
-          items: [
-            {
-              label: "1.1.1",
-              to: "docs/",
-            },
-            {
-              label: "1.1.0",
-              to: "docs/1.1.0/",
-            },
-          ],
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "Tutorial",
-              to: "/docs/intro",
+              label: 'Tutorial',
+              to: '/docs/intro',
             },
           ],
         },
         {
-          title: "Community",
+          title: 'Community',
           items: [
             {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
             },
             {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
             },
             {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "Blog",
-              to: "/blog",
+              label: 'Blog',
+              to: '/blog',
             },
             {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
             },
           ],
         },
@@ -182,33 +86,22 @@ module.exports = {
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          // path: "../docs",
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
-          remarkPlugins: [
-            linkifyRegex(
-              /{\@inject\:\s?(((?!endpoint)[^}])+):([^}]+):([^}]+)}/,
-              injectLinkParse
-            ),
-            linkifyRegex(
-              /{\@inject\:\s?endpoint\|([^}]+)}/,
-              injectLinkParseForEndpoint
-            ),
-          ],
+            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
--- site2/website-next/versioned_docs/version-1.1.0/intro.md
+++
@@ -1,6 +0,0 @@
----
-sidebar_position: 1
-slug: /
----
-
-1.1.0
