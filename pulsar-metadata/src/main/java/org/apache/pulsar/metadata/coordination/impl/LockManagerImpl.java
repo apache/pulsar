bc49191d1816bedf8cb6a8ba156b8593b921b27a
Matteo Merli <mmerli@apache.org>
2021-09-01 14:47:47 -0700

    Fixed ZKSessionTest.testReacquireLocksAfterSessionLost (#11886)

--- pulsar-metadata/src/main/java/org/apache/pulsar/metadata/coordination/impl/LockManagerImpl.java
+++ pulsar-metadata/src/main/java/org/apache/pulsar/metadata/coordination/impl/LockManagerImpl.java
@@ -107,6 +107,9 @@ class LockManagerImpl<T> implements LockManager<T> {
         if (se == SessionEvent.SessionReestablished) {
             log.info("Metadata store session has been re-established. Revalidating all the existing locks.");
             locks.values().forEach(ResourceLockImpl::revalidate);
+        } else if (se == SessionEvent.Reconnected) {
+            log.info("Metadata store connection has been re-established. Revalidating locks that were pending.");
+            locks.values().forEach(ResourceLockImpl::revalidateIfNeededAfterReconnection);
         }
     }
 
