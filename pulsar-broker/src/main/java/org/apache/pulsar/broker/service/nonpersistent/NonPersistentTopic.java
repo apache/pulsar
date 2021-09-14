524b1030fe16d26290d4c15cb16890c3ad3d7267
Shoothzj <shoothzj@gmail.com>
2021-09-15 00:46:59 +0800

    Fix log placeholders not correct (#12014)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/nonpersistent/NonPersistentTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/nonpersistent/NonPersistentTopic.java
@@ -146,7 +146,7 @@ public class NonPersistentTopic extends AbstractTopic implements Topic {
                 .getAsync(AdminResource.path(POLICIES, TopicName.get(topic).getNamespace()))
                 .thenAccept(optPolicies -> {
                     if (!optPolicies.isPresent()) {
-                        log.warn("[{}] Policies not present {} and isEncryptionRequired will be set to false", topic);
+                        log.warn("[{}] Policies not present and isEncryptionRequired will be set to false", topic);
                         isEncryptionRequired = false;
                     } else {
                         Policies policies = optPolicies.get();
