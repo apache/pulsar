f59aadea8864963c09f87191bae2c4e50d9c7069
penghui <penghui@apache.org>
2021-09-27 23:17:15 +0800

    Fix returned wrong hash ranges for the consumer with same consumer name
    Currently, we are using the consumer name to generate the hash ranges to the admin client.
    If there are consumers with the same name, we will get same hash ranges for different consumers,
    this will confuse when troubleshooting issue. The following is an example:
    
    ```
    "consumers" : [ {
            "msgRateOut" : 0.0,
            "msgThroughputOut" : 0.0,
            "bytesOutCounter" : 46320,
            "msgOutCounter" : 1020,
            "msgRateRedeliver" : 0.0,
            "chunkedMessageRate" : 0.0,
            "consumerName" : "5253f",
            "availablePermits" : -20,
            "unackedMessages" : 1000,
            "avgMessagesPerEntry" : 56,
            "blockedConsumerOnUnackedMsgs" : false,
            "readPositionWhenJoining" : "10:11494",
            "lastAckedTimestamp" : 1632731049993,
            "lastConsumedTimestamp" : 1632731030268,
            "keyHashRanges" : [ "[0, 16384]" ],
            "metadata" : { },
            "address" : "/127.0.0.1:54702",
            "connectedSince" : "2021-09-27T16:23:49.891+08:00",
            "clientVersion" : "2.8.1"
          }, {
            "msgRateOut" : 0.0,
            "msgThroughputOut" : 0.0,
            "bytesOutCounter" : 0,
            "msgOutCounter" : 0,
            "msgRateRedeliver" : 0.0,
            "chunkedMessageRate" : 0.0,
            "consumerName" : "my-name",
            "availablePermits" : 10,
            "unackedMessages" : 0,
            "avgMessagesPerEntry" : 1000,
            "blockedConsumerOnUnackedMsgs" : false,
            "readPositionWhenJoining" : "10:19505",
            "lastAckedTimestamp" : 0,
            "lastConsumedTimestamp" : 0,
            "keyHashRanges" : [ "[16385, 40960]", "[40961, 65536]" ],
            "metadata" : { },
            "address" : "/127.0.0.1:54708",
            "connectedSince" : "2021-09-27T16:23:59.031+08:00",
            "clientVersion" : "2.8.1"
          }, {
            "msgRateOut" : 0.0,
            "msgThroughputOut" : 0.0,
            "bytesOutCounter" : 0,
            "msgOutCounter" : 0,
            "msgRateRedeliver" : 0.0,
            "chunkedMessageRate" : 0.0,
            "consumerName" : "my-name",
            "availablePermits" : 10,
            "unackedMessages" : 0,
            "avgMessagesPerEntry" : 1000,
            "blockedConsumerOnUnackedMsgs" : false,
            "readPositionWhenJoining" : "10:19514",
            "lastAckedTimestamp" : 0,
            "lastConsumedTimestamp" : 0,
            "keyHashRanges" : [ "[16385, 40960]", "[40961, 65536]" ],
            "metadata" : { },
            "address" : "/127.0.0.1:54717",
            "connectedSince" : "2021-09-27T16:24:03.927+08:00",
            "clientVersion" : "2.8.1"
          } ],
    ```
    
    The fix is to use the equals method of the consumer to generate the key hash ranges.
    New tests added.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ConsistentHashingStickyKeyConsumerSelector.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ConsistentHashingStickyKeyConsumerSelector.java
@@ -30,6 +30,7 @@ import java.util.TreeMap;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.pulsar.broker.service.BrokerServiceException.ConsumerAssignException;
+import org.apache.pulsar.client.api.Range;
 import org.apache.pulsar.common.util.Murmur3_32Hash;
 
 /**
@@ -126,15 +127,15 @@ public class ConsistentHashingStickyKeyConsumerSelector implements StickyKeyCons
     }
 
     @Override
-    public Map<String, List<String>> getConsumerKeyHashRanges() {
-        Map<String, List<String>> result = new LinkedHashMap<>();
+    public Map<Consumer, List<Range>> getConsumerKeyHashRanges() {
+        Map<Consumer, List<Range>> result = new LinkedHashMap<>();
         rwLock.readLock().lock();
         try {
             int start = 0;
             for (Map.Entry<Integer, List<Consumer>> entry: hashRing.entrySet()) {
                 for (Consumer consumer: entry.getValue()) {
-                    result.computeIfAbsent(consumer.consumerName(), key -> new ArrayList<>())
-                            .add("[" + start + ", " + entry.getKey() + "]");
+                    result.computeIfAbsent(consumer, key -> new ArrayList<>())
+                            .add(Range.of(start, entry.getKey()));
                 }
                 start = entry.getKey() + 1;
             }
--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/HashRangeAutoSplitStickyKeyConsumerSelector.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/HashRangeAutoSplitStickyKeyConsumerSelector.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentSkipListMap;
 import org.apache.pulsar.broker.service.BrokerServiceException.ConsumerAssignException;
+import org.apache.pulsar.client.api.Range;
 
 /**
  * This is a consumer selector based fixed hash range.
@@ -112,12 +113,12 @@ public class HashRangeAutoSplitStickyKeyConsumerSelector implements StickyKeyCon
     }
 
     @Override
-    public Map<String, List<String>> getConsumerKeyHashRanges() {
-        Map<String, List<String>> result = new HashMap<>();
+    public Map<Consumer, List<Range>> getConsumerKeyHashRanges() {
+        Map<Consumer, List<Range>> result = new HashMap<>();
         int start = 0;
         for (Map.Entry<Integer, Consumer> entry: rangeMap.entrySet()) {
-            result.computeIfAbsent(entry.getValue().consumerName(), key -> new ArrayList<>())
-                    .add("[" + start + ", " + entry.getKey() + "]");
+            result.computeIfAbsent(entry.getValue(), key -> new ArrayList<>())
+                    .add(Range.of(start, entry.getKey()));
             start = entry.getKey() + 1;
         }
         return result;
