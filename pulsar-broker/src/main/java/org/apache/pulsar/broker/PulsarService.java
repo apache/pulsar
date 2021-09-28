84143cac6c1721e7dda1fdb6a628bffb9008b7b1
penghui <penghui@apache.org>
2021-09-28 19:24:01 +0800

    [Metrics] Add support for splitting topic and partition label in Prometheus
    Fix: #11432
    
    Currently, we are only expose the partition name for the topic label in Prometheus metrics, which is difficult to
    have an aggregated metrics for a partitioned topic.
    
    Before this change, we can only get (topic=xxx-partition-0) in the metrics. After this change, we can get 2 labels (topic=xxx, partition=0).
    By default, the broker expose the single tag for topic. It need to change `splitTopicAndPartitionLabelInPrometheus=true` in the broker.conf
    
    New tests added.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -661,7 +661,8 @@ public class PulsarService implements AutoCloseable, ShutdownService {
             this.metricsServlet = new PrometheusMetricsServlet(
                     this, config.isExposeTopicLevelMetricsInPrometheus(),
                     config.isExposeConsumerLevelMetricsInPrometheus(),
-                    config.isExposeProducerLevelMetricsInPrometheus());
+                    config.isExposeProducerLevelMetricsInPrometheus(),
+                    config.isSplitTopicAndPartitionLabelInPrometheus());
             if (pendingMetricsProviders != null) {
                 pendingMetricsProviders.forEach(provider -> metricsServlet.addRawMetricsProvider(provider));
                 this.pendingMetricsProviders = null;
