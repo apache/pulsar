06b68bb1d5859fd969f66c29888feda271817ec5
Yunze Xu <xyzinfernity@163.com>
2021-10-13 11:05:26 +0800

    [C++] Handle OAuth 2.0 exceptional cases gracefully (#12335)
    Fixes #12324
    
    ### Motivation
    
    Currently if any error happened during OAuth 2.0 authentication in C++ client, a runtime error would be thrown and could only be caught when creating an `AuthOauth` object, but could not be caught in `Client`'s method like `createProducer`. It's not graceful. What's worse, there's no way for Python client that is a wrapper of C++ client to caught this exception.
    
    ### Modifications
    
    When `ClientCredentialFlow::authenticate` returns an invalid `Oauth2TokenResult`, catch the `runtime_error` thrown in `Oauth2CachedToken`'s constructor and returns `ResultAuthenticationError` as `AuthOauth2::getAuthData`'s returned value. Since `getAuthData` always returns `ResultOk` before this PR, the related docs are also modified.
    
    Then when a CONNECT or AUTH_RESPONSE command is created, expose the result of `getAuthData`. If it's not `ResultOk`, close the connection and complete the connection's future with the result. After that, the `Client`'s API will be completed with the result.
    
    In addition, this PR also makes the error code of libcurl human readable by configuring `CURLOPT_ERRORBUFFER`.
    
    ### Verifying this change
    
    - [x] Make sure that the change passes the CI checks.
    
    This change added tests `AuthPluginTest.testOauth2Failure` to verify when OAuth 2.0 authentication failed, the `createProducer` would return `ResultAuthenticationError` without any exception thrown.

--- pulsar-client-cpp/tests/AuthPluginTest.cc
+++ pulsar-client-cpp/tests/AuthPluginTest.cc
@@ -366,9 +366,7 @@ TEST(AuthPluginTest, testOauth2WrongSecret) {
     LOG_INFO("PARAMS: " << params);
     pulsar::AuthenticationPtr auth = pulsar::AuthOauth2::create(params);
     ASSERT_EQ(auth->getAuthMethodName(), "token");
-
-    EXPECT_THROW(auth->getAuthData(data), std::runtime_error)
-        << "Expected fail for wrong secret when to get token from server";
+    ASSERT_EQ(auth->getAuthData(data), ResultAuthenticationError);
 }
 
 TEST(AuthPluginTest, testOauth2CredentialFile) {
@@ -419,4 +417,50 @@ TEST(AuthPluginTest, testOauth2RequestBody) {
 )";
     ClientCredentialFlow flow2(params);
     ASSERT_EQ(flow2.generateJsonBody(), expectedJson);
+}
+
+TEST(AuthPluginTest, testOauth2Failure) {
+    ParamMap params;
+    auto addKeyValue = [&](const std::string& key, const std::string& value) {
+        params[key] = value;
+        LOG_INFO("Configure \"" << key << "\" to \"" << value << "\"");
+    };
+
+    auto createClient = [&]() -> Client {
+        ClientConfiguration conf;
+        conf.setAuth(AuthOauth2::create(params));
+        return {"pulsar://localhost:6650", conf};
+    };
+
+    const std::string topic = "AuthPluginTest-testOauth2Failure";
+    Producer producer;
+
+    // No issuer_url
+    auto client1 = createClient();
+    ASSERT_EQ(client1.createProducer(topic, producer), ResultAuthenticationError);
+    client1.close();
+
+    // Invalid issuer_url
+    addKeyValue("issuer_url", "hello");
+    auto client2 = createClient();
+    ASSERT_EQ(client2.createProducer(topic, producer), ResultAuthenticationError);
+    client2.close();
+
+    addKeyValue("issuer_url", "https://google.com");
+    auto client3 = createClient();
+    ASSERT_EQ(client3.createProducer(topic, producer), ResultAuthenticationError);
+    client3.close();
+
+    // No client id and secret
+    addKeyValue("issuer_url", "https://dev-kt-aa9ne.us.auth0.com");
+    auto client4 = createClient();
+    ASSERT_EQ(client4.createProducer(topic, producer), ResultAuthenticationError);
+    client4.close();
+
+    // Invalid client_id and client_secret
+    addKeyValue("client_id", "my_id");
+    addKeyValue("client_secret", "my-secret");
+    auto client5 = createClient();
+    ASSERT_EQ(client5.createProducer(topic, producer), ResultAuthenticationError);
+    client5.close();
 }
