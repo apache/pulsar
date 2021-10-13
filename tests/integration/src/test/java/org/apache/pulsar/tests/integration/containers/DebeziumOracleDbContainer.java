52b6512dccb149b2e1a527eec2f8ae2bea8d76bf
Andrey Yegorov <8622884+dlg99@users.noreply.github.com>
2021-10-13 08:36:12 -0700

    Fix: flaky oracle tests (#12306)
    * Revert "[Build] Disable PulsarDebeziumOracleSourceTest until the problem is resolved (#12031)"
    
    This reverts commit 7180c9cab5a477df2f12e2c47b5e817c77208a47.
    
    * Added more memory to the oracle connector, disabled web console

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/containers/DebeziumOracleDbContainer.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/containers/DebeziumOracleDbContainer.java
@@ -50,7 +50,8 @@ public class DebeziumOracleDbContainer extends ChaosContainer<DebeziumOracleDbCo
         super.configure();
         this.withNetworkAliases(NAME)
             .withExposedPorts(PORTS)
-            .withEnv("DBCA_TOTAL_MEMORY", "1024")
+            .withEnv("DBCA_TOTAL_MEMORY", "2048")
+            .withEnv("WEB_CONSOLE", "false")
             .withStartupTimeout(Duration.of(300, ChronoUnit.SECONDS))
             .withCreateContainerCmdModifier(createContainerCmd -> {
                 createContainerCmd.withHostName(NAME);
