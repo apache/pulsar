7141b4e63ddcc484adebca0e69d6c2cf0f190ed0
liangyepianzhou <xymeng@streamnative.io>
2021-10-16 11:21:23 +0800

    Fix maxReadPosition with normal publish

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionTest.java
@@ -20,11 +20,14 @@ package org.apache.pulsar.broker.transaction;
 
 import static org.apache.pulsar.transaction.coordinator.impl.MLTransactionLogImpl.TRANSACTION_LOG_PREFIX;
 import com.google.common.collect.Sets;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.bookkeeper.mledger.ManagedLedgerFactory;
@@ -36,6 +39,7 @@ import org.apache.pulsar.broker.service.Topic;
 import org.apache.pulsar.broker.service.persistent.PersistentSubscription;
 import org.apache.pulsar.broker.service.persistent.PersistentTopic;
 import org.apache.pulsar.broker.transaction.buffer.impl.TopicTransactionBuffer;
+import org.apache.pulsar.broker.transaction.buffer.impl.TopicTransactionBufferState;
 import org.apache.pulsar.broker.transaction.pendingack.PendingAckStore;
 import org.apache.pulsar.broker.transaction.buffer.matadata.TransactionBufferSnapshot;
 import org.apache.pulsar.broker.transaction.pendingack.impl.MLPendingAckStore;
@@ -357,5 +361,21 @@ public class TransactionTest extends TransactionTestBase {
         PositionImpl position4 = topicTransactionBuffer.getMaxReadPosition();
         Assert.assertEquals(position4.getLedgerId(), messageId4.getLedgerId());
         Assert.assertEquals(position4.getEntryId(), messageId4.getEntryId());
+
+        //test publishing messages will not change maxReadPosition if the state o TB is Initializing.
+
+        Class<TopicTransactionBufferState> transactionBufferStateClass =
+                (Class<TopicTransactionBufferState>) topicTransactionBuffer.getClass().getSuperclass();
+        Class<TopicTransactionBuffer> topicTransactionBufferClass = TopicTransactionBuffer.class;
+        Field maxReadPositionField = topicTransactionBufferClass.getDeclaredField("maxReadPosition");
+        maxReadPositionField.setAccessible(true);
+        Field field = transactionBufferStateClass.getDeclaredField("state");
+        field.setAccessible(true);
+        field.set(topicTransactionBuffer, TopicTransactionBufferState.State.Initializing);
+        MessageIdImpl messageId5 = (MessageIdImpl) normalProducer.newMessage().value("normal message").send();
+        PositionImpl position5 = (PositionImpl) maxReadPositionField.get(topicTransactionBuffer);
+        Assert.assertEquals(position5.getLedgerId(), messageId4.getLedgerId());
+        Assert.assertEquals(position5.getEntryId(), messageId4.getEntryId());
+
         }
 }
