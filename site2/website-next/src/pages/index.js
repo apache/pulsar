8152da6376d1db5cd025be4d0b7bbcbe554ccc65
LiLi <urfreespace@gmail.com>
2021-10-18 10:28:39 +0800

    patch
    Signed-off-by: LiLi <urfreespace@gmail.com>

--- site2/website-next/src/pages/index.js
+++ site2/website-next/src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 import Svg from "../components/Svg";
-// import Button from "../components/Button";
 import { docUrl, githubUrl } from "../utils/index";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
@@ -18,12 +16,6 @@ function HomepageHeader(props) {
       <div className="flex flex-col items-center container mt-24">
         <Svg src="/img/pulsar.svg" className="h-36" />
         <h3 className="mt-12 font-medium mb-12">{siteConfig.tagline}</h3>
-        {/* <div className={styles.buttons + " mt-6"}>
-          <Button href={docUrl("", props.language)} className="mr-6">
-            READ THE DOCS
-          </Button>
-          <Button href={githubUrl()}>GITHUB</Button>
-        </div> */}
         <Stack spacing={2} direction="row">
           <Button
             variant="contained"
