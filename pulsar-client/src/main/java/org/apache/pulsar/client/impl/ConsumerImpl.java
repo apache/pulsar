3694aa1554e7f408a90eda2ba46eae17b425140a
chenlin <1572139390@qq.com>
2021-10-31 17:14:00 +0800

    Add close method in the class of NegativeAcksTracker (#12469)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -978,6 +978,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
         if (batchReceiveTimeout != null) {
             batchReceiveTimeout.cancel();
         }
+        negativeAcksTracker.close();
         stats.getStatTimeout().ifPresent(Timeout::cancel);
     }
 
