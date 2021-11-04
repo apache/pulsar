60c7418f12d3d07ea9b8b12bdb1092438eca134d
clooker <clooker@163.com>
2021-11-04 12:28:07 +0800

    Fix String should use equals but not ==.

--- managed-ledger/src/test/java/org/apache/bookkeeper/test/BookKeeperClusterTestCase.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/test/BookKeeperClusterTestCase.java
@@ -170,7 +170,7 @@ public abstract class BookKeeperClusterTestCase {
 
         // Create Bookie Servers (B1, B2, B3)
         for (int i = 0; i < numBookies; i++) {
-            if (ledgerPath != "") {
+            if (!"".equals(ledgerPath)) {
                 ServerConfiguration configuration = newServerConfiguration(ledgerPath + "/ledgers");
                 startBookie(configuration, ledgerPath + "/ledgers");
             }else {
@@ -226,9 +226,9 @@ public abstract class BookKeeperClusterTestCase {
             File[] ledgerDirs, String ledgerRootPath) {
         ServerConfiguration conf = new ServerConfiguration(baseConf);
         conf.setBookiePort(port);
-        if (ledgerRootPath != "") {
+        if (!"".equals(ledgerRootPath)) {
             conf.setMetadataServiceUri("zk://" + zkUtil.getZooKeeperConnectString() + ledgerRootPath);
-        }else {
+        } else {
             conf.setZkServers(zkServers);
         }
         conf.setJournalDirName(journalDir.getPath());
