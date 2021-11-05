e8d4754fe02e469dbc56e78b6d7e3c796a52c5ad
liangyepianzhou <xymeng@streamnative.io>
2021-11-05 11:42:17 +0800

    Fix bug

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionTest.java
@@ -20,6 +20,12 @@ package org.apache.pulsar.broker.transaction;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.pulsar.transaction.coordinator.impl.MLTransactionLogImpl.TRANSACTION_LOG_PREFIX;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertEquals;
 import com.google.common.collect.Sets;
 import io.netty.buffer.Unpooled;
 import java.lang.reflect.Field;
@@ -30,14 +36,21 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.mledger.AsyncCallbacks;
+import org.apache.bookkeeper.mledger.ManagedCursor;
 import org.apache.bookkeeper.mledger.ManagedLedgerException;
 import org.apache.bookkeeper.mledger.ManagedLedgerFactory;
+import org.apache.bookkeeper.mledger.impl.ManagedCursorContainer;
 import org.apache.bookkeeper.mledger.impl.ManagedLedgerFactoryImpl;
 import org.apache.bookkeeper.mledger.impl.ManagedLedgerImpl;
+import org.apache.bookkeeper.mledger.impl.PositionImpl;
 import org.apache.pulsar.broker.PulsarService;
 import org.apache.pulsar.broker.service.Topic;
 import org.apache.pulsar.broker.service.persistent.PersistentSubscription;
 import org.apache.pulsar.broker.service.persistent.PersistentTopic;
+import org.apache.pulsar.broker.transaction.buffer.TransactionBuffer;
+import org.apache.pulsar.broker.transaction.buffer.impl.TopicTransactionBuffer;
+import org.apache.pulsar.broker.transaction.buffer.impl.TopicTransactionBufferState;
 import org.apache.pulsar.broker.transaction.pendingack.PendingAckStore;
 import org.apache.pulsar.broker.transaction.buffer.matadata.TransactionBufferSnapshot;
 import org.apache.pulsar.broker.transaction.pendingack.impl.MLPendingAckStore;
@@ -55,6 +68,7 @@ import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.api.SubscriptionType;
 import org.apache.pulsar.client.api.transaction.Transaction;
 import org.apache.pulsar.client.api.transaction.TxnID;
+import org.apache.pulsar.client.impl.MessageIdImpl;
 import org.apache.pulsar.common.events.EventsTopicNames;
 import org.apache.pulsar.common.naming.NamespaceName;
 import org.apache.pulsar.common.naming.TopicDomain;
@@ -77,36 +91,12 @@ import org.testng.annotations.Test;
 @Test(groups = "broker")
 public class TransactionTest extends TransactionTestBase {
 
-    private static final String TENANT = "tnx";
-    private static final String NAMESPACE1 = TENANT + "/ns1";
     private static final int NUM_BROKERS = 1;
     private static final int NUM_PARTITIONS = 1;
 
     @BeforeMethod
     protected void setup() throws Exception {
-        this.setBrokerCount(NUM_BROKERS);
-        this.internalSetup();
-
-        String[] brokerServiceUrlArr = getPulsarServiceList().get(0).getBrokerServiceUrl().split(":");
-        String webServicePort = brokerServiceUrlArr[brokerServiceUrlArr.length - 1];
-        admin.clusters().createCluster(CLUSTER_NAME, ClusterData.builder()
-                .serviceUrl("http://localhost:" + webServicePort).build());
-        admin.tenants().createTenant(TENANT,
-                new TenantInfoImpl(Sets.newHashSet("appid1"), Sets.newHashSet(CLUSTER_NAME)));
-        admin.namespaces().createNamespace(NAMESPACE1);
-
-        admin.tenants().createTenant(NamespaceName.SYSTEM_NAMESPACE.getTenant(),
-                new TenantInfoImpl(Sets.newHashSet("appid1"), Sets.newHashSet(CLUSTER_NAME)));
-        admin.namespaces().createNamespace(NamespaceName.SYSTEM_NAMESPACE.toString());
-        admin.topics().createPartitionedTopic(TopicName.TRANSACTION_COORDINATOR_ASSIGN.toString(), NUM_PARTITIONS);
-        pulsarClient.close();
-        pulsarClient = PulsarClient.builder()
-                .serviceUrl(getPulsarServiceList().get(0).getBrokerServiceUrl())
-                .statsInterval(0, TimeUnit.SECONDS)
-                .enableTransaction(true)
-                .build();
-        // wait tc init success to ready state
-        waitForCoordinatorToBeAvailable(NUM_PARTITIONS);
+       setUpBase(NUM_BROKERS, NUM_PARTITIONS, NAMESPACE1 + "/test", 0);
     }
 
     @Test
@@ -367,5 +357,125 @@ public class TransactionTest extends TransactionTestBase {
             countDownLatch.await();
             return true;
         });
+    }
+
+    @Test
+    public void testMaxReadPositionForNormalPublish() throws Exception{
+        String topic = "persistent://" + NAMESPACE1 + "/NormalPublish";
+        admin.topics().createNonPartitionedTopic(topic);
+        PersistentTopic persistentTopic = (PersistentTopic) getPulsarServiceList().get(0).getBrokerService()
+                  .getTopic(topic, false).get().get();
+
+        TopicTransactionBuffer topicTransactionBuffer = (TopicTransactionBuffer) persistentTopic.getTransactionBuffer();
+        PulsarClient noTxnClient = PulsarClient.builder().enableTransaction(false)
+                .serviceUrl(getPulsarServiceList().get(0).getBrokerServiceUrl()).build();
+
+        //test the state of TransactionBuffer is NoSnapshot
+        //before build Producer by pulsarClient that enables transaction.
+        Producer<String> normalProducer = noTxnClient.newProducer(Schema.STRING)
+                .producerName("testNormalPublish")
+                .topic(topic)
+                .sendTimeout(0, TimeUnit.SECONDS)
+                .create();
+        Awaitility.await().untilAsserted(() -> Assert.assertTrue(topicTransactionBuffer.checkIfNoSnapshot()));
+
+        //test publishing normal messages will change maxReadPosition in the state of NoSnapshot.
+        MessageIdImpl messageId = (MessageIdImpl) normalProducer.newMessage().value("normal message").send();
+        PositionImpl position = topicTransactionBuffer.getMaxReadPosition();
+        Assert.assertEquals(position.getLedgerId(), messageId.getLedgerId());
+        Assert.assertEquals(position.getEntryId(), messageId.getEntryId());
+
+        //test the state of TransactionBuffer is Ready after build Producer by pulsarClient that enables transaction.
+        Producer<String> txnProducer = pulsarClient.newProducer(Schema.STRING)
+                .producerName("testTransactionPublish")
+                .topic(topic)
+                .sendTimeout(0, TimeUnit.SECONDS)
+                .create();
+
+        Awaitility.await().untilAsserted(() ->Assert.assertTrue(topicTransactionBuffer.checkIfReady()));
+        //test publishing txn messages will not change maxReadPosition if don`t commit or abort.
+        Transaction transaction = pulsarClient.newTransaction()
+                .withTransactionTimeout(5, TimeUnit.SECONDS).build().get();
+        MessageIdImpl messageId1 = (MessageIdImpl) txnProducer.newMessage(transaction).value("txn message").send();
+        PositionImpl position1 = topicTransactionBuffer.getMaxReadPosition();
+        Assert.assertEquals(position1.getLedgerId(), messageId.getLedgerId());
+        Assert.assertEquals(position1.getEntryId(), messageId.getEntryId());
+
+        MessageIdImpl messageId2 = (MessageIdImpl) normalProducer.newMessage().value("normal message").send();
+        PositionImpl position2 = topicTransactionBuffer.getMaxReadPosition();
+        Assert.assertEquals(position2.getLedgerId(), messageId.getLedgerId());
+        Assert.assertEquals(position2.getEntryId(), messageId.getEntryId());
+        transaction.commit().get();
+        PositionImpl position3 = topicTransactionBuffer.getMaxReadPosition();
+
+        Assert.assertEquals(position3.getLedgerId(), messageId2.getLedgerId());
+        Assert.assertEquals(position3.getEntryId(), messageId2.getEntryId() + 1);
+
+        //test publishing normal messages will change maxReadPosition if the state of TB
+        //is Ready and ongoingTxns is empty.
+        MessageIdImpl messageId4 = (MessageIdImpl) normalProducer.newMessage().value("normal message").send();
+        PositionImpl position4 = topicTransactionBuffer.getMaxReadPosition();
+        Assert.assertEquals(position4.getLedgerId(), messageId4.getLedgerId());
+        Assert.assertEquals(position4.getEntryId(), messageId4.getEntryId());
+
+        //test publishing normal messages will not change maxReadPosition if the state o TB is Initializing.
+        Class<TopicTransactionBufferState> transactionBufferStateClass =
+                (Class<TopicTransactionBufferState>) topicTransactionBuffer.getClass().getSuperclass();
+        Field field = transactionBufferStateClass.getDeclaredField("state");
+        field.setAccessible(true);
+        Class<TopicTransactionBuffer> topicTransactionBufferClass = TopicTransactionBuffer.class;
+        Field maxReadPositionField = topicTransactionBufferClass.getDeclaredField("maxReadPosition");
+        maxReadPositionField.setAccessible(true);
+        field.set(topicTransactionBuffer, TopicTransactionBufferState.State.Initializing);
+        MessageIdImpl messageId5 = (MessageIdImpl) normalProducer.newMessage().value("normal message").send();
+        PositionImpl position5 = (PositionImpl) maxReadPositionField.get(topicTransactionBuffer);
+        Assert.assertEquals(position5.getLedgerId(), messageId4.getLedgerId());
+        Assert.assertEquals(position5.getEntryId(), messageId4.getEntryId());
+
+        }
+    @Test
+    public void testEndTBRecoveringWhenManagerLedgerDisReadable() throws Exception{
+        String topic = NAMESPACE1 + "/testEndTBRecoveringWhenManagerLedgerDisReadable";
+        admin.topics().createNonPartitionedTopic(topic);
+        @Cleanup
+        Producer<String> producer = pulsarClient.newProducer(Schema.STRING)
+                .producerName("test")
+                .enableBatching(false)
+                .sendTimeout(0, TimeUnit.SECONDS)
+                .topic(topic)
+                .create();
+        PersistentTopic persistentTopic = (PersistentTopic) getPulsarServiceList().get(0).getBrokerService()
+                .getTopic(topic, false).get().get();
+        persistentTopic.getManagedLedger().getConfig().setAutoSkipNonRecoverableData(true);
+
+        ManagedCursor managedCursor = mock(ManagedCursor.class);
+        doReturn("transaction-buffer-sub").when(managedCursor).getName();
+        doReturn(true).when(managedCursor).hasMoreEntries();
+        doAnswer(invocation -> {
+            AsyncCallbacks.ReadEntriesCallback callback = invocation.getArgument(1);
+            callback.readEntriesFailed(new ManagedLedgerException.NonRecoverableLedgerException("No ledger exist"),
+                    null);
+            return null;
+        }).when(managedCursor).asyncReadEntries(anyInt(), any(), any(), any());
+        Class<ManagedLedgerImpl> managedLedgerClass = ManagedLedgerImpl.class;
+        Field field = managedLedgerClass.getDeclaredField("cursors");
+        field.setAccessible(true);
+        ManagedCursorContainer managedCursors = (ManagedCursorContainer) field.get(persistentTopic.getManagedLedger());
+        managedCursors.removeCursor("transaction-buffer-sub");
+        managedCursors.add(managedCursor);
+
+        TransactionBuffer buffer1 = new TopicTransactionBuffer(persistentTopic);
+        Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted(() ->
+                assertEquals(buffer1.getStats().state, "Ready"));
+
+        doAnswer(invocation -> {
+            AsyncCallbacks.ReadEntriesCallback callback = invocation.getArgument(1);
+            callback.readEntriesFailed(new ManagedLedgerException.ManagedLedgerFencedException(), null);
+            return null;
+        }).when(managedCursor).asyncReadEntries(anyInt(), any(), any(), any());
+
+        TransactionBuffer buffer2 = new TopicTransactionBuffer(persistentTopic);
+        Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted(() ->
+                assertEquals(buffer2.getStats().state, "Ready"));
     }
 }
