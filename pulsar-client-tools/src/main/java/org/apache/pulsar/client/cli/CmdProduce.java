97c79e20be5e291d6de03daecd2507d96e0b27f9
Masahiro Sakamoto <massakam@yahoo-corp.jp>
2021-11-08 20:19:02 +0900

    Enable CLI to publish non-batched messages (#12641)
    ### Motivation
    
    Currently, messages produced by the `pulsar-client` command are always batched. However, zero queue consumers cannot receive these batched messages. I think it would be useful to be able to easily produce non-batched messages.
    
    ### Modifications
    
    Added an option to disable batching to the `pulsar-client` command:
    ```sh
    $ ./bin/pulsar-client produce -m hello -n 10 --disable-batching persistent://public/default/t1
    ```
    
    (cherry picked from commit a1bad71728d0b39e5b38fdd4ea9a7578629ac975)

--- pulsar-client-tools/src/main/java/org/apache/pulsar/client/cli/CmdProduce.java
+++ pulsar-client-tools/src/main/java/org/apache/pulsar/client/cli/CmdProduce.java
@@ -98,6 +98,9 @@ public class CmdProduce {
                description = "Rate (in msg/sec) at which to produce," +
                        " value 0 means to produce messages as fast as possible.")
     private double publishRate = 0;
+
+    @Parameter(names = { "-db", "--disable-batching" }, description = "Disable batch sending of messages")
+    private boolean disableBatching = false;
     
     @Parameter(names = { "-c",
             "--chunking" }, description = "Should split the message and publish in chunks if message size is larger than allowed max size")
@@ -215,6 +218,8 @@ public class CmdProduce {
             ProducerBuilder<byte[]> producerBuilder = client.newProducer().topic(topic);
             if (this.chunkingAllowed) {
                 producerBuilder.enableChunking(true);
+                producerBuilder.enableBatching(false);
+            } else if (this.disableBatching) {
                 producerBuilder.enableBatching(false);
             }
             if (isNotBlank(this.encKeyName) && isNotBlank(this.encKeyValue)) {
