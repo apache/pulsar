4bcc59d117f761d5da268d829fdb1f796a3ff9a1
Li Li <urfreespace@gmail.com>
2021-12-01 18:48:34 +0800

    [website][upgrade]feat: website upgrade / docs migration - 2.5.2 / get started/concepts/schema (#13009)
    Signed-off-by: LiLi <urfreespace@gmail.com>
    
    Co-authored-by: Anonymitaet <50226895+Anonymitaet@users.noreply.github.com>

--- site2/website-next/docusaurus.config.js
+++ site2/website-next/docusaurus.config.js
@@ -176,6 +176,10 @@ module.exports = {
               to: "docs/2.6.0/",
             },
             {
+              label: "2.5.2",
+              to: "docs/2.5.2/",
+            },
+            {
               label: "2.5.1",
               to: "docs/2.5.1/"
             },
