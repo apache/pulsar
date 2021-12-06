2e34d86d0acdc62546b32c01891b8237c8a0bafd
Yan <yanzhang@streamnative.io>
2021-12-06 10:30:07 +0800

    commit 2.5.0 chapter Get Started Concepts and Architecture Pulsar Schema (#13097)

--- site2/website-next/docusaurus.config.js
+++ site2/website-next/docusaurus.config.js
@@ -184,6 +184,10 @@ module.exports = {
               to: "docs/2.5.1/"
             },
             {
+              label: "2.5.0",
+              to: "docs/2.5.0/"
+            },
+            {
               label: "2.2.0",
               to: "docs/2.2.0/",
             },
