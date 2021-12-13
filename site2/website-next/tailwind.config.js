b8c849f570b9395f0130facafd691e4348007675
LiLi <urfreespace@gmail.com>
2021-12-13 12:11:19 +0800

    fix: patch
    Signed-off-by: LiLi <urfreespace@gmail.com>

--- site2/website-next/tailwind.config.js
+++ site2/website-next/tailwind.config.js
@@ -1,5 +1,4 @@
 const plugin = require("tailwindcss/plugin");
-const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx"],
