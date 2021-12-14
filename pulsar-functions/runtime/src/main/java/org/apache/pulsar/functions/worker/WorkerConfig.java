5dab438b0993c4f0cf54cdb1bc26c1b8a2b3802c
Enrico Olivelli <eolivelli@apache.org>
2021-12-14 09:43:22 +0100

    fix build

--- pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/worker/WorkerConfig.java
+++ pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/worker/WorkerConfig.java
@@ -33,6 +33,7 @@ import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
