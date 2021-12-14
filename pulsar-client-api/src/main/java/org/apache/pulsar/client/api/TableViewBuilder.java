15f5613b05f204b5ddb5565d03c300b1161b2703
Neng Lu <nlu@streamnative.io>
2021-12-14 15:19:12 -0800

    update doc for TableViewBuilder

--- pulsar-client-api/src/main/java/org/apache/pulsar/client/api/TableViewBuilder.java
+++ pulsar-client-api/src/main/java/org/apache/pulsar/client/api/TableViewBuilder.java
@@ -19,25 +19,78 @@
 
 package org.apache.pulsar.client.api;
 
+import org.apache.pulsar.common.classification.InterfaceAudience;
+import org.apache.pulsar.common.classification.InterfaceStability;
+
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * {@link TableViewBuilder} is used to configure and create instances of {@link TableView}.
+ *
+ * @see PulsarClient#newTableViewBuilder(Schema) ()
+ *
+ * @since 2.10.0
+ */
+@InterfaceAudience.Public
+@InterfaceStability.Evolving
 public interface TableViewBuilder<T> {
 
+    /**
+     * Load the configuration from provided <tt>config</tt> map.
+     *
+     *  <p>Example:
+     *
+     *  <pre>{@code
+     *  Map<String, Object> config = new HashMap<>();
+     *  config.put("topicName", "test-topic");
+     *  config.put("autoUpdatePartitionsSeconds", "300");
+     *
+     *  TableViewBuilder<byte[]> builder = ...;
+     *  builder = builder.loadConf(config);
+     *
+     *  TableView<byte[]> tableView = builder.create();
+     *  }</pre>
+     *
+     * @param config configuration to load
+     * @return the {@link TableViewBuilder} instance
+     */
     TableViewBuilder<T> loadConf(Map<String, Object> config);
 
+    /**
+     * Finalize the creation of the {@link TableView} instance.
+     *
+     * <p>This method will block until the tableView is created successfully or an exception is thrown.
+     *
+     * @return the {@link TableView} instance
+     * @throws PulsarClientException
+     *              if the tableView creation fails
+     */
     TableView<T> create() throws PulsarClientException;
 
+    /**
+     * Finalize the creation of the {@link TableView} instance in asynchronous mode.
+     *
+     *  <p>This method will return a {@link CompletableFuture} that can be used to access the instance when it's ready.
+     *
+     * @return the {@link TableView} instance
+     */
     CompletableFuture<TableView<T>> createAsync();
 
+    /**
+     * Set the topic name of the {@link TableView}
+     *
+     * @param topic the name of the topic to create the {@link TableView}
+     * @return the {@link TableViewBuilder} builder instance
+     */
     TableViewBuilder<T> topic(String topic);
 
     /**
      * Set the interval of updating partitions <i>(default: 1 minute)</i>
      * @param interval the interval of updating partitions
      * @param unit the time unit of the interval
-     * @return the consumer builder instance
+     * @return the {@link TableViewBuilder} builder instance
      */
     TableViewBuilder<T> autoUpdatePartitionsInterval(int interval, TimeUnit unit);
 }
