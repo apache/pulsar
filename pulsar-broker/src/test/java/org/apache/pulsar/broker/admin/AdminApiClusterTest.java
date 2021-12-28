apache/pulsar

commit 89841aaf82b16ac4dfe7b7839211b3b38d6320a4

Refactored PulsarResources to avoid building metadata paths everywhere (#11693)
* Refactored PulsarResources to avoid building metadata paths everywhere

* Fixed usages of ZkAdminPaths

* Removed unused import

* Fixed proxy

* Fixed ResourceGroupServiceTest

* Added MetadataStore deleteRecursive operation

* Fixed merge issues

* Use constant

* Removed unused import

* Fixes for ResourceGroupConfigListenerTest

* Fixed cluster resources cleanup

* Fixed topic list rest call

* Fixed NPE in PersistentDispatcherFailoverConsumerTest setup

* Fixed flaky test ReplicatorTest.testRemoveClusterFromNamespace

* Fixed handling notification for V1 path namespaces

* Fixed LocalPoliciesResources create

* Fixed deadlock when making blocking calls from MetadataStore callback thread

* Fixed usage of NamespaceName as key

* Fixed missing mock in BacklogQuotaManagerConfigurationTest

* Fixed RGUsageMTAggrWaitForAllMesgsTest

* fixed ServerCnxTest

* Fixed mock in PersistentSubscriptionTest

* Fixed PersistentTopicTest

* Fixed getting partitions list from NamespaceService

* Fixed tenant not found expectation

* Fixed ProxyConnection to check for existence of auth_data field

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/resources/ClusterResources.java
@@ -18,17 +18,25 @@
  */
 package org.apache.pulsar.broker.resources;
 
+import static org.apache.pulsar.broker.cache.ConfigurationCacheService.POLICIES;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import lombok.Getter;
+import org.apache.pulsar.broker.ServiceConfiguration;
+import org.apache.pulsar.broker.cache.ConfigurationCacheService;
+import org.apache.pulsar.common.naming.Metadata;
 import org.apache.pulsar.common.policies.data.ClusterData;
+import org.apache.pulsar.common.policies.data.FailureDomain;
 import org.apache.pulsar.common.policies.data.FailureDomainImpl;
 import org.apache.pulsar.metadata.api.MetadataStore;
 import org.apache.pulsar.metadata.api.MetadataStoreException;
 
 public class ClusterResources extends BaseResources<ClusterData> {
 
-    public static final String CLUSTERS_ROOT = "/admin/clusters";
     @Getter
     private FailureDomainResources failureDomainResources;
 
@@ -38,7 +46,55 @@ public class ClusterResources extends BaseResources<ClusterData> {
     }
 
     public Set<String> list() throws MetadataStoreException {
-        return new HashSet<>(super.getChildren(CLUSTERS_ROOT));
+        return new HashSet<>(super.getChildren(BASE_CLUSTERS_PATH));
+    }
+
+    public Optional<ClusterData> getCluster(String clusterName) throws MetadataStoreException {
+        return get(joinPath(BASE_CLUSTERS_PATH, clusterName));
+    }
+
+    public List<String> getNamespacesForCluster(String tenant, String clusterName) throws MetadataStoreException {
+        return getChildren(joinPath(BASE_POLICIES_PATH, tenant, clusterName));
+    }
+
+    public void createCluster(String clusterName, ClusterData clusterData) throws MetadataStoreException {
+        create(joinPath(BASE_CLUSTERS_PATH, clusterName), clusterData);
+    }
+
+    public void updateCluster(String clusterName, Function<ClusterData, ClusterData> modifyFunction)
+            throws MetadataStoreException {
+        set(joinPath(BASE_CLUSTERS_PATH, clusterName), modifyFunction);
+    }
+
+    public void deleteCluster(String clusterName) throws MetadataStoreException {
+        delete(joinPath(BASE_CLUSTERS_PATH, clusterName));
+    }
+
+    public boolean isClusterUsed(String clusterName) throws MetadataStoreException {
+        for (String tenant : getCache().getChildren(BASE_POLICIES_PATH).join()) {
+            if (!getCache().getChildren(joinPath(BASE_POLICIES_PATH, tenant, clusterName)).join().isEmpty()) {
+                // We found a tenant that has at least a namespace in this cluster
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean clusterExists(String clusterName) throws MetadataStoreException {
+        return exists(joinPath(BASE_CLUSTERS_PATH, clusterName));
+    }
+
+    public CompletableFuture<Boolean> clusterExistsAsync(String clusterName) {
+        return getCache().exists(joinPath(BASE_CLUSTERS_PATH, clusterName));
+    }
+
+    public static boolean pathRepresentsClusterName(String path) {
+        return path.startsWith(BASE_CLUSTERS_PATH);
+    }
+
+    public static String clusterNameFromPath(String path) {
+        return path.substring(BASE_CLUSTERS_PATH.length() + 1);
     }
 
     public static class FailureDomainResources extends BaseResources<FailureDomainImpl> {
@@ -47,6 +103,39 @@ public class ClusterResources extends BaseResources<ClusterData> {
         public FailureDomainResources(MetadataStore store, Class<FailureDomainImpl> clazz,
                 int operationTimeoutSec) {
             super(store, clazz, operationTimeoutSec);
+        }
+
+        public List<String> listFailureDomains(String clusterName) throws MetadataStoreException {
+            return getChildren(joinPath(BASE_CLUSTERS_PATH, clusterName, FAILURE_DOMAIN));
+        }
+
+        public Optional<FailureDomainImpl> getFailureDomain(String clusterName, String domainName)
+                throws MetadataStoreException {
+            return get(joinPath(BASE_CLUSTERS_PATH, clusterName, FAILURE_DOMAIN, domainName));
+        }
+
+        public void deleteFailureDomain(String clusterName, String domainName) throws MetadataStoreException {
+            String path = joinPath(BASE_CLUSTERS_PATH, clusterName, FAILURE_DOMAIN, domainName);
+            if (exists(path)) {
+                delete(path);
+            }
+        }
+
+        public void deleteFailureDomains(String clusterName) throws MetadataStoreException {
+            String failureDomainPath = joinPath(BASE_CLUSTERS_PATH, clusterName, FAILURE_DOMAIN);
+            for (String domain : getChildren(failureDomainPath)) {
+                delete(joinPath(failureDomainPath, domain));
+            }
+
+            if (exists(failureDomainPath)) {
+                delete(failureDomainPath);
+            }
+        }
+
+        public void setFailureDomainWithCreate(String clusterName, String domainName,
+                                               Function<Optional<FailureDomainImpl>, FailureDomainImpl> createFunction)
+                throws MetadataStoreException {
+            setWithCreate(joinPath(BASE_CLUSTERS_PATH, clusterName, FAILURE_DOMAIN, domainName), createFunction);
         }
     }
 }

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/authorization/PulsarAuthorizationProvider.java
@@ -58,9 +58,7 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
 
     public ServiceConfiguration conf;
     private PulsarResources pulsarResources;
-    private static final String POLICY_ROOT = "/admin/policies/";
-    public static final String POLICIES = "policies";
-    private static final String POLICIES_READONLY_FLAG_PATH = "/admin/flags/policies-readonly";
+
 
     public PulsarAuthorizationProvider() {
     }
@@ -109,7 +107,8 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
             AuthenticationDataSource authenticationData, String subscription) {
         CompletableFuture<Boolean> permissionFuture = new CompletableFuture<>();
         try {
-            pulsarResources.getNamespaceResources().getAsync(POLICY_ROOT + topicName.getNamespace()).thenAccept(policies -> {
+            pulsarResources.getNamespaceResources().getPoliciesAsync(topicName.getNamespaceObject())
+                    .thenAccept(policies -> {
                 if (!policies.isPresent()) {
                     if (log.isDebugEnabled()) {
                         log.debug("Policies node couldn't be found for topic : {}", topicName);
@@ -229,7 +228,7 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
                                                                        AuthAction authAction) {
         CompletableFuture<Boolean> permissionFuture = new CompletableFuture<>();
         try {
-            pulsarResources.getNamespaceResources().getAsync(POLICY_ROOT + namespaceName.toString()).thenAccept(policies -> {
+            pulsarResources.getNamespaceResources().getPoliciesAsync(namespaceName).thenAccept(policies -> {
                 if (!policies.isPresent()) {
                     if (log.isDebugEnabled()) {
                         log.debug("Policies node couldn't be found for namespace : {}", namespaceName);
@@ -285,9 +284,8 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
             result.completeExceptionally(e);
         }
 
-        final String policiesPath = String.format("/%s/%s/%s", "admin", POLICIES, namespaceName.toString());
         try {
-            pulsarResources.getNamespaceResources().set(policiesPath, (policies)->{
+            pulsarResources.getNamespaceResources().setPolicies(namespaceName, policies -> {
                 policies.auth_policies.getNamespaceAuthentication().put(role, actions);
                 return policies;
             });
@@ -300,7 +298,7 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
         } catch (BadVersionException e) {
             log.warn("[{}] Failed to set permissions for namespace {}: concurrent modification", role, namespaceName);
             result.completeExceptionally(new IllegalStateException(
-                    "Concurrent modification on zk path: " + policiesPath + ", " + e.getMessage()));
+                    "Concurrent modification on metadata: " + namespaceName + ", " + e.getMessage()));
         } catch (Exception e) {
             log.error("[{}] Failed to get permissions for namespace {}", role, namespaceName, e);
             result.completeExceptionally(
@@ -332,11 +330,9 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
             result.completeExceptionally(e);
         }
 
-        final String policiesPath = String.format("/%s/%s/%s", "admin", POLICIES, namespace.toString());
-
         try {
-            Policies policies = pulsarResources.getNamespaceResources().get(policiesPath)
-                    .orElseThrow(() -> new NotFoundException(policiesPath + " not found"));
+            Policies policies = pulsarResources.getNamespaceResources().getPolicies(namespace)
+                    .orElseThrow(() -> new NotFoundException(namespace + " not found"));
             if (remove) {
                 if (policies.auth_policies.getSubscriptionAuthentication().get(subscriptionName) != null) {
                     policies.auth_policies.getSubscriptionAuthentication().get(subscriptionName).removeAll(roles);
@@ -348,7 +344,7 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
             } else {
                 policies.auth_policies.getSubscriptionAuthentication().put(subscriptionName, roles);
             }
-            pulsarResources.getNamespaceResources().set(policiesPath, (data)->policies);
+            pulsarResources.getNamespaceResources().setPolicies(namespace, (data)->policies);
 
             log.info("[{}] Successfully granted access for role {} for sub = {}", namespace, subscriptionName, roles);
             result.complete(null);
@@ -358,7 +354,7 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
         } catch (BadVersionException e) {
             log.warn("[{}] Failed to set permissions for {} on namespace {}: concurrent modification", subscriptionName, roles, namespace);
             result.completeExceptionally(new IllegalStateException(
-                    "Concurrent modification on zk path: " + policiesPath + ", " + e.getMessage()));
+                    "Concurrent modification on metadata path: " + namespace + ", " + e.getMessage()));
         } catch (Exception e) {
             log.error("[{}] Failed to get permissions for role {} on namespace {}", subscriptionName, roles, namespace, e);
             result.completeExceptionally(
@@ -388,7 +384,8 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
     public CompletableFuture<Boolean> checkPermission(TopicName topicName, String role, AuthAction action) {
         CompletableFuture<Boolean> permissionFuture = new CompletableFuture<>();
         try {
-            pulsarResources.getNamespaceResources().getAsync(POLICY_ROOT + topicName.getNamespace()).thenAccept(policies -> {
+            pulsarResources.getNamespaceResources().getPoliciesAsync(topicName.getNamespaceObject())
+                    .thenAccept(policies -> {
                 if (!policies.isPresent()) {
                     if (log.isDebugEnabled()) {
                         log.debug("Policies node couldn't be found for topic : {}", topicName);
@@ -497,10 +494,10 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
         boolean arePoliciesReadOnly = true;
 
         try {
-            arePoliciesReadOnly = pulsarResources.getNamespaceResources().exists(POLICIES_READONLY_FLAG_PATH);
+            arePoliciesReadOnly = pulsarResources.getNamespaceResources().getPoliciesReadOnly();
         } catch (Exception e) {
-            log.warn("Unable to fetch contents of [{}] from global zookeeper", POLICIES_READONLY_FLAG_PATH, e);
-            throw new IllegalStateException("Unable to fetch content from global zk");
+            log.warn("Unable to check if policies are read-only", e);
+            throw new IllegalStateException("Unable to fetch content from configuration metadata store");
         }
 
         if (arePoliciesReadOnly) {
@@ -632,7 +629,7 @@ public class PulsarAuthorizationProvider implements AuthorizationProvider {
                     } else {
                         try {
                             TenantInfo tenantInfo = pulsarResources.getTenantResources()
-                                    .get(path(POLICIES, tenantName))
+                                    .getTenant(tenantName)
                                     .orElseThrow(() -> new RestException(Response.Status.NOT_FOUND, "Tenant does not exist"));
                             return isTenantAdmin(tenantName, role, tenantInfo, authData);
                         } catch (NotFoundException e) {

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/resources/BaseResources.java
@@ -19,6 +19,10 @@
 package org.apache.pulsar.broker.resources;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.base.Joiner;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -26,9 +30,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.metadata.api.MetadataCache;
 import org.apache.pulsar.metadata.api.MetadataStore;
 import org.apache.pulsar.metadata.api.MetadataStoreException;
+import org.apache.zookeeper.common.PathUtils;
 
 /**
  * Base class for all configuration resources to access configurations from metadata-store.
@@ -36,7 +42,11 @@ import org.apache.pulsar.metadata.api.MetadataStoreException;
  * @param <T>
  *            type of configuration-resources.
  */
+@Slf4j
 public class BaseResources<T> {
+
+    protected static final String BASE_POLICIES_PATH = "/admin/policies";
+    protected static final String BASE_CLUSTERS_PATH = "/admin/clusters";
 
     @Getter
     private final MetadataStore store;
@@ -56,7 +66,7 @@ public class BaseResources<T> {
         this.operationTimeoutSec = operationTimeoutSec;
     }
 
-    public List<String> getChildren(String path) throws MetadataStoreException {
+    protected List<String> getChildren(String path) throws MetadataStoreException {
         try {
             return getChildrenAsync(path).get(operationTimeoutSec, TimeUnit.SECONDS);
         } catch (ExecutionException e) {
@@ -67,11 +77,11 @@ public class BaseResources<T> {
         }
     }
 
-    public CompletableFuture<List<String>> getChildrenAsync(String path) {
+    protected CompletableFuture<List<String>> getChildrenAsync(String path) {
         return cache.getChildren(path);
     }
 
-    public Optional<T> get(String path) throws MetadataStoreException {
+    protected Optional<T> get(String path) throws MetadataStoreException {
         try {
             return getAsync(path).get(operationTimeoutSec, TimeUnit.SECONDS);
         } catch (ExecutionException e) {
@@ -82,11 +92,11 @@ public class BaseResources<T> {
         }
     }
 
-    public CompletableFuture<Optional<T>> getAsync(String path) {
+    protected CompletableFuture<Optional<T>> getAsync(String path) {
         return cache.get(path);
     }
 
-    public void set(String path, Function<T, T> modifyFunction) throws MetadataStoreException {
+    protected void set(String path, Function<T, T> modifyFunction) throws MetadataStoreException {
         try {
             setAsync(path, modifyFunction).get(operationTimeoutSec, TimeUnit.SECONDS);
         } catch (ExecutionException e) {
@@ -97,11 +107,11 @@ public class BaseResources<T> {
         }
     }
 
-    public CompletableFuture<Void> setAsync(String path, Function<T, T> modifyFunction) {
+    protected CompletableFuture<Void> setAsync(String path, Function<T, T> modifyFunction) {
         return cache.readModifyUpdate(path, modifyFunction).thenApply(__ -> null);
     }
 
-    public void setWithCreate(String path, Function<Optional<T>, T> createFunction) throws MetadataStoreException {
+    protected void setWithCreate(String path, Function<Optional<T>, T> createFunction) throws MetadataStoreException {
         try {
             setWithCreateAsync(path, createFunction).get(operationTimeoutSec, TimeUnit.SECONDS);
         } catch (ExecutionException e) {
@@ -112,11 +122,11 @@ public class BaseResources<T> {
         }
     }
 
-    public CompletableFuture<Void> setWithCreateAsync(String path, Function<Optional<T>, T> createFunction) {
+    protected CompletableFuture<Void> setWithCreateAsync(String path, Function<Optional<T>, T> createFunction) {
         return cache.readModifyUpdateOrCreate(path, createFunction).thenApply(__ -> null);
     }
 
-    public void create(String path, T data) throws MetadataStoreException {
+    protected void create(String path, T data) throws MetadataStoreException {
         try {
             createAsync(path, data).get(operationTimeoutSec, TimeUnit.SECONDS);
         } catch (ExecutionException e) {
@@ -127,11 +137,11 @@ public class BaseResources<T> {
         }
     }
 
-    public CompletableFuture<Void> createAsync(String path, T data) {
+    protected CompletableFuture<Void> createAsync(String path, T data) {
         return cache.create(path, data);
     }
 
-    public void delete(String path) throws MetadataStoreException {
+    protected void delete(String path) throws MetadataStoreException {
         try {
             deleteAsync(path).get(operationTimeoutSec, TimeUnit.SECONDS);
         } catch (ExecutionException e) {
@@ -142,13 +152,13 @@ public class BaseResources<T> {
         }
     }
 
-    public CompletableFuture<Void> deleteAsync(String path) {
+    protected CompletableFuture<Void> deleteAsync(String path) {
         return cache.delete(path);
     }
 
-    public boolean exists(String path) throws MetadataStoreException {
+    protected boolean exists(String path) throws MetadataStoreException {
         try {
-            return existsAsync(path).get(operationTimeoutSec, TimeUnit.SECONDS);
+            return cache.exists(path).get(operationTimeoutSec, TimeUnit.SECONDS);
         } catch (ExecutionException e) {
             throw (e.getCause() instanceof MetadataStoreException) ? (MetadataStoreException) e.getCause()
                     : new MetadataStoreException(e.getCause());
@@ -161,7 +171,43 @@ public class BaseResources<T> {
         return operationTimeoutSec;
     }
 
-    public CompletableFuture<Boolean> existsAsync(String path) {
-        return cache.exists(path);
+    protected static String joinPath(String... parts) {
+        StringBuilder sb = new StringBuilder();
+        Joiner.on('/').appendTo(sb, parts);
+        return sb.toString();
+    }
+
+
+
+    protected static void deleteRecursive(BaseResources resources, final String pathRoot) throws MetadataStoreException {
+        PathUtils.validatePath(pathRoot);
+        List<String> tree = listSubTreeBFS(resources, pathRoot);
+        log.debug("Deleting {} with size {}", tree, tree.size());
+        log.debug("Deleting " + tree.size() + " subnodes ");
+        for (int i = tree.size() - 1; i >= 0; --i) {
+            // Delete the leaves first and eventually get rid of the root
+            resources.delete(tree.get(i));
+        }
+    }
+
+    protected static List<String> listSubTreeBFS(BaseResources resources, final String pathRoot)
+            throws MetadataStoreException {
+        Deque<String> queue = new LinkedList<>();
+        List<String> tree = new ArrayList<>();
+        queue.add(pathRoot);
+        tree.add(pathRoot);
+        while (true) {
+            String node = queue.pollFirst();
+            if (node == null) {
+                break;
+            }
+            List<String> children = resources.getChildren(node);
+            for (final String child : children) {
+                final String childPath = node + "/" + child;
+                queue.add(childPath);
+                tree.add(childPath);
+            }
+        }
+        return tree;
     }
 }

commit 7f4aac55a0c9d82fc689e6de60afc9c611a534dc

[Broker] Remove check resource when delete failure domain (#13421)
### Motivation

This PR is used to keep same behavior with Pulsar 2.8 and 2.7 version.

In Pulsar 2.7 and 2.8, when delete a non-existent failure domain resource, it will return 404 status code.
In Pulsar 2.9, when delete a non-existent failure domain resource, it will return 200 status code.

Reproduce in Pulsar 2.8:
```
$ docker run -itd \
  -p 6650:6650 \
  -p 8080:8080 \
  apachepulsar/pulsar:2.8.1 \
  bin/pulsar standalone
$ pulsarctl clusters delete-failure-domain standalone non-existent-failure-domain
[✖]  code: 404 reason: Domain-name non-existent-failure-domain or cluster standalone does not exist
```

Reproduce in Pulsar 2.9:
```
$ docker run -itd \
  -p 6650:6650 \
  -p 8080:8080 \
  apachepulsar/pulsar:2.9.0 \
  bin/pulsar standalone
$ pulsarctl clusters delete-failure-domain standalone non-existent-failure-domain
Delete failure domain [non-existent-failure-domain] for cluster [standalone] succeed
```
                   
Affected version: 2.9.x

This issue was caused by https://github.com/apache/pulsar/pull/11693.

### Modifications

- Remove check resource exists when call the `ClusterResources#deleteFailureDomain()`
- Add check resource exists when call the  `ClusterResources#deleteFailureDomains()`

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/resources/ClusterResources.java
@@ -121,20 +121,20 @@ public class ClusterResources extends BaseResources<ClusterData> {
 
         public void deleteFailureDomain(String clusterName, String domainName) throws MetadataStoreException {
             String path = joinPath(BASE_CLUSTERS_PATH, clusterName, FAILURE_DOMAIN, domainName);
-            if (exists(path)) {
-                delete(path);
-            }
+            delete(path);
         }
 
         public void deleteFailureDomains(String clusterName) throws MetadataStoreException {
             String failureDomainPath = joinPath(BASE_CLUSTERS_PATH, clusterName, FAILURE_DOMAIN);
+            if (!exists(failureDomainPath)) {
+                return;
+            }
+
             for (String domain : getChildren(failureDomainPath)) {
                 delete(joinPath(failureDomainPath, domain));
             }
 
-            if (exists(failureDomainPath)) {
-                delete(failureDomainPath);
-            }
+            delete(failureDomainPath);
         }
 
         public void setFailureDomainWithCreate(String clusterName, String domainName,

pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiClusterTest.java
@@ -1,0 +1,98 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.broker.admin;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.collect.Sets;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest;
+import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.apache.pulsar.common.policies.data.ClusterData;
+import org.apache.pulsar.common.policies.data.FailureDomain;
+import org.awaitility.Awaitility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Test(groups = "broker")
+@Slf4j
+public class AdminApiClusterTest extends MockedPulsarServiceBaseTest {
+    private final String CLUSTER = "test";
+
+    @BeforeMethod
+    @Override
+    public void setup() throws Exception {
+        resetConfig();
+        super.internalSetup();
+        admin.clusters()
+                .createCluster(CLUSTER, ClusterData.builder().serviceUrl(pulsar.getWebServiceAddress()).build());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    @Override
+    public void cleanup() throws Exception {
+        super.internalCleanup();
+    }
+
+    @Test
+    public void testDeleteNonExistCluster() {
+        String cluster = "test-non-exist-cluster-" + UUID.randomUUID();
+
+        assertThrows(PulsarAdminException.NotFoundException.class, () -> admin.clusters().deleteCluster(cluster));
+    }
+
+    @Test
+    public void testDeleteExistCluster() throws PulsarAdminException {
+        String cluster = "test-exist-cluster-" + UUID.randomUUID();
+
+        admin.clusters()
+                .createCluster(cluster, ClusterData.builder().serviceUrl(pulsar.getWebServiceAddress()).build());
+        Awaitility.await().untilAsserted(() -> assertNotNull(admin.clusters().getCluster(cluster)));
+
+        admin.clusters().deleteCluster(cluster);
+    }
+
+    @Test
+    public void testDeleteNonExistentFailureDomain() {
+        assertThrows(PulsarAdminException.NotFoundException.class,
+                () -> admin.clusters().deleteFailureDomain(CLUSTER, "non-existent-failure-domain"));
+    }
+
+    @Test
+    public void testDeleteNonExistentFailureDomainInNonExistCluster() {
+        assertThrows(PulsarAdminException.PreconditionFailedException.class,
+                () -> admin.clusters().deleteFailureDomain(CLUSTER + UUID.randomUUID(),
+                        "non-existent-failure-domain"));
+    }
+
+    @Test
+    public void testDeleteExistFailureDomain() throws PulsarAdminException {
+        String domainName = CLUSTER + "-failure-domain";
+        FailureDomain domain = FailureDomain.builder()
+                .brokers(Sets.newHashSet("b1", "b2", "b3"))
+                .build();
+        admin.clusters().createFailureDomain(CLUSTER, domainName, domain);
+        Awaitility.await().untilAsserted(() -> admin.clusters().getFailureDomain(CLUSTER, domainName));
+
+        admin.clusters().deleteFailureDomain(CLUSTER, domainName);
+    }
+}