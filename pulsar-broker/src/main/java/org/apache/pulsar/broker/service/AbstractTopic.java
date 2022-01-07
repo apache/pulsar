703b6993ac5d94acec353477e032168134fea8ae
Qiang Zhao <mattisonchao@gmail.com>
2022-01-07 10:45:55 +0800

    delete blank

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/AbstractTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/AbstractTopic.java
@@ -941,7 +941,7 @@ public abstract class AbstractTopic implements Topic, TopicPolicyListener<TopicP
     }
 
     public boolean isDeleteWhileInactive() {
-        return  topicPolicies.getInactiveTopicPolicies().get().isDeleteWhileInactive();
+        return topicPolicies.getInactiveTopicPolicies().get().isDeleteWhileInactive();
     }
 
     public boolean deletePartitionedTopicMetadataWhileInactive() {
