fc396430805b47caadd95f1365a74d29e1861ef2
baomingyu <baomingyu_5@163.com>
2022-01-07 12:51:41 +0800

    Fix issues 11964, deadlock bug when use key_shared mode (#11965)
    (cherry picked from commit fd3ba55f8e39992ecbc96830af5ee58e3247a25a)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorImpl.java
@@ -1992,7 +1992,6 @@ public class ManagedCursorImpl implements ManagedCursor {
 
             if (individualDeletedMessages.isEmpty()) {
                 // No changes to individually deleted messages, so nothing to do at this point
-                callback.deleteComplete(ctx);
                 return;
             }
 
@@ -2024,6 +2023,9 @@ public class ManagedCursorImpl implements ManagedCursor {
             return;
         } finally {
             lock.writeLock().unlock();
+            if (individualDeletedMessages.isEmpty()) {
+                callback.deleteComplete(ctx);
+            }
         }
 
         // Apply rate limiting to mark-delete operations
