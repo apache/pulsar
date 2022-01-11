4eb74a6395b2f6941098cbadb6ef94dbeca65b49
Anonymitaet <anonymitaet_@hotmail.com>
2022-01-11 17:44:22 +0800

    update

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/SourcesBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/SourcesBase.java
@@ -116,10 +116,9 @@ public class SourcesBase extends AdminResource {
                             + "  Any flags that you want to pass to the runtime.\n"
                             + "2. Encapsulate the JSON object to a multipart object.",
                     examples = @Example(
-                            value = {
-                                @ExampleProperty(
+                            value = @ExampleProperty(
                                     mediaType = MediaType.APPLICATION_JSON,
-                                    value = "Example 1 \n{ \n"
+                                    value = "{\n"
                                             + "  \"tenant\": public\n"
                                             + "  \"namespace\": default\n"
                                             + "  \"name\": pulsar-io-mysql\n"
@@ -129,21 +128,7 @@ public class SourcesBase extends AdminResource {
                                             + "  \"archive\": /connectors/pulsar-io-mysql-0.0.1.nar\n"
                                             + "  \"schemaType\": avro\n"
                                             + "}\n"
-                                            + "Example 2 \n{\n"
-                                            + "\"inputs\": ["
-                                            + "\"persistent://public/default/source-topic\"],\n"
-                                            + "\"parallelism\":1,\n"
-                                            + "\"sourceType\":\"jdbc\",\n"
-                                            + "\"archive\":\"builtin://jdbc\",\n"
-                                            + "\"configs\": {\n"
-                                            + "\t\"userName\": \"root\",\n"
-                                            + "\t\"password\": \"jdbc\",\n"
-                                            + "\t\"jdbcUrl\": \"jdbc:mysql://127.0.0.1:3306/"
-                                            + "pulsar_mysql_jdbc_source\",\n"
-                                            + "\t\"tableName\": \"pulsar_mysql_jdbc_source\"\n"
-                                            + "}"
-                                    )
-                            }
+                            )
                     )
             )
             final @FormDataParam("sourceConfig") SourceConfig sourceConfig) {
