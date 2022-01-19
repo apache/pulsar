c6ab9fda94027e7f495ed022c4016f50b5b4b443
Qiang Zhao <mattisonchao@gmail.com>
2022-01-19 18:56:10 +0800

    fixes wrong error catch.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
@@ -887,9 +887,7 @@ public abstract class PulsarWebResource {
         if (pulsar().getConfiguration().isAuthenticationEnabled()
             && pulsar().getBrokerService().isAuthorizationEnabled()) {
             if (!isClientAuthenticated(clientAppId())) {
-                return FutureUtil
-                        .failedFuture(new RestException(Status.FORBIDDEN,
-                                "Need to authenticate to perform the request"));
+                throw new RestException(Status.FORBIDDEN, "Need to authenticate to perform the request");
             }
 
             return pulsar().getBrokerService().getAuthorizationService()
