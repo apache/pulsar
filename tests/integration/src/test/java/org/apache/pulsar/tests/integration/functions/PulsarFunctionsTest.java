58ebbe807c789bf6f620c293d19d9e108a8a65a1
wangjialing <wangjialing@cmss.chinamobile.com>
2022-01-25 14:32:09 +0800

    update test code

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/functions/PulsarFunctionsTest.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/functions/PulsarFunctionsTest.java
@@ -1456,14 +1456,14 @@ public abstract class PulsarFunctionsTest extends PulsarFunctionsTestBase {
         submitFunction(runtime, inputTopicName, outputTopicName, functionName, null, InitializableFunction.class.getName(), schema);
 
         // check initialize is called when function start
-        TimeUnit.MILLISECONDS.sleep(500);
+        TimeUnit.MILLISECONDS.sleep(5000);
         assertTrue(InitializableFunction.isInitialized());
 
         // delete function
         deleteFunction(functionName);
 
         // close method is called after function deleted
-        TimeUnit.MILLISECONDS.sleep(500);
+        TimeUnit.MILLISECONDS.sleep(5000);
         assertFalse(InitializableFunction.isInitialized());
     }
 
