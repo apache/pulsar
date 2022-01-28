54c898f18bf8c53d64c7f87049bb63764137acb0
Michael Marshall <mmarshall@apache.org>
2022-01-27 22:49:42 -0600

    [ServerCnx] Improve error logging for topic not found (#13950)
    * [ServerCnx] Improve error logging for topic not found
    
    * Log topic not found at INFO level

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
@@ -1274,12 +1274,13 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
                     return null;
                 }
 
+                // Do not print stack traces for expected exceptions
                 if (cause instanceof NoSuchElementException) {
                     cause = new TopicNotFoundException("Topic Not Found.");
-                }
-                if (!Exceptions.areExceptionsPresentInChain(cause,
+                    log.info("[{}] Failed to load topic {}, producerId={}: Topic not found", remoteAddress, topicName,
+                            producerId);
+                } else if (!Exceptions.areExceptionsPresentInChain(cause,
                         ServiceUnitNotReadyException.class, ManagedLedgerException.class)) {
-                    // Do not print stack traces for expected exceptions
                     log.error("[{}] Failed to create topic {}, producerId={}",
                             remoteAddress, topicName, producerId, exception);
                 }
