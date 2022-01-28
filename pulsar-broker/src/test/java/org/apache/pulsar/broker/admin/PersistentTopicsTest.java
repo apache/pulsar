a9308a872ec5d9f063337e128b52b79c35d1449e
Zixuan Liu <nodeces@gmail.com>
2022-01-28 10:55:19 +0800

    Remove unset import
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/PersistentTopicsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/PersistentTopicsTest.java
@@ -94,7 +94,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import org.testng.collections.Maps;
 
