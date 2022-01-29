a27c61e0d6cc8d57a4b6388a31cc1af427e83a66
mattison chao <mattisonchao@gmail.com>
2022-01-29 14:42:29 +0800

    fixes checkstyle

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v1/PersistentTopics.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v1/PersistentTopics.java
@@ -24,7 +24,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.ws.rs.DELETE;
