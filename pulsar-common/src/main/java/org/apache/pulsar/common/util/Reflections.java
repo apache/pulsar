c9afb99399f6e18b80965d7fe96998403c2a9e16
penghui <penghui@apache.org>
2022-02-08 18:41:43 +0800

    Apply comment

--- pulsar-common/src/main/java/org/apache/pulsar/common/util/Reflections.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/util/Reflections.java
@@ -24,7 +24,6 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URLClassLoader;
 import java.util.Arrays;
 import java.util.HashMap;
