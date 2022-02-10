bd15441895e84b019e1b4f01923fdbfe84e008b0
gaozhangmin <gaozhangmin456@qq.com>
2022-02-10 22:17:26 +0800

    [Broker] Reduce redundant debug log (#14208)
    ### Motivation
    
    Redundant debug log
    
    ### Modifications
    
    delete redundant debug log
    
    ### Documentation
    
    Check the box below or label this PR directly (if you have committer privilege).
    
    Need to update docs? 
    
    - [ ] `doc-required` 
      
      (If you need help on updating docs, create a doc issue)
      
    - [x] `no-need-doc` 
      
      (Please explain why)
      
    - [ ] `doc` 
      
      (If this PR contains doc changes)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -2480,14 +2480,12 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
         if (backlogQuotaLimitInBytes < 0) {
             return false;
         }
-        if (log.isDebugEnabled()) {
-            log.debug("[{}] - backlog quota limit = [{}]", getName(), backlogQuotaLimitInBytes);
-        }
 
         // check if backlog exceeded quota
         long storageSize = getBacklogSize();
         if (log.isDebugEnabled()) {
-            log.debug("[{}] Storage size = [{}], limit [{}]", getName(), storageSize, backlogQuotaLimitInBytes);
+            log.debug("[{}] Storage size = [{}], backlog quota limit [{}]",
+                    getName(), storageSize, backlogQuotaLimitInBytes);
         }
 
         return (storageSize >= backlogQuotaLimitInBytes);
