a19879ba17b10df45913540a031edef77cd483b9
Lari Hotari <lhotari@users.noreply.github.com>
2022-03-01 16:17:55 +0200

    [Build] Make the test JVM exit if OOME occurs (#14509)
    - OOMEs can make the build to take very long to complete.
      It's better to fail fast in tests when OOMEs occur.
    
    (cherry picked from commit 89a36f9e4a0cd37676b7501184004f01a7c47150)

--- pulsar-broker/src/test/java/org/apache/pulsar/client/api/ClientDeduplicationFailureTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/client/api/ClientDeduplicationFailureTest.java
@@ -189,7 +189,8 @@ public class ClientDeduplicationFailureTest {
         }
     }
 
-    @Test(timeOut = 300000, groups = "quarantine")
+    // TODO: Test disabled since it results in a OOME
+    @Test(timeOut = 300000, groups = "quarantine", enabled = false)
     public void testClientDeduplicationCorrectnessWithFailure() throws Exception {
         final String namespacePortion = "dedup";
         final String replNamespace = tenant + "/" + namespacePortion;
