69fc8b34fd7c2b57b2ba6be6c8076929a1914225
aloyszhang <lofterzhang@gmail.com>
2022-03-02 10:35:47 +0800

    remove un-used import

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v2/Namespaces.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v2/Namespaces.java
@@ -25,7 +25,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
