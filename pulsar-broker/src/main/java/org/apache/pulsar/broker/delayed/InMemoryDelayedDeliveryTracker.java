818da66dda6c3dfe6c61a4b67f061673d777b748
leizhiyuan <leizhiyuan@gmail.com>
2022-03-18 10:55:42 +0800

    Revert "chore: fix for cr"
    This reverts commit e0e923217c1e25a2c15b3463ed77c79f22ff4ed7.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/delayed/InMemoryDelayedDeliveryTracker.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/delayed/InMemoryDelayedDeliveryTracker.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.broker.delayed;
 
+import com.google.common.collect.ComparisonChain;
 import io.netty.util.Timeout;
 import io.netty.util.Timer;
 import io.netty.util.TimerTask;
@@ -81,15 +82,18 @@ public class InMemoryDelayedDeliveryTracker implements DelayedDeliveryTracker, T
             // trying to dispatch to the consumer.
             return false;
         }
-        if (ledgerId < lastLedgerId || (ledgerId == lastLedgerId && entryId <= lastEntryId)) {
-            if (log.isInfoEnabled()) {
-                log.info("[{}] Add message  {}:{}  less than last {} {} ", dispatcher.getName(), ledgerId, entryId,
-                        lastLedgerId, lastEntryId);
+
+        synchronized (this) {
+            if (ComparisonChain.start().compare(ledgerId, lastLedgerId).compare(entryId, lastEntryId).result() <= 0) {
+                if (log.isWarnEnabled()) {
+                    log.warn("[{}] Add message  {}:{}  less than last {} {} ", dispatcher.getName(), ledgerId, entryId,
+                            lastLedgerId, lastEntryId);
+                }
+                return true;
             }
-            return true;
+            lastLedgerId = ledgerId;
+            lastEntryId = entryId;
         }
-        lastLedgerId = ledgerId;
-        lastEntryId = entryId;
         priorityQueue.add(deliveryAt, ledgerId, entryId);
         updateTimer();
         return true;
