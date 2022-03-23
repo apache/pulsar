5c76d96e6e335b26b0e183704621ecdf123bf4de
liangyepianzhou <xymeng@streamnative.io>
2022-03-23 10:01:24 +0800

    [Fix][Transaction] Fix transaction can not be enable when allowAutoUpdateSchemaEnabled = false ### Motivation Fix transaction can not be enable when allowAutoUpdateSchemaEnabled = false ### Modification Create the schema at the first time TRANSACTION_BUFFER_SNAPSHOT is used in this namespace

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -743,7 +743,7 @@ public class PulsarService implements AutoCloseable, ShutdownService {
 
             // Register pulsar system namespaces and start transaction meta store service
             if (config.isTransactionCoordinatorEnabled()) {
-                this.transactionBufferSnapshotService = new SystemTopicBaseTxnBufferSnapshotService(getClient());
+                this.transactionBufferSnapshotService = new SystemTopicBaseTxnBufferSnapshotService(getClient(), this);
                 this.transactionTimer =
                         new HashedWheelTimer(new DefaultThreadFactory("pulsar-transaction-timer"));
                 transactionBufferClient = TransactionBufferClientImpl.create(getClient(), transactionTimer);
