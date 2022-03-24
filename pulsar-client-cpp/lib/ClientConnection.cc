a62c371148160027984b4312734f02d4e990b227
Yunze Xu <xyzinfernity@163.com>
2022-03-24 15:29:16 +0800

    [C++] Fix segmentation fault when creating socket failed (#14834)
    ### Motivation
    
    https://github.com/apache/pulsar/pull/14823 fixes the flaky
    `testConnectTimeout` but it's also a regression of
    https://github.com/apache/pulsar/pull/14587. Because when the fd limit
    is reached, the `connectionTimeoutTask_` won't be initialized with a
    non-null value. Calling `stop` method on it directly will cause
    segmentation fault.
    
    See
    https://github.com/apache/pulsar/blob/0fe921f32cefe7648ca428cd9861f9163c69767d/pulsar-client-cpp/lib/ClientConnection.cc#L178-L185
    
    ### Modifications
    
    Add the null check for `connectionTimeoutTask_` in `ClientConnection::close`.
    
    (cherry picked from commit 54c368ed3744a40240205c17bdcac5cef48130e4)

--- pulsar-client-cpp/lib/ClientConnection.cc
+++ pulsar-client-cpp/lib/ClientConnection.cc
@@ -1549,7 +1549,9 @@ void ClientConnection::close(Result result) {
         consumerStatsRequestTimer_.reset();
     }
 
-    connectTimeoutTask_->stop();
+    if (connectTimeoutTask_) {
+        connectTimeoutTask_->stop();
+    }
 
     lock.unlock();
     LOG_INFO(cnxString_ << "Connection closed");
