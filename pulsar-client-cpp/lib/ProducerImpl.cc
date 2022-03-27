52e4cf6d4cb417e03b3ae495bef3736e04076530
Yunze Xu <xyzinfernity@163.com>
2022-03-27 19:38:55 +0800

    Add processAndClear() to batch message container

--- pulsar-client-cpp/lib/ProducerImpl.cc
+++ pulsar-client-cpp/lib/ProducerImpl.cc
@@ -269,12 +269,14 @@ std::shared_ptr<ProducerImpl::PendingCallbacks> ProducerImpl::getPendingCallback
     }
 
     if (batchMessageContainer_) {
-        clearPendingBatches([this, &callbacks](Result result, const OpSendMsg& opSendMsg) {
-            if (result == ResultOk) {
-                callbacks->opSendMsgs.emplace_back(opSendMsg);
-            }
-            releaseSemaphoreForSendOp(opSendMsg);
-        });
+        batchMessageContainer_->processAndClear(
+            [this, &callbacks](Result result, const OpSendMsg& opSendMsg) {
+                if (result == ResultOk) {
+                    callbacks->opSendMsgs.emplace_back(opSendMsg);
+                }
+                releaseSemaphoreForSendOp(opSendMsg);
+            },
+            nullptr);
     }
     pendingMessagesQueue_.clear();
 
@@ -292,29 +294,6 @@ void ProducerImpl::failPendingMessages(Result result, bool withLock) {
     } else {
         getPendingCallbacksWhenFailed()->complete(result);
     }
-}
-
-void ProducerImpl::clearPendingBatches(std::function<void(Result, const OpSendMsg&)> opSendMsgCallback,
-                                       FlushCallback flushCallback) {
-    if (PULSAR_UNLIKELY(batchMessageContainer_->isEmpty())) {
-        if (flushCallback) {
-            flushCallback(ResultOk);
-        }
-    } else {
-        const auto numBatches = batchMessageContainer_->getNumBatches();
-        if (numBatches == 1) {
-            OpSendMsg opSendMsg;
-            Result result = batchMessageContainer_->createOpSendMsg(opSendMsg, flushCallback);
-            opSendMsgCallback(result, opSendMsg);
-        } else if (numBatches > 1) {
-            std::vector<OpSendMsg> opSendMsgs;
-            std::vector<Result> results = batchMessageContainer_->createOpSendMsgs(opSendMsgs, flushCallback);
-            for (size_t i = 0; i < results.size(); i++) {
-                opSendMsgCallback(results[i], opSendMsgs[i]);
-            }
-        }  // else numBatches is 0, do nothing
-    }
-    batchMessageContainer_->clear();
 }
 
 void ProducerImpl::resendMessages(ClientConnectionPtr cnx) {
@@ -592,7 +571,7 @@ PendingFailures ProducerImpl::batchMessageAndSend(const FlushCallback& flushCall
     LOG_DEBUG("batchMessageAndSend " << *batchMessageContainer_);
     batchTimer_->cancel();
 
-    clearPendingBatches(
+    batchMessageContainer_->processAndClear(
         [this, &failures](Result result, const OpSendMsg& opSendMsg) {
             if (result == ResultOk) {
                 sendMessage(opSendMsg);
