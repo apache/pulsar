7ad5a0132a39b443fb2c3bf6785d1774b189cef6
Enrico Olivelli <eolivelli@apache.org>
2022-03-28 11:30:27 +0200

    fix checkstyle

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -44,7 +44,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
