5acdd64c4286b580e010d5db56dd3bbd8dc712a6
552356593@qq.com <552356593@qq.com>
2022-04-01 01:22:42 +0800

    fix license error

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/containers/EtcdMetadataStoreContainer.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/containers/EtcdMetadataStoreContainer.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -42,7 +42,7 @@ public class EtcdMetadataStoreContainer implements MetadataStoreContainer, Booki
 
     @Override
     public String getBookieConnString(String host) {
-        return "etcd://" + host + ":" + EtcdContainer.PORT + "/clusters/" + etcdContainer.clusterName;
+        return "etcd://" + host + ":" + etcdContainer.getMappedPort(EtcdContainer.PORT) + "/clusters/" + etcdContainer.clusterName;
     }
 
     @Override
