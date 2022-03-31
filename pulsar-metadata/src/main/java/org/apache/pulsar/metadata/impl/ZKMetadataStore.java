98b824dae05b5de71e37e56380d24b623a86e729
wangjialing <wangjialing_yewu@cmss.chinamobile.com>
2022-03-31 14:58:37 +0800

    add test

--- pulsar-metadata/src/main/java/org/apache/pulsar/metadata/impl/ZKMetadataStore.java
+++ pulsar-metadata/src/main/java/org/apache/pulsar/metadata/impl/ZKMetadataStore.java
@@ -386,7 +386,7 @@ public class ZKMetadataStore extends AbstractBatchedMetadataStore
                                 put(opPut.getPath(), opPut.getData(), Optional.of(-1L)).thenAccept(
                                                 s -> future.complete(s))
                                         .exceptionally(ex -> {
-                                            if (ex instanceof BadVersionException) {
+                                            if (ex.getCause() instanceof BadVersionException) {
                                                 // The z-node exist now, let's overwrite it
                                                 internalStorePut(opPut);
                                             } else {
