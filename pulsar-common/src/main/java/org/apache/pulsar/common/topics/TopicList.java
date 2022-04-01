0be7c514ef169bbfbf8dac5c98b9cf2acdff0118
Andras Beni <andras.beni@streamnative.io>
2022-04-01 15:17:14 +0200

    Fix code style error

--- pulsar-common/src/main/java/org/apache/pulsar/common/topics/TopicList.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/topics/TopicList.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-
 import lombok.experimental.UtilityClass;
 import org.apache.pulsar.common.naming.TopicName;
 
