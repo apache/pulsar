4b38819249f15e870fd79766bd06750f660b08e7
liangyepianzhou <xymeng@streamnative.io>
2022-04-03 20:10:36 +0800

    delete pendingack test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/v3/AdminApiTransactionMultiBrokerTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/v3/AdminApiTransactionMultiBrokerTest.java
@@ -18,16 +18,11 @@
  */
 package org.apache.pulsar.broker.admin.v3;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.broker.transaction.TransactionTestBase;
-import org.apache.pulsar.client.api.Consumer;
-import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.PulsarClient;
-import org.apache.pulsar.client.api.Schema;
-import org.apache.pulsar.client.api.transaction.Transaction;
 import org.apache.pulsar.common.naming.TopicName;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -38,12 +33,12 @@ public class AdminApiTransactionMultiBrokerTest extends TransactionTestBase {
 
     private static final int NUM_BROKERS = 16;
     private static final int NUM_PARTITIONS = 16;
-    
+
     @BeforeMethod
     protected void setup() throws Exception {
         setUpBase(NUM_BROKERS, NUM_PARTITIONS, NAMESPACE1 + "/test", 0);
     }
-    
+
     @AfterMethod(alwaysRun = true)
     protected void cleanup() throws Exception {
         super.internalCleanup();
@@ -66,42 +61,5 @@ public class AdminApiTransactionMultiBrokerTest extends TransactionTestBase {
         for (int i = 0; i < NUM_PARTITIONS; i++) {
             admin.transactions().getCoordinatorInternalStats(i, false);
         }
-    }
-
-    @Test
-    public void testRedirectOfGetPendingAckInternalStats() throws Exception {
-        String topic1 = NAMESPACE1 + "/test1";
-
-        admin.topics().createNonPartitionedTopic(topic1);
-        //check they are different broker
-        while (admin.lookups().lookupTopic(topic1)
-                .equals(getPulsarServiceList().get(0).getBrokerServiceUrl())) {
-         admin.topics().delete(topic1);
-         admin.topics().createNonPartitionedTopic(topic1);
-        }
-        //init pendingAck
-        Consumer<byte[]> consumer1 = pulsarClient.newConsumer()
-                .topic(topic1)
-                .subscriptionName("sub1")
-                .subscribe();
-
-        pulsarClient.newProducer(Schema.BYTES)
-                .topic(topic1)
-                .sendTimeout(0, TimeUnit.SECONDS)
-                .create()
-                .newMessage()
-                .value("test".getBytes(StandardCharsets.UTF_8)).send();
-
-        Transaction transaction = pulsarClient.newTransaction()
-                .withTransactionTimeout(5, TimeUnit.SECONDS)
-                .build()
-                .get();
-
-        Message<byte[]> message1 = consumer1.receive();
-        consumer1.acknowledgeAsync(message1.getMessageId(), transaction);
-
-        transaction.commit().get();
-        //check pendingAck stats
-        admin.transactions().getPendingAckInternalStats(topic1, "sub1", false);
     }
 }
