99d868645298f5debf15d3ab2d72927bcdbe4386
Matteo Merli <mmerli@apache.org>
2022-04-03 17:52:11 -0700

    [Python] Added build for Arm64 on Linux and Python 3.10 (#15004)
    * [Python] Added build for Arm64 on Linux and Python 3.10
    
    * Fixed docker-build-python3.9.sh
    
    * Added arch in docker/pulsar/pom.xml
    
    * Fixed spacing in docker/pulsar/pom.xml
    
    * Fixed build-wheels.sh when taking args
    
    * Removed debug statement
    
    * Fixed variable expansion
    
    * Pass the arch to the script inside docker

--- pulsar-client-cpp/docker/create-images.sh
+++ pulsar-client-cpp/docker/create-images.sh
@@ -24,13 +24,18 @@
 set -e
 
 PYTHON_VERSIONS=(
-   '2.7 cp27-cp27mu manylinux1'
-   '2.7 cp27-cp27m manylinux1'
-   '3.5 cp35-cp35m manylinux2014'
-   '3.6 cp36-cp36m manylinux2014'
-   '3.7 cp37-cp37m manylinux2014'
-   '3.8 cp38-cp38 manylinux2014'
-   '3.9 cp39-cp39 manylinux2014'
+   '2.7  cp27-cp27mu manylinux1    x86_64'
+   '2.7  cp27-cp27m  manylinux1    x86_64'
+   '3.5  cp35-cp35m  manylinux2014 x86_64'
+   '3.6  cp36-cp36m  manylinux2014 x86_64'
+   '3.7  cp37-cp37m  manylinux2014 x86_64'
+   '3.8  cp38-cp38   manylinux2014 x86_64'
+   '3.9  cp39-cp39   manylinux2014 x86_64'
+   '3.10 cp310-cp310 manylinux2014 x86_64'
+   '3.7  cp37-cp37m  manylinux2014 aarch64'
+   '3.8  cp38-cp38   manylinux2014 aarch64'
+   '3.9  cp39-cp39   manylinux2014 aarch64'
+   '3.10 cp310-cp310 manylinux2014 aarch64'
 )
 
 for line in "${PYTHON_VERSIONS[@]}"; do
@@ -38,13 +43,15 @@ for line in "${PYTHON_VERSIONS[@]}"; do
     PYTHON_VERSION=${PY[0]}
     PYTHON_SPEC=${PY[1]}
     BASE_IMAGE=${PY[2]}
-    echo "--------- Build Docker image for $PYTHON_VERSION -- $PYTHON_SPEC"
+    ARCH=${PY[3]}
+    echo "--------- Build Docker image for $PYTHON_VERSION -- $PYTHON_SPEC -- $ARCH"
 
-    IMAGE_NAME=pulsar-build:manylinux-$PYTHON_SPEC
+    IMAGE_NAME=pulsar-build:manylinux-$PYTHON_SPEC-$ARCH
 
     docker build -t $IMAGE_NAME $BASE_IMAGE \
             --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-            --build-arg PYTHON_SPEC=$PYTHON_SPEC
+            --build-arg PYTHON_SPEC=$PYTHON_SPEC \
+            --build-arg ARCH=$ARCH
 
     echo "==== Successfully built image $IMAGE_NAME"
 done
