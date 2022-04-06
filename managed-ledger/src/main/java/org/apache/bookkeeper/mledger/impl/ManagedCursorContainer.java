504264ebb76d7942d46a5806939cca273dd496b5
LinChen <1572139390@qq.com>
2022-04-06 15:53:58 +0800

    Add a cache eviction policy：Evicting cache data by the slowest markDeletedPosition (#14985)
    (cherry picked from commit 9b36dcd777b7e3b159b57cd409784a3622483132)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorContainer.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorContainer.java
@@ -122,6 +122,10 @@ public class ManagedCursorContainer implements Iterable<ManagedCursor> {
         }
     }
 
+    public PositionImpl getSlowestMarkDeletedPositionForActiveCursors() {
+        return heap.isEmpty() ? null : (PositionImpl) heap.get(0).cursor.getMarkDeletedPosition();
+    }
+
     public ManagedCursor get(String name) {
         long stamp = rwLock.readLock();
         try {
