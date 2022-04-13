ac0a242a0e93fc8166f162fffcd74f6964e5b166
Dezhi LIiu <33149602+liudezhi2098@users.noreply.github.com>
2022-04-13 08:58:14 +0800

    fix debug log authenticate role error (#14784)
    (cherry picked from commit 97b7e77a9075159dd58a279d64f834e85472f437)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
@@ -815,8 +815,13 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
             authenticationData = authState.getAuthDataSource();
 
             if (log.isDebugEnabled()) {
-                log.debug("[{}] Authenticate role : {}", remoteAddress,
-                    authState != null ? authState.getAuthRole() : null);
+                String role = "";
+                if (authState != null && authState.isComplete()) {
+                    role = authState.getAuthRole();
+                } else {
+                    role = "authentication incomplete or null";
+                }
+                log.debug("[{}] Authenticate role : {}", remoteAddress, role);
             }
 
             state = doAuthentication(clientData, clientProtocolVersion, clientVersion);
