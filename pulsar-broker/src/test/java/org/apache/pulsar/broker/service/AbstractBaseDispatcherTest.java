abc0eb03cc1209167fef42ac1517e4d43c883695
horizonzy <horizonzy@apache.org>
2022-04-26 01:06:17 +0800

    style format.

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/AbstractBaseDispatcherTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/AbstractBaseDispatcherTest.java
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.pulsar.broker.service;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
