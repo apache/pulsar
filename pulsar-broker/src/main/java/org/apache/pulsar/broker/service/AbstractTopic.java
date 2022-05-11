602e8ad73b1a9d9cd637269fb16c9f0875d6e7e1
Jiwei Guo <technoboy@apache.org>
2022-05-11 15:38:15 +0800

    [cleanup][broker] Override close method to avoid caching exception (#15529)
    (cherry picked from commit 526979a87e3cea5c4c90667ce86aed52927fd631)
    (cherry picked from commit e3add83871c96dfdcbbb6b225a9400417ba54842)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/AbstractTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/AbstractTopic.java
@@ -987,9 +987,7 @@ public abstract class AbstractTopic implements Topic, TopicPolicyListener<TopicP
         }
 
         // attach the resource-group level rate limiters, if set
-        String rgName = policies.resource_group_name != null
-          ? policies.resource_group_name
-          : null;
+        String rgName = policies.resource_group_name;
         if (rgName != null) {
             final ResourceGroup resourceGroup =
               brokerService.getPulsar().getResourceGroupServiceManager().resourceGroupGet(rgName);
