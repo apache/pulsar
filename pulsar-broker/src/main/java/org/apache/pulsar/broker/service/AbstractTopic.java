6bbeb80b60d686f2f2a0f9c862e036ee72212572
Xiaoyu Hou <anonhxygo@gmail.com>
2022-05-11 14:24:42 +0800

    remove

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/AbstractTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/AbstractTopic.java
@@ -1164,13 +1164,8 @@ public abstract class AbstractTopic implements Topic, TopicPolicyListener<TopicP
             }
         } else {
             log.info("Disabling publish throttling for {}", this.topic);
-            if (topicPublishRateLimiter != null
-                && topicPublishRateLimiter != PublishRateLimiter.DISABLED_RATE_LIMITER) {
-                try {
-                    topicPublishRateLimiter.close();
-                } catch (Exception e) {
-                    log.warn("[{}] close publish rate limiter exception", this, e);
-                }
+            if (topicPublishRateLimiter != null) {
+                topicPublishRateLimiter.close();
             }
             this.topicPublishRateLimiter = PublishRateLimiter.DISABLED_RATE_LIMITER;
             enableProducerReadForPublishRateLimiting();
