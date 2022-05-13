4040977025da6b885b2cc2d0195f5589fceb7704
Matteo Merli <mmerli@apache.org>
2022-05-12 21:06:46 -0700

    [fix][broker] Fix deadlock in broker after race condition in topic creation failure (#15570)
    * Fix deadlock in broker after race condition in topic creation failure
    
    * Fixed checkstyle

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -1360,9 +1360,9 @@ public class BrokerService implements Closeable {
                                     if (topicFuture.isCompletedExceptionally()) {
                                         log.warn("{} future is already completed with failure {}, closing the topic",
                                                 topic, FutureUtil.getException(topicFuture));
-                                        persistentTopic.stopReplProducers().whenComplete((v, exception) -> {
+                                        persistentTopic.stopReplProducers().whenCompleteAsync((v, exception) -> {
                                             topics.remove(topic, topicFuture);
-                                        });
+                                        }, executor());
                                     } else {
                                         addTopicToStatsMaps(topicName, persistentTopic);
                                         topicFuture.complete(Optional.of(persistentTopic));
@@ -1372,10 +1372,10 @@ public class BrokerService implements Closeable {
                                             "Replication or dedup check failed."
                                                     + " Removing topic from topics list {}, {}",
                                             topic, ex);
-                                    persistentTopic.stopReplProducers().whenComplete((v, exception) -> {
+                                    persistentTopic.stopReplProducers().whenCompleteAsync((v, exception) -> {
                                         topics.remove(topic, topicFuture);
                                         topicFuture.completeExceptionally(ex);
-                                    });
+                                    }, executor());
 
                                     return null;
                                 });
