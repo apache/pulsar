d569aa9e11d12f44a85f63af105ae0e0aefed037
Matteo Merli <mmerli@apache.org>
2022-05-17 11:28:45 +0200

    Fixed extra semicolon

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/topologies/PulsarStandaloneTestBase.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/topologies/PulsarStandaloneTestBase.java
@@ -82,7 +82,7 @@ public abstract class PulsarStandaloneTestBase extends PulsarTestBase {
         container = new StandaloneContainer(clusterName, pulsarImageName)
             .withNetwork(network)
             .withNetworkAliases(StandaloneContainer.NAME + "-" + clusterName)
-            .withEnv("PF_stateStorageServiceUrl", "bk://localhost:4181");
+            .withEnv("PF_stateStorageServiceUrl", "bk://localhost:4181")
             .withEnv("PULSAR_STANDALONE_USE_ZOOKEEPER", "true")
         container.start();
         log.info("Pulsar cluster {} is up running:", clusterName);
