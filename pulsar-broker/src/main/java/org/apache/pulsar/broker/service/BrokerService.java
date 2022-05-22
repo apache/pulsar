19bd8598f52b8a6fc86b0e1e8abc72d708a10340
feynmanlin <315157973@qq.com>
2022-05-22 09:37:48 +0800

    [PIP 81]: Part-2 Add range set wrapper to support record dirty ledgers (#15607)
    ### Motivation
    Pulsar persists the individual acknowledgments into Ledger for each subscription.
    acknowledgment hole uses guava range to record.
    Now a single entry is used to record these holes, but a single entry can not stores more than 5MB data.
    Therefore, this PIP stores these holes in multiple entries separately. And use LRU to ensure low memory usage.
    In this way, we can support almost unlimited acknowledgment hole.
    
    Doc: https://docs.google.com/document/d/1uQtyb8t6X04v2vrSrdGWLFkuCkBcGYZbqK8XsVJ4qkU/edit
    
    We do not need to persist the data of all entries every time, only the entries whose Ledger data has changed need to be re-saved. Therefore, we add a wrapper class to record dirty pages.
    
    ### Modifications
    1. Add a wrapper class
    2. Record dirty pages where `add` and `remove`
    
    ### Verifying this change
    RangeSetWrapperTest.java
    1. Make sure dirty pages are recorded correctly
    2. Make sure the behavior of wrapper class is the same as other RangeSets

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -1534,6 +1534,8 @@ public class BrokerService implements Closeable {
 
                     managedLedgerConfig
                             .setMaxUnackedRangesToPersist(serviceConfig.getManagedLedgerMaxUnackedRangesToPersist());
+                    managedLedgerConfig.setPersistentUnackedRangesWithMultipleEntriesEnabled(
+                            serviceConfig.isPersistentUnackedRangesWithMultipleEntriesEnabled());
                     managedLedgerConfig.setMaxUnackedRangesToPersistInMetadataStore(
                             serviceConfig.getManagedLedgerMaxUnackedRangesToPersistInMetadataStore());
                     managedLedgerConfig.setMaxEntriesPerLedger(serviceConfig.getManagedLedgerMaxEntriesPerLedger());
