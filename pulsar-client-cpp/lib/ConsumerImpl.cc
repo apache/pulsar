2c4d04f0890ff253b6d49190dc41366f6f80d45f
Matteo Merli <mmerli@apache.org>
2022-05-23 12:46:34 -0700

    [C++] Unblock all threads when Pulsar client is closed

--- pulsar-client-cpp/lib/ConsumerImpl.cc
+++ pulsar-client-cpp/lib/ConsumerImpl.cc
@@ -722,11 +722,14 @@ Result ConsumerImpl::fetchSingleMessageFromBroker(Message& msg) {
     sendFlowPermitsToBroker(currentCnx, 1);
 
     while (true) {
-        incomingMessages_.pop(msg);
+        if (!incomingMessages_.pop(msg)) {
+            return ResultInterrupted;
+        }
+
         {
             // Lock needed to prevent race between connectionOpened and the check "msg.impl_->cnx_ ==
             // currentCnx.get())"
-            Lock localLock(mutex_);
+            Lock localLock1(mutex_);
             // if message received due to an old flow - discard it and wait for the message from the
             // latest flow command
             if (msg.impl_->cnx_ == currentCnx.get()) {
@@ -787,7 +790,10 @@ Result ConsumerImpl::receiveHelper(Message& msg) {
         return fetchSingleMessageFromBroker(msg);
     }
 
-    incomingMessages_.pop(msg);
+    if (!incomingMessages_.pop(msg)) {
+        return ResultInterrupted;
+    }
+
     messageProcessed(msg);
     return ResultOk;
 }
@@ -998,6 +1004,7 @@ void ConsumerImpl::closeAsync(ResultCallback callback) {
 
     LOG_INFO(getName() << "Closing consumer for topic " << topic_);
     state_ = Closing;
+    incomingMessages_.close();
 
     // Flush pending grouped ACK requests.
     if (ackGroupingTrackerPtr_) {
--- pulsar-client-cpp/lib/ProducerImpl.cc
+++ pulsar-client-cpp/lib/ProducerImpl.cc
@@ -526,9 +526,13 @@ int ProducerImpl::getNumOfChunks(uint32_t size, uint32_t maxMessageSize) {
 Result ProducerImpl::canEnqueueRequest(uint32_t payloadSize) {
     if (conf_.getBlockIfQueueFull()) {
         if (semaphore_) {
-            semaphore_->acquire();
+            if (!semaphore_->acquire()) {
+                return ResultInterrupted;
+            }
         }
-        memoryLimitController_.reserveMemory(payloadSize);
+        if (!memoryLimitController_.reserveMemory(payloadSize)) {
+            return ResultInterrupted;
+        }
         return ResultOk;
     } else {
         if (semaphore_) {
@@ -645,6 +649,10 @@ void ProducerImpl::closeAsync(CloseCallback callback) {
     ProducerImplPtr ptr = shared_from_this();
 
     cancelTimers();
+
+    if (semaphore_) {
+        semaphore_->close();
+    }
 
     // ensure any remaining send callbacks are called before calling the close callback
     failPendingMessages(ResultAlreadyClosed, false);
