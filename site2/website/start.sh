c26b0fd2580c5af90c59ee3e0d5c70a18dbd970f
Li Li <urfree@apache.org>
2022-05-24 11:43:21 +0800

    update
    Signed-off-by: Li Li <urfree@apache.org>

--- site2/website/start.sh
+++ site2/website/start.sh
@@ -6,12 +6,13 @@ cd .preview
 
 if [ -d "$ROOT_DIR/site2/.preview/pulsar-site" ]; then
     cd pulsar-site
-    git checkout .
-    git pull origin main
+    # git checkout .
+    # git pull origin main
 else
     git clone -b main --depth 1 https://github.com/apache/pulsar-site.git
 fi
 
 cd $ROOT_DIR/site2/.preview/pulsar-site/site2/website-next
-sh scripts/sync-docs.sh pulsar
-sh preview.sh $@
+
+sh scripts/sync-docs.sh $ROOT_DIR/site2/
+node scripts/watch.js $ROOT_DIR/site2 $@
