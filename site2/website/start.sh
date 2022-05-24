b60c2a58d11f20700a937d2084cd712c22e9c8f4
Li Li <urfree@apache.org>
2022-05-24 11:53:52 +0800

    [feature][website] add script to auto watch md files change and auto … (#15721)

--- site2/website/start.sh
+++ site2/website/start.sh
@@ -5,6 +5,7 @@ mkdir -p .preview
 cd .preview
 
 if [ -d "$ROOT_DIR/site2/.preview/pulsar-site" ]; then
+    cd pulsar-site
     git checkout .
     git pull origin main
 else
@@ -12,5 +13,6 @@ else
 fi
 
 cd $ROOT_DIR/site2/.preview/pulsar-site/site2/website-next
-sh scripts/sync-docs.sh pulsar
-sh preview.sh $@
+
+sh scripts/sync-docs.sh $ROOT_DIR/site2/
+node scripts/watch.js $ROOT_DIR/site2 $@
