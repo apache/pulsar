af2694139bd4b52bfef77a38a65d22cbe9d1387c
Li Li <urfree@apache.org>
2022-05-24 16:48:47 +0800

    update start.sh
    Signed-off-by: Li Li <urfree@apache.org>

--- site2/website/start.sh
+++ site2/website/start.sh
@@ -6,13 +6,21 @@ cd .preview
 
 if [ -d "$ROOT_DIR/site2/.preview/pulsar-site" ]; then
     cd pulsar-site
-    git checkout .
-    git pull origin main
+    # git checkout .
+    # git pull origin main
 else
     git clone -b main --depth 1 https://github.com/apache/pulsar-site.git
 fi
 
 cd $ROOT_DIR/site2/.preview/pulsar-site/site2/website-next
 
-sh scripts/sync-docs.sh $ROOT_DIR/site2/
+sh scripts/sync-docs.sh $ROOT_DIR/site2
+
+if [ "$?" = "0" ]; then
+    echo "full sync done..."
+else
+    echo "Error: start fail, please update your local pulsar repo by run cmd: git pull" 1>&2
+    exit 1
+fi
+
 node scripts/watch.js $ROOT_DIR/site2 $@
