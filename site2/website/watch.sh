ac645c47b2156f2575cc16be2217df019bfac404
Li Li <urfree@apache.org>
2022-05-24 11:49:36 +0800

    update
    Signed-off-by: Li Li <urfree@apache.org>

--- site2/website/watch.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-ROOT_DIR=$(git rev-parse --show-toplevel)
-cd $ROOT_DIR/site2/.preview/pulsar-site/site2/website-next
-
-echo "monitoring file changes..."
-while true; do
-    sh scripts/sync-docs.sh pulsar
-    sleep 1
-done
