23078c6646fba70be8dd57e4ed964e846419fc02
mattison chao <mattisonchao@gmail.com>
2022-05-26 17:43:39 +0800

    Revert "Revert "[fix][sql] Fix the decimal type error convert in json schema (#15687)""
    This reverts commit f3754e9df3f53c64e4b2d6b3657bb7e94ea89863.

--- pulsar-sql/presto-pulsar/src/main/java/org/apache/pulsar/sql/presto/decoder/json/PulsarJsonFieldDecoder.java
+++ pulsar-sql/presto-pulsar/src/main/java/org/apache/pulsar/sql/presto/decoder/json/PulsarJsonFieldDecoder.java
@@ -58,6 +58,8 @@ import io.prestosql.spi.type.ArrayType;
 import io.prestosql.spi.type.BigintType;
 import io.prestosql.spi.type.BooleanType;
 import io.prestosql.spi.type.DateType;
+import io.prestosql.spi.type.DecimalType;
+import io.prestosql.spi.type.Decimals;
 import io.prestosql.spi.type.DoubleType;
 import io.prestosql.spi.type.IntegerType;
 import io.prestosql.spi.type.MapType;
@@ -70,6 +72,7 @@ import io.prestosql.spi.type.TinyintType;
 import io.prestosql.spi.type.Type;
 import io.prestosql.spi.type.VarbinaryType;
 import io.prestosql.spi.type.VarcharType;
+import java.math.BigInteger;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -120,6 +123,9 @@ public class PulsarJsonFieldDecoder
     }
 
     private boolean isSupportedType(Type type) {
+        if (type instanceof DecimalType) {
+            return true;
+        }
         if (isVarcharType(type)) {
             return true;
         }
@@ -228,6 +234,13 @@ public class PulsarJsonFieldDecoder
                     return floatToIntBits((Float) parseFloat(value.asText()));
                 }
 
+                // If it is decimalType, need to eliminate the decimal point,
+                // and give it to presto to set the decimal point
+                if (type instanceof DecimalType) {
+                    String decimalLong = value.asText().replace(".", "");
+                    return Long.valueOf(decimalLong);
+                }
+
                 long longValue;
                 if (value.isIntegralNumber() && !value.isBigInteger()) {
                     longValue = value.longValue();
@@ -267,6 +280,15 @@ public class PulsarJsonFieldDecoder
 
         private static Slice getSlice(JsonNode value, Type type, String columnName) {
             String textValue = value.isValueNode() ? value.asText() : value.toString();
+
+            // If it is decimalType, need to eliminate the decimal point,
+            // and give it to presto to set the decimal point
+            if (type instanceof DecimalType) {
+                textValue = textValue.replace(".", "");
+                BigInteger bigInteger = new BigInteger(textValue);
+                return Decimals.encodeUnscaledValue(bigInteger);
+            }
+
             Slice slice = utf8Slice(textValue);
             if (isVarcharType(type)) {
                 slice = truncateToLength(slice, type);
