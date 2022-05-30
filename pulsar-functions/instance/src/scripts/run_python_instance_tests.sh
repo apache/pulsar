23cd61b71878178a322cef9238dbfb732f72fdab
Yong Zhang <zhangyong1025.zy@gmail.com>
2022-05-30 19:07:47 +0800

    Using  as a workarounds

--- pulsar-functions/instance/src/scripts/run_python_instance_tests.sh
+++ pulsar-functions/instance/src/scripts/run_python_instance_tests.sh
@@ -28,4 +28,6 @@ CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 PULSAR_HOME=$CUR_DIR/../../../../
 
 # run instance tests
-PULSAR_HOME=${PULSAR_HOME} PYTHONPATH=${PULSAR_HOME}/pulsar-functions/instance/target/python-instance python3 -m unittest discover -v ${PULSAR_HOME}/pulsar-functions/instance/target/python-instance/tests
+# Keep the `PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python` until the bookkeeper client upgrade the protobuf
+# https://github.com/apache/pulsar/pull/15827
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python PULSAR_HOME=${PULSAR_HOME} PYTHONPATH=${PULSAR_HOME}/pulsar-functions/instance/target/python-instance python3 -m unittest discover -v ${PULSAR_HOME}/pulsar-functions/instance/target/python-instance/tests
