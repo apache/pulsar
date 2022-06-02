d847e1cde5b5aee85d6a3ae9fd5b2bb3fa6fd874
Lari Hotari <lhotari@users.noreply.github.com>
2022-06-02 16:53:07 +0300

    [Cli tools] Disable Pulsar client memory limit by default (#15748)
    - There's a regression with the tools since the memory limit cannot be adjusted
      - It's better to default to the previous setting of disabling memory limits
        so that the performance profile doesn't change because of the memory limit.
    
    (cherry picked from commit a6a7516aaf5a27ecfec083f0eb868e4ebbcbbef8)

--- pulsar-testclient/src/main/java/org/apache/pulsar/testclient/PerformanceTransaction.java
+++ pulsar-testclient/src/main/java/org/apache/pulsar/testclient/PerformanceTransaction.java
@@ -62,6 +62,7 @@ import org.apache.pulsar.client.api.ProducerBuilder;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.client.api.SizeUnit;
 import org.apache.pulsar.client.api.SubscriptionInitialPosition;
 import org.apache.pulsar.client.api.SubscriptionType;
 import org.apache.pulsar.client.api.transaction.Transaction;
@@ -295,7 +296,9 @@ public class PerformanceTransaction {
         }
 
         ClientBuilder clientBuilder =
-                PulsarClient.builder().enableTransaction(!arguments.isDisableTransaction)
+                PulsarClient.builder()
+                        .memoryLimit(0, SizeUnit.BYTES)
+                        .enableTransaction(!arguments.isDisableTransaction)
                         .serviceUrl(arguments.serviceURL)
                         .connectionsPerBroker(arguments.maxConnections)
                         .statsInterval(0, TimeUnit.SECONDS)
