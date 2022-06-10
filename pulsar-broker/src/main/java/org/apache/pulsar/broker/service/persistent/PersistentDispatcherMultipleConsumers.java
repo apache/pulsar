36690f5a7f79fb744c1d61969f2a277cadd0e704
Yunze Xu <xyzinfernity@163.com>
2022-06-10 15:12:31 +0800

    [fix][broker] Avoid storing `MessageMetadata` instances returned by `peekMessageMetadata` (#15983)
    ### Motivation
    
    https://github.com/apache/pulsar/pull/15967 removed the `EntryWrapper`,
    which holds an `Entry` instance that is never used. Instead, after the
    refactoring, the `MessageMetadata` array is useless, see
    https://github.com/apache/pulsar/blob/298a573295f845e46f8a55cee366b6db63e997c2/pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentDispatcherMultipleConsumers.java#L517-L519
    
    Each `MessageMetadata` instance in the array is returned by
    `peekMessageMetadata`, whose returned value references a thread local
    object `Commands#LOCAL_MESSAGE_METADATA`. It brings a problem that if
    multiple entries were read, all `MessageMetadata` elements in the array
    reference the same object.
    
    However, accidentally, the wrong invocation of `Optional#orElse` saves
    it. See
    https://github.com/apache/pulsar/blob/298a573295f845e46f8a55cee366b6db63e997c2/pulsar-broker/src/main/java/org/apache/pulsar/broker/service/AbstractBaseDispatcher.java#L133-L134
    
    Each time `peekMessageMetadata` is called, the thread local message
    metadata will be updated. Unlike `orElseGet`, the expression in `orElse`
    is always called no matter if the optional is empty.
    
    This behavior change increases the invocations count of
    `peekMessageMetadata` and the `metadataArray` cache became redundant.
    
    ### Modifications
    
    - Use `orElseGet` instead of `orElse` in `AbstractBaseDispatcher`.
    - Add a new static method `Commands#peekAndCopyMessageMetadata` that
      returns a `MessageMetadata` instance allocated from heap memory.
    - Call `peekAndCopyMessageMetadata` to cache all message metadata
      instances in `PersistentDispatcherMultipleConsumers`.
    
    ### Verifying this change
    
    It's hard to add tests. As I've explained before, #15967 only degrades the
    performance and doesn't affect the correctness.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentDispatcherMultipleConsumers.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentDispatcherMultipleConsumers.java
@@ -515,7 +515,7 @@ public class PersistentDispatcherMultipleConsumers extends AbstractDispatcherMul
             return;
         }
         final MessageMetadata[] metadataArray = entries.stream()
-                .map(entry -> Commands.peekMessageMetadata(entry.getDataBuffer(), subscription.toString(), -1))
+                .map(entry -> Commands.peekAndCopyMessageMetadata(entry.getDataBuffer(), subscription.toString(), -1))
                 .toArray(MessageMetadata[]::new);
         int remainingMessages = Stream.of(metadataArray).filter(Objects::nonNull)
                 .map(MessageMetadata::getNumMessagesInBatch)
