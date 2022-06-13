470206c89be8ee88c80eefb9a8d0ed4253e6f2bd
Zixuan Liu <nodeces@gmail.com>
2022-06-13 11:12:21 +0800

    Remove unused import
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- pulsar-client/src/test/java/org/apache/pulsar/client/impl/ClientCnxTest.java
+++ pulsar-client/src/test/java/org/apache/pulsar/client/impl/ClientCnxTest.java
@@ -20,7 +20,6 @@ package org.apache.pulsar.client.impl;
 
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
