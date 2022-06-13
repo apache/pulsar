4fb769e0283b539554f7d9d7e3c8d474cef27bfe
Zixuan Liu <nodeces@gmail.com>
2022-06-13 15:16:39 +0800

    [fix][client] Remove producer when close producer command is received (#16028)
    (cherry picked from commit 5ef895af7d8dec851167e56cdf3e8bec11080f8d)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientCnx.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientCnx.java
@@ -115,7 +115,8 @@ public class ClientCnx extends PulsarHandler {
     // LookupRequests that waiting in client side.
     private final Queue<Pair<Long, Pair<ByteBuf, TimedCompletableFuture<LookupDataResult>>>> waitingLookupRequests;
 
-    private final ConcurrentLongHashMap<ProducerImpl<?>> producers =
+    @VisibleForTesting
+    final ConcurrentLongHashMap<ProducerImpl<?>> producers =
             ConcurrentLongHashMap.<ProducerImpl<?>>newBuilder()
                     .expectedItems(16)
                     .concurrencyLevel(1)
@@ -726,7 +727,7 @@ public class ClientCnx extends PulsarHandler {
     protected void handleCloseProducer(CommandCloseProducer closeProducer) {
         log.info("[{}] Broker notification of Closed producer: {}", remoteAddress, closeProducer.getProducerId());
         final long producerId = closeProducer.getProducerId();
-        ProducerImpl<?> producer = producers.get(producerId);
+        ProducerImpl<?> producer = producers.remove(producerId);
         if (producer != null) {
             producer.connectionClosed(this);
         } else {
