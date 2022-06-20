b372f014a3fc164651431acc6d045843fdffe30d
Yang Yang <yyang@streamnative.io>
2022-06-20 15:23:03 +0800

    Use LinkedHashSet for empty values.

--- pulsar-common/src/main/java/org/apache/pulsar/common/util/FieldParser.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/util/FieldParser.java
@@ -31,7 +31,6 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -213,7 +212,7 @@ public final class FieldParser {
             if (field.getType().equals(List.class)) {
                 field.set(obj, new ArrayList<>());
             } else if (field.getType().equals(Set.class)) {
-                field.set(obj, new HashSet<>());
+                field.set(obj, new LinkedHashSet<>());
             } else if (field.getType().equals(Optional.class)) {
                 field.set(obj, Optional.empty());
             } else {
