dc2916866d69c3a03f69dc159a6bedb35733bb3e
Nicolò Boschi <boschi1997@gmail.com>
2022-06-21 14:53:11 +0200

    fix compile

--- pulsar-io/elastic-search/src/test/java/org/apache/pulsar/io/elasticsearch/testcontainers/ElasticToxiproxiContainer.java
+++ pulsar-io/elastic-search/src/test/java/org/apache/pulsar/io/elasticsearch/testcontainers/ElasticToxiproxiContainer.java
@@ -54,7 +54,7 @@ public class ElasticToxiproxiContainer extends ToxiproxyContainer {
 
     public String getHttpHostAddress() {
         Objects.nonNull(proxy);
-        return proxy.getHost() + ":" + proxy.getProxyPort();
+        return proxy.getContainerIpAddress() + ":" + proxy.getProxyPort();
     }
 
     public ToxiproxyContainer.ContainerProxy getProxy() {
