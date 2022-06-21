10d774315392ef2ea5a8085cffa2090265043f88
penghui <penghui@apache.org>
2022-06-21 18:56:27 +0800

    [improve][java-client] Only trigger the batch receive timeout when having pending batch receives requests
    ### Motivation
    
    The consumer will apply the default batch receive policy even if the user will not use the batch receive API.
    
    https://github.com/apache/pulsar/blob/6704f12104219611164aa2bb5bbdfc929613f1bf/pulsar-client-api/src/main/java/org/apache/pulsar/client/api/BatchReceivePolicy.java#L60-L61
    
    This will consume lots of CPU if the client have many consumers (100k consumers)
    
    The Pulsar perf tool can also reproduce the problem if run the test with many consumers
    
    ### Modification
    
    If there is no pending batch receive operation for a consumer, no need to trigger the
    batch timeout task periodically. We can only start the timeout check after adding batch
    receive request to pending request queue.
    
    Remove the lock in MultiTopicsConsumerImpl as #10352 does
    
    ### Verification
    
    Added new test to verify the batch receive timeout task will not start if no batch
    receive request

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -548,6 +548,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                 expectMoreIncomingMessages();
                 OpBatchReceive<T> opBatchReceive = OpBatchReceive.of(result);
                 pendingBatchReceives.add(opBatchReceive);
+                triggerBatchReceiveTimeoutTask();
                 cancellationHandler.setCancelAction(() -> pendingBatchReceives.remove(opBatchReceive));
             }
         });
