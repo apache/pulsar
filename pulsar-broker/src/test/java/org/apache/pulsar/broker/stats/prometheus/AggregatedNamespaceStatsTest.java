357d4faae0d13974ada95f533a2fa98fbe0a222d
Mark Silcox <mark.silcox@hawkeyeinnovations.com>
2022-06-23 17:49:50 +0100

    [fix][broker] Update writing to use existing data structures

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/prometheus/AggregatedNamespaceStatsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/prometheus/AggregatedNamespaceStatsTest.java
@@ -20,7 +20,6 @@ package org.apache.pulsar.broker.stats.prometheus;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
-
 import org.testng.annotations.Test;
 
 @Test(groups = "broker")
