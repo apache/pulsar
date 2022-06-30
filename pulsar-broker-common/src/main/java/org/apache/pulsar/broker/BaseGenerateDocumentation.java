93cafccf0399327232f9d58f688d4b0b96788022
horizonzy <horizonzy@apache.org>
2022-06-30 22:55:21 +0800

    ignore the deprecated.

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/BaseGenerateDocumentation.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/BaseGenerateDocumentation.java
@@ -90,7 +90,7 @@ public abstract class BaseGenerateDocumentation {
         sb.append("|---|---|---|---|---|\n");
         for (Field field : fields) {
             FieldContext fieldContext = field.getAnnotation(FieldContext.class);
-            if (fieldContext == null || fieldContext.deprecated()) {
+            if (fieldContext == null) {
                 continue;
             }
             field.setAccessible(true);
