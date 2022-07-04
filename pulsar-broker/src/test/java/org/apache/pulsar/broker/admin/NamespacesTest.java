aad5ef280d1b7732ee8cd25e5e700ead9175656a
Jiwe Guo <technoboy@apache.org>
2022-07-04 16:37:18 +0800

    fix checkstyle.

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/NamespacesTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/NamespacesTest.java
@@ -25,7 +25,6 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
