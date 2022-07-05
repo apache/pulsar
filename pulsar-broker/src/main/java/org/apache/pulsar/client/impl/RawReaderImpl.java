eb961ab7c3c5183982f637e305a7f846cd379193
chenhang <chenhang@apache.org>
2022-07-05 12:33:58 +0800

    address comments

--- pulsar-broker/src/main/java/org/apache/pulsar/client/impl/RawReaderImpl.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/client/impl/RawReaderImpl.java
@@ -211,9 +211,11 @@ public class RawReaderImpl implements RawReader {
                 log.debug("[{}][{}] Received raw message: {}/{}/{}", topic, subscription,
                         messageId.getEntryId(), messageId.getLedgerId(), messageId.getPartition());
             }
-            incomingRawMessages.add(
+            internalPinnedExecutor.execute(() -> {
+                incomingRawMessages.add(
                     new RawMessageAndCnx(new RawMessageImpl(messageId, headersAndPayload), cnx));
-            internalPinnedExecutor.execute(this::tryCompletePending);
+                tryCompletePending();
+            });
         }
     }
 
