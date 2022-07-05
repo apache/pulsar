8e36ae6773e8e3fe949053025d7ffba84b8b614c
horizonzy <horizonzy@apache.org>
2022-07-05 18:24:34 +0800

    add some comment.

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/rubbish/RubbishInfo.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/rubbish/RubbishInfo.java
@@ -1,6 +1,7 @@
 package org.apache.bookkeeper.mledger.rubbish;
 
 
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,15 +14,36 @@ import org.apache.bookkeeper.mledger.ManagedLedgerInfo;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RubbishInfo {
-
+    /**
+     * Partitioned topic name without domain. Likes public/default/test-topic-partition-1 or
+     * public/default/test-topic
+     */
     private String topicName;
-    
+
+    /**
+     * The rubbish source. managed-ledger, managed-cursor and schema-storage.
+     */
     private RubbishSource rubbishSource;
-    
+
+    /**
+     * The rubbish type. ledger or offload-ledger.
+     */
     private RubbishType rubbishType;
 
+    /**
+     * ledgerInfo. If ledger, just holds ledgerId. If offload-ledger, holds ledgerId and offload context uuid.
+     */
     private ManagedLedgerInfo.LedgerInfo ledgerInfo;
 
+    /**
+     * When consumer received rubbish info, maybe the ledger still in use, we need check the ledger is in use.
+     * In some cases, we needn't check the ledger still in use.
+     */
     private boolean checkLedgerStillInUse;
+
+    /**
+     * Extent properties.
+     */
+    private Map<String, String> properties;
 
 }
