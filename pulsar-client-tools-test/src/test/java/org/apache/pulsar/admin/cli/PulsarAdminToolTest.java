f99ae32bd44549e29ffd5716e38de8c7f90fe7ed
liangyepianzhou <xymeng@streamnative.io>
2022-07-06 15:05:00 +0800

    fix test

--- pulsar-client-tools-test/src/test/java/org/apache/pulsar/admin/cli/PulsarAdminToolTest.java
+++ pulsar-client-tools-test/src/test/java/org/apache/pulsar/admin/cli/PulsarAdminToolTest.java
@@ -2173,7 +2173,7 @@ public class PulsarAdminToolTest {
         verify(transactions).scaleTransactionCoordinators(3);
 
         cmdTransactions = new CmdTransactions(() -> admin);
-        cmdTransactions.run(split("check-position-in-pending-ack-state -t test -s test -l 1 -e 1 -b 1"));
+        cmdTransactions.run(split("position-stats-in-pending-ack -t test -s test -l 1 -e 1 -b 1"));
         verify(transactions).getPositionStatsInPendingAck("test", "test", 1L, 1L, 1);
     }
 
