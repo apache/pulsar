ba3d680542d5bab4692299ccdb458bc085a935dd
gaozhangmin <zhangmingao@apache.org>
2022-07-14 10:40:37 +0800

    Update pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v1/Namespaces.java
    Co-authored-by: Zixuan Liu <nodeces@gmail.com>

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v1/Namespaces.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v1/Namespaces.java
@@ -820,7 +820,7 @@ public class Namespaces extends NamespacesBase {
                 .thenCompose(__ -> getNamespacePoliciesAsync(namespaceName))
                 .thenAccept(policies -> asyncResponse.resume(policies.bundles))
                 .exceptionally(ex -> {
-                    log.error("[{}] Failed to get bundle data for namespace {}: {} ", clientAppId(),
+                    log.error("[{}] Failed to get bundle data for namespace {} ", clientAppId(),
                             namespaceName, ex);
                     resumeAsyncResponseExceptionally(asyncResponse, ex);
                     return null;
