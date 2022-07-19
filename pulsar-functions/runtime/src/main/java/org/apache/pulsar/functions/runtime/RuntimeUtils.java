7b9c433963e9bda83a75ca6c4c42cf8d836ae99a
Christophe Bornet <cbornet@hotmail.com>
2022-07-19 10:35:02 +0200

    [fix][functions] Fix netty.DnsResolverUtil compat issue on JDK9+ for the function Runtimes (#16423)

--- pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/runtime/RuntimeUtils.java
+++ pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/runtime/RuntimeUtils.java
@@ -41,7 +41,9 @@ import io.prometheus.client.hotspot.ThreadExports;
 import io.prometheus.client.hotspot.VersionInfoExports;
 import io.prometheus.jmx.JmxCollector;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.JavaVersion;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.pulsar.common.util.ObjectMapperFactory;
 import org.apache.pulsar.functions.instance.AuthenticationConfig;
 import org.apache.pulsar.functions.instance.InstanceConfig;
@@ -256,7 +258,6 @@ public class RuntimeUtils {
         return args;
     }
 
-
     public static List<String> getCmd(InstanceConfig instanceConfig,
                                       String instanceFile,
                                       String extraDependenciesDir, /* extra dependencies for running instances */
@@ -319,6 +320,12 @@ public class RuntimeUtils {
                     shardId));
 
             args.add("-Dio.netty.tryReflectionSetAccessible=true");
+
+            // Needed for netty.DnsResolverUtil on JDK9+
+            if (SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_9)) {
+                args.add("--add-opens");
+                args.add("java.base/sun.net=ALL-UNNAMED");
+            }
 
             if (instanceConfig.getAdditionalJavaRuntimeArguments() != null) {
                 args.addAll(instanceConfig.getAdditionalJavaRuntimeArguments());
