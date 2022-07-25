96b25e0671824636e6e4e3256ba6dd3011f1c802
Jiwei Guo <technoboy@apache.org>
2022-07-25 16:02:59 +0800

    Fix Flaky-test: ManagedLedgerBkTest.asyncMarkDeleteAndClose. (#16730)
    (cherry picked from commit de00ed3b7c9a8375ff6c4b0b4c7af33c169aef20)

--- managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerBkTest.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerBkTest.java
@@ -329,10 +329,10 @@ public class ManagedLedgerBkTest extends BookKeeperClusterTestCase {
             }, null);
         }
 
+        counter.await();
+
         cursor.close();
         ledger.close();
-
-        counter.await();
 
         assertFalse(gotException.get());
     }
