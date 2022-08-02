7ee272443ff926929587a1ff79543ef090a99f7f
mattison chao <mattisonchao@gmail.com>
2022-08-02 10:13:25 +0800

    Revert "Revert "Change ``rollCurrentLedgerIfFull`` logic to follow lazy creation of ledger (#14672)""
    This reverts commit 6568339a2337a30ce23cc5e24f25a9db8365c56f.

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -1706,7 +1706,6 @@ public class ManagedLedgerImpl implements ManagedLedger, CreateCallback {
                     }
 
                     ledgerClosed(lh);
-                    createLedgerAfterClosed();
                 }
             }, System.nanoTime());
         }
