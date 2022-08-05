38b93b2ec32aa0361cb1009fd9068d616ac85ac1
Yunze Xu <xyzinfernity@163.com>
2022-08-05 17:23:23 +0800

    Revert "[fix][client]Fix client memory limit currentUsage leak and semaphore release duplicated in ProducerImpl (#16837)"
    Even after importing the mockito dependency, the
    testProducerBatchSendTimeoutMemoryRelease will still fail.
    
    This reverts commit 9610640e26b634df2061f9f6ff46c028ca75bd8e.

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ProducerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ProducerImpl.java
@@ -1787,10 +1787,8 @@ public class ProducerImpl<T> extends ProducerBase<T> implements TimerTask, Conne
             return;
         }
         final int numMessagesInBatch = batchMessageContainer.getNumMessagesInBatch();
-        final long currentBatchSize = batchMessageContainer.getCurrentBatchSize();
         batchMessageContainer.discard(ex);
         semaphoreRelease(numMessagesInBatch);
-        client.getMemoryLimitController().releaseMemory(currentBatchSize);
     }
 
     @Override
@@ -1832,7 +1830,10 @@ public class ProducerImpl<T> extends ProducerBase<T> implements TimerTask, Conne
                 for (OpSendMsg opSendMsg : opSendMsgs) {
                     processOpSendMsg(opSendMsg);
                 }
+            } catch (PulsarClientException e) {
+                semaphoreRelease(batchMessageContainer.getNumMessagesInBatch());
             } catch (Throwable t) {
+                semaphoreRelease(batchMessageContainer.getNumMessagesInBatch());
                 log.warn("[{}] [{}] error while create opSendMsg by batch message container", topic, producerName, t);
             }
         }
