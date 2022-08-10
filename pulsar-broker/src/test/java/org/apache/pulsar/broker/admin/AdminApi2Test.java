82ad3d6009f9e8d777bebcd576494ff144df69a3
youzipi <blake_12@qq.com>
2022-08-10 19:48:11 +0800

    [cleanup][broker] Add Maven Modernizer plugin in pulsar-broker

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApi2Test.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApi2Test.java
@@ -42,6 +42,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import javax.ws.rs.NotAcceptableException;
--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiClusterTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiClusterTest.java
@@ -31,6 +31,7 @@ import org.awaitility.Awaitility;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 @Test(groups = "broker-admin")
 @Slf4j
 public class AdminApiClusterTest extends MockedPulsarServiceBaseTest {
