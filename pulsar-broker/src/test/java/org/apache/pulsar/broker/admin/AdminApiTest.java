d8c32ca61af14caadd200a94c0736b07e007d6d7
LinChen <1572139390@qq.com>
2022-08-18 10:09:45 +0800

    [fix][broker] Support loadBalancerSheddingIntervalMinutes dynamic configuration (#16408)

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiTest.java
@@ -507,6 +507,19 @@ public class AdminApiTest extends MockedPulsarServiceBaseTest {
         assertEquals(admin.clusters().getClusters(), Lists.newArrayList());
     }
 
+    public void testUpdateDynamicLoadBalancerSheddingIntervalMinutes() throws Exception {
+        // update configuration
+        admin.brokers().updateDynamicConfiguration("loadBalancerSheddingIntervalMinutes", "10");
+
+        // wait config to be updated
+        Awaitility.await().until(() -> {
+            return conf.getLoadBalancerSheddingIntervalMinutes() == 10;
+        });
+
+        // verify value is updated
+        assertEquals(conf.getLoadBalancerSheddingIntervalMinutes(), 10);
+    }
+
     @Test
     public void testUpdateDynamicCacheConfigurationWithZkWatch() throws Exception {
         // update configuration
