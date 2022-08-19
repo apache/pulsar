a28ee0e3b34163594b9b946170f371f162d77102
Qiang Huang <HQebupt@users.noreply.github.com>
2022-08-19 13:57:52 +0800

    [fix][broker]optimize the shutdown sequence of broker service when it close (#16756)
    (cherry picked from commit 14912a6a8db750e972de577542b937aeac467987)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -446,6 +446,11 @@ public class PulsarService implements AutoCloseable, ShutdownService {
                                             (long) (GRACEFUL_SHUTDOWN_TIMEOUT_RATIO_OF_TOTAL_TIMEOUT
                                                     * getConfiguration()
                                                     .getBrokerShutdownTimeoutMs())));
+            // close protocol handler before closing broker service
+            if (protocolHandlers != null) {
+                protocolHandlers.close();
+                protocolHandlers = null;
+            }
 
             List<CompletableFuture<Void>> asyncCloseFutures = new ArrayList<>();
             if (this.brokerService != null) {
@@ -524,10 +529,6 @@ public class PulsarService implements AutoCloseable, ShutdownService {
 
             offloadersCache.close();
 
-            if (protocolHandlers != null) {
-                protocolHandlers.close();
-                protocolHandlers = null;
-            }
 
             if (transactionBufferClient != null) {
                 transactionBufferClient.close();
