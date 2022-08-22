28f052f44f49b2b9c854b7cbf4246cc83e41ef57
huangzegui <huangzegui@didiglobal.com>
2022-08-22 21:30:35 +0800

    reduce unnecessary exceptions

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/NamespacesBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/NamespacesBase.java
@@ -1576,12 +1576,9 @@ public abstract class NamespacesBase extends AdminResource {
         return updatePoliciesAsync(namespaceName, policies -> {
             policies.persistence = persistence;
             return policies;
-        }).thenAccept(v -> {
-            try {
-                log.info("[{}] Successfully updated persistence configuration: namespace={}, map={}", clientAppId(),
-                        namespaceName, jsonMapper().writeValueAsString(persistence));
-            } catch (Exception ignore) {}
-        }).exceptionally(ex -> {
+        }).thenAccept(__ -> log.info("[{}] Successfully updated persistence configuration: namespace={}, map={}",
+                clientAppId(), namespaceName, persistence)
+        ).exceptionally(ex -> {
             log.error("[{}] Failed to update persistence configuration for namespace {}", clientAppId(), namespaceName,
                     ex);
             throw new RestException(ex);
