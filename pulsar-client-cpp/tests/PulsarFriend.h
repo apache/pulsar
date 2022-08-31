91471e7f3911be87c03caa1446d6b6f1eec5dfdd
Baodi Shi <wudixiaolongbao@icloud.com>
2022-08-31 10:15:02 +0800

    [refactor][client c++] Delete PartitionedConsumerImpl, use MultiTopicsConsumerImpl instead (#16969)
    ### Motivation
    
    The code of the C++ client is still relatively old, after #1365, the java client used `MultiTopicConsumerImpl` instead of `PartitionedConsumerImpl`.
    
    ### Modifications
    - Delete PartitionedConsumerImpl, use MultiTopicsConsumerImpl instead.
    - For MultiTopicConsumerImpl, support seek message and topic partition listener feature.
    
    (cherry picked from commit 3a3ae23427bc9c8642b3ec9a081ce01000339875)
    (cherry picked from commit 23da64b11ad4baf2cbf00af85100d1620d80fbc6)

--- pulsar-client-cpp/tests/PulsarFriend.h
+++ pulsar-client-cpp/tests/PulsarFriend.h
@@ -23,7 +23,6 @@
 #include "lib/ProducerImpl.h"
 #include "lib/PartitionedProducerImpl.h"
 #include "lib/ConsumerImpl.h"
-#include "lib/PartitionedConsumerImpl.h"
 #include "lib/MultiTopicsConsumerImpl.h"
 #include "lib/ReaderImpl.h"
 
@@ -90,10 +89,6 @@ class PulsarFriend {
         auto consumerImpl = getConsumerImplPtr(consumer);
         ConsumerImpl::Lock lock(consumerImpl->chunkProcessMutex_);
         return consumerImpl->chunkedMessageCache_;
-    }
-
-    static std::shared_ptr<PartitionedConsumerImpl> getPartitionedConsumerImplPtr(Consumer consumer) {
-        return std::static_pointer_cast<PartitionedConsumerImpl>(consumer.impl_);
     }
 
     static std::shared_ptr<MultiTopicsConsumerImpl> getMultiTopicsConsumerImplPtr(Consumer consumer) {
