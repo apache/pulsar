be247b644f34c879e32f07974c188189a733b8b9
huangzegui <huangzegui@didiglobal.com>
2022-09-07 11:30:08 +0800

    fix the check style issue

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/NamespacesTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/NamespacesTest.java
@@ -112,7 +112,6 @@ import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
