a4e299d3255d4d9689fb04e0669ef70616e48f31
Nicolò Boschi <boschi1997@gmail.com>
2022-09-08 17:49:07 +0200

    more

--- managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/OffloadPrefixTest.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/OffloadPrefixTest.java
@@ -52,6 +52,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+@Test(groups = "flaky")
 public class OffloadPrefixTest extends MockedBookKeeperTestCase {
     private static final Logger log = LoggerFactory.getLogger(OffloadPrefixTest.class);
 
