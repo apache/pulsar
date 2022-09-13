9e8bc73cb679b4cfddeb8713ac05df1ac113b6d2
ken <1647023764@qq.com>
2022-09-13 22:54:35 +0800

    [fix][broker]fix catching ConflictException when update topic partition (#17374)
    Co-authored-by: fanjianye <fanjianye@bigo.sg>

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -4360,10 +4360,6 @@ public class PersistentTopicsBase extends AdminResource {
             });
             return future;
         }).thenAccept(__ -> result.complete(null)).exceptionally(ex -> {
-            if (force && ex.getCause() instanceof PulsarAdminException.ConflictException) {
-                result.complete(null);
-                return null;
-            }
             result.completeExceptionally(ex);
             return null;
         });
@@ -4421,7 +4417,7 @@ public class PersistentTopicsBase extends AdminResource {
                                 future.complete(null);
                             } else {
                                 if (ignoreConflictException
-                                        && ex.getCause() instanceof PulsarAdminException.ConflictException) {
+                                        && ex instanceof PulsarAdminException.ConflictException) {
                                     future.complete(null);
                                 } else {
                                     future.completeExceptionally(ex);
