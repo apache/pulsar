cda2ea7cb0814f5bb6dc6dd829ea6719822d2555
Yuanhao Ji <jiyuanhao@apache.org>
2022-09-20 18:59:54 +0800

    [feature][pulsar-io-mongo] Add support for full message synchronization (#16003)
    ### Motivation
    
    Now, the MongoDB source connector only supports the incremental message synchronization.
    This PR adds support for full message synchronization.
    
    Since MongDB 4.0, we can set the starting point for the change stream by the `startAtOperationTime` field.
    So, we can set it to `0` to make start point the earliest.
    See https://www.mongodb.com/docs/v4.2/reference/method/db.collection.watch/ for more information.

--- pulsar-io/mongo/src/main/java/org/apache/pulsar/io/mongodb/MongoSource.java
+++ pulsar-io/mongo/src/main/java/org/apache/pulsar/io/mongodb/MongoSource.java
@@ -40,6 +40,8 @@ import org.apache.pulsar.io.core.PushSource;
 import org.apache.pulsar.io.core.SourceContext;
 import org.apache.pulsar.io.core.annotations.Connector;
 import org.apache.pulsar.io.core.annotations.IOType;
+import org.bson.BsonDocument;
+import org.bson.BsonTimestamp;
 import org.bson.Document;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -51,18 +53,16 @@ import org.reactivestreams.Subscription;
         name = "mongo",
         type = IOType.SOURCE,
         help = "A source connector that sends mongodb documents to pulsar",
-        configClass = MongoConfig.class
+        configClass = MongoSourceConfig.class
 )
 @Slf4j
 public class MongoSource extends PushSource<byte[]> {
 
     private final Supplier<MongoClient> clientProvider;
 
-    private MongoConfig mongoConfig;
+    private MongoSourceConfig mongoSourceConfig;
 
     private MongoClient mongoClient;
-
-    private Thread streamThread;
 
     private ChangeStreamPublisher<Document> stream;
 
@@ -79,38 +79,47 @@ public class MongoSource extends PushSource<byte[]> {
     public void open(Map<String, Object> config, SourceContext sourceContext) throws Exception {
         log.info("Open MongoDB Source");
 
-        mongoConfig = MongoConfig.load(config);
-        mongoConfig.validate(false, false);
+        mongoSourceConfig = MongoSourceConfig.load(config);
+        mongoSourceConfig.validate();
 
         if (clientProvider != null) {
             mongoClient = clientProvider.get();
         } else {
-            mongoClient = MongoClients.create(mongoConfig.getMongoUri());
+            mongoClient = MongoClients.create(mongoSourceConfig.getMongoUri());
         }
 
-        if (StringUtils.isEmpty(mongoConfig.getDatabase())) {
+        String mongoDatabase = mongoSourceConfig.getDatabase();
+        if (StringUtils.isEmpty(mongoDatabase)) {
             // Watch all databases
-            log.info("Watch all");
+            log.info("Watch all databases");
             stream = mongoClient.watch();
 
         } else {
-            final MongoDatabase db = mongoClient.getDatabase(mongoConfig.getDatabase());
-
-            if (StringUtils.isEmpty(mongoConfig.getCollection())) {
+            final MongoDatabase db = mongoClient.getDatabase(mongoDatabase);
+            String mongoCollection = mongoSourceConfig.getCollection();
+            if (StringUtils.isEmpty(mongoCollection)) {
                 // Watch all collections in a database
                 log.info("Watch db: {}", db.getName());
                 stream = db.watch();
 
             } else {
                 // Watch a collection
-
-                final MongoCollection<Document> collection = db.getCollection(mongoConfig.getCollection());
-                log.info("Watch collection: {} {}", db.getName(), mongoConfig.getCollection());
+                final MongoCollection<Document> collection = db.getCollection(mongoCollection);
+                log.info("Watch collection: {}.{}", db.getName(), mongoCollection);
                 stream = collection.watch();
             }
         }
 
-        stream.batchSize(mongoConfig.getBatchSize()).fullDocument(FullDocument.UPDATE_LOOKUP);
+        stream.batchSize(mongoSourceConfig.getBatchSize())
+                .fullDocument(FullDocument.UPDATE_LOOKUP);
+
+        if (mongoSourceConfig.getSyncType() == SyncType.FULL_SYNC) {
+            // sync currently existing messages
+            // startAtOperationTime is the starting point for the change stream
+            // setting startAtOperationTime to 0 means the start point is the earliest
+            // see https://www.mongodb.com/docs/v4.2/reference/method/db.collection.watch/ for more information
+            stream.startAtOperationTime(new BsonTimestamp(0L));
+        }
 
         stream.subscribe(new Subscriber<ChangeStreamDocument<Document>>() {
             private ObjectMapper mapper = new ObjectMapper();
@@ -127,6 +136,12 @@ public class MongoSource extends PushSource<byte[]> {
                 try {
                     log.info("New change doc: {}", doc);
 
+                    BsonDocument documentKey = doc.getDocumentKey();
+                    if (documentKey == null) {
+                        log.warn("The document key is null");
+                        return;
+                    }
+
                     // Build a record with the essential information
                     final Map<String, Object> recordValue = new HashMap<>();
                     recordValue.put("fullDocument", doc.getFullDocument());
@@ -134,7 +149,7 @@ public class MongoSource extends PushSource<byte[]> {
                     recordValue.put("operation", doc.getOperationType());
 
                     consume(new DocRecord(
-                            Optional.of(doc.getDocumentKey().toJson()),
+                            Optional.of(documentKey.toJson()),
                             mapper.writeValueAsString(recordValue).getBytes(StandardCharsets.UTF_8)));
 
                 } catch (JsonProcessingException e) {
