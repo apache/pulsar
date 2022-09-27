e7378d0fcdaa95f30b8e05cc6ae71c8dba5d31cd
Xiaoyu Hou <houxiaoyu@apache.org>
2022-09-27 13:48:49 +0800

    Change default messages num to 32

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/AbstractBatchMessageContainer.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/AbstractBatchMessageContainer.java
@@ -47,7 +47,7 @@ public abstract class AbstractBatchMessageContainer implements BatchMessageConta
     protected long currentTxnidLeastBits = -1L;
 
     protected static final int INITIAL_BATCH_BUFFER_SIZE = 1024;
-    protected static final int INITIAL_MESSAGES_NUM = 64;
+    protected static final int INITIAL_MESSAGES_NUM = 32;
 
     // This will be the largest size for a batch sent from this particular producer. This is used as a baseline to
     // allocate a new buffer that can hold the entire batch without needing costly reallocations
