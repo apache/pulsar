a8500520e5b83fce6d05cd528c92492fcced4ac3
leolinchen <leolinchen@tencent.com>
2022-09-28 14:30:33 +0800

    reset skipped when deallocate

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/EntryImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/EntryImpl.java
@@ -188,6 +188,7 @@ public final class EntryImpl extends AbstractCASReferenceCounted implements Entr
         timestamp = -1;
         ledgerId = -1;
         entryId = -1;
+        skipped = false;
         recyclerHandle.recycle(this);
     }
 
