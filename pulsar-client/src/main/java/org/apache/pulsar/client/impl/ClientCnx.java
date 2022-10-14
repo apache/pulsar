b4518802f0dfad857abf3575758a1f69aa9457f8
ZhangJian He <shoothzj@gmail.com>
2022-10-14 19:14:56 +0800

    [fix] [pulsar-client] Fix pendingLookupRequestSemaphore leak when channel inactive (#17856)
    ### Motivation
    https://github.com/apache/pulsar/blob/b89c1451551a6bbe681465726906a2e61c9d8a69/pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientCnx.java#L282-L297 
    The `pendingLookupRequestSemaphore` will leak when channel inactive. There are `LookUpRequestSemaphore` not released when removing it from `pendingRequests`
    
    ### Modifications
    We can't easily release the semaphore in `channelInactive`, because there are not only `LookUpRequest`. So release the semaphore when connectionException
    
    ### Verifying this change
    Add unit test case to cover this change
    
    ### Documentation
    
    <!-- DO NOT REMOVE THIS SECTION. CHECK THE PROPER BOX ONLY. -->
    
    - [ ] `doc-required` 
    (Your PR needs to update docs and you will update later)
    
    - [x] `doc-not-needed` 
    bug fixs, no need doc
    
    - [ ] `doc` 
    (Your PR contains doc changes)
    
    - [ ] `doc-complete`
    (Docs have been already added)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientCnx.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientCnx.java
@@ -141,6 +141,9 @@ public class ClientCnx extends PulsarHandler {
 
     private final CompletableFuture<Void> connectionFuture = new CompletableFuture<Void>();
     private final ConcurrentLinkedQueue<RequestTime> requestTimeoutQueue = new ConcurrentLinkedQueue<>();
+
+    @VisibleForTesting
+    @Getter(AccessLevel.PACKAGE)
     private final Semaphore pendingLookupRequestSemaphore;
     private final Semaphore maxLookupRequestSemaphore;
     private final EventLoopGroup eventLoopGroup;
@@ -780,6 +783,11 @@ public class ClientCnx extends PulsarHandler {
         TimedCompletableFuture<LookupDataResult> future = new TimedCompletableFuture<>();
 
         if (pendingLookupRequestSemaphore.tryAcquire()) {
+            future.whenComplete((lookupDataResult, throwable) -> {
+                if (throwable instanceof ConnectException) {
+                    pendingLookupRequestSemaphore.release();
+                }
+            });
             addPendingLookupRequests(requestId, future);
             ctx.writeAndFlush(request).addListener(writeFuture -> {
                 if (!writeFuture.isSuccess()) {
