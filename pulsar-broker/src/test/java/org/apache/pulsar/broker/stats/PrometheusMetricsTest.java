ee2a9029430c4a1b7d1a09c30b8bf0d7ef5d9e7e
Nicolò Boschi <boschi1997@gmail.com>
2022-10-17 23:05:10 +0200

    fix

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/PrometheusMetricsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/PrometheusMetricsTest.java
@@ -839,8 +839,7 @@ public class PrometheusMetricsTest extends BrokerTestBase {
                 } else if (metricName.endsWith("_count")) {
                     String summaryMetricName = metricName.substring(0, metricName.indexOf("_count"));
                     if (!typeDefs.containsKey(summaryMetricName)) {
-                        fail("Metric " + metricName + " does not have a corresponding summary type definition, only "
-                                + typeDefs);
+                        fail("Metric " + metricName + " does not have a corresponding summary type definition");
                     }
                 } else if (metricName.endsWith("_bucket")) {
                     String summaryMetricName = metricName.substring(0, metricName.indexOf("_bucket"));
