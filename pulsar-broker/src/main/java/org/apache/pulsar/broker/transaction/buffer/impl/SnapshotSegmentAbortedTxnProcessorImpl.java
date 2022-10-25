15f721d5bfceea5c3211b4ba133a68eaadec20b5
liangyepianzhou <xymeng@streamnative.io>
2022-10-25 12:05:11 +0800

    add License

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/transaction/buffer/impl/SnapshotSegmentAbortedTxnProcessorImpl.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/transaction/buffer/impl/SnapshotSegmentAbortedTxnProcessorImpl.java
@@ -1,10 +1,27 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pulsar.broker.transaction.buffer.impl;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.util.Timeout;
 import io.netty.util.Timer;
-import io.netty.util.TimerTask;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -391,9 +408,9 @@ public class SnapshotSegmentAbortedTxnProcessorImpl implements AbortedTxnProcess
         return FutureUtil.waitForAll(completableFutures)
                 .thenCompose((ignore) -> snapshotIndexWriterFuture
                         .thenCompose(indexesWriter -> indexesWriter.writeAsync(topic.getName(), null)))
-                .thenRun(() -> {
-                    log.info("Successes to clear the snapshot segment and indexes for the topic [{}]", topic.getName());
-                }).exceptionally(e -> {
+                .thenRun(() -> log.info("Successes to clear the snapshot segment and indexes for the topic [{}]",
+                        topic.getName()))
+                .exceptionally(e -> {
                     log.error("Failed to clear the snapshot segment and indexes for the topic [{}]",
                             topic.getName(), e);
                     return null;
