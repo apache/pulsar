2909c5a62e21b817c3e489e5f5b4636f57b74239
Jiwei Guo <technoboy@apache.org>
2022-10-27 09:49:37 +0800

    [improve][broker] Support setting forceDeleteTenantAllowed dynamically (#18192)
    (cherry picked from commit 1c78e0aabf576145c5f7831e931d89ae795bc2cc)

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/BrokerServiceTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/BrokerServiceTest.java
@@ -1360,4 +1360,16 @@ public class BrokerServiceTest extends BrokerTestBase {
             assertTrue(conf.isForceDeleteNamespaceAllowed());
         });
     }
+
+    @Test
+    public void testDynamicConfigurationsForceDeleteTenantAllowed() throws Exception {
+        cleanup();
+        conf.setForceDeleteTenantAllowed(false);
+        setup();
+        admin.brokers()
+                .updateDynamicConfiguration("forceDeleteTenantAllowed", "true");
+        Awaitility.await().untilAsserted(()->{
+            assertTrue(conf.isForceDeleteTenantAllowed());
+        });
+    }
 }
