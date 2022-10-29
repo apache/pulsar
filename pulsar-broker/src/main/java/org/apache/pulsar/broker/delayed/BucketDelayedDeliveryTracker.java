99fd20bc8ec8a0a65273402e1ea3192dcdd2cab6
coderzc <zhaocong@apache.org>
2022-10-29 14:23:07 +0800

    fix style

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/delayed/BucketDelayedDeliveryTracker.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/delayed/BucketDelayedDeliveryTracker.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -61,8 +61,8 @@ import org.roaringbitmap.RoaringBitmap;
 @ThreadSafe
 public class BucketDelayedDeliveryTracker extends InMemoryDelayedDeliveryTracker {
 
-    protected static final int AsyncOperationTimeoutSeconds = 30;
-    protected static final int MaxRetryTimes = 3;
+    private static final int AsyncOperationTimeoutSeconds = 30;
+    private static final int MaxRetryTimes = 3;
 
     private final long minIndexCountPerBucket;
 
@@ -72,7 +72,7 @@ public class BucketDelayedDeliveryTracker extends InMemoryDelayedDeliveryTracker
 
     private final ManagedCursor cursor;
 
-    public final BucketSnapshotStorage bucketSnapshotStorage;
+    private final BucketSnapshotStorage bucketSnapshotStorage;
 
     private long numberDelayedMessages;
 
