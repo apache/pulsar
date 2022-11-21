738f186b9b42f35a1c121f508563cdfd6ff7c404
Paul Gier <paul.gier@datastax.com>
2022-11-20 21:27:51 -0600

    [improve][test] Remove prometheus tests from flaky group (#18554)
    Signed-off-by: Paul Gier <paul.gier@datastax.com>

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/PrometheusMetricsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/PrometheusMetricsTest.java
@@ -86,7 +86,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-@Test(groups = "flaky")
+@Test(groups = "broker")
 public class PrometheusMetricsTest extends BrokerTestBase {
 
     @BeforeMethod(alwaysRun = true)
