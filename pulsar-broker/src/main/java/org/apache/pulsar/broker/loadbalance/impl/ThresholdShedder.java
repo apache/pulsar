42dc572bf34fb0889109026219aaa2254d9b5d69
Jiwe Guo <technoboy@apache.org>
2022-11-27 20:47:09 +0800

    Revert "[fix][load-balancer] skip mis-configured resource usage(>100%) in load computation (#16937)"
    This reverts commit eb833e6ffed472a82e4b2ea5ce6271c59246e119.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/impl/ThresholdShedder.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/impl/ThresholdShedder.java
@@ -56,37 +56,16 @@ public class ThresholdShedder implements LoadSheddingStrategy {
     private static final double LOWER_BOUNDARY_THRESHOLD_MARGIN = 0.5;
 
     private static final double MB = 1024 * 1024;
-
-    private static final long LOAD_LOG_SAMPLE_DELAY_IN_SEC = 5 * 60; // 5 mins
     private final Map<String, Double> brokerAvgResourceUsage = new HashMap<>();
-    private long lastSampledLoadLogTS = 0;
-
-
-    private static int toPercentage(double usage) {
-        return (int) (usage * 100);
-    }
-
-    private boolean canSampleLog() {
-        long now = System.currentTimeMillis() / 1000;
-        boolean sampleLog = now - lastSampledLoadLogTS >= LOAD_LOG_SAMPLE_DELAY_IN_SEC;
-        if (sampleLog) {
-            lastSampledLoadLogTS = now;
-        }
-        return sampleLog;
-    }
 
     @Override
     public Multimap<String, String> findBundlesForUnloading(final LoadData loadData, final ServiceConfiguration conf) {
         selectedBundlesCache.clear();
-        boolean sampleLog = canSampleLog();
         final double threshold = conf.getLoadBalancerBrokerThresholdShedderPercentage() / 100.0;
         final Map<String, Long> recentlyUnloadedBundles = loadData.getRecentlyUnloadedBundles();
         final double minThroughputThreshold = conf.getLoadBalancerBundleUnloadMinThroughputThreshold() * MB;
 
-        final double avgUsage = getBrokerAvgUsage(loadData, conf, sampleLog);
-        if (sampleLog) {
-            log.info("brokers' resource avgUsage:{}%", toPercentage(avgUsage));
-        }
+        final double avgUsage = getBrokerAvgUsage(loadData, conf.getLoadBalancerHistoryResourcePercentage(), conf);
 
         if (avgUsage == 0) {
             log.warn("average max resource usage is 0");
@@ -98,9 +77,8 @@ public class ThresholdShedder implements LoadSheddingStrategy {
             final double currentUsage = brokerAvgResourceUsage.getOrDefault(broker, 0.0);
 
             if (currentUsage < avgUsage + threshold) {
-                if (sampleLog) {
-                    log.info("[{}] broker is not overloaded, ignoring at this point, currentUsage:{}%",
-                            broker, toPercentage(currentUsage));
+                if (log.isDebugEnabled()) {
+                    log.debug("[{}] broker is not overloaded, ignoring at this point", broker);
                 }
                 return;
             }
@@ -111,13 +89,14 @@ public class ThresholdShedder implements LoadSheddingStrategy {
             double minimumThroughputToOffload = brokerCurrentThroughput * percentOfTrafficToOffload;
 
             if (minimumThroughputToOffload < minThroughputThreshold) {
-                if (sampleLog) {
-                    log.info("[{}] broker is planning to shed throughput {} MByte/s less than "
+                if (log.isDebugEnabled()) {
+                    log.debug("[{}] broker is planning to shed throughput {} MByte/s less than "
                                     + "minimumThroughputThreshold {} MByte/s, skipping bundle unload.",
                             broker, minimumThroughputToOffload / MB, minThroughputThreshold / MB);
                 }
                 return;
             }
+
             log.info(
                     "Attempting to shed load on {}, which has max resource usage above avgUsage  and threshold {}%"
                             + " > {}% + {}% -- Offloading at least {} MByte/s of traffic, left throughput {} MByte/s",
@@ -168,16 +147,15 @@ public class ThresholdShedder implements LoadSheddingStrategy {
                 });
     }
 
-    private double getBrokerAvgUsage(final LoadData loadData,
-                                     final ServiceConfiguration conf, boolean sampleLog) {
-        double historyPercentage = conf.getLoadBalancerHistoryResourcePercentage();
+    private double getBrokerAvgUsage(final LoadData loadData, final double historyPercentage,
+                                     final ServiceConfiguration conf) {
         double totalUsage = 0.0;
         int totalBrokers = 0;
 
         for (Map.Entry<String, BrokerData> entry : loadData.getBrokerData().entrySet()) {
             LocalBrokerData localBrokerData = entry.getValue().getLocalData();
             String broker = entry.getKey();
-            totalUsage += updateAvgResourceUsage(broker, localBrokerData, historyPercentage, conf, sampleLog);
+            totalUsage += updateAvgResourceUsage(broker, localBrokerData, historyPercentage, conf);
             totalBrokers++;
         }
 
@@ -185,8 +163,7 @@ public class ThresholdShedder implements LoadSheddingStrategy {
     }
 
     private double updateAvgResourceUsage(String broker, LocalBrokerData localBrokerData,
-                                          final double historyPercentage, final ServiceConfiguration conf,
-                                          boolean sampleLog) {
+                                          final double historyPercentage, final ServiceConfiguration conf) {
         Double historyUsage =
                 brokerAvgResourceUsage.get(broker);
         double resourceUsage = localBrokerData.getMaxResourceUsageWithWeight(
@@ -194,41 +171,6 @@ public class ThresholdShedder implements LoadSheddingStrategy {
                 conf.getLoadBalancerMemoryResourceWeight(), conf.getLoadBalancerDirectMemoryResourceWeight(),
                 conf.getLoadBalancerBandwithInResourceWeight(),
                 conf.getLoadBalancerBandwithOutResourceWeight());
-
-        if (sampleLog) {
-            log.info("{} broker load: historyUsage={}%, resourceUsage={}%",
-                    broker,
-                    historyUsage == null ? 0 : toPercentage(historyUsage),
-                    toPercentage(resourceUsage));
-        }
-
-        // wrap if resourceUsage is bigger than 1.0
-        if (resourceUsage > 1.0) {
-            log.error("{} broker resourceUsage is bigger than 100%. "
-                            + "Some of the resource limits are mis-configured. "
-                            + "Try to disable the error resource signals by setting their weights to zero "
-                            + "or fix the resource limit configurations. "
-                            + "Ref:https://pulsar.apache.org/docs/administration-load-balance/#thresholdshedder "
-                            + "ResourceUsage:[{}], "
-                            + "CPUResourceWeight:{}, MemoryResourceWeight:{}, DirectMemoryResourceWeight:{}, "
-                            + "BandwithInResourceWeight:{}, BandwithOutResourceWeight:{}",
-                    broker,
-                    localBrokerData.printResourceUsage(),
-                    conf.getLoadBalancerCPUResourceWeight(),
-                    conf.getLoadBalancerMemoryResourceWeight(),
-                    conf.getLoadBalancerDirectMemoryResourceWeight(),
-                    conf.getLoadBalancerBandwithInResourceWeight(),
-                    conf.getLoadBalancerBandwithOutResourceWeight());
-
-            resourceUsage = localBrokerData.getMaxResourceUsageWithWeightWithinLimit(
-                    conf.getLoadBalancerCPUResourceWeight(),
-                    conf.getLoadBalancerMemoryResourceWeight(), conf.getLoadBalancerDirectMemoryResourceWeight(),
-                    conf.getLoadBalancerBandwithInResourceWeight(),
-                    conf.getLoadBalancerBandwithOutResourceWeight());
-
-            log.warn("{} broker recomputed max resourceUsage={}%. Skipped usage signals bigger than 100%",
-                    broker, toPercentage(resourceUsage));
-        }
         historyUsage = historyUsage == null
                 ? resourceUsage : historyUsage * historyPercentage + (1 - historyPercentage) * resourceUsage;
 
@@ -239,7 +181,7 @@ public class ThresholdShedder implements LoadSheddingStrategy {
     private void tryLowerBoundaryShedding(LoadData loadData, ServiceConfiguration conf) {
         // Select the broker with the most resource usage.
         final double threshold = conf.getLoadBalancerBrokerThresholdShedderPercentage() / 100.0;
-        final double avgUsage = getBrokerAvgUsage(loadData, conf, canSampleLog());
+        final double avgUsage = getBrokerAvgUsage(loadData, conf.getLoadBalancerHistoryResourcePercentage(), conf);
         Pair<Boolean, String> result = getMaxUsageBroker(loadData, threshold, avgUsage);
         boolean hasBrokerBelowLowerBound = result.getLeft();
         String maxUsageBroker = result.getRight();
