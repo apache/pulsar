beec9d2c5508c101724daa866a58e94e4891bc9e
tison <wander4096@gmail.com>
2022-12-09 11:34:26 +0800

    license header
    Signed-off-by: tison <wander4096@gmail.com>

--- pulsar-common/src/main/java/org/apache/pulsar/common/util/ExceptionHandler.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/util/ExceptionHandler.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.pulsar.common.util;
 
 /**
