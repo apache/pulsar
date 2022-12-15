4ceb60321ce310c7900a9e8cb80a76838bb1cab4
Yang Yang <yyang@streamnative.io>
2022-12-15 16:00:35 +0800

    Fix the conversion from the response to string in client error handling

--- pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/BaseResource.java
+++ pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/BaseResource.java
@@ -324,7 +324,7 @@ public abstract class BaseResource {
         } catch (Exception ex) {
             try {
                 return ObjectMapperFactory.getThreadLocal().readValue(
-                        e.getResponse().getEntity().toString(), ErrorData.class).reason;
+                        e.getResponse().readEntity(String.class), ErrorData.class).reason;
             } catch (Exception ex1) {
                 try {
                     return ObjectMapperFactory.getThreadLocal().readValue(e.getMessage(), ErrorData.class).reason;
