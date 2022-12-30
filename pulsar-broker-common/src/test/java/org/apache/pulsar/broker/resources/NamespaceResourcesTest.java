39e04ef1d5479a2c99aae1593195eb198198ca3f
Ayman Khalil <ayman.khalil@datastax.com>
2022-12-29 16:40:16 -0800

    Add GetPolicesAsync tests

--- pulsar-broker-common/src/test/java/org/apache/pulsar/broker/resources/NamespaceResourcesTest.java
+++ pulsar-broker-common/src/test/java/org/apache/pulsar/broker/resources/NamespaceResourcesTest.java
@@ -20,15 +20,45 @@ package org.apache.pulsar.broker.resources;
 
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+import java.util.concurrent.CompletableFuture;
+import org.apache.pulsar.common.naming.NamespaceName;
+import org.apache.pulsar.common.policies.data.Policies;
+import org.apache.pulsar.metadata.api.MetadataCache;
+import org.apache.pulsar.metadata.api.MetadataStore;
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class NamespaceResourcesTest {
+
+    private MetadataStore mockMetadataStore;
+    private NamespaceResources namespaceResources;
+
+    @BeforeMethod
+    public void setUp() {
+        mockMetadataStore = Mockito.mock(MetadataStore.class);
+        Mockito.doReturn(Mockito.mock(MetadataCache.class)).when(mockMetadataStore).getMetadataCache(Policies.class);
+        Mockito.doReturn(CompletableFuture.completedFuture(null)).when(mockMetadataStore).sync(Mockito.anyString());
+        namespaceResources = new NamespaceResources(mockMetadataStore, 0);
+    }
     @Test
     public void test_pathIsFromNamespace() {
         assertFalse(NamespaceResources.pathIsFromNamespace("/admin/clusters"));
         assertFalse(NamespaceResources.pathIsFromNamespace("/admin/policies"));
         assertFalse(NamespaceResources.pathIsFromNamespace("/admin/policies/my-tenant"));
         assertTrue(NamespaceResources.pathIsFromNamespace("/admin/policies/my-tenant/my-ns"));
+    }
+
+    @Test
+    public void testGetPolicesAsync() {
+        namespaceResources.getPoliciesAsync(NamespaceName.get("public/default"));
+        Mockito.verify(mockMetadataStore, Mockito.never()).sync(Mockito.anyString());
+    }
+
+    @Test
+    public void testGetPolicesAsyncAndRefresh() {
+        namespaceResources.getPoliciesAsync(NamespaceName.get("public/default"), true);
+        Mockito.verify(mockMetadataStore, Mockito.times(1)).sync("/admin/policies/public/default");
     }
 }
