a6516a8d19896316907c9904d7ed823e9282aef2
Nicolò Boschi <boschi1997@gmail.com>
2023-01-04 16:25:36 +0100

    [fix][broker] Topic could be in fenced state forever if deletion fails (#19129)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -1268,9 +1268,9 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
                                                                 topic, exception.getMessage());
                                                         deleteLedgerComplete(ctx);
                                                     } else {
-                                                        unfenceTopicToResume();
                                                         log.error("[{}] Error deleting topic",
                                                                 topic, exception);
+                                                        unfenceTopicToResume();
                                                         deleteFuture.completeExceptionally(
                                                                 new PersistenceException(exception));
                                                     }
@@ -1289,6 +1289,11 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
                 });
 
                 return deleteFuture;
+                }).whenComplete((value, ex) -> {
+                    if (ex != null) {
+                        log.error("[{}] Error deleting topic", topic, ex);
+                        unfenceTopicToResume();
+                    }
                 });
         } finally {
             lock.writeLock().unlock();
