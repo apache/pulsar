3e63d5494b46e2d9c7f1f3ef3f1edaddbd837346
Qiang Zhao <mattisonchao@apache.org>
2023-01-09 09:41:54 +0800

    Update pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/LinuxInfoUtils.java

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/LinuxInfoUtils.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/LinuxInfoUtils.java
@@ -79,7 +79,6 @@ public class LinuxInfoUtils {
      * @return Total cpu limit
      */
     public static double getTotalCpuLimit(boolean isCGroupsEnabled) {
-
         if (isCGroupsEnabled) {
             try {
                 long quota = readLongFromFile(Paths.get(CGROUPS_CPU_LIMIT_QUOTA_PATH));
