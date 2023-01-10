d0e0aa4353ba4f99674f36a300ce4b91aec56609
Nicolò Boschi <boschi1997@gmail.com>
2023-01-10 11:36:40 +0100

    javadoc

--- pulsar-common/src/main/java/org/apache/pulsar/common/util/netty/NettyChannelUtil.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/util/netty/NettyChannelUtil.java
@@ -21,18 +21,43 @@ package org.apache.pulsar.common.util.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelOutboundInvoker;
+import io.netty.channel.VoidChannelPromise;
 
 /**
  * Contains utility methods for working with Netty Channels.
  */
-public class NettyChannelUtil {
+public final class NettyChannelUtil {
 
-    public static void writeAndFlushWithVoidPromise(ChannelOutboundInvoker ctx, ByteBuf cmd) {
-        ctx.writeAndFlush(cmd, ctx.voidPromise());
+    private NettyChannelUtil() {
     }
 
-    public static void writeAndFlushWithClosePromise(ChannelOutboundInvoker ctx, ByteBuf cmd) {
-        ctx.writeAndFlush(cmd).addListener(ChannelFutureListener.CLOSE);
+    /**
+     * Write and flush the message to the channel.
+     *
+     * The promise is an instance of {@link VoidChannelPromise} that properly propagates exceptions up to the pipeline.
+     * Netty has many ad-hoc optimization if the promise is an instance of {@link VoidChannelPromise}.
+     * Lastly, it reduces pollution of useless {@link io.netty.channel.ChannelPromise} objects created
+     * by the default write and flush method {@link ChannelOutboundInvoker#writeAndFlush(Object)}.
+     * See https://stackoverflow.com/q/54169262 and https://stackoverflow.com/a/9030420 for more details.
+     *
+     * @param ctx channel's context
+     * @param msg buffer to write in the channel
+     */
+    public static void writeAndFlushWithVoidPromise(ChannelOutboundInvoker ctx, ByteBuf msg) {
+        ctx.writeAndFlush(msg, ctx.voidPromise());
+    }
+
+    /**
+     * Write and flush the message to the channel and the close the channel.
+     *
+     * This method is particularly helpful when the connection is in an invalid state
+     * and therefore a new connection must be created to continue.
+     *
+     * @param ctx channel's context
+     * @param msg buffer to write in the channel
+     */
+    public static void writeAndFlushWithClosePromise(ChannelOutboundInvoker ctx, ByteBuf msg) {
+        ctx.writeAndFlush(msg).addListener(ChannelFutureListener.CLOSE);
     }
 
 }
