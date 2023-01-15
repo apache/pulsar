efb251cec6ff7dd7a92bc8ff99e8b2fe04d1eb00
Michael Marshall <mmarshall@apache.org>
2023-01-15 05:18:21 -0600

    [cleanup][broker] Remove AuthorizationProvider methods deprecated in 2.7 and 2.9 (#19182)

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/authorization/AuthorizationProvider.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/authorization/AuthorizationProvider.java
@@ -23,10 +23,8 @@ import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.pulsar.broker.ServiceConfiguration;
 import org.apache.pulsar.broker.authentication.AuthenticationDataSource;
-import org.apache.pulsar.broker.cache.ConfigurationCacheService;
 import org.apache.pulsar.broker.resources.PulsarResources;
 import org.apache.pulsar.common.naming.NamespaceName;
 import org.apache.pulsar.common.naming.TopicName;
@@ -92,31 +90,12 @@ public interface AuthorizationProvider extends Closeable {
      *
      * @param conf
      *            broker config object
-     * @param configCache
-     *            pulsar zk configuration cache service
-     * @throws IOException
-     *             if the initialization fails
-     *
-     * @deprecated ConfigurationCacheService is not supported anymore as a way to get access to metadata.
-     * @see #initialize(ServiceConfiguration, PulsarResources)
-     */
-    @Deprecated
-    default void initialize(ServiceConfiguration conf, ConfigurationCacheService configCache) throws IOException {
-    }
-
-    /**
-     * Perform initialization for the authorization provider.
-     *
-     * @param conf
-     *            broker config object
      * @param pulsarResources
      *            Resources component for access to metadata
      * @throws IOException
      *             if the initialization fails
      */
     default void initialize(ServiceConfiguration conf, PulsarResources pulsarResources) throws IOException {
-        // For compatibility, call the old deprecated initialize
-        initialize(conf, (ConfigurationCacheService) null);
     }
 
     /**
@@ -248,39 +227,6 @@ public interface AuthorizationProvider extends Closeable {
             String authDataJson);
 
     /**
-     * Grant authorization-action permission on a tenant to the given client.
-     * @param tenantName
-     * @param originalRole role not overriden by proxy role if request do pass through proxy
-     * @param role originalRole | proxyRole if the request didn't pass through proxy
-     * @param operation
-     * @param authData
-     * @return CompletableFuture<Boolean>
-     */
-    @Deprecated
-    default CompletableFuture<Boolean> allowTenantOperationAsync(String tenantName, String originalRole, String role,
-                                                            TenantOperation operation,
-                                                            AuthenticationDataSource authData) {
-        return allowTenantOperationAsync(
-            tenantName,
-            StringUtils.isBlank(originalRole) ? role : originalRole,
-            operation,
-            authData
-        );
-    }
-
-    @Deprecated
-    default Boolean allowTenantOperation(String tenantName, String originalRole, String role, TenantOperation operation,
-                                      AuthenticationDataSource authData) {
-        try {
-            return allowTenantOperationAsync(tenantName, originalRole, role, operation, authData).get();
-        } catch (InterruptedException e) {
-            throw new RestException(e);
-        } catch (ExecutionException e) {
-            throw new RestException(e.getCause());
-        }
-    }
-
-    /**
      * Check if a given <tt>role</tt> is allowed to execute a given <tt>operation</tt> on the tenant.
      *
      * @param tenantName tenant name
@@ -349,44 +295,6 @@ public interface AuthorizationProvider extends Closeable {
     }
 
     /**
-     * Grant authorization-action permission on a namespace to the given client.
-     *
-     * @param namespaceName
-     * @param role
-     * @param operation
-     * @param authData
-     * @return CompletableFuture<Boolean>
-     */
-    @Deprecated
-    default CompletableFuture<Boolean> allowNamespaceOperationAsync(NamespaceName namespaceName,
-                                                                    String originalRole,
-                                                                    String role,
-                                                                    NamespaceOperation operation,
-                                                                    AuthenticationDataSource authData) {
-        return allowNamespaceOperationAsync(
-            namespaceName,
-            StringUtils.isBlank(originalRole) ? role : originalRole,
-            operation,
-            authData
-        );
-    }
-
-    @Deprecated
-    default Boolean allowNamespaceOperation(NamespaceName namespaceName,
-                                            String originalRole,
-                                            String role,
-                                            NamespaceOperation operation,
-                                            AuthenticationDataSource authData) {
-        try {
-            return allowNamespaceOperationAsync(namespaceName, originalRole, role, operation, authData).get();
-        } catch (InterruptedException e) {
-            throw new RestException(e);
-        } catch (ExecutionException e) {
-            throw new RestException(e.getCause());
-        }
-    }
-
-    /**
      * Check if a given <tt>role</tt> is allowed to execute a given policy <tt>operation</tt> on the namespace.
      *
      * @param namespaceName namespace name
@@ -425,48 +333,6 @@ public interface AuthorizationProvider extends Closeable {
     }
 
     /**
-     * Grant authorization-action permission on a namespace to the given client.
-     * @param namespaceName
-     * @param originalRole role not overriden by proxy role if request do pass through proxy
-     * @param role originalRole | proxyRole if the request didn't pass through proxy
-     * @param operation
-     * @param authData
-     * @return CompletableFuture<Boolean>
-     */
-    @Deprecated
-    default CompletableFuture<Boolean> allowNamespacePolicyOperationAsync(NamespaceName namespaceName,
-                                                                          PolicyName policy,
-                                                                          PolicyOperation operation,
-                                                                          String originalRole,
-                                                                          String role,
-                                                                          AuthenticationDataSource authData) {
-        return allowNamespacePolicyOperationAsync(
-            namespaceName,
-            policy,
-            operation,
-            StringUtils.isBlank(originalRole) ? role : originalRole,
-            authData
-        );
-    }
-
-    @Deprecated
-    default Boolean allowNamespacePolicyOperation(NamespaceName namespaceName,
-                                                  PolicyName policy,
-                                                  PolicyOperation operation,
-                                                  String originalRole,
-                                                  String role,
-                                                  AuthenticationDataSource authData) {
-        try {
-            return allowNamespacePolicyOperationAsync(
-                namespaceName, policy, operation, originalRole, role, authData).get();
-        } catch (InterruptedException e) {
-            throw new RestException(e);
-        } catch (ExecutionException e) {
-            throw new RestException(e.getCause());
-        }
-    }
-
-    /**
      * Check if a given <tt>role</tt> is allowed to execute a given topic <tt>operation</tt> on the topic.
      *
      * @param topic topic name
@@ -494,44 +360,6 @@ public interface AuthorizationProvider extends Closeable {
                                         AuthenticationDataSource authData) {
         try {
             return allowTopicOperationAsync(topicName, role, operation, authData).get();
-        } catch (InterruptedException e) {
-            throw new RestException(e);
-        } catch (ExecutionException e) {
-            throw new RestException(e.getCause());
-        }
-    }
-
-    /**
-     * Grant authorization-action permission on a topic to the given client.
-     * @param topic
-     * @param originalRole role not overriden by proxy role if request do pass through proxy
-     * @param role originalRole | proxyRole if the request didn't pass through proxy
-     * @param operation
-     * @param authData
-     * @return CompletableFuture<Boolean>
-     */
-    @Deprecated
-    default CompletableFuture<Boolean> allowTopicOperationAsync(TopicName topic,
-                                                                String originalRole,
-                                                                String role,
-                                                                TopicOperation operation,
-                                                                AuthenticationDataSource authData) {
-        return allowTopicOperationAsync(
-            topic,
-            StringUtils.isBlank(originalRole) ? role : originalRole,
-            operation,
-            authData
-        );
-    }
-
-    @Deprecated
-    default Boolean allowTopicOperation(TopicName topicName,
-                                        String originalRole,
-                                        String role,
-                                        TopicOperation operation,
-                                        AuthenticationDataSource authData) {
-        try {
-            return allowTopicOperationAsync(topicName, originalRole, role, operation, authData).get();
         } catch (InterruptedException e) {
             throw new RestException(e);
         } catch (ExecutionException e) {
