d0425d4cf0113ad1aa04112500ab9fe173434761
Michael Marshall <mmarshall@apache.org>
2023-02-07 23:42:25 -0600

    Add AuthorizationService test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/auth/AuthorizationTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/auth/AuthorizationTest.java
@@ -23,8 +23,11 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 import com.google.common.collect.Sets;
+import java.util.Collections;
 import java.util.EnumSet;
+import org.apache.pulsar.broker.ServiceConfiguration;
 import org.apache.pulsar.broker.authorization.AuthorizationService;
+import org.apache.pulsar.broker.resources.PulsarResources;
 import org.apache.pulsar.client.admin.PulsarAdmin;
 import org.apache.pulsar.client.admin.PulsarAdminBuilder;
 import org.apache.pulsar.common.naming.TopicDomain;
@@ -33,6 +36,7 @@ import org.apache.pulsar.common.policies.data.AuthAction;
 import org.apache.pulsar.common.policies.data.ClusterData;
 import org.apache.pulsar.common.policies.data.SubscriptionAuthMode;
 import org.apache.pulsar.common.policies.data.TenantInfoImpl;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -227,6 +231,30 @@ public class AuthorizationTest extends MockedPulsarServiceBaseTest {
         admin.namespaces().deleteNamespace("p1/c1/ns1");
         admin.tenants().deleteTenant("p1");
         admin.clusters().deleteCluster("c1");
+    }
+
+    @Test
+    public void testOriginalRoleValidation() throws Exception {
+        ServiceConfiguration conf = new ServiceConfiguration();
+        conf.setProxyRoles(Collections.singleton("proxy"));
+        AuthorizationService auth = new AuthorizationService(conf, Mockito.mock(PulsarResources.class));
+
+        // Original principal should be supplied when authenticatedPrincipal is proxy role
+        assertTrue(auth.validateOriginalPrincipal("proxy", "client", null));
+
+        // Non proxy role should not supply originalPrincipal
+        assertTrue(auth.validateOriginalPrincipal("client", "", null));
+        assertTrue(auth.validateOriginalPrincipal("client", null, null));
+
+        // Proxy role must supply an original principal
+        assertFalse(auth.validateOriginalPrincipal("proxy", "", null));
+        assertFalse(auth.validateOriginalPrincipal("proxy", null, null));
+
+        // OriginalPrincipal cannot be proxy role
+        assertFalse(auth.validateOriginalPrincipal("proxy", "proxy", null));
+        assertFalse(auth.validateOriginalPrincipal("client", "proxy", null));
+        assertFalse(auth.validateOriginalPrincipal("", "proxy", null));
+        assertFalse(auth.validateOriginalPrincipal(null, "proxy", null));
     }
 
     @Test
