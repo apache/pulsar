67ae17b748c5513b891a24e4ee9633adb2ad3ec0
Zixuan Liu <nodeces@gmail.com>
2023-02-21 22:55:41 +0800

    Fix style
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -1419,7 +1419,8 @@ public class BrokerService implements Closeable {
 
                 boolean isTlsEnabled = data.isBrokerClientTlsEnabled() || conf.isBrokerClientTlsEnabled();
                 if (isTlsEnabled && StringUtils.isEmpty(data.getServiceUrlTls())) {
-                    throw new IllegalArgumentException("serviceUrlTls is empty, brokerClientTlsEnabled: " + isTlsEnabled);
+                    throw new IllegalArgumentException("serviceUrlTls is empty, brokerClientTlsEnabled: "
+                            + isTlsEnabled);
                 } else if (StringUtils.isEmpty(data.getServiceUrl())) {
                     throw new IllegalArgumentException("serviceUrl is empty, brokerClientTlsEnabled: " + isTlsEnabled);
                 }
