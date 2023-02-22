336b02a88b1a2a0938be4ff48a2ecaf0ae104425
Zixuan Liu <nodeces@gmail.com>
2023-02-22 10:18:13 +0800

    [fix][broker] Fix geo-replication admin
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ReplicatorAdminTlsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ReplicatorAdminTlsTest.java
@@ -19,26 +19,24 @@
 package org.apache.pulsar.broker.service;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 import java.util.List;
 import java.util.Optional;
+import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.client.admin.internal.PulsarAdminImpl;
 import org.apache.pulsar.client.impl.conf.ClientConfigurationData;
-import org.apache.pulsar.client.impl.PulsarClientImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.pulsar.common.util.collections.ConcurrentOpenHashMap;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 @Test(groups = "broker")
-public class ReplicatorTlsTest extends ReplicatorTestBase {
+public class ReplicatorAdminTlsTest extends ReplicatorTestBase {
 
     @Override
     @BeforeClass(timeOut = 300000)
     public void setup() throws Exception {
-        config1.setBrokerClientTlsEnabled(true);
-        config2.setBrokerClientTlsEnabled(true);
-        config3.setBrokerClientTlsEnabled(true);
         super.setup();
     }
 
@@ -49,25 +47,23 @@ public class ReplicatorTlsTest extends ReplicatorTestBase {
     }
 
     @Test
-    public void testReplicationClient() throws Exception {
-        log.info("--- Starting ReplicatorTlsTest::testReplicationClient ---");
+    public void testReplicationAdmin() throws Exception {
         for (BrokerService ns : List.of(ns1, ns2, ns3)) {
-            // load the client
-            ns.getReplicationClient(cluster1, Optional.of(admin1.clusters().getCluster(cluster1)));
-            ns.getReplicationClient(cluster2, Optional.of(admin1.clusters().getCluster(cluster2)));
-            ns.getReplicationClient(cluster3, Optional.of(admin1.clusters().getCluster(cluster3)));
+            // load the admin
+            ns.getClusterPulsarAdmin(cluster1, Optional.of(admin1.clusters().getCluster(cluster1)));
+            ns.getClusterPulsarAdmin(cluster2, Optional.of(admin1.clusters().getCluster(cluster2)));
+            ns.getClusterPulsarAdmin(cluster3, Optional.of(admin1.clusters().getCluster(cluster3)));
 
-            // verify the client
-            ns.getReplicationClients().forEach((cluster, client) -> {
-                ClientConfigurationData configuration = ((PulsarClientImpl) client).getConfiguration();
-                assertTrue(configuration.isUseTls());
-                assertEquals(configuration.getTlsTrustCertsFilePath(), caCertFilePath);
-                assertEquals(configuration.getTlsKeyFilePath(), clientKeyFilePath);
-                assertEquals(configuration.getTlsCertificateFilePath(), clientCertFilePath);
+            // verify the admin
+            ConcurrentOpenHashMap<String, PulsarAdmin> clusterAdmins = ns.getClusterAdmins();
+            assertFalse(clusterAdmins.isEmpty());
+            clusterAdmins.forEach((cluster, admin) -> {
+                ClientConfigurationData clientConfigData = ((PulsarAdminImpl) admin).getClientConfigData();
+                assertEquals(clientConfigData.getTlsTrustCertsFilePath(), caCertFilePath);
+                assertEquals(clientConfigData.getTlsKeyFilePath(), clientKeyFilePath);
+                assertEquals(clientConfigData.getTlsCertificateFilePath(), clientCertFilePath);
+                assertTrue(clientConfigData.isUseTls());
             });
         }
     }
-
-    private static final Logger log = LoggerFactory.getLogger(ReplicatorTlsTest.class);
-
 }
