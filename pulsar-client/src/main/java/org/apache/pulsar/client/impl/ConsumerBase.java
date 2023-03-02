bad210406b86112497dfa93ea6450b1ace5bd6ef
fengyubiao <yubiao.feng@streamnative.io>
2023-03-02 14:33:04 +0800

    remove unnecessary imports

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerBase.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerBase.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
