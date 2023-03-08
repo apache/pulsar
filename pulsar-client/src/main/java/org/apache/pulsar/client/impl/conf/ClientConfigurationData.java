5f3d4cbb0aa209d35aa79d7f2928608874b094f4
Yunze Xu <xyzinfernity@163.com>
2023-03-08 17:09:11 +0800

    PIP-254: Support configuring client version
    ### Motivation
    
    https://github.com/apache/pulsar/issues/19705
    
    ### Modifications
    
    - Add the `ClientBuilderImpl#description` method to add the description
      to the original client version string that is set in `CommandConnect`
      and `CommandAuthResponse`.
    - Add `testClientVersion` to cover these two cases.

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/conf/ClientConfigurationData.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/conf/ClientConfigurationData.java
@@ -379,6 +379,12 @@ public class ClientConfigurationData implements Serializable, Cloneable {
     @Secret
     private String socks5ProxyPassword;
 
+    @ApiModelProperty(
+            name = "description",
+            value = "The extra description of the client version."
+    )
+    private String description;
+
     /**
      * Gets the authentication settings for the client.
      *
