4561c40dcbb887f2fd7baf91cfebca0631e05da2
thetumbled <843221020@qq.com>
2023-03-10 10:49:58 +0800

    add code.

--- pulsar-testclient/src/main/java/org/apache/pulsar/testclient/PerformanceProducer.java
+++ pulsar-testclient/src/main/java/org/apache/pulsar/testclient/PerformanceProducer.java
@@ -479,6 +479,7 @@ public class PerformanceProducer {
 
             oldTime = now;
         }
+        executor.shutdown();
     }
 
     static IMessageFormatter getMessageFormatter(String formatterClass) {
