0dff064d421ec89b2749cbc09ce4292e332d25a5
Qiang Zhao <mattisonchao@apache.org>
2023-03-20 16:39:09 +0800

    [fix][meta] Follow up #19817, Fix race condition between ResourceLock update and invalidation (#19844)
    (cherry picked from commit 2637bda91d46f443762b22ac9963935725af2c8a)

--- pulsar-metadata/src/main/java/org/apache/pulsar/metadata/coordination/impl/LockManagerImpl.java
+++ pulsar-metadata/src/main/java/org/apache/pulsar/metadata/coordination/impl/LockManagerImpl.java
@@ -125,7 +125,7 @@ class LockManagerImpl<T> implements LockManager<T> {
             if (se == SessionEvent.SessionReestablished) {
                 log.info("Metadata store session has been re-established. Revalidating all the existing locks.");
                 for (ResourceLockImpl<T> lock : locks.values()) {
-                    futures.add(lock.revalidate(lock.getValue(), true, true));
+                    futures.add(lock.silentRevalidateOnce());
                 }
 
             } else if (se == SessionEvent.Reconnected) {
