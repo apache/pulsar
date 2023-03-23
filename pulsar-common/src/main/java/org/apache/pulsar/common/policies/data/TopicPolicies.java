b441b2f784b401e4b02a79a3db7f1120dd863895
Jiwei Guo <technoboy@apache.org>
2023-03-23 20:09:57 +0800

    [fix][broker] Fix NPE when update topic policy. (#19875)

--- pulsar-common/src/main/java/org/apache/pulsar/common/policies/data/TopicPolicies.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/policies/data/TopicPolicies.java
@@ -20,6 +20,7 @@ package org.apache.pulsar.common.policies.data;
 
 import com.google.common.collect.Sets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -182,5 +183,9 @@ public class TopicPolicies {
 
     public Set<String> getReplicationClustersSet() {
         return replicationClusters != null ? Sets.newTreeSet(this.replicationClusters) : null;
+    }
+
+    public Map<String, SubscriptionPolicies> getSubscriptionPolicies() {
+        return subscriptionPolicies == null ? Collections.emptyMap() : subscriptionPolicies;
     }
 }
