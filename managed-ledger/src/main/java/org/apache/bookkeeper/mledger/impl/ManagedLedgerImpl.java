9381b8552d9f25330df552465ea09ca2b84e0fd6
Zhangao <zhangmingao@apache.org>
2023-04-03 14:37:32 +0800

    [branch-2.10][fix][broker] Fix index generator is not rollback after entries are failed added (#19980)
    Co-authored-by: gavingaozhangmin <gavingaozhangmin@didiglobal.com>

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -840,6 +840,13 @@ public class ManagedLedgerImpl implements ManagedLedger, CreateCallback {
         lastAddEntryTimeMs = System.currentTimeMillis();
     }
 
+    protected void afterFailedAddEntry(int numOfMessages) {
+        if (managedLedgerInterceptor == null) {
+            return;
+        }
+        managedLedgerInterceptor.afterFailedAddEntry(numOfMessages);
+    }
+
     private boolean beforeAddEntry(OpAddEntry addOperation) {
         // if no interceptor, just return true to make sure addOperation will be initiate()
         if (managedLedgerInterceptor == null) {
