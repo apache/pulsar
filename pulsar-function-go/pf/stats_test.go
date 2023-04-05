6a86cd69a59d448b17a35c7fb94ae98902cc6aa7
Andy Walker <andy@andy.dev>
2023-04-05 14:45:56 -0400

    check user metric len before iterating
    - fixes #20020

--- pulsar-function-go/pf/stats_test.go
+++ pulsar-function-go/pf/stats_test.go
@@ -20,6 +20,7 @@
 package pf
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"math"
@@ -28,6 +29,7 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 
@@ -256,4 +258,28 @@ func TestUserMetrics(t *testing.T) {
 	resp.Body.Close()
 	gi.close()
 	metricsServicer.close()
+}
+
+func TestInstanceControlMetrics(t *testing.T) {
+	instance := newGoInstance()
+	t.Cleanup(instance.close)
+	instanceClient := instanceCommunicationClient(t, instance)
+	metrics, err := instanceClient.GetMetrics(context.Background(), &empty.Empty{})
+	assert.NoError(t, err, "err communicating with instance control: %v", err)
+	assert.Len(t, metrics.UserMetrics, 0, "user metrics are not empty before recording a user metric")
+
+	testLabels := []string{"userMetricTest1", "userMetricTest2"}
+
+	for value, label := range testLabels {
+		instance.context.RecordMetric(label, float64(value+1))
+	}
+	time.Sleep(time.Second)
+
+	metrics, err = instanceClient.GetMetrics(context.Background(), &empty.Empty{})
+	assert.NoError(t, err, "err communicating with instance control: %v", err)
+	assert.Greater(t, len(metrics.UserMetrics), 0, "user metrics are empty after recording a user metric")
+	for value, label := range testLabels {
+		assert.Contains(t, metrics.UserMetrics, label)
+		assert.EqualValuesf(t, value+1, metrics.UserMetrics[label], "test metric %s != %d", label, value+1)
+	}
 }
