20715ecb2b5e94700ee460768866755034deb56f
fengyubiao <yubiao.feng@streamnative.io>
2023-04-10 12:58:58 +0800

    remove args MaxDirectMemorySize

--- conf/pulsar_tools_env.sh
+++ conf/pulsar_tools_env.sh
@@ -45,10 +45,11 @@
 # Discard parameter "-Xms" of $PULSAR_MEM, which tends to be the Broker's minimum memory, to avoid using too much
 # memory by tools.
 if [ -n "$PULSAR_MEM" ]; then
+  PULSAR_MEM_ARR=("${PULSAR_MEM}")
   PULSAR_MEM_REWRITE=""
-  for i in $PULSAR_MEM
+  for i in ${PULSAR_MEM_ARR}
   do
-    if [ "${i:0:4}" != "-Xms" ];then
+    if [ "${i:0:4}" != "-Xms" ]; then
        PULSAR_MEM_REWRITE="$PULSAR_MEM_REWRITE $i";
     fi
   done
