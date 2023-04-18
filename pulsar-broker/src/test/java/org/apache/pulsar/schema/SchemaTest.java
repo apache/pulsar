da07cd9a5b4f2521952d638ef3cbccfa1dafc4c5
penghui <penghui@apache.org>
2023-04-18 11:37:18 +0800

    Revert "[fix][broker] forbid uploading BYTES schema with admin API (#18995)"
    This reverts commit ea4d3b4840ba57cfb8edfcf14b6e28e859fdce9b.

--- pulsar-broker/src/test/java/org/apache/pulsar/schema/SchemaTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/schema/SchemaTest.java
@@ -513,17 +513,13 @@ public class SchemaTest extends MockedPulsarServiceBaseTest {
         admin.topics().createPartitionedTopic(topic, 2);
 
         // set schema
-        if (!schema.equals(SchemaType.BYTES)) {
-            // don't upload bytes schema with admin API
-            // because null schema means the BYTES schema
-            SchemaInfo schemaInfo = SchemaInfoImpl
-                    .builder()
-                    .schema(new byte[0])
-                    .name("dummySchema")
-                    .type(schema)
-                    .build();
-            admin.schemas().createSchema(topic, schemaInfo);
-        }
+        SchemaInfo schemaInfo = SchemaInfoImpl
+                .builder()
+                .schema(new byte[0])
+                .name("dummySchema")
+                .type(schema)
+                .build();
+        admin.schemas().createSchema(topic, schemaInfo);
 
         Producer<byte[]> producer = pulsarClient
                 .newProducer()
@@ -548,8 +544,7 @@ public class SchemaTest extends MockedPulsarServiceBaseTest {
         Message<GenericRecord> message2 = consumer2.receive();
         if (schema == SchemaType.BYTES) {
             assertEquals(schema, message.getReaderSchema().get().getSchemaInfo().getType());
-            // default schema of AUTO_CONSUME is BYTES
-            assertTrue(message2.getReaderSchema().get().toString().contains("BYTES"));
+            assertEquals(schema, message2.getReaderSchema().get().getSchemaInfo().getType());
         } else if (schema == SchemaType.NONE) {
             // schema NONE is always reported as BYTES
             assertEquals(SchemaType.BYTES, message.getReaderSchema().get().getSchemaInfo().getType());
