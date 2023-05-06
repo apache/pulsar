4a9d75c1b1ad8efa3545b1fa9bcee4361ecda2cb
Jiwe Guo <technoboy@apache.org>
2023-05-06 22:34:24 +0800

    Fix checkstyle and compile issue

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/OneWayReplicatorTestBase.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/OneWayReplicatorTestBase.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -182,7 +182,7 @@ public abstract class OneWayReplicatorTestBase extends TestRetrySupport {
         config.setBrokerServicePortTls(Optional.of(0));
         config.setBacklogQuotaCheckIntervalInSeconds(5);
         config.setDefaultNumberOfNamespaceBundles(1);
-        config.setAllowAutoTopicCreationType(TopicType.NON_PARTITIONED);
+        config.setAllowAutoTopicCreationType(TopicType.NON_PARTITIONED.toString());
         config.setEnableReplicatedSubscriptions(true);
         config.setReplicatedSubscriptionsSnapshotFrequencyMillis(1000);
     }
