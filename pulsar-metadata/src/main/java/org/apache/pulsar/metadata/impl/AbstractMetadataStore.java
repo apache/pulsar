45aa84ca01eeb16f0cc6014544c7277211d80493
萧易客 <kmter@live.com>
2023-05-24 21:56:30 +0800

    [fix][broker] Invalidate metadata children cache after key deleted (#20363)
    (cherry picked from commit 7dcb3eab2916d42bb58fc1639dc58e11b5230997)

--- pulsar-metadata/src/main/java/org/apache/pulsar/metadata/impl/AbstractMetadataStore.java
+++ pulsar-metadata/src/main/java/org/apache/pulsar/metadata/impl/AbstractMetadataStore.java
@@ -185,6 +185,7 @@ public abstract class AbstractMetadataStore implements MetadataStoreExtended, Co
 
         if (type == NotificationType.Created || type == NotificationType.Deleted) {
             existsCache.synchronous().invalidate(path);
+            childrenCache.synchronous().invalidate(path);
             String parent = parent(path);
             if (parent != null) {
                 childrenCache.synchronous().invalidate(parent);
@@ -208,6 +209,7 @@ public abstract class AbstractMetadataStore implements MetadataStoreExtended, Co
         return storeDelete(path, expectedVersion)
                 .thenRun(() -> {
                     existsCache.synchronous().invalidate(path);
+                    childrenCache.synchronous().invalidate(path);
                     String parent = parent(path);
                     if (parent != null) {
                         childrenCache.synchronous().invalidate(parent);
