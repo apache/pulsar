b0cebfdd9a2db86c1c8b72bc9e61f5125a3bb59f
Penghui Li <penghui@apache.org>
2023-06-27 11:46:43 +0800

    Use ConcurrentHashMap

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/MessageDeduplication.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/MessageDeduplication.java
@@ -20,7 +20,6 @@ package org.apache.pulsar.broker.service.persistent;
 
 import com.google.common.annotations.VisibleForTesting;
 import io.netty.buffer.ByteBuf;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
