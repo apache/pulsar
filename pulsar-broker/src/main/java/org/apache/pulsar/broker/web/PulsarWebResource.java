45aa66aa5785eccc45a1f4fca2354238157e6cde
Demogorgon314 <kwang@streamnative.io>
2023-06-27 10:07:33 +0800

    Remove unused import

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
@@ -45,7 +45,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
