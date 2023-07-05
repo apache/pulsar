8bc3da60be36b9b95767cfcfeb1e56c59c195288
mattisonchao <mattisonchao@apache.org>
2023-07-05 16:15:08 +0800

    Support upper endpoint

--- pulsar-testclient/src/main/java/org/apache/pulsar/testclient/PerformanceProducer.java
+++ pulsar-testclient/src/main/java/org/apache/pulsar/testclient/PerformanceProducer.java
@@ -623,7 +623,8 @@ public class PerformanceProducer {
                         messageBuilder.deliverAfter(arguments.delay, TimeUnit.SECONDS);
                     } else if (arguments.delayRange != null) {
                         final long deliverAfter = ThreadLocalRandom.current()
-                                .nextLong(arguments.delayRange.lowerEndpoint(), arguments.delayRange.upperEndpoint());
+                                .nextLong(arguments.delayRange.lowerEndpoint(),
+                                        arguments.delayRange.upperEndpoint() + 1);
                         messageBuilder.deliverAfter(deliverAfter, TimeUnit.SECONDS);
                     }
                     if (arguments.setEventTime) {
