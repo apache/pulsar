a2356551d03c9ac8eadf100439029d74822164ef
fengyubiao <yubiao.feng@streamnative.io>
2023-07-06 20:55:27 +0800

    clear previous consumer use local varible cnx

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -827,7 +827,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
         synchronized (this) {
             setClientCnx(cnx);
             final SchemaInfo finalSi = si;
-            askBrokerClearPreviousConsumer().thenCompose(ignore -> {
+            askBrokerClearPreviousConsumer(cnx).thenCompose(ignore -> {
                 long requestId = client.newRequestId();
                 ByteBuf request = Commands.newSubscribe(topic, subscription, consumerId, requestId, getSubType(),
                         priorityLevel, consumerName, isDurable, startMessageIdData, metadata, readCompacted,
@@ -882,7 +882,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                     // in case it was indeed created, otherwise it might prevent new create consumer operation,
                     // since we are not necessarily closing the connection.
                     if (e.getCause() instanceof PulsarClientException.TimeoutException) {
-                        askBrokerClearPreviousConsumer();
+                        askBrokerClearPreviousConsumer(cnx);
                     }
                     // unable to create new consumer, fail operation
                     setState(State.Failed);
@@ -918,10 +918,10 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
         return future;
     }
 
-    protected CompletableFuture<ProducerResponse> askBrokerClearPreviousConsumer() {
+    protected CompletableFuture<ProducerResponse> askBrokerClearPreviousConsumer(ClientCnx cnx) {
         long closeRequestId = client.newRequestId();
         ByteBuf cmd = Commands.newCloseConsumer(consumerId, closeRequestId);
-        return connectionHandler.cnx().sendRequestWithId(cmd, closeRequestId);
+        return cnx.sendRequestWithId(cmd, closeRequestId);
     }
 
     protected void consumerIsReconnectedToBroker(ClientCnx cnx, int currentQueueSize) {
