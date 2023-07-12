668a4ee777c460f91f9091135e0b3e41cd769e0e
Cong Zhao <zhaocong@apache.org>
2023-07-12 20:03:50 +0800

    [fix][broker] Fix get topic policies as null during clean cache (#20763)

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/TopicPoliciesTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/TopicPoliciesTest.java
@@ -2986,6 +2986,7 @@ public class TopicPoliciesTest extends MockedPulsarServiceBaseTest {
             });
         }
     }
+
     @Test
     public void testGlobalTopicPolicies() throws Exception {
         final String topic = testTopic + UUID.randomUUID();
