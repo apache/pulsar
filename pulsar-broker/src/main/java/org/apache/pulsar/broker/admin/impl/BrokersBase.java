1c3c5a41f13b0bcd5828ecd2479a319d69036a28
joohyukkim <beanskobe@gmail.com>
2023-07-17 19:25:22 +0900

    Remove line

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/BrokersBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/BrokersBase.java
@@ -552,7 +552,6 @@ public class BrokersBase extends AdminResource {
         doShutDownBrokerGracefullyAsync(maxConcurrentUnloadPerSec, forcedTerminateTopic)
                 .thenAccept(__ -> {
                     LOG.info("[{}] Successfully shutdown broker gracefully", clientAppId());
-                    asyncResponse.resume(Response.noContent().build());
                 })
                 .exceptionally(ex -> {
             LOG.error("[{}] Failed to shutdown broker gracefully", clientAppId(), ex);
