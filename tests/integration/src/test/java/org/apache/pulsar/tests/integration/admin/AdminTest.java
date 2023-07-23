2cc4994acfb2b930b7389e3d513ac668d5ba379c
mattisonchao <mattisonchao@apache.org>
2023-07-23 16:30:54 +0800

    [fix][broker] Fix topic concurrent creation by admin

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/admin/AdminTest.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/admin/AdminTest.java
@@ -18,20 +18,30 @@
  */
 package org.apache.pulsar.tests.integration.admin;
 
-import static org.testng.Assert.assertNotNull;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.function.Supplier;
+
 import lombok.Cleanup;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.client.api.MessageId;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.policies.data.PersistentTopicInternalStats;
+import org.apache.pulsar.common.util.FutureUtil;
 import org.apache.pulsar.tests.integration.messaging.MessagingBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 /**
  * Integration tests for Pulsar Admin.
@@ -49,13 +59,11 @@ public class AdminTest extends MessagingBase {
                 .serviceHttpUrl(adminUrl.get())
                 .build();
 
-        @Cleanup
-        final PulsarClient client = PulsarClient.builder()
+        @Cleanup final PulsarClient client = PulsarClient.builder()
                 .serviceUrl(serviceUrl.get())
                 .build();
 
-        @Cleanup
-        final Producer<String> producer = client.newProducer(Schema.STRING)
+        @Cleanup final Producer<String> producer = client.newProducer(Schema.STRING)
                 .topic(topicName)
                 .enableBatching(false)
                 .create();
@@ -72,4 +80,34 @@ public class AdminTest extends MessagingBase {
             Assert.assertFalse(ledger.underReplicated);
         }
     }
+
+
+    @SneakyThrows
+    @Test
+    public void partitionedAndNonPartitionedConcurrentCreate() {
+        final String nsName = "public/" + generateNamespaceName();
+        pulsarAdmin.namespaces().createNamespace(nsName);
+        final String topicPrefix = "persistent://" + nsName + "/";
+        for (int i = 0; i < 300; i++) {
+            final String tmpTopicName = topicPrefix + UUID.randomUUID();
+            final List<CompletableFuture<Void>> futures = new ArrayList<>(2);
+            final CompletableFuture<Void> partitionedFuture = pulsarAdmin.topics().createPartitionedTopicAsync(tmpTopicName, 1);
+            final CompletableFuture<Void> nonPartitionedFuture = pulsarAdmin.topics().createNonPartitionedTopicAsync(tmpTopicName);
+            futures.add(partitionedFuture);
+            futures.add(nonPartitionedFuture);
+            try {
+                FutureUtil.waitForAll(futures).join();
+                final List<String> topics = pulsarAdmin.topics().getList(nsName);
+                Assert.assertTrue(topics.contains(tmpTopicName));
+                Assert.assertTrue(topics.contains(TopicName.get(tmpTopicName).getPartition(0).toString()));
+                fail("Expect admin conflict exception.");
+            } catch (CompletionException ex) {
+                Assert.assertTrue(ex.getCause() instanceof PulsarAdminException.ConflictException);
+            }
+            final List<String> topics = pulsarAdmin.topics().getList(nsName);
+            assertTrue(topics.contains(tmpTopicName)
+                    ^ topics.contains(TopicName.get(tmpTopicName).getPartition(0).toString()));
+        }
+    }
+
 }
