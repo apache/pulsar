858f04edce29569dae9d5a10f2c0b6ce2fdca79d
Kai Wang <kwang@apache.org>
2023-07-23 12:56:04 +0800

    [improve][broker] Avoid print redirect exception log when get list from bundle (#20846)
    (cherry picked from commit 9256407cdca1ab6d9b3a59ce404dccb09953ab24)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v2/NonPersistentTopics.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/v2/NonPersistentTopics.java
@@ -498,19 +498,19 @@ public class NonPersistentTopics extends PersistentTopics {
                     }
                     asyncResponse.resume(topicList);
                 } catch (Exception e) {
-                    log.error("[{}] Failed to list topics on namespace bundle {}/{}", clientAppId(),
-                            namespaceName, bundleRange, e);
+                    if (!isRedirectException(e)) {
+                        log.error("[{}] Failed to list topics on namespace bundle {}/{}", clientAppId(),
+                                namespaceName, bundleRange, e);
+                    }
                     asyncResponse.resume(new RestException(e));
                 }
             }
         }).exceptionally(ex -> {
-            log.error("[{}] Failed to list topics on namespace bundle {}/{}", clientAppId(),
-                namespaceName, bundleRange, ex);
-            if (ex.getCause() instanceof WebApplicationException) {
-                asyncResponse.resume(ex.getCause());
-            } else {
-                asyncResponse.resume(new RestException(ex.getCause()));
+            if (!isRedirectException(ex)) {
+                log.error("[{}] Failed to list topics on namespace bundle {}/{}", clientAppId(),
+                        namespaceName, bundleRange, ex);
             }
+            resumeAsyncResponseExceptionally(asyncResponse, ex);
             return null;
         });
     }
