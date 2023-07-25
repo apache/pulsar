907df11d00bbc66308067f3be194f4a33b2a5cda
mattisonchao <mattisonchao@apache.org>
2023-07-25 10:38:13 +0800

    Apply comments

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/SubscribeRateLimiter.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/SubscribeRateLimiter.java
@@ -28,6 +28,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import org.apache.pulsar.broker.service.BrokerService;
+import org.apache.pulsar.common.naming.NamespaceName;
+import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.policies.data.Policies;
 import org.apache.pulsar.common.policies.data.SubscribeRate;
 import org.apache.pulsar.common.util.RateLimiter;
@@ -159,15 +161,20 @@ public class SubscribeRateLimiter {
     }
 
     /**
-     * Gets configured subscribe-rate from namespace policies. Returns null if subscribe-rate is not configured
-     *
-     * @return
+     * @deprecated Avoid using the deprecated method
+     * #{@link org.apache.pulsar.broker.resources.NamespaceResources#getPoliciesIfCached(NamespaceName)} and blocking
+     * call.
      */
     @Deprecated
     public SubscribeRate getPoliciesSubscribeRate() {
         return getPoliciesSubscribeRate(brokerService, topicName);
     }
 
+    /**
+     * @deprecated Avoid using the deprecated method
+     * #{@link org.apache.pulsar.broker.resources.NamespaceResources#getPoliciesIfCached(NamespaceName)} and blocking
+     * call.
+     */
     @Deprecated
     public static SubscribeRate getPoliciesSubscribeRate(BrokerService brokerService, final String topicName) {
         final String cluster = brokerService.pulsar().getConfiguration().getClusterName();
@@ -175,7 +182,6 @@ public class SubscribeRateLimiter {
         return getPoliciesSubscribeRate(cluster, policies, topicName);
     }
 
-    @Deprecated
     public static SubscribeRate getPoliciesSubscribeRate(final String cluster, final Optional<Policies> policies,
             String topicName) {
         // return policy-subscribe rate only if it's enabled in policies
