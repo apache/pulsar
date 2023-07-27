b8425c14a00b40cee66ebe40dd48a35787f5d8d1
fengyubiao <yubiao.feng@streamnative.io>
2023-07-27 15:54:55 +0800

    address comment

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/OneWayReplicatorTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/OneWayReplicatorTest.java
@@ -19,6 +19,7 @@
 package org.apache.pulsar.broker.service;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
