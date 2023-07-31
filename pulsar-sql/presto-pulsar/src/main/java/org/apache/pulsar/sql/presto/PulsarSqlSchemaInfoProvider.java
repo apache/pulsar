ca880c6f29aafc283c7308621c256334e8dc51b9
crossoverJie <crossoverJie@gmail.com>
2023-07-31 20:40:29 +0800

    [improve][sql] Fix the wrong format of the logs (#20907)

--- pulsar-sql/presto-pulsar/src/main/java/org/apache/pulsar/sql/presto/PulsarSqlSchemaInfoProvider.java
+++ pulsar-sql/presto-pulsar/src/main/java/org/apache/pulsar/sql/presto/PulsarSqlSchemaInfoProvider.java
@@ -71,7 +71,7 @@ public class PulsarSqlSchemaInfoProvider implements SchemaInfoProvider {
             }
             return completedFuture(cache.get(BytesSchemaVersion.of(schemaVersion)));
         } catch (ExecutionException e) {
-            LOG.error("Can't get generic schema for topic {} schema version {}",
+            LOG.error("Can't get generic schema for topic %s schema version %s",
                     topicName.toString(), new String(schemaVersion, StandardCharsets.UTF_8), e);
             return FutureUtil.failedFuture(e.getCause());
         }
