42e2ccdc53229e4d304ea733d3d1488ba05648d3
coderzc <zhaocong@apache.org>
2023-08-29 09:43:40 +0800

    Revert "[fix][broker] Fix get topic policies as null during clean cache (#20763)"
    This reverts commit 644c11a10213b69c9433430abc26027ac58ae13e.

--- pulsar-broker/src/main/java/org/apache/pulsar/compaction/TwoPhaseCompactor.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/compaction/TwoPhaseCompactor.java
@@ -284,7 +284,6 @@ public class TwoPhaseCompactor extends Compactor {
                                     promise.complete(null);
                                 }
                             });
-                            return;
                         }
                     } catch (InterruptedException ie) {
                         Thread.currentThread().interrupt();
