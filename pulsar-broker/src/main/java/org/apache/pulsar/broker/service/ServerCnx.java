fc91c0028b77c3da8004c179f90ff2ec97738374
fengyubiao <yubiao.feng@streamnative.io>
2023-09-05 22:26:26 +0800

    Revert "[fix][broker] Expect msgs after server initiated CloseProducer (#19446)"
    This reverts commit 524288cfbf0b83690b69e89344a809a001393228.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
@@ -46,7 +46,6 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
@@ -186,7 +185,6 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
     private final BrokerService service;
     private final SchemaRegistryService schemaService;
     private final String listenerName;
-    private final HashMap<Long, Long> recentlyClosedProducers;
     private final ConcurrentLongHashMap<CompletableFuture<Producer>> producers;
     private final ConcurrentLongHashMap<CompletableFuture<Consumer>> consumers;
     private final boolean enableSubscriptionPatternEvaluation;
@@ -291,7 +289,6 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
                 .expectedItems(8)
                 .concurrencyLevel(1)
                 .build();
-        this.recentlyClosedProducers = new HashMap<>();
         this.replicatorPrefix = conf.getReplicatorPrefix();
         this.maxNonPersistentPendingMessages = conf.getMaxConcurrentNonPersistentMessagePerConnection();
         this.schemaValidationEnforced = conf.isSchemaValidationEnforced();
@@ -1696,14 +1693,6 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
         CompletableFuture<Producer> producerFuture = producers.get(send.getProducerId());
 
         if (producerFuture == null || !producerFuture.isDone() || producerFuture.isCompletedExceptionally()) {
-            if (recentlyClosedProducers.containsKey(send.getProducerId())) {
-                if (log.isDebugEnabled()) {
-                    log.debug("[{}] Received message, but the producer was recently closed : {}. Ignoring message.",
-                            remoteAddress, send.getProducerId());
-                }
-                // We expect these messages because we recently closed the producer. Do not close the connection.
-                return;
-            }
             log.warn("[{}] Received message, but the producer is not ready : {}. Closing the connection.",
                     remoteAddress, send.getProducerId());
             close();
@@ -2972,17 +2961,6 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
         safelyRemoveProducer(producer);
         if (getRemoteEndpointProtocolVersion() >= v5.getValue()) {
             writeAndFlush(Commands.newCloseProducer(producer.getProducerId(), -1L));
-            // The client does not necessarily know that the producer is closed, but the connection is still
-            // active, and there could be messages in flight already. We want to ignore these messages for a time
-            // because they are expected. Once the interval has passed, the client should have received the
-            // CloseProducer command and should not send any additional messages until it sends a create Producer
-            // command.
-            final long epoch = producer.getEpoch();
-            final long producerId = producer.getProducerId();
-            recentlyClosedProducers.put(producerId, epoch);
-            ctx.executor().schedule(() -> {
-                recentlyClosedProducers.remove(producerId, epoch);
-            }, service.getKeepAliveIntervalSeconds(), TimeUnit.SECONDS);
         } else {
             close();
         }
