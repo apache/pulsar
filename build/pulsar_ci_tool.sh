6940175b0bca5f5f7815bd4a340b34d205a8ee60
Yuan Dao <dao.yuan.gl@gmail.com>
2023-09-05 19:45:57 +0800

    Update pulsar_ci_tool.sh

--- build/pulsar_ci_tool.sh
+++ build/pulsar_ci_tool.sh
@@ -16,7 +16,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-#
+# test
 
 # shell function library for Pulsar CI builds
 
