9084fa8f4fe43ea091b7579aef015f5c65d307c6
Jiwe Guo <technoboy@apache.org>
2023-10-16 13:12:46 +0800

    fix checkstyle issue

--- pulsar-metadata/src/test/java/org/apache/bookkeeper/replication/AuditorPeriodicCheckTest.java
+++ pulsar-metadata/src/test/java/org/apache/bookkeeper/replication/AuditorPeriodicCheckTest.java
@@ -46,7 +46,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 /**
