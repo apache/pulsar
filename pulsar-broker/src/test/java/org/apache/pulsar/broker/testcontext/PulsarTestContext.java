877d176060473a8d05ff37a7708d7bd4d1a5f98b
Lari Hotari <lhotari@users.noreply.github.com>
2023-10-30 10:32:42 +0200

    [fix][test] Cleanup resources if starting PulsarService fails in PulsarTestContext (#21467)
    (cherry picked from commit a14af46bcf080ca9c356826f0e8f8fc5bec5243e)

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/testcontext/PulsarTestContext.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/testcontext/PulsarTestContext.java
@@ -198,6 +198,10 @@ public class PulsarTestContext implements AutoCloseable {
      * @throws Exception if there is an error closing the resources
      */
     public void close() throws Exception {
+        callCloseables(closeables);
+    }
+
+    private static void callCloseables(List<AutoCloseable> closeables) {
         for (int i = closeables.size() - 1; i >= 0; i--) {
             try {
                 closeables.get(i).close();
@@ -246,6 +250,7 @@ public class PulsarTestContext implements AutoCloseable {
         protected boolean configOverrideCalled = false;
         protected Function<BrokerService, BrokerService> brokerServiceCustomizer = Function.identity();
         protected WithMockZooKeeperOrTestZKServer withMockZooKeeperOrTestZKServer;
+        protected PulsarTestContext otherContextToClose;
 
         /**
          * Initialize the ServiceConfiguration with default values.
@@ -429,7 +434,15 @@ public class PulsarTestContext implements AutoCloseable {
          * The other PulsarTestContext will be closed when this one is closed.
          */
         public Builder chainClosing(PulsarTestContext otherContext) {
-            registerCloseable(otherContext);
+            otherContextToClose = otherContext;
+            return this;
+        }
+
+        /**
+         * Registers a closeable to close as the last one by prepending it to the closeables list.
+         */
+        public Builder prependCloseable(AutoCloseable closeable) {
+            closeables.add(0, closeable);
             return this;
         }
 
@@ -584,8 +597,13 @@ public class PulsarTestContext implements AutoCloseable {
                 try {
                     super.pulsarService.start();
                 } catch (Exception e) {
+                    callCloseables(super.closeables);
+                    super.closeables.clear();
                     throw new RuntimeException(e);
                 }
+            }
+            if (otherContextToClose != null) {
+                prependCloseable(otherContextToClose);
             }
             brokerService(super.pulsarService.getBrokerService());
             return super.build();
