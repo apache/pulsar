37ac67c901213a8620d2be34b5ecd93035b14c14
Lari Hotari <lhotari@apache.org>
2023-11-01 19:51:39 +0200

    Fix leak in ProxyAdditionalServletTest

--- pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyAdditionalServletTest.java
+++ pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyAdditionalServletTest.java
@@ -178,6 +178,7 @@ public class ProxyAdditionalServletTest extends MockedPulsarServiceBaseTest {
         internalCleanup();
 
         proxyService.close();
+        proxyWebServer.stop();
     }
 
     @Test
