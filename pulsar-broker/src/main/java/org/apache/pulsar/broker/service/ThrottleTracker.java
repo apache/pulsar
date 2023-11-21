e982a14a94299f6d26bd7696584c9fdab629bc54
Lari Hotari <lhotari@apache.org>
2023-11-21 16:05:32 +0200

    Start working on ThrottleTracker

---
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ThrottleTracker.java
@@ -0,0 +1,58 @@
+package org.apache.pulsar.broker.service;
+
+import io.netty.channel.ChannelHandlerContext;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.function.Supplier;
+
+public class ThrottleTracker {
+    private final Supplier<ChannelHandlerContext> ctxSupplier;
+    private final AtomicIntegerFlags flags = new AtomicIntegerFlags();
+    private static final AtomicIntegerFieldUpdater<ThrottleTracker> THROTTLE_COUNT_UPDATER =
+            AtomicIntegerFieldUpdater.newUpdater(
+                    ThrottleTracker.class, "throttleCount");
+    private volatile int throttleCount;
+
+    public ThrottleTracker(Supplier<ChannelHandlerContext> ctxSupplier) {
+        this.ctxSupplier = ctxSupplier;
+    }
+
+    public void setFlag(int index, boolean enabled) {
+        if (flags.changeFlag(index, enabled)) {
+            if (enabled) {
+                incrementThrottleCount();
+            } else {
+                decrementThrottleCount();
+            }
+        }
+    }
+
+    public void throttleForNanos(long nanos) {
+        ChannelHandlerContext ctx = ctxSupplier.get();
+        if (ctx != null) {
+            incrementThrottleCount();
+            ctx.executor().schedule(this::decrementThrottleCount, nanos, TimeUnit.NANOSECONDS);
+        }
+    }
+
+    public void decrementThrottleCount() {
+        int currentThrottleCount = THROTTLE_COUNT_UPDATER.decrementAndGet(this);
+        if (currentThrottleCount == 0) {
+            changeAutoRead(true);
+        }
+    }
+
+    public void incrementThrottleCount() {
+        int currentThrottleCount = THROTTLE_COUNT_UPDATER.incrementAndGet(this);
+        if (currentThrottleCount == 1) {
+            changeAutoRead(false);
+        }
+    }
+
+    private void changeAutoRead(boolean autoRead) {
+        ChannelHandlerContext ctx = ctxSupplier.get();
+        if (ctx != null) {
+            ctx.channel().config().setAutoRead(autoRead);
+        }
+    }
+}
---
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ThrottleTrackerTest.java
@@ -0,0 +1,7 @@
+package org.apache.pulsar.broker.service;
+
+import static org.testng.Assert.*;
+
+public class ThrottleTrackerTest {
+
+}
