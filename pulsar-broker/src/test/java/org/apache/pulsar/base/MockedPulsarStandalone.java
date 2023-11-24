65579624500a4a397faaefaccc353cacbe1282e7
Mattison Chao <mattisonchao@apache.org>
2023-11-24 21:16:59 +0800

    [improve][security] Support Tls with EC private key algorithm

---
+++ pulsar-broker/src/test/java/org/apache/pulsar/base/MockedPulsarStandalone.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.base;
+
+import static java.util.Objects.requireNonNull;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Sets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.apache.pulsar.broker.PulsarService;
+import org.apache.pulsar.broker.ServiceConfiguration;
+import org.apache.pulsar.broker.authentication.AuthenticationProviderTls;
+import org.apache.pulsar.broker.testcontext.PulsarTestContext;
+import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.client.impl.auth.AuthenticationTls;
+import org.apache.pulsar.common.policies.data.ClusterData;
+import org.apache.pulsar.common.policies.data.TenantInfo;
+
+
+public abstract class MockedPulsarStandalone implements AutoCloseable {
+
+    @Getter
+    private final ServiceConfiguration serviceConfiguration = new ServiceConfiguration();
+    private PulsarTestContext pulsarTestContext;
+
+    @Getter
+    private PulsarService pulsarService;
+    private PulsarAdmin serviceInternalAdmin;
+
+
+    {
+        serviceConfiguration.setClusterName(TEST_CLUSTER_NAME);
+        serviceConfiguration.setBrokerShutdownTimeoutMs(0L);
+        serviceConfiguration.setBrokerServicePort(Optional.of(0));
+        serviceConfiguration.setBrokerServicePortTls(Optional.of(0));
+        serviceConfiguration.setAdvertisedAddress("localhost");
+        serviceConfiguration.setWebServicePort(Optional.of(0));
+        serviceConfiguration.setWebServicePortTls(Optional.of(0));
+        serviceConfiguration.setNumExecutorThreadPoolSize(5);
+        serviceConfiguration.setExposeBundlesMetricsInPrometheus(true);
+    }
+
+    @SneakyThrows
+    protected void loadECTlsCertificate() {
+        serviceConfiguration.setTlsEnabled(true);
+        serviceConfiguration.setBrokerServicePort(Optional.empty());
+        serviceConfiguration.setWebServicePort(Optional.empty());
+        serviceConfiguration.setTlsTrustCertsFilePath(TLS_EC_TRUSTED_CERT_PATH);
+        serviceConfiguration.setTlsCertificateFilePath(TLS_EC_SERVER_CERT_PATH);
+        serviceConfiguration.setTlsKeyFilePath(TLS_EC_SERVER_KEY_PATH);
+        serviceConfiguration.setBrokerClientTlsEnabled(true);
+        serviceConfiguration.setBrokerClientTrustCertsFilePath(TLS_EC_TRUSTED_CERT_PATH);
+        serviceConfiguration.setBrokerClientAuthenticationPlugin(AuthenticationTls.class.getName());
+        final Map<String, String> brokerClientAuthParams = new HashMap<>();
+        brokerClientAuthParams.put("tlsCertFile", TLS_EC_BROKER_CLIENT_CERT_PATH);
+        brokerClientAuthParams.put("tlsKeyFile", TLS_EC_BROKER_CLIENT_KEY_PATH);
+        serviceConfiguration.setBrokerClientAuthenticationParameters(mapper.writeValueAsString(brokerClientAuthParams));
+    }
+
+    protected void enableTlsAuthentication() {
+        serviceConfiguration.setAuthenticationEnabled(true);
+        serviceConfiguration.setAuthenticationProviders(Sets.newHashSet(AuthenticationProviderTls.class.getName()));
+    }
+
+    @SneakyThrows
+    protected void start() {
+        this.pulsarTestContext = PulsarTestContext.builder()
+                .spyByDefault()
+                .config(serviceConfiguration)
+                .withMockZookeeper(false)
+                .build();
+        this.pulsarService = pulsarTestContext.getPulsarService();
+        this.serviceInternalAdmin = pulsarService.getAdminClient();
+        setupDefaultTenantAndNamespace();
+    }
+
+    private void setupDefaultTenantAndNamespace() throws Exception {
+        if (!serviceInternalAdmin.clusters().getClusters().contains(TEST_CLUSTER_NAME)) {
+            serviceInternalAdmin.clusters().createCluster(TEST_CLUSTER_NAME,
+                    ClusterData.builder().serviceUrl(pulsarService.getWebServiceAddress()).build());
+        }
+        if (!serviceInternalAdmin.tenants().getTenants().contains(DEFAULT_TENANT)) {
+            serviceInternalAdmin.tenants().createTenant(DEFAULT_TENANT, TenantInfo.builder().allowedClusters(
+                    Sets.newHashSet(TEST_CLUSTER_NAME)).build());
+        }
+        if (!serviceInternalAdmin.namespaces().getNamespaces(DEFAULT_TENANT).contains(DEFAULT_NAMESPACE)) {
+            serviceInternalAdmin.namespaces().createNamespace(DEFAULT_NAMESPACE);
+        }
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        if (pulsarTestContext != null) {
+            pulsarTestContext.close();
+        }
+    }
+
+    private static String getPathFromResource(String resourceName) {
+        return requireNonNull(MockedPulsarStandalone.class.getClassLoader().getResource(resourceName)).getPath();
+    }
+
+    // Utils
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    // Static name
+    private static final String DEFAULT_TENANT = "public";
+    private static final String DEFAULT_NAMESPACE = "public/default";
+    private static final String TEST_CLUSTER_NAME = "test-standalone";
+
+    // EC certificate
+    protected static final String TLS_EC_TRUSTED_CERT_PATH =
+            getPathFromResource("authentication/tls/ec/ca.cert.pem");
+    private static final String TLS_EC_SERVER_KEY_PATH =
+            getPathFromResource("authentication/tls/ec/server.key-pk8.pem");
+    private static final String TLS_EC_SERVER_CERT_PATH =
+            getPathFromResource("authentication/tls/ec/server.cert.pem");
+    private static final String TLS_EC_BROKER_CLIENT_KEY_PATH =
+            getPathFromResource("authentication/tls/ec/broker_client.key-pk8.pem");
+    private static final String TLS_EC_BROKER_CLIENT_CERT_PATH =
+            getPathFromResource("authentication/tls/ec/broker_client.cert.pem");
+    protected static final String TLS_EC_CLIENT_KEY_PATH =
+            getPathFromResource("authentication/tls/ec/client.key-pk8.pem");
+    protected static final String TLS_EC_CLIENT_CERT_PATH =
+            getPathFromResource("authentication/tls/ec/client.cert.pem");
+}
