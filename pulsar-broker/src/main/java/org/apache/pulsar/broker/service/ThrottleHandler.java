effe93359a6f7e436405ced86d2376acf6ee9b2d
Lari Hotari <lhotari@apache.org>
2023-11-25 15:01:23 +0200

    Improve ThrottleHandler

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ThrottleHandler.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ThrottleHandler.java
@@ -11,12 +11,22 @@ import java.util.function.LongSupplier;
 @FunctionalInterface
 public interface ThrottleHandler {
     /**
-     * Throttle the resource for the specified time. After the time expires, the handler should
-     * call the additionalPauseTimeSupplier to get the additional time to throttle the resource.
-     * This should be handled in a loop until additionalPauseTimeSupplier returns 0.
+     * Throttles the resource for a specified initial duration, and then continues to throttle it for additional
+     * durationsas provided by a supplier until the supplier returns 0.
      *
-     * @param initialPauseTimeNanos initial time to throttle the resource
-     * @param additionalPauseTimeSupplier additional time to throttle the resource
+     * The method works as follows:
+     * 1. If the initialPauseTimeNanos is greater than 0, the resource is throttled for this duration.
+     * 2. After the initial pause time expires, the method calls the additionalPauseTimeSupplier to get an additional
+     * pause time.
+     * 3. If the additional pause time is greater than 0, the resource is throttled for this additional duration.
+     * 4. Steps 2 and 3 are repeated in a loop until the additionalPauseTimeSupplier returns 0, indicating that no
+     * further throttling is required.
+     *
+     * @param initialPauseTimeNanos       The initial duration (in nanoseconds) for which the resource should be
+     *                                    throttled. If this is 0 or less, no initial throttling occurs.
+     * @param additionalPauseTimeSupplier A supplier providing additional durations (in nanoseconds) for which the
+     *                                    resource should be throttled after the initial pause time. The supplier should
+     *                                    return 0 when no further throttling is required.
      */
     void throttle(long initialPauseTimeNanos, LongSupplier additionalPauseTimeSupplier);
 }
