0060c95109e8fd64213e46e34f7b99035cb123cf
jack zhang <jack333zhang@gmail.com>
2023-11-30 17:58:32 +0800

    [fix][broker] Fix incorrect unack count when using shared subscription on non-persistent topic (#21592)
    Fixes #21568
    
    Motivation
    Fix incorrect unack count when using shared subscription on non-persistent topic
    
    Modifications
    In the case of a non-persistent topic, the consumer does not send an ack to the broker (see org.apache.pulsar.client.impl.NonPersistentAcknowledgmentGroupingTracker# addAcknowledgment)
    
    To work around this, we can update unackedMessages when the broker sends a message to the consumer successfully.

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/Consumer.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/Consumer.java
@@ -116,6 +116,9 @@ public class Consumer {
     private final ConsumerStatsImpl stats;
 
     private final boolean isDurable;
+
+    private final boolean isPersistentTopic;
+
     private static final AtomicIntegerFieldUpdater<Consumer> UNACKED_MESSAGES_UPDATER =
             AtomicIntegerFieldUpdater.newUpdater(Consumer.class, "unackedMessages");
     private volatile int unackedMessages = 0;
@@ -174,6 +177,7 @@ public class Consumer {
         this.readCompacted = readCompacted;
         this.consumerName = consumerName;
         this.isDurable = isDurable;
+        this.isPersistentTopic = subscription.getTopic() instanceof PersistentTopic;
         this.keySharedMeta = keySharedMeta;
         this.cnx = cnx;
         this.msgOut = new Rate();
@@ -242,6 +246,7 @@ public class Consumer {
         this.pendingAcks = null;
         this.stats = null;
         this.isDurable = false;
+        this.isPersistentTopic = false;
         this.metadata = null;
         this.keySharedMeta = null;
         this.clientAddress = null;
@@ -1089,7 +1094,7 @@ public class Consumer {
 
     private int addAndGetUnAckedMsgs(Consumer consumer, int ackedMessages) {
         int unackedMsgs = 0;
-        if (Subscription.isIndividualAckMode(subType)) {
+        if (isPersistentTopic && Subscription.isIndividualAckMode(subType)) {
             subscription.addUnAckedMessages(ackedMessages);
             unackedMsgs = UNACKED_MESSAGES_UPDATER.addAndGet(consumer, ackedMessages);
         }
