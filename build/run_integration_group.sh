4f88a7498a48fafa482f57d5b3eb805431339ce4
nikhil-ctds <ext-nikhil.erigila@datastax.com>
2023-12-20 16:52:06 +0530

    Add -fae option to mvn initialize while discovering sub-modules. mvn initialize fails to discover all the sub-modules if the release versions are not available in mvn repo. This would fail only incase of new releases where the artifacts are not yet published into the mvn repo.
    Error: Failed to execute goal on project pulsar-client-all-shade-test: Could not resolve dependencies for project org.apache.pulsar.tests:pulsar-client-all-shade-test:jar:3.1.2: Failed to collect dependencies at com.datastax.oss:pulsar-client-all:jar:3.1.2: Failed to read artifact descriptor for com.datastax.oss:pulsar-client-all:jar:3.1.2: Could not transfer artifact com.datastax.oss:pulsar-client-all:pom:3.1.2 from/to

--- build/run_integration_group.sh
+++ build/run_integration_group.sh
@@ -33,7 +33,7 @@ TESTNG_VERSION="7.3.0"
 # returns a CSV value
 mvn_list_modules() {
   (
-    mvn -B -ntp -Dscan=false "$@" initialize \
+    mvn -fae -B -ntp -Dscan=false "$@" initialize \
       | grep -- "-< .* >-" \
       | sed -E 's/.*-< (.*) >-.*/\1/' \
       | tr '\n' ',' | sed 's/,$/\n/'
