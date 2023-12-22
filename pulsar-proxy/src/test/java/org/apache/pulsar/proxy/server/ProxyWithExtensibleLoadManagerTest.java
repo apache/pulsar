e7bf8b549db7263f4011b49102f368f9ab353ac1
Dragos Misca <dragosvictor@users.noreply.github.com>
2023-12-22 13:27:33 -0800

    Add proxy reconnect test

--- pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyWithExtensibleLoadManagerTest.java
+++ pulsar-proxy/src/test/java/org/apache/pulsar/proxy/server/ProxyWithExtensibleLoadManagerTest.java
@@ -25,6 +25,9 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import java.net.InetSocketAddress;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
@@ -36,6 +39,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.pulsar.broker.BrokerTestUtil;
 import org.apache.pulsar.broker.MultiBrokerBaseTest;
@@ -43,22 +47,29 @@ import org.apache.pulsar.broker.PulsarService;
 import org.apache.pulsar.broker.ServiceConfiguration;
 import org.apache.pulsar.broker.authentication.AuthenticationService;
 import org.apache.pulsar.broker.loadbalance.extensions.ExtensibleLoadManagerImpl;
+import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.impl.ConsumerImpl;
 import org.apache.pulsar.client.impl.LookupService;
+import org.apache.pulsar.client.impl.ProducerImpl;
 import org.apache.pulsar.client.impl.PulsarClientImpl;
+import org.apache.pulsar.client.impl.ServiceNameResolver;
 import org.apache.pulsar.common.configuration.PulsarConfigurationLoader;
 import org.apache.pulsar.common.naming.NamespaceName;
 import org.apache.pulsar.common.naming.TopicDomain;
 import org.apache.pulsar.common.naming.TopicName;
+import org.apache.pulsar.common.net.ServiceURI;
 import org.apache.pulsar.metadata.impl.ZKMetadataStore;
+import org.jetbrains.annotations.NotNull;
 import org.mockito.Mockito;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Slf4j
 public class ProxyWithExtensibleLoadManagerTest extends MultiBrokerBaseTest {
 
     private static final int TEST_TIMEOUT_MS = 30_000;
@@ -100,21 +111,40 @@ public class ProxyWithExtensibleLoadManagerTest extends MultiBrokerBaseTest {
         return proxyConfig;
     }
 
-    private LookupService spyLookupService(PulsarClient client) throws IllegalAccessException {
-        LookupService svc = (LookupService) FieldUtils.readDeclaredField(client, "lookup", true);
-        var lookup = spy(svc);
-        FieldUtils.writeDeclaredField(client, "lookup", lookup, true);
-        return lookup;
+    private <T> T spyField(Object target, String fieldName) throws IllegalAccessException {
+        T t = (T) FieldUtils.readDeclaredField(target, fieldName, true);
+        var fieldSpy = spy(t);
+        FieldUtils.writeDeclaredField(target, fieldName, fieldSpy, true);
+        return fieldSpy;
     }
 
     private PulsarClientImpl createClient(ProxyService proxyService) {
         try {
             return Mockito.spy((PulsarClientImpl) PulsarClient.builder().
+                    // operationTimeout(5, TimeUnit.SECONDS).
+                    // connectionTimeout(5, TimeUnit.SECONDS).
+                    // lookupTimeout(5, TimeUnit.SECONDS).
                     serviceUrl(proxyService.getServiceUrl()).
                     build());
         } catch (PulsarClientException e) {
             throw new CompletionException(e);
         }
+    }
+
+    @NotNull
+    private InetSocketAddress getSourceBrokerInetAddress(TopicName topicName) throws PulsarAdminException {
+        var srcBrokerUrl = admin.lookups().lookupTopic(topicName.toString());
+        var serviceUri = ServiceURI.create(srcBrokerUrl);
+        var uri = serviceUri.getUri();
+        return InetSocketAddress.createUnresolved(uri.getHost(), uri.getPort());
+    }
+
+    private String getDstBrokerLookupUrl(TopicName topicName) throws Exception {
+        var srcBrokerUrl = admin.lookups().lookupTopic(topicName.toString());
+        return getAllBrokers().stream().
+                filter(pulsarService -> !Objects.equals(srcBrokerUrl, pulsarService.getBrokerServiceUrl())).
+                map(PulsarService::getLookupServiceAddress).
+                findAny().orElseThrow(() -> new Exception("Could not determine destination broker lookup URL"));
     }
 
     @BeforeMethod(alwaysRun = true)
@@ -135,7 +165,7 @@ public class ProxyWithExtensibleLoadManagerTest extends MultiBrokerBaseTest {
         }
     }
 
-    @Test(timeOut = TEST_TIMEOUT_MS)
+    @Test(timeOut = TEST_TIMEOUT_MS, invocationCount = 100)
     public void testProxyProduceConsume() throws Exception {
         var namespaceName = NamespaceName.get("public", "default");
         var topicName = TopicName.get(TopicDomain.persistent.toString(), namespaceName,
@@ -151,7 +181,7 @@ public class ProxyWithExtensibleLoadManagerTest extends MultiBrokerBaseTest {
         var producerClient = producerClientFuture.get();
         @Cleanup
         var producer = producerClient.newProducer(Schema.INT32).topic(topicName.toString()).create();
-        var producerLookupServiceSpy = spyLookupService(producerClient);
+        LookupService producerLookupServiceSpy = spyField(producerClient, "lookup");
 
         @Cleanup
         var consumerClient = consumerClientFuture.get();
@@ -161,7 +191,7 @@ public class ProxyWithExtensibleLoadManagerTest extends MultiBrokerBaseTest {
                 subscriptionName(BrokerTestUtil.newUniqueName("my-sub")).
                 ackTimeout(1000, TimeUnit.MILLISECONDS).
                 subscribe();
-        var consumerLookupServiceSpy = spyLookupService(consumerClient);
+        LookupService consumerLookupServiceSpy = spyField(consumerClient, "lookup");
 
         var bundleRange = admin.lookups().getBundleRange(topicName.toString());
 
@@ -200,13 +230,9 @@ public class ProxyWithExtensibleLoadManagerTest extends MultiBrokerBaseTest {
         var unloadFuture = CompletableFuture.runAsync(() -> {
             try {
                 cdl.await();
-                var srcBrokerUrl = admin.lookups().lookupTopic(topicName.toString());
-                var dstBrokerUrl = getAllBrokers().stream().
-                        filter(pulsarService -> !Objects.equals(srcBrokerUrl, pulsarService.getBrokerServiceUrl())).
-                        map(PulsarService::getLookupServiceAddress).
-                        findAny().orElseThrow(() -> new Exception("Could not determine destination broker URL"));
+                var dstBrokerLookupUrl = getDstBrokerLookupUrl(topicName);
                 semSend.release(messagesBeforeUnload);
-                admin.namespaces().unloadNamespaceBundle(namespaceName.toString(), bundleRange, dstBrokerUrl);
+                admin.namespaces().unloadNamespaceBundle(namespaceName.toString(), bundleRange, dstBrokerLookupUrl);
                 semSend.release(messagesAfterUnload);
             } catch (Exception e) {
                 throw new CompletionException(e);
@@ -225,5 +251,101 @@ public class ProxyWithExtensibleLoadManagerTest extends MultiBrokerBaseTest {
 
         verify(consumerClient, times(1)).getProxyConnection(any(), anyInt());
         verify(consumerLookupServiceSpy, never()).getBroker(topicName);
+    }
+
+    @Test(timeOut = TEST_TIMEOUT_MS, invocationCount = 100)
+    public void testClientReconnectsToBrokerOnProxyClosing() throws Exception {
+        var namespaceName = NamespaceName.get("public", "default");
+        var topicName = TopicName.get(TopicDomain.persistent.toString(), namespaceName,
+                BrokerTestUtil.newUniqueName("testProxyReconnect"));
+
+        @Cleanup("shutdownNow")
+        var threadPool = Executors.newCachedThreadPool();
+
+        var producerClientFuture = CompletableFuture.supplyAsync(() -> createClient(proxyService), threadPool);
+        var consumerClientFuture = CompletableFuture.supplyAsync(() -> createClient(proxyService), threadPool);
+
+        @Cleanup
+        var producerClient = producerClientFuture.get();
+        @Cleanup
+        var producer = (ProducerImpl<Integer>) producerClient.newProducer(Schema.INT32).topic(topicName.toString()).
+                create();
+        LookupService producerLookupServiceSpy = spyField(producerClient, "lookup");
+        when(((ServiceNameResolver) spyField(producerLookupServiceSpy, "serviceNameResolver")).resolveHost()).
+                thenCallRealMethod().then(invocation -> getSourceBrokerInetAddress(topicName));
+
+        @Cleanup
+        var consumerClient = consumerClientFuture.get();
+        @Cleanup
+        var consumer = (ConsumerImpl<Integer>) consumerClient.newConsumer(Schema.INT32).topic(topicName.toString()).
+                subscriptionInitialPosition(SubscriptionInitialPosition.Earliest).
+                subscriptionName(BrokerTestUtil.newUniqueName("my-sub")).
+                ackTimeout(1000, TimeUnit.MILLISECONDS).
+                subscribe();
+        LookupService consumerLookupServiceSpy = spyField(consumerClient, "lookup");
+        when(((ServiceNameResolver) spyField(consumerLookupServiceSpy, "serviceNameResolver")).resolveHost()).
+                thenCallRealMethod().then(invocation -> getSourceBrokerInetAddress(topicName));
+
+        var bundleRange = admin.lookups().getBundleRange(topicName.toString());
+
+        var semSend = new Semaphore(0);
+        var messagesPerPhase = 100;
+        var phases = 4;
+        var totalMessages = messagesPerPhase * phases;
+        var cdlSentMessages = new CountDownLatch(messagesPerPhase * 2);
+
+        var pendingMessageIds = Collections.synchronizedSet(new HashSet<Integer>());
+        var producerFuture = CompletableFuture.runAsync(() -> {
+            try {
+                for (int i = 0; i < totalMessages; i++) {
+                    semSend.acquire();
+                    pendingMessageIds.add(i);
+                    producer.send(i);
+                    cdlSentMessages.countDown();
+                }
+            } catch (Exception e) {
+                throw new CompletionException(e);
+            }
+        }, threadPool).orTimeout(TEST_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+
+        var consumerFuture = CompletableFuture.runAsync(() -> {
+            while (!producerFuture.isDone() || !pendingMessageIds.isEmpty()) {
+                try {
+                    var recvMessage = consumer.receive(1_500, TimeUnit.MILLISECONDS);
+                    if (recvMessage != null) {
+                        consumer.acknowledge(recvMessage);
+                        pendingMessageIds.remove(recvMessage.getValue());
+                    }
+                } catch (PulsarClientException e) {
+                    // Retry
+                }
+            }
+        }, threadPool).orTimeout(TEST_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+
+        var dstBrokerLookupUrl = getDstBrokerLookupUrl(topicName);
+        semSend.release(messagesPerPhase);
+        admin.namespaces().unloadNamespaceBundle(namespaceName.toString(), bundleRange, dstBrokerLookupUrl);
+        semSend.release(messagesPerPhase);
+
+        cdlSentMessages.await();
+        assertEquals(FieldUtils.readDeclaredField(producer.getConnectionHandler(), "useProxy", true), Boolean.TRUE);
+        assertEquals(FieldUtils.readDeclaredField(consumer.getConnectionHandler(), "useProxy", true), Boolean.TRUE);
+        semSend.release(messagesPerPhase);
+        proxyService.close();
+        proxyService = null;
+        semSend.release(messagesPerPhase);
+
+        // Verify produce/consume futures completed successfully.
+        producerFuture.get();
+        consumerFuture.get();
+
+        assertEquals(FieldUtils.readDeclaredField(producer.getConnectionHandler(), "useProxy", true), Boolean.FALSE);
+        assertEquals(FieldUtils.readDeclaredField(consumer.getConnectionHandler(), "useProxy", true), Boolean.FALSE);
+
+        verify(producerClient, times(1)).getProxyConnection(any(), anyInt());
+        verify(producerLookupServiceSpy, times(1)).getBroker(topicName);
+
+        verify(consumerClient, times(1)).getProxyConnection(any(), anyInt());
+        verify(consumerLookupServiceSpy, times(1)).getBroker(topicName);
     }
 }
