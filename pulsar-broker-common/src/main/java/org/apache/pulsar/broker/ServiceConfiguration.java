1f9936b58e2ae75b996ab6b39ca15727f4979ee9
feynmanlin <315157973@qq.com>
2024-01-22 14:49:35 +0800

    code style

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
@@ -21,7 +21,6 @@ package org.apache.pulsar.broker;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Objects;
