422bb118cb120d1d7878894d9b7185defcf8ba05
hanmz <gunnhan@tencent.com>
2024-02-04 15:12:09 +0800

    [fix][doc] Fix typos in class PersistentReplicator and PersistentDispatcherSingleActiveConsumer

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentDispatcherSingleActiveConsumer.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentDispatcherSingleActiveConsumer.java
@@ -263,7 +263,7 @@ public class PersistentDispatcherSingleActiveConsumer extends AbstractDispatcher
             }
         } else if (readOnActiveConsumerTask != null) {
             if (log.isDebugEnabled()) {
-                log.debug("[{}-{}] Ignoring flow control message since consumer is waiting for cursor to be rewinded",
+                log.debug("[{}-{}] Ignoring flow control message since consumer is waiting for cursor to be rewound",
                         name, consumer);
             }
         } else {
@@ -296,7 +296,7 @@ public class PersistentDispatcherSingleActiveConsumer extends AbstractDispatcher
         }
 
         if (readOnActiveConsumerTask != null) {
-            log.info("[{}-{}] Ignoring reDeliverUnAcknowledgedMessages: consumer is waiting for cursor to be rewinded",
+            log.info("[{}-{}] Ignoring reDeliverUnAcknowledgedMessages: consumer is waiting for cursor to be rewound",
                     name, consumer);
             return;
         }
@@ -304,7 +304,7 @@ public class PersistentDispatcherSingleActiveConsumer extends AbstractDispatcher
         havePendingRead = false;
         cursor.rewind(consumer.readCompacted());
         if (log.isDebugEnabled()) {
-            log.debug("[{}-{}] Cursor rewinded, redelivering unacknowledged messages. ", name, consumer);
+            log.debug("[{}-{}] Cursor rewound, redelivering unacknowledged messages. ", name, consumer);
         }
         readMoreEntries(consumer);
     }
