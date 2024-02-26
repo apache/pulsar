27d4275bb3e4f94309250c8a830e9d6cdc1147ad
zifengmo <38554710+zifengmo@users.noreply.github.com>
2024-02-26 18:06:28 +0800

    [fix] [client] Do no retrying for error subscription not found when disabled allowAutoSubscriptionCreation (#22078)
    (cherry picked from commit bbf6ddf9244c1dc02cc157a014c153a09f45be1f)
    (cherry picked from commit 7eec36e743fd3d70addce777a36f17f9b45fa5e9)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientCnx.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientCnx.java
@@ -1291,6 +1291,8 @@ public class ClientCnx extends PulsarHandler {
             return new PulsarClientException.IncompatibleSchemaException(errorMsg);
         case TopicNotFound:
             return new PulsarClientException.TopicDoesNotExistException(errorMsg);
+        case SubscriptionNotFound:
+            return new PulsarClientException.SubscriptionNotFoundException(errorMsg);
         case ConsumerAssignError:
             return new PulsarClientException.ConsumerAssignException(errorMsg);
         case NotAllowedError:
