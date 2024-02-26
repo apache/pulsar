60fed0db44a0690269d8a576e269171865a5fa51
Xiangying Meng <55571188+liangyepianzhou@users.noreply.github.com>
2024-02-26 23:40:02 +0800

    [fix][sec] Add a check for the input time value (#22023)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientBuilderImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientBuilderImpl.java
@@ -166,6 +166,7 @@ public class ClientBuilderImpl implements ClientBuilder {
 
     @Override
     public ClientBuilder lookupTimeout(int lookupTimeout, TimeUnit unit) {
+        checkArgument(lookupTimeout >= 0, "lookupTimeout must not be negative");
         conf.setLookupTimeoutMs(unit.toMillis(lookupTimeout));
         return this;
     }
@@ -331,6 +332,7 @@ public class ClientBuilderImpl implements ClientBuilder {
 
     @Override
     public ClientBuilder connectionTimeout(int duration, TimeUnit unit) {
+        checkArgument(duration >= 0, "connectionTimeout needs to be >= 0");
         conf.setConnectionTimeoutMs((int) unit.toMillis(duration));
         return this;
     }
