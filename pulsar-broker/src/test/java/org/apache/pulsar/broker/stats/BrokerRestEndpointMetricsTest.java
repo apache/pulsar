434ff8d636d4acd7b0bc681c59e967e6f728e206
dao-jun <daojun@apache.org>
2024-03-12 22:42:06 +0800

    fix test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/BrokerRestEndpointMetricsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/BrokerRestEndpointMetricsTest.java
@@ -17,26 +17,29 @@
  * under the License.
  */
 package org.apache.pulsar.broker.stats;
-//import io.opentelemetry.api.common.Attributes;
-//import io.opentelemetry.sdk.metrics.data.Data;
-//import io.opentelemetry.sdk.metrics.data.HistogramPointData;
-//import io.opentelemetry.sdk.metrics.data.MetricData;
-//import io.opentelemetry.sdk.metrics.data.MetricDataType;
-//import io.opentelemetry.semconv.SemanticAttributes;
-//import java.util.Collection;
-//import java.util.Optional;
-//import java.util.Set;
-//import java.util.UUID;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.sdk.metrics.data.Data;
+import io.opentelemetry.sdk.metrics.data.HistogramPointData;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+import io.opentelemetry.sdk.metrics.data.MetricDataType;
+import io.opentelemetry.semconv.SemanticAttributes;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 import org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest;
-//import org.apache.pulsar.broker.testcontext.PulsarTestContext;
-//import org.apache.pulsar.common.policies.data.ClusterData;
-//import org.apache.pulsar.common.policies.data.TenantInfo;
-//import org.testng.Assert;
+import org.apache.pulsar.broker.testcontext.PulsarTestContext;
+import org.apache.pulsar.common.policies.data.ClusterData;
+import org.apache.pulsar.common.policies.data.TenantInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @Test(groups = "broker")
 public class BrokerRestEndpointMetricsTest extends MockedPulsarServiceBaseTest {
+    private static final Logger log = LoggerFactory.getLogger(BrokerRestEndpointMetricsTest.class);
 
     @BeforeMethod(alwaysRun = true)
     @Override
@@ -50,78 +53,78 @@ public class BrokerRestEndpointMetricsTest extends MockedPulsarServiceBaseTest {
         super.internalCleanup();
     }
 
-//    @Override
-//    protected void customizeMainPulsarTestContextBuilder(PulsarTestContext.Builder builder) {
-//        super.customizeMainPulsarTestContextBuilder(builder);
-//        builder.enableOpenTelemetry(true);
-//    }
-//
-//
-//    @Test
-//    public void testMetrics() throws Exception {
-//        admin.clusters().createCluster("test", ClusterData.builder().serviceUrl(brokerUrl.toString()).build());
-//        admin.tenants().createTenant("test", TenantInfo.builder().allowedClusters(Set.of("test")).build());
-//        admin.namespaces().createNamespace("test/test");
-//        String topic = "persistent://test/test/test_" + UUID.randomUUID();
-//        admin.topics().createNonPartitionedTopic(topic);
-//        admin.topics().getList("test/test");
-//
-//        // This request will be failed
-//        try {
-//            admin.topics().createNonPartitionedTopic("persistent://test1/test1/test1");
-//        } catch (Exception e) {
-//            // ignore
-//        }
-//
-//        admin.topics().delete(topic, true);
-//        admin.namespaces().deleteNamespace("test/test");
-//        admin.tenants().deleteTenant("test");
-//
-//        Collection<MetricData> metricDatas = pulsarTestContext.getOpenTelemetryMetricReader().collectAllMetrics();
-//        Optional<MetricData> optional = metricDatas.stream().filter(m -> m.getName()
-//                .equals("pulsar_broker_rest_endpoint_latency")).findFirst();
-//        Assert.assertTrue(optional.isPresent());
-//
-//        MetricData metricData = optional.get();
-//        Assert.assertFalse(metricData.getDescription().isEmpty());
-//        Assert.assertEquals(metricData.getUnit(), "ms");
-//        Assert.assertEquals(metricData.getType(), MetricDataType.HISTOGRAM);
-//
-//        @SuppressWarnings("unchecked")
-//        Data<HistogramPointData> data = (Data<HistogramPointData>) metricData.getData();
-//        data.getPoints().forEach(point -> {
-//            hasAttributes(point);
-//            Assert.assertTrue(point.getCount() > 0);
-//            Assert.assertTrue(point.getSum() > 0);
-//        });
-//
-//        Assert.assertTrue(hasPoint(data, "/persistent/:tenant/:namespace/:topic", "DELETE"));
-//        Assert.assertTrue(hasPoint(data, "/persistent/:tenant/:namespace/:topic", "PUT"));
-//        Assert.assertTrue(hasPoint(data, "/tenants/:tenant", "PUT"));
-//        Assert.assertTrue(hasPoint(data, "/tenants/:tenant", "DELETE"));
-//        Assert.assertTrue(hasPoint(data, "/clusters/:cluster", "PUT"));
-//        Assert.assertTrue(hasPoint(data, "/namespaces/:tenant/:namespace", "PUT"));
-//        Assert.assertTrue(hasPoint(data, "/namespaces/:tenant/:namespace", "DELETE"));
-//    }
-//
-//    private static boolean hasPoint(Data<HistogramPointData> data, String uri, String method) {
-//        Collection<HistogramPointData> points = data.getPoints();
-//        for (HistogramPointData point : points) {
-//            Attributes attrs = point.getAttributes();
-//
-//            if (attrs.get(SemanticAttributes.HTTP_REQUEST_METHOD).equals(method)
-//                    && attrs.get(SemanticAttributes.URL_PATH).equals(uri)) {
-//                return true;
-//            }
-//        }
-//
-//        return false;
-//    }
-//
-//    private static void hasAttributes(HistogramPointData data) {
-//        Attributes attrs = data.getAttributes();
-//        Assert.assertNotNull(attrs.get(SemanticAttributes.HTTP_REQUEST_METHOD));
-//        Assert.assertNotNull(attrs.get(SemanticAttributes.URL_PATH));
-//        Assert.assertNotNull(attrs.get(SemanticAttributes.HTTP_RESPONSE_STATUS_CODE));
-//    }
+    @Override
+    protected void customizeMainPulsarTestContextBuilder(PulsarTestContext.Builder builder) {
+        super.customizeMainPulsarTestContextBuilder(builder);
+        builder.enableOpenTelemetry(true);
+    }
+
+
+    @Test
+    public void testMetrics() throws Exception {
+        admin.clusters().createCluster("test", ClusterData.builder().serviceUrl(brokerUrl.toString()).build());
+        admin.tenants().createTenant("test", TenantInfo.builder().allowedClusters(Set.of("test")).build());
+        admin.namespaces().createNamespace("test/test");
+        String topic = "persistent://test/test/test_" + UUID.randomUUID();
+        admin.topics().createNonPartitionedTopic(topic);
+        admin.topics().getList("test/test");
+
+        // This request will be failed
+        try {
+            admin.topics().createNonPartitionedTopic("persistent://test1/test1/test1");
+        } catch (Exception e) {
+            // ignore
+        }
+
+        admin.topics().delete(topic, true);
+        admin.namespaces().deleteNamespace("test/test");
+        admin.tenants().deleteTenant("test");
+
+        Collection<MetricData> metricDatas = pulsarTestContext.getOpenTelemetryMetricReader().collectAllMetrics();
+        Optional<MetricData> optional = metricDatas.stream().peek(m -> log.info("metric name: {}", m.getName()))
+                .filter(m -> m.getName().equals("pulsar_broker_rest_endpoint_latency")).findFirst();
+        Assert.assertTrue(optional.isPresent());
+
+        MetricData metricData = optional.get();
+        Assert.assertFalse(metricData.getDescription().isEmpty());
+        Assert.assertEquals(metricData.getUnit(), "ms");
+        Assert.assertEquals(metricData.getType(), MetricDataType.HISTOGRAM);
+
+        @SuppressWarnings("unchecked")
+        Data<HistogramPointData> data = (Data<HistogramPointData>) metricData.getData();
+        data.getPoints().forEach(point -> {
+            hasAttributes(point);
+            Assert.assertTrue(point.getCount() > 0);
+            Assert.assertTrue(point.getSum() > 0);
+        });
+
+        Assert.assertTrue(hasPoint(data, "/persistent/:tenant/:namespace/:topic", "DELETE"));
+        Assert.assertTrue(hasPoint(data, "/persistent/:tenant/:namespace/:topic", "PUT"));
+        Assert.assertTrue(hasPoint(data, "/tenants/:tenant", "PUT"));
+        Assert.assertTrue(hasPoint(data, "/tenants/:tenant", "DELETE"));
+        Assert.assertTrue(hasPoint(data, "/clusters/:cluster", "PUT"));
+        Assert.assertTrue(hasPoint(data, "/namespaces/:tenant/:namespace", "PUT"));
+        Assert.assertTrue(hasPoint(data, "/namespaces/:tenant/:namespace", "DELETE"));
+    }
+
+    private static boolean hasPoint(Data<HistogramPointData> data, String uri, String method) {
+        Collection<HistogramPointData> points = data.getPoints();
+        for (HistogramPointData point : points) {
+            Attributes attrs = point.getAttributes();
+
+            if (attrs.get(SemanticAttributes.HTTP_REQUEST_METHOD).equals(method)
+                    && attrs.get(SemanticAttributes.URL_PATH).equals(uri)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void hasAttributes(HistogramPointData data) {
+        Attributes attrs = data.getAttributes();
+        Assert.assertNotNull(attrs.get(SemanticAttributes.HTTP_REQUEST_METHOD));
+        Assert.assertNotNull(attrs.get(SemanticAttributes.URL_PATH));
+        Assert.assertNotNull(attrs.get(SemanticAttributes.HTTP_RESPONSE_STATUS_CODE));
+    }
 }
