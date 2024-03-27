40ec88dd01172f64ade1c0f1430bedae32bef23c
Xiangying Meng <55571188+liangyepianzhou@users.noreply.github.com>
2024-03-28 02:22:08 +0800

    [fix][client] the nullValue in msgMetadata should be true by default

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/TypedMessageBuilderImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/TypedMessageBuilderImpl.java
@@ -62,6 +62,7 @@ public class TypedMessageBuilderImpl<T> implements TypedMessageBuilder<T> {
         this.schema = schema;
         this.content = EMPTY_CONTENT;
         this.txn = txn;
+        this.msgMetadata.setNullValue(true);
     }
 
     private long beforeSend() {
@@ -141,9 +142,9 @@ public class TypedMessageBuilderImpl<T> implements TypedMessageBuilder<T> {
     @Override
     public TypedMessageBuilder<T> value(T value) {
         if (value == null) {
-            msgMetadata.setNullValue(true);
             return this;
         }
+        msgMetadata.setNullValue(false);
 
         return getKeyValueSchema().map(keyValueSchema -> {
             if (keyValueSchema.getKeyValueEncodingType() == KeyValueEncodingType.SEPARATED) {
