8f17446355095d9d24d781446bd9d60ca4a275ef
Lari Hotari <lhotari@apache.org>
2024-03-28 12:36:44 +0200

    [improve][test][branch-3.0] Improve ManagedLedgerTest.testGetNumberOfEntriesInStorage
    - improve the test case based on suggestion from @poorbarcode

--- managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerTest.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerTest.java
@@ -2640,10 +2640,10 @@ public class ManagedLedgerTest extends MockedBookKeeperTestCase {
         Awaitility.await().untilAsserted(() -> {
            assertEquals(managedLedger.getState(), ManagedLedgerImpl.State.ClosedLedger);
         });
-        managedLedger.rollCurrentLedgerIfFull();
+        managedLedger.createLedgerAfterClosed();
         Awaitility.await().untilAsserted(() -> {
-            assertEquals(managedLedger.getLedgersInfo().size(), 2);
-            assertEquals(managedLedger.getState(), ManagedLedgerImpl.State.ClosedLedger);
+            assertEquals(managedLedger.getLedgersInfo().size(), 3);
+            assertEquals(managedLedger.getState(), ManagedLedgerImpl.State.LedgerOpened);
         });
         assertEquals(5, managedLedger.getLedgersInfoAsList().get(0).getEntries());
         assertEquals(5, managedLedger.getLedgersInfoAsList().get(1).getEntries());
