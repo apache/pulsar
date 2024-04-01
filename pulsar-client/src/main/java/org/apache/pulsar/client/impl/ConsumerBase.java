7e16c32c87a2159bc79990de1ccdc32aabad4327
Xiangying Meng <55571188+liangyepianzhou@users.noreply.github.com>
2024-04-01 10:01:43 +0800

    address some comments

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerBase.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerBase.java
@@ -461,9 +461,9 @@ public abstract class ConsumerBase<T> extends HandlerState implements Consumer<T
         }
         if (conf.getSubscriptionType() != SubscriptionType.Shared
                 && conf.getSubscriptionType() != SubscriptionType.Key_Shared) {
-            log.warn("[{}] Message {} will be reconsumed after delay {} {}. This isn't recommended for "
-                            + "subscription {} with type {}.", this.topic, message, delayTime, unit.toString(),
-                    this.subscription, conf.getSubscriptionType());
+            log.warn("[{}] Message {} will be reconsumed without delayed delivery. reconsumeLater isn't supported for "
+                            + "subscription {} with type {}.", this.topic, message, this.subscription,
+                    conf.getSubscriptionType());
         }
         try {
             reconsumeLaterAsync(message, customProperties, delayTime, unit).get();
