dedaac830d388c56c59375f816e5c07189cd8278
dao-jun <daojun@apache.org>
2024-04-03 14:41:38 +0800

    improve tests

--- managed-ledger/src/test/java/org/apache/bookkeeper/mledger/OffloadReadHandleTest.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/mledger/OffloadReadHandleTest.java
@@ -23,7 +23,9 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.bookkeeper.client.api.LedgerEntries;
 import org.apache.bookkeeper.client.api.LedgerEntry;
 import org.apache.bookkeeper.client.api.ReadHandle;
@@ -43,6 +45,7 @@ public class OffloadReadHandleTest extends MockedBookKeeperTestCase {
         return new Object[][]{
                 {-1L},
                 {0L},
+                {50L},
                 {100L},
                 {10000L}
         };
@@ -50,51 +53,90 @@ public class OffloadReadHandleTest extends MockedBookKeeperTestCase {
 
     @Test(dataProvider = "flowPermits")
     public void testFlowPermits(long flowPermits) throws Exception {
+        OffloadReadHandle handle = (OffloadReadHandle) initializeReadHandle(flowPermits);
+        try {
+            long start = System.currentTimeMillis();
+            handle.read(1, 1);
+            handle.read(1, 1);
+            handle.read(1, 1);
+            handle.read(1, 1);
+            handle.read(1, 1);
+
+            long actualDuration = System.currentTimeMillis() - start;
+            if (flowPermits <= 0L) {
+                Assert.assertEquals(actualDuration, 4000D, 4000D);
+            } else if (flowPermits == 50L) {
+                long expectDuration = 8000;
+                Assert.assertEquals(actualDuration, expectDuration, expectDuration * 0.2D);
+            } else if (flowPermits == 100L) {
+                long expectDuration = 4000;
+                Assert.assertEquals(actualDuration, expectDuration, expectDuration * 0.2D);
+            } else if (flowPermits == 10000L) {
+                Assert.assertEquals(actualDuration, 1000D, 1000D);
+            }
+        } finally {
+            handle.close();
+            handle.reset();
+        }
+    }
+
+
+    @Test
+    public void testOffloadFlowPermitsMultiThreads() throws Exception {
+        OffloadReadHandle handle = (OffloadReadHandle) initializeReadHandle(1000);
+        AtomicBoolean failed = new AtomicBoolean(false);
+        CountDownLatch latch = new CountDownLatch(10);
+        try {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < 10; i++) {
+                new Thread(() -> {
+                    for (int j = 0; j < 10; j++) {
+                        try {
+                            handle.read(1, 1);
+                        } catch (Exception e) {
+                            failed.set(true);
+                        }
+                    }
+                    latch.countDown();
+                }).start();
+            }
+
+            latch.await();
+
+            Assert.assertFalse(failed.get());
+            long actualDuration = System.currentTimeMillis() - start;
+            long expectDuration = TimeUnit.SECONDS.toMillis(10);
+            Assert.assertEquals(actualDuration, expectDuration, expectDuration * 0.2D);
+        } finally {
+            handle.close();
+            handle.reset();
+        }
+    }
+
+
+    private ReadHandle initializeReadHandle(long flowPermits) throws Exception {
         ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(100);
         for (int a = 0; a < 100; a++) {
             buf.writeByte(0);
         }
         LedgerEntry entry = LedgerEntryImpl.create(1, 1, buf.readableBytes(), buf);
         List<LedgerEntry> entryList = Lists.newArrayList(entry);
-        OffloadReadHandle h = null;
-        try (LedgerEntries entries = LedgerEntriesImpl.create(entryList)) {
-            ReadHandle handle = Mockito.mock(ReadHandle.class);
-            Mockito.doAnswer(inv -> CompletableFuture.completedFuture(entries)).when(handle)
-                    .readAsync(Mockito.anyLong(), Mockito.anyLong());
+        LedgerEntries entries = LedgerEntriesImpl.create(entryList);
+        ReadHandle handle = Mockito.mock(ReadHandle.class);
+        Mockito.doAnswer(inv -> CompletableFuture.completedFuture(entries)).when(handle)
+                .readAsync(Mockito.anyLong(), Mockito.anyLong());
+        Mockito.doAnswer(inv -> {
+            entries.close();
+            return CompletableFuture.completedFuture(null);
+        }).when(handle).closeAsync();
 
-            long start = System.currentTimeMillis();
-            ManagedLedgerConfig config = new ManagedLedgerConfig();
-            config.setManagedLedgerOffloadFlowPermitsPerSecond(flowPermits);
+        ManagedLedgerConfig config = new ManagedLedgerConfig();
+        config.setManagedLedgerOffloadFlowPermitsPerSecond(flowPermits);
 
-            CompletableFuture<ReadHandle> future = OffloadReadHandle.create(handle, config,
-                    MLDataFormats.ManagedLedgerInfo.LedgerInfo.newBuilder().setLedgerId(1)
-                            .setEntries(1).setSize(100).build());
-            h = (OffloadReadHandle) future.get();
+        CompletableFuture<ReadHandle> future = OffloadReadHandle.create(handle, config,
+                MLDataFormats.ManagedLedgerInfo.LedgerInfo.newBuilder().setLedgerId(1)
+                        .setEntries(1).setSize(100).build());
 
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-            h.read(1, 1);
-
-            long actualDuration = System.currentTimeMillis() - start;
-            if (flowPermits <= 0L) {
-                Assert.assertEquals(actualDuration, 1000D, 1000D);
-            } else if (flowPermits == 100L || flowPermits == 50L) {
-                long expectDuration = TimeUnit.SECONDS.toMillis(8);
-                Assert.assertEquals(actualDuration, expectDuration, expectDuration * 0.2D);
-            } else if (flowPermits == 10000L) {
-                Assert.assertEquals(actualDuration, 1000D, 1000D);
-            }
-        } finally {
-            if (null != h) {
-                h.reset();
-            }
-        }
+        return future.get();
     }
 }
