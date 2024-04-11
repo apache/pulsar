af56a8bff89ecaab74a626df8b8caad791ea6daa
fengyubiao <yubiao.feng@streamnative.io>
2024-04-11 17:16:04 +0800

    add a new fix

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/DeduplicationDisabledBrokerLevelTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/DeduplicationDisabledBrokerLevelTest.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.broker.service;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 import java.time.Duration;
@@ -25,11 +26,13 @@ import org.apache.bookkeeper.mledger.impl.ManagedCursorImpl;
 import org.apache.bookkeeper.mledger.impl.ManagedLedgerImpl;
 import org.apache.bookkeeper.mledger.impl.PositionImpl;
 import org.apache.pulsar.broker.BrokerTestUtil;
+import org.apache.pulsar.broker.service.persistent.MessageDeduplication;
 import org.apache.pulsar.broker.service.persistent.PersistentTopic;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.ProducerConsumerBase;
 import org.apache.pulsar.client.api.Schema;
 import org.awaitility.Awaitility;
+import org.awaitility.reflect.WhiteboxImpl;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -37,6 +40,7 @@ import org.testng.annotations.Test;
 public class DeduplicationDisabledBrokerLevelTest extends ProducerConsumerBase {
 
     private int deduplicationSnapshotFrequency = 5;
+    private int brokerDeduplicationEntriesInterval = 1000;
 
     @BeforeClass
     @Override
@@ -54,6 +58,7 @@ public class DeduplicationDisabledBrokerLevelTest extends ProducerConsumerBase {
     protected void doInitConf() throws Exception {
         this.conf.setBrokerDeduplicationEnabled(false);
         this.conf.setBrokerDeduplicationSnapshotFrequencyInSeconds(deduplicationSnapshotFrequency);
+        this.conf.setBrokerDeduplicationEntriesInterval(brokerDeduplicationEntriesInterval);
     }
 
     @Test
@@ -88,5 +93,70 @@ public class DeduplicationDisabledBrokerLevelTest extends ProducerConsumerBase {
 
         // cleanup.
         admin.topics().delete(topic);
+    }
+
+    @Test
+    public void testSnapshotCounterAfterUnload() throws Exception {
+        final int originalDeduplicationSnapshotFrequency = deduplicationSnapshotFrequency;
+        deduplicationSnapshotFrequency = 3600;
+        cleanup();
+        setup();
+
+        // Create a topic and wait deduplication is started.
+        final String topic = BrokerTestUtil.newUniqueName("persistent://public/default/tp");
+        admin.topics().createNonPartitionedTopic(topic);
+        final PersistentTopic persistentTopic1 =
+                (PersistentTopic) pulsar.getBrokerService().getTopic(topic, false).join().get();
+        final ManagedLedgerImpl ml1 = (ManagedLedgerImpl) persistentTopic1.getManagedLedger();
+        admin.topicPolicies().setDeduplicationStatus(topic, true);
+        Awaitility.await().untilAsserted(() -> {
+            ManagedCursorImpl cursor1 = (ManagedCursorImpl) ml1.getCursors().get(PersistentTopic.DEDUPLICATION_CURSOR_NAME);
+            assertNotNull(cursor1);
+        });
+        final MessageDeduplication deduplication1 = persistentTopic1.getMessageDeduplication();
+
+        // 1. Send 999 messages, it is less than "brokerDeduplicationEntriesIntervaddl".
+        // 2. Unload topic.
+        // 3. Send 1 messages, there are 1099 messages have not been snapshot now.
+        // 4. Verify the snapshot has been taken.
+        // step 1.
+        final Producer<String> producer = pulsarClient.newProducer(Schema.STRING).topic(topic).create();
+        for (int i = 0; i < brokerDeduplicationEntriesInterval - 1; i++) {
+            producer.send(i + "");
+        }
+        int snapshotCounter1 = WhiteboxImpl.getInternalState(deduplication1, "snapshotCounter");
+        assertEquals(snapshotCounter1, brokerDeduplicationEntriesInterval - 1);
+        admin.topics().unload(topic);
+        PersistentTopic persistentTopic2 =
+                (PersistentTopic) pulsar.getBrokerService().getTopic(topic, false).join().get();
+        ManagedLedgerImpl ml2 = (ManagedLedgerImpl) persistentTopic2.getManagedLedger();
+        MessageDeduplication deduplication2 = persistentTopic2.getMessageDeduplication();
+        admin.topicPolicies().setDeduplicationStatus(topic, true);
+        Awaitility.await().untilAsserted(() -> {
+            ManagedCursorImpl cursor = (ManagedCursorImpl) ml2.getCursors().get(PersistentTopic.DEDUPLICATION_CURSOR_NAME);
+            assertNotNull(cursor);
+        });
+        // step 2.
+        // Note: repeat read will make the counter is larger than expected.
+        int snapshotCounter2 = WhiteboxImpl.getInternalState(deduplication2, "snapshotCounter");
+        assertTrue(snapshotCounter2 >= brokerDeduplicationEntriesInterval - 1);
+        // step 3.
+        producer.send((brokerDeduplicationEntriesInterval - 1) + "");
+        // step 4.
+        final ManagedCursorImpl cursor2 = (ManagedCursorImpl) ml2.getCursors().get(PersistentTopic.DEDUPLICATION_CURSOR_NAME);
+        Awaitility.await().untilAsserted(() -> {
+            int snapshotCounter3 = WhiteboxImpl.getInternalState(deduplication2, "snapshotCounter");
+            assertTrue(snapshotCounter3 < snapshotCounter2);
+            PositionImpl LAC = (PositionImpl) ml2.getLastConfirmedEntry();
+            PositionImpl cursorMD = (PositionImpl) cursor2.getMarkDeletedPosition();
+            assertTrue(LAC.compareTo(cursorMD) <= 0);
+        });
+
+        // cleanup.
+        producer.close();
+        admin.topics().delete(topic);
+        deduplicationSnapshotFrequency = originalDeduplicationSnapshotFrequency;
+        cleanup();
+        setup();
     }
 }
