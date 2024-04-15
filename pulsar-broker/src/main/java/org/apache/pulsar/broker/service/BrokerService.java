ca62ee3aba102b79ae8b42c03d112efe19791302
fengyubiao <yubiao.feng@streamnative.io>
2024-04-15 10:59:47 +0800

    [fix] [broker] Prevent long deduplication cursor backlog so that topic loading wouldn't timeout (#22479)
    (cherry picked from commit 837f8bca7ddbbad4354f9a89e36fcd6aea1be85c)
    (cherry picked from commit 0fbcbb240394b241e940225c2067ccf1ac3d3b32)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -580,8 +580,10 @@ public class BrokerService implements Closeable {
     }
 
     protected void startDeduplicationSnapshotMonitor() {
+        // We do not know whether users will enable deduplication on namespace level/topic level or not, so keep this
+        // scheduled task runs.
         int interval = pulsar().getConfiguration().getBrokerDeduplicationSnapshotFrequencyInSeconds();
-        if (interval > 0 && pulsar().getConfiguration().isBrokerDeduplicationEnabled()) {
+        if (interval > 0) {
             this.deduplicationSnapshotMonitor = OrderedScheduler.newSchedulerBuilder()
                     .name("deduplication-snapshot-monitor")
                     .numThreads(1)
