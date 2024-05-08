5bd7fcee4bf0fbe03498b25f24f58f3b977631f5
fengyubiao <yubiao.feng@streamnative.io>
2024-05-08 17:09:21 +0800

    [fix] [broker] Fix nothing happend after remove a dynamic config

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiDynamicConfigurationsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/AdminApiDynamicConfigurationsTest.java
@@ -18,15 +18,20 @@
  */
 package org.apache.pulsar.broker.admin;
 
+import static org.apache.pulsar.common.naming.NamespaceName.SYSTEM_NAMESPACE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertThrows;
 import static org.testng.Assert.fail;
 import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.broker.BrokerTestUtil;
 import org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest;
 import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.awaitility.Awaitility;
@@ -106,5 +111,70 @@ public class AdminApiDynamicConfigurationsTest extends MockedPulsarServiceBaseTe
         admin.brokers().deleteDynamicConfiguration(key);
         allDynamicConfigurations = admin.brokers().getAllDynamicConfigurations();
         assertThat(allDynamicConfigurations).doesNotContainKey(key);
+    }
+
+    @Test
+    public void testDeleteStringDynamicConfig() throws PulsarAdminException {
+        String syncEventTopic = BrokerTestUtil.newUniqueName(SYSTEM_NAMESPACE + "/tp");
+        // The default value is null;
+        Awaitility.await().untilAsserted(() -> {
+            assertNull(pulsar.getConfig().getConfigurationMetadataSyncEventTopic());
+        });
+        // Set dynamic config.
+        admin.brokers().updateDynamicConfiguration("configurationMetadataSyncEventTopic", syncEventTopic);
+        Awaitility.await().untilAsserted(() -> {
+            assertEquals(pulsar.getConfig().getConfigurationMetadataSyncEventTopic(), syncEventTopic);
+        });
+        // Remove dynamic config.
+        admin.brokers().deleteDynamicConfiguration("configurationMetadataSyncEventTopic");
+        Awaitility.await().untilAsserted(() -> {
+            assertNull(pulsar.getConfig().getConfigurationMetadataSyncEventTopic());
+        });
+    }
+
+    @Test
+    public void testDeleteIntDynamicConfig() throws PulsarAdminException {
+        // Record the default value;
+        int defaultValue = pulsar.getConfig().getMaxConcurrentTopicLoadRequest();
+        // Set dynamic config.
+        int newValue = defaultValue + 1000;
+        admin.brokers().updateDynamicConfiguration("maxConcurrentTopicLoadRequest", newValue + "");
+        Awaitility.await().untilAsserted(() -> {
+            assertEquals(pulsar.getConfig().getMaxConcurrentTopicLoadRequest(), newValue);
+        });
+        // Verify: it has been reverted to the default value.
+        admin.brokers().deleteDynamicConfiguration("maxConcurrentTopicLoadRequest");
+        Awaitility.await().untilAsserted(() -> {
+            assertEquals(pulsar.getConfig().getMaxConcurrentTopicLoadRequest(), defaultValue);
+        });
+    }
+
+    @Test
+    public void testDeleteCustomizedDynamicConfig() throws PulsarAdminException {
+        // Record the default value;
+        String customizedConfigName = "a123";
+        pulsar.getBrokerService().registerCustomDynamicConfiguration(customizedConfigName, v -> true);
+
+        AtomicReference<Object> currentValue = new AtomicReference<>();
+        pulsar.getBrokerService().registerConfigurationListener(customizedConfigName, v -> {
+            currentValue.set(v);
+        });
+
+        // The default value is null;
+        Awaitility.await().untilAsserted(() -> {
+            assertNull(currentValue.get());
+        });
+
+        // Set dynamic config.
+        admin.brokers().updateDynamicConfiguration(customizedConfigName, "xxx");
+        Awaitility.await().untilAsserted(() -> {
+            assertEquals(currentValue.get(), "xxx");
+        });
+
+        // Remove dynamic config.
+        admin.brokers().deleteDynamicConfiguration(customizedConfigName);
+        Awaitility.await().untilAsserted(() -> {
+            assertNull(currentValue.get());
+        });
     }
 }
