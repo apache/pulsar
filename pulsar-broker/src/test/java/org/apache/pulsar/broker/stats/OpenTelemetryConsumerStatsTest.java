8d5f4a89ea1698089e4dc080d364cb2c31a0e716
Dragos Misca <dragosvictor@users.noreply.github.com>
2024-05-08 21:04:14 -0700

    Test fixes

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/OpenTelemetryConsumerStatsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/OpenTelemetryConsumerStatsTest.java
@@ -22,21 +22,30 @@ import static org.apache.pulsar.broker.stats.BrokerOpenTelemetryTestUtil.assertM
 import static org.assertj.core.api.Assertions.assertThat;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.metrics.data.MetricData;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.Cleanup;
+import lombok.SneakyThrows;
 import org.apache.pulsar.broker.BrokerTestUtil;
+import org.apache.pulsar.broker.intercept.BrokerInterceptor;
 import org.apache.pulsar.broker.service.BrokerTestBase;
+import org.apache.pulsar.broker.service.Consumer;
 import org.apache.pulsar.broker.testcontext.PulsarTestContext;
 import org.apache.pulsar.client.api.SubscriptionInitialPosition;
 import org.apache.pulsar.client.api.SubscriptionType;
 import org.apache.pulsar.opentelemetry.OpenTelemetryAttributes;
 import org.awaitility.Awaitility;
+import org.mockito.Mockito;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class OpenTelemetryConsumerStatsTest extends BrokerTestBase {
+
+    private final BrokerInterceptor brokerInterceptor = Mockito.mock(BrokerInterceptor.class);
 
     @BeforeMethod(alwaysRun = true)
     @Override
@@ -50,13 +59,16 @@ public class OpenTelemetryConsumerStatsTest extends BrokerTestBase {
         super.internalCleanup();
     }
 
+    @SneakyThrows
     @Override
     protected void customizeMainPulsarTestContextBuilder(PulsarTestContext.Builder builder) {
         super.customizeMainPulsarTestContextBuilder(builder);
         builder.enableOpenTelemetry(true);
+        Mockito.doCallRealMethod().when(brokerInterceptor).onFilter(Mockito.any(), Mockito.any(), Mockito.any());
+        builder.brokerInterceptor(brokerInterceptor);
     }
 
-    @Test(timeOut = 30_000, invocationCount = 100)
+    @Test(timeOut = 30_000, invocationCount = 1)
     public void testMessagingMetrics() throws Exception {
         var topicName = BrokerTestUtil.newUniqueName("persistent://prop/ns-abc/testConsumerMessagingMetrics");
         admin.topics().createNonPartitionedTopic(topicName);
@@ -67,6 +79,17 @@ public class OpenTelemetryConsumerStatsTest extends BrokerTestBase {
         var subscriptionName = BrokerTestUtil.newUniqueName("test");
         var receiverQueueSize = 100;
 
+        var consumerRef = new AtomicReference<Consumer>();
+        // Intercept calls to create consumer, in order to fetch client information.
+        Mockito.doAnswer(invocation -> {
+            consumerRef.set(invocation.getArgument(1));
+            return null;
+        }).doNothing()
+                .when(brokerInterceptor)
+                .consumerCreated(Mockito.any(),
+                        Mockito.argThat(argument -> argument.getSubscription().getName().equals(subscriptionName)),
+                        Mockito.any());
+
         @Cleanup
         var consumer = pulsarClient.newConsumer()
                 .topic(topicName)
@@ -75,7 +98,11 @@ public class OpenTelemetryConsumerStatsTest extends BrokerTestBase {
                 .subscriptionType(SubscriptionType.Shared)
                 .ackTimeout(1, TimeUnit.SECONDS)
                 .receiverQueueSize(receiverQueueSize)
+                .subscriptionProperties(Map.of("prop1", "value1"))
                 .subscribe();
+
+        var serverConsumer = consumerRef.get();
+        assertThat(serverConsumer).isNotNull();
 
         @Cleanup
         var producer = pulsarClient.newProducer()
@@ -101,6 +128,11 @@ public class OpenTelemetryConsumerStatsTest extends BrokerTestBase {
                 .put(OpenTelemetryAttributes.PULSAR_SUBSCRIPTION_TYPE, SubscriptionType.Shared.toString())
                 .put(OpenTelemetryAttributes.PULSAR_CONSUMER_NAME, consumer.getConsumerName())
                 .put(OpenTelemetryAttributes.PULSAR_CONSUMER_ID, 0)
+                .put(OpenTelemetryAttributes.PULSAR_CONSUMER_CONNECTED_SINCE,
+                        serverConsumer.getConnectedSince().getEpochSecond())
+                .put(OpenTelemetryAttributes.PULSAR_CLIENT_ADDRESS, serverConsumer.getClientAddressAndPort())
+                .put(OpenTelemetryAttributes.PULSAR_CLIENT_VERSION, serverConsumer.getClientVersion())
+                .put(OpenTelemetryAttributes.PULSAR_CONSUMER_METADATA, Collections.emptyList())
                 .build();
 
         Awaitility.await().untilAsserted(() -> {
