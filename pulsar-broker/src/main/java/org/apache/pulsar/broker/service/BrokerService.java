03da7438b759002fa6dda62287e93176d9a59c34
fengyubiao <yubiao.feng@streamnative.io>
2024-05-10 08:38:49 +0800

    [fix] [broker] Fix configurationMetadataSyncEventTopic is marked supporting dynamic setting, but not implemented  (#22684)
    (cherry picked from commit ff4853e06259d2c278d76d393dd9b650ad3edf4a)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -2827,6 +2827,11 @@ public class BrokerService implements Closeable {
             pulsar.getWebService().updateHttpRequestsFailOnUnknownPropertiesEnabled((boolean) enabled);
         });
 
+        // add listener to notify web service httpRequestsFailOnUnknownPropertiesEnabled changed.
+        registerConfigurationListener("configurationMetadataSyncEventTopic", enabled -> {
+            pulsar.initConfigMetadataSynchronizerIfNeeded();
+        });
+
         // add more listeners here
 
         // (3) create dynamic-config if not exist.
