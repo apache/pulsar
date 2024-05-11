bd7e9ba16720ad406262f8c212cc45f4aec2f41f
shasank112001 <shasanksekharpandey@gmail.com>
2024-05-11 10:07:13 +0200

    Fix checkstyle issues

--- pulsar-testclient/src/main/java/org/apache/pulsar/testclient/DefaultMessageFormatter.java
+++ pulsar-testclient/src/main/java/org/apache/pulsar/testclient/DefaultMessageFormatter.java
@@ -19,7 +19,6 @@
 package org.apache.pulsar.testclient;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Locale;
 import java.util.Random;
 import org.apache.commons.lang3.RandomStringUtils;
 
