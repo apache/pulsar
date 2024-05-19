20dccab5e0f8c00151cfa74904faf5535b40d8df
fengyubiao <yubiao.feng@streamnative.io>
2024-05-19 11:28:21 +0800

    [fix] [broker] fix deadlock when disable topic level Geo-Replication (#22738)
    (cherry picked from commit 6372b9c8f5448757cdb56dc70aaf0ebb52a1063e)
    (cherry picked from commit 1a8ba1356a18e6794a411239e411202c77965c4d)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -3402,12 +3402,13 @@ public class PersistentTopicsBase extends AdminResource {
         Set<String> replicationClusters = Sets.newHashSet(clusterIds);
         return validateTopicPolicyOperationAsync(topicName, PolicyName.REPLICATION, PolicyOperation.WRITE)
                 .thenCompose(__ -> validatePoliciesReadOnlyAccessAsync())
-                .thenCompose(__ -> {
+                .thenAccept(__ -> {
                     if (replicationClusters.contains("global")) {
                         throw new RestException(Status.PRECONDITION_FAILED,
                                 "Cannot specify global in the list of replication clusters");
                     }
-                    Set<String> clusters = clusters();
+                }).thenCompose(__ -> clustersAsync())
+                .thenCompose(clusters -> {
                     List<CompletableFuture<Void>> futures = new ArrayList<>(replicationClusters.size());
                     for (String clusterId : replicationClusters) {
                         if (!clusters.contains(clusterId)) {
