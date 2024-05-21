59d9eecb1f97c0ab86900dc0ac462039b0d98e2e
Malla Sandeep <sandeep.malla78@gmail.com>
2024-05-21 16:42:25 +0530

    Pulsar-client-tools TestCmdNamespaces.java updated with list, create, delete namespace tests

--- pulsar-client-tools/src/test/java/org/apache/pulsar/admin/cli/TestCmdNamespaces.java
+++ pulsar-client-tools/src/test/java/org/apache/pulsar/admin/cli/TestCmdNamespaces.java
@@ -18,12 +18,25 @@
  */
 package org.apache.pulsar.admin.cli;
 
+import com.google.common.collect.Lists;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import lombok.Cleanup;
 import org.apache.pulsar.client.admin.Namespaces;
 import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.common.policies.data.Policies;
 import org.apache.pulsar.common.policies.data.RetentionPolicies;
+import org.mockito.ArgumentCaptor;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.io.IOException;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -31,22 +44,61 @@ import static org.mockito.Mockito.when;
 
 public class TestCmdNamespaces {
 
+    private PulsarAdmin pulsarAdmin;
+    private CmdNamespaces cmdNamespaces;
+    private Namespaces namespaces;
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        namespaces = mock(Namespaces.class);
+        pulsarAdmin = mock(PulsarAdmin.class);
+        when(pulsarAdmin.namespaces()).thenReturn(namespaces);
+        cmdNamespaces = new CmdNamespaces(() -> pulsarAdmin);
+    }
+
     @AfterMethod(alwaysRun = true)
     public void cleanup() throws IOException {
         //NOTHING FOR NOW
     }
 
+    @Test
+    public void testListCmd() throws Exception {
+        List<String> namespaceList = Lists.newArrayList("namespace1", "namespace2", "namespace3");
+        doReturn(namespaceList).when(namespaces).getNamespaces(anyString());
+        @Cleanup
+        StringWriter stringWriter = new StringWriter();
+        @Cleanup
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        cmdNamespaces.getCommander().setOut(printWriter);
+        cmdNamespaces.run("list tenant1".split("\\s+"));
+        Assert.assertEquals(stringWriter.toString(), String.join("\n", namespaceList) + "\n");
+    }
+
+    @Test
+    public void testCreateCmd() throws Exception {
+        ArgumentCaptor<String> namespaceCapture = ArgumentCaptor.forClass(String.class);
+        doNothing().when(namespaces).createNamespace(namespaceCapture.capture(), any(Policies.class));
+        cmdNamespaces.run("create tenant1/namespace1".split("\\s+"));
+        verify(namespaces, times(1))
+                .createNamespace(anyString(), any(Policies.class));
+        Assert.assertEquals(namespaceCapture.getValue(), "tenant1/namespace1");
+    }
+
+    @Test
+    public void testDeleteCmd() throws Exception {
+        ArgumentCaptor<String> namespaceCapture = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Boolean> forceCapture = ArgumentCaptor.forClass(Boolean.class);
+        doNothing().when(namespaces).deleteNamespace(namespaceCapture.capture(), forceCapture.capture());
+        cmdNamespaces.run("delete tenant1/namespace1".split("\\s+"));
+        verify(namespaces, times(1))
+                .deleteNamespace(namespaceCapture.getValue(), forceCapture.getValue());
+        Assert.assertEquals(namespaceCapture.getValue(), "tenant1/namespace1");
+        Assert.assertEquals(forceCapture.getValue(), false);
+    }
 
     @Test
     public void testSetRetentionCmd() throws Exception {
-        Namespaces namespaces = mock(Namespaces.class);
-
-        PulsarAdmin admin = mock(PulsarAdmin.class);
-        when(admin.namespaces()).thenReturn(namespaces);
-
-        CmdNamespaces cmd = new CmdNamespaces(() -> admin);
-
-        cmd.run("set-retention public/default -s 2T -t 2h".split("\\s+"));
+        cmdNamespaces.run("set-retention public/default -s 2T -t 2h".split("\\s+"));
         verify(namespaces, times(1)).setRetention("public/default", new RetentionPolicies(120, 2 * 1024 * 1024));
-   }
+    }
 }
