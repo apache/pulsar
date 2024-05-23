73f2d935ad075e46b03523b181297f0c1b10a23f
thetumbled <843221020@qq.com>
2024-05-23 15:35:41 +0800

    fix doc.

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
@@ -2662,8 +2662,9 @@ public class ServiceConfiguration implements PulsarConfiguration {
                     + "The load balancer distributes bundles across brokers, "
                     + "based on topK bundle load data and other broker load data."
                     + "The bigger value will increase the overhead of reporting many bundles in load data. "
-                    + "Used for ExtensibleLoadManagerImpl and ModularLoadManagerImpl. "
-                    + "For ModularLoadManagerImpl, value not greater than 0 will disable the bundle filtering feature."
+                    + "Used for ExtensibleLoadManagerImpl and ModularLoadManagerImpl, default value is 10. "
+                    + "User can disable the bundle filtering feature of ModularLoadManagerImpl by setting this value to -1. "
+                    + "Enabling this feature can reduce the pressure on the zookeeper when doing load report."
     )
     private int loadBalancerMaxNumberOfBundlesInBundleLoadReport = 10;
     @FieldContext(
