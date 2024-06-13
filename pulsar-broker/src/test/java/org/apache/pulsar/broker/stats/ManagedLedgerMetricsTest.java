a1f832be4c9d86ef9e4d729a822a44f427dd86c2
Dragos Misca <dragosvictor@users.noreply.github.com>
2024-06-13 09:56:04 -0700

    Update ML ledger metrics test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/ManagedLedgerMetricsTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/stats/ManagedLedgerMetricsTest.java
@@ -18,27 +18,37 @@
  */
 package org.apache.pulsar.broker.stats;
 
+import static org.apache.pulsar.broker.stats.BrokerOpenTelemetryTestUtil.assertMetricLongSumValue;
 import static org.apache.pulsar.transaction.coordinator.impl.DisabledTxnLogBufferedWriterMetricsStats.DISABLED_BUFFERED_WRITER_METRICS;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.google.common.collect.Sets;
 import io.netty.util.HashedWheelTimer;
 import io.netty.util.concurrent.DefaultThreadFactory;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
-import com.google.common.collect.Sets;
+import lombok.Cleanup;
 import org.apache.bookkeeper.mledger.ManagedLedgerConfig;
+import org.apache.bookkeeper.mledger.impl.ManagedLedgerAttributes;
 import org.apache.bookkeeper.mledger.impl.ManagedLedgerFactoryImpl;
 import org.apache.bookkeeper.mledger.impl.ManagedLedgerImpl;
 import org.apache.bookkeeper.mledger.impl.ManagedLedgerMBeanImpl;
+import org.apache.bookkeeper.mledger.impl.OpenTelemetryManagedLedgerStats;
+import org.apache.pulsar.broker.BrokerTestUtil;
 import org.apache.pulsar.broker.ServiceConfiguration;
 import org.apache.pulsar.broker.service.BrokerTestBase;
 import org.apache.pulsar.broker.stats.metrics.ManagedLedgerMetrics;
+import org.apache.pulsar.broker.testcontext.PulsarTestContext;
 import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
 import org.apache.pulsar.common.naming.NamespaceName;
+import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.policies.data.TenantInfoImpl;
 import org.apache.pulsar.common.stats.Metrics;
 import org.apache.pulsar.transaction.coordinator.TransactionCoordinatorID;
 import org.apache.pulsar.transaction.coordinator.impl.MLTransactionLogImpl;
 import org.apache.pulsar.transaction.coordinator.impl.TxnLogBufferedWriterConfig;
+import org.awaitility.Awaitility;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -68,6 +78,12 @@ public class ManagedLedgerMetricsTest extends BrokerTestBase {
         super.internalCleanup();
     }
 
+    @Override
+    protected void customizeMainPulsarTestContextBuilder(PulsarTestContext.Builder pulsarTestContextBuilder) {
+        super.customizeMainPulsarTestContextBuilder(pulsarTestContextBuilder);
+        pulsarTestContextBuilder.enableOpenTelemetry(true);
+    }
+
     @Test
     public void testManagedLedgerMetrics() throws Exception {
         ManagedLedgerMetrics metrics = new ManagedLedgerMetrics(pulsar);
@@ -76,15 +92,20 @@ public class ManagedLedgerMetricsTest extends BrokerTestBase {
         List<Metrics> list1 = metrics.generate();
         Assert.assertTrue(list1.isEmpty());
 
-        Producer<byte[]> producer = pulsarClient.newProducer().topic("persistent://my-property/use/my-ns/my-topic1")
-                .create();
+        var topicName = "persistent://my-property/use/my-ns/my-topic1";
+        @Cleanup
+        Producer<byte[]> producer = pulsarClient.newProducer().topic(topicName).create();
+
+        @Cleanup
+        var consumer = pulsarClient.newConsumer().topic(topicName).subscriptionName("sub1").subscribe();
+
         for (int i = 0; i < 10; i++) {
             String message = "my-message-" + i;
             producer.send(message.getBytes());
         }
 
-        for (Entry<String, ManagedLedgerImpl> ledger : ((ManagedLedgerFactoryImpl) pulsar.getManagedLedgerFactory())
-                .getManagedLedgers().entrySet()) {
+        var managedLedgerFactory = (ManagedLedgerFactoryImpl) pulsar.getManagedLedgerFactory();
+        for (Entry<String, ManagedLedgerImpl> ledger : managedLedgerFactory.getManagedLedgers().entrySet()) {
             ManagedLedgerMBeanImpl stats = (ManagedLedgerMBeanImpl) ledger.getValue().getStats();
             stats.refreshStats(1, TimeUnit.SECONDS);
         }
@@ -96,14 +117,70 @@ public class ManagedLedgerMetricsTest extends BrokerTestBase {
             String message = "my-message-" + i;
             producer.send(message.getBytes());
         }
-        for (Entry<String, ManagedLedgerImpl> ledger : ((ManagedLedgerFactoryImpl) pulsar.getManagedLedgerFactory())
-                .getManagedLedgers().entrySet()) {
+        for (Entry<String, ManagedLedgerImpl> ledger : managedLedgerFactory.getManagedLedgers().entrySet()) {
             ManagedLedgerMBeanImpl stats = (ManagedLedgerMBeanImpl) ledger.getValue().getStats();
             stats.refreshStats(1, TimeUnit.SECONDS);
         }
         List<Metrics> list3 = metrics.generate();
         Assert.assertEquals(list3.get(0).getMetrics().get(addEntryRateKey), 5.0D);
 
+        // Validate OpenTelemetry metrics.
+        var ledgers = managedLedgerFactory.getManagedLedgers();
+        var mlName = TopicName.get(topicName).getPersistenceNamingEncoding();
+        assertThat(ledgers).containsKey(mlName);
+        var ml = ledgers.get(mlName);
+        var managedLedgerAttributes = new ManagedLedgerAttributes(ml);
+        var attribCommon = managedLedgerAttributes.getAttributes();
+        var metricReader = pulsarTestContext.getOpenTelemetryMetricReader();
+
+        Awaitility.await().untilAsserted(() -> {
+            var otelMetrics = metricReader.collectAllMetrics();
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.BACKLOG_COUNTER, attribCommon, 15);
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.MARK_DELETE_COUNTER, attribCommon, 0);
+        });
+
+        for (int i = 0; i < 10; i++) {
+            var msg = consumer.receive(1, TimeUnit.SECONDS);
+            consumer.acknowledge(msg);
+        }
+
+        Awaitility.await().untilAsserted(() -> {
+            var otelMetrics = metricReader.collectAllMetrics();
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.BACKLOG_COUNTER, attribCommon, 5);
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.MARK_DELETE_COUNTER, attribCommon,
+                    value -> assertThat(value).isPositive());
+        });
+
+        Awaitility.await().untilAsserted(() -> {
+            @Cleanup
+            var cons = pulsarClient.newConsumer()
+                    .topic(topicName)
+                    .subscriptionName(BrokerTestUtil.newUniqueName("sub"))
+                    .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
+                    .subscribe();
+            cons.receive(1, TimeUnit.SECONDS);
+
+            var attribSucceed = managedLedgerAttributes.getAttributesOperationSucceed();
+            var attribFailed = managedLedgerAttributes.getAttributesOperationFailure();
+            var attribActive = managedLedgerAttributes.getAttributesOperationActive();
+            var otelMetrics = metricReader.collectAllMetrics();
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.ADD_ENTRY_COUNTER, attribSucceed, 15);
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.ADD_ENTRY_COUNTER, attribFailed, 0);
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.ADD_ENTRY_COUNTER, attribActive, 0);
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.BYTES_OUT_COUNTER, attribCommon,
+                    value -> assertThat(value).isPositive());
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.BYTES_OUT_WITH_REPLICAS_COUNTER,
+                    attribCommon, value -> assertThat(value).isPositive());
+
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.READ_ENTRY_COUNTER, attribSucceed,
+                    value -> assertThat(value).isPositive());
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.READ_ENTRY_COUNTER, attribFailed, 0);
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.READ_ENTRY_COUNTER, attribActive, 0);
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.BYTES_IN_COUNTER, attribCommon,
+                    value -> assertThat(value).isPositive());
+            assertMetricLongSumValue(otelMetrics, OpenTelemetryManagedLedgerStats.READ_ENTRY_CACHE_MISS_COUNTER,
+                    attribCommon, value -> assertThat(value).isPositive());
+        });
     }
 
     @Test
