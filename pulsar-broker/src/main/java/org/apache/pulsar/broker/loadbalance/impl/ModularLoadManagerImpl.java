d58461d7d562610d648fdf7e5629484e3693a84c
Wenzhi Feng <52550727+thetumbled@users.noreply.github.com>
2024-07-19 15:52:38 +0800

    [improve][broker][branch-3.0] PIP-364: Introduce a new load balance algorithm AvgShedder (#23053)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/impl/ModularLoadManagerImpl.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/impl/ModularLoadManagerImpl.java
@@ -266,7 +266,21 @@ public class ModularLoadManagerImpl implements ModularLoadManager {
                             () -> LoadManagerShared.refreshBrokerToFailureDomainMap(pulsar, brokerToFailureDomainMap));
                 });
 
-        loadSheddingPipeline.add(createLoadSheddingStrategy());
+        if (placementStrategy instanceof LoadSheddingStrategy) {
+            // if the placement strategy is also a load shedding strategy
+            // we need to check two strategies are the same
+            if (!conf.getLoadBalancerLoadSheddingStrategy().equals(
+                    conf.getLoadBalancerPlacementStrategy())) {
+                throw new IllegalArgumentException("The load shedding strategy: "
+                        + conf.getLoadBalancerLoadSheddingStrategy()
+                        + " can't work with the placement strategy: "
+                        + conf.getLoadBalancerPlacementStrategy());
+            }
+            // bind the load shedding strategy and the placement strategy
+            loadSheddingPipeline.add((LoadSheddingStrategy) placementStrategy);
+        } else {
+            loadSheddingPipeline.add(createLoadSheddingStrategy());
+        }
     }
 
     public void handleDataNotification(Notification t) {
