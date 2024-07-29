f9ba4d885278e5958558ef20e1cbd1a2fada9579
Hideaki Oguni <22386882+izumo27@users.noreply.github.com>
2024-07-29 16:29:59 +0900

    [fix][client] Fix negative acknowledgement by messageId (#23060)
    (cherry picked from commit d4bbf10f58771e2d43e576dc3422e502834b1de4)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -798,7 +798,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
         negativeAcksTracker.add(messageId);
 
         // Ensure the message is not redelivered for ack-timeout, since we did receive an "ack"
-        unAckedMessageTracker.remove(messageId);
+        unAckedMessageTracker.remove(discardBatch(messageId));
     }
 
     @Override
