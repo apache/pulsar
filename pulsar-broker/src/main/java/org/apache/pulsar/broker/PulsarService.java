92bcd077c01058597fb910a49cbe01dffea41b36
Aurora Twinkle <foreverlove15q@gmail.com>
2024-07-31 21:42:56 +0800

    [fix][broker]: fix irregular method name (#23108)
    Co-authored-by: duanlinlin <duanlinllin@xiaohongshu.com>
    
    Fixes: fix irregular method name

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -2016,7 +2016,7 @@ public class PulsarService implements AutoCloseable, ShutdownService {
     }
 
     @VisibleForTesting
-    public void setTransactionExecutorProvider(TransactionBufferProvider transactionBufferProvider) {
+    public void setTransactionBufferProvider(TransactionBufferProvider transactionBufferProvider) {
         this.transactionBufferProvider = transactionBufferProvider;
     }
 
