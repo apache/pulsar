0c5e5eb16fc532295ca4638a03a8ca2a6c7088cb
zymap <zhangyong1025.zy@gmail.com>
2024-08-06 19:02:58 +0800

    [Fix] Allow the content be empty when deserialize in MetadataCache ---
    ### Motivation
    
    When using admin to create topic, it will set the content to byte[0]
    then put it into the value. Sometimes we need to update the value
    then we will received the deserailization error.
    So we need to check the value type to make it as optional empty
    when the value is null.

--- pulsar-metadata/src/test/java/org/apache/pulsar/metadata/MetadataCacheTest.java
+++ pulsar-metadata/src/test/java/org/apache/pulsar/metadata/MetadataCacheTest.java
@@ -19,8 +19,10 @@
 package org.apache.pulsar.metadata;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertNotSame;
+import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertSame;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
@@ -446,6 +448,26 @@ public class MetadataCacheTest extends BaseMetadataStoreTest {
     }
 
     @Test(dataProvider = "impl")
+    public void testReadModifyUpdateOrCreateWithEmptyValue(String provider, Supplier<String> urlSupplier) throws Exception {
+        @Cleanup
+        MetadataStore store = MetadataStoreFactory.create(urlSupplier.get(), MetadataStoreConfig.builder().build());
+
+        String path = "/testReadModifyUpdateOrCreateWithEmptyValue";
+        store.put(path, new byte[0], Optional.of(-1L)).get();
+
+        MetadataCache<Policies> objCache = store.getMetadataCache(Policies.class);
+
+        Optional<Policies> policies = objCache.get(path).get();
+        assertFalse(policies.isPresent());
+        Policies policies1 = objCache.readModifyUpdateOrCreate(path, (rp) -> {
+            Policies p = rp.orElse(new Policies());
+            p.max_unacked_messages_per_consumer = 100;
+            return p;
+        }).get();
+        assertEquals(policies1.max_unacked_messages_per_consumer.intValue(), 100);
+    }
+
+    @Test(dataProvider = "impl")
     public void readModifyUpdate(String provider, Supplier<String> urlSupplier) throws Exception {
         @Cleanup
         MetadataStore store = MetadataStoreFactory.create(urlSupplier.get(), MetadataStoreConfig.builder().build());
@@ -473,6 +495,36 @@ public class MetadataCacheTest extends BaseMetadataStoreTest {
         } catch (CompletionException e) {
             assertEquals(e.getCause().getClass(), NotFoundException.class);
         }
+    }
+
+    @Test(dataProvider = "impl")
+    public void testReadModifyUpdateWithEmptyValue(String provider, Supplier<String> urlSupplier) throws Exception {
+        @Cleanup
+        MetadataStore store = MetadataStoreFactory.create(urlSupplier.get(), MetadataStoreConfig.builder().build());
+
+        String path = "/testReadModifyUpdateWithEmptyValue";
+        store.put(path, new byte[0], Optional.of(-1L)).get();
+
+        MetadataCache<Policies> objCache = store.getMetadataCache(Policies.class);
+
+        Optional<Policies> policies = objCache.get(path).get();
+        assertFalse(policies.isPresent());
+        Policies policies1 = objCache.readModifyUpdate(path, (rp) -> {
+            if (rp != null) {
+                rp.max_unacked_messages_per_consumer = 100;
+            }
+            return rp;
+        }).get();
+        assertNull(policies1);
+
+        Policies policies2 = objCache.readModifyUpdate(path, (rp) -> {
+            if (rp == null) {
+                rp = new Policies();
+            }
+            rp.max_unacked_messages_per_consumer = 100;
+            return rp;
+        }).get();
+        assertEquals(policies2.max_unacked_messages_per_consumer.intValue(), 100);
     }
 
     /**
