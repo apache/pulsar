42e9157a7e033b49b121a46a7fc7e99668ba6ae6
Lari Hotari <lhotari@users.noreply.github.com>
2024-08-16 06:33:18 +0300

    [fix][client] Copy orderingKey to retry letter topic and DLQ messages and fix bug in copying (#23182)
    Fixes #23173
    Fixes #23181
    
    See #23173 and #23181
    
    - copy ordering key to messages sent to retry letter topic and DLQ topic
    
    (cherry picked from commit 67fc5b9f5342bd35d3fdacf37cf172a629ee15f9)
    (cherry picked from commit c83428fa328b327c9e7e1ae48878e8ddcd99a5db)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -595,13 +595,16 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
         }
     }
 
-    private static void copyMessageKeyIfNeeded(Message<?> message, TypedMessageBuilder<?> typedMessageBuilderNew) {
+    private static void copyMessageKeysIfNeeded(Message<?> message, TypedMessageBuilder<?> typedMessageBuilderNew) {
         if (message.hasKey()) {
             if (message.hasBase64EncodedKey()) {
                 typedMessageBuilderNew.keyBytes(message.getKeyBytes());
             } else {
                 typedMessageBuilderNew.key(message.getKey());
             }
+        }
+        if (message.hasOrderingKey()) {
+            typedMessageBuilderNew.orderingKey(message.getOrderingKey());
         }
     }
 
@@ -660,6 +663,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                                 dlqProducer.newMessage(Schema.AUTO_PRODUCE_BYTES(retryMessage.getReaderSchema().get()))
                                         .value(retryMessage.getData())
                                         .properties(propertiesMap);
+                        copyMessageKeysIfNeeded(message, typedMessageBuilderNew);
                         typedMessageBuilderNew.sendAsync().thenAccept(msgId -> {
                             doAcknowledge(finalMessageId, ackType, Collections.emptyMap(), null).thenAccept(v -> {
                                 result.complete(null);
@@ -686,7 +690,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                         if (delayTime > 0) {
                             typedMessageBuilderNew.deliverAfter(delayTime, unit);
                         }
-                        copyMessageKeyIfNeeded(message, typedMessageBuilderNew);
+                        copyMessageKeysIfNeeded(message, typedMessageBuilderNew);
                         typedMessageBuilderNew.sendAsync()
                                 .thenCompose(__ -> doAcknowledge(finalMessageId, ackType, Collections.emptyMap(), null))
                                 .thenAccept(v -> result.complete(null))
@@ -2135,7 +2139,7 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                             producerDLQ.newMessage(Schema.AUTO_PRODUCE_BYTES(message.getReaderSchema().get()))
                             .value(message.getData())
                             .properties(getPropertiesMap(message, originMessageIdStr, originTopicNameStr));
-                    copyMessageKeyIfNeeded(message, typedMessageBuilderNew);
+                    copyMessageKeysIfNeeded(message, typedMessageBuilderNew);
                     typedMessageBuilderNew.sendAsync()
                             .thenAccept(messageIdInDLQ -> {
                                 possibleSendToDeadLetterTopicMessages.remove(messageId);
