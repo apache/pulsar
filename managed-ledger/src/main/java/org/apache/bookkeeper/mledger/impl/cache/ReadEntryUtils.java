8c4eebfc120624c3285944a2ac1b2e190702d9bc
dao-jun <daojun@apache.org>
2024-08-22 03:26:52 +0800

    Improve code

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/cache/ReadEntryUtils.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/cache/ReadEntryUtils.java
@@ -19,28 +19,41 @@
 package org.apache.bookkeeper.mledger.impl.cache;
 
 import com.google.common.annotations.VisibleForTesting;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import org.apache.bookkeeper.client.LedgerEntry;
+import org.apache.bookkeeper.client.LedgerHandle;
+import org.apache.bookkeeper.client.api.BKException;
 import org.apache.bookkeeper.client.api.LedgerEntries;
 import org.apache.bookkeeper.client.api.LedgerMetadata;
 import org.apache.bookkeeper.client.api.ReadHandle;
+import org.apache.bookkeeper.client.impl.LedgerEntriesImpl;
+import org.apache.bookkeeper.client.impl.LedgerEntryImpl;
 import org.apache.bookkeeper.mledger.ManagedLedger;
 import org.apache.bookkeeper.mledger.ManagedLedgerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @VisibleForTesting
 public class ReadEntryUtils {
 
+    private static final Logger log = LoggerFactory.getLogger(ReadEntryUtils.class);
+
     @VisibleForTesting
     public static CompletableFuture<LedgerEntries> readAsync(ManagedLedger ml, ReadHandle handle, long firstEntry,
                                                       long lastEntry, boolean useBookkeeperV2WireProtocol) {
+        int entriesToRead = (int) (lastEntry - firstEntry + 1);
+        // Batch read is not supported for striped ledgers.
+        LedgerMetadata m = handle.getLedgerMetadata();
+        boolean isStriped = m.getEnsembleSize() != m.getWriteQuorumSize();
+        boolean useBatchRead = useBatchRead(entriesToRead, useBookkeeperV2WireProtocol, isStriped);
         if (ml.getOptionalLedgerInfo(handle.getId()).isEmpty()) {
             // The read handle comes from another managed ledger, in this case, we can only compare the entry range with
             // the LAC of that read handle. Specifically, it happens when this method is called by a
             // ReadOnlyManagedLedgerImpl object.
-            int entriesToRead = (int) (lastEntry - firstEntry + 1);
-            // Batch read is not supported for striped ledgers.
-            LedgerMetadata m = handle.getLedgerMetadata();
-            boolean isStriped = m.getEnsembleSize() != m.getWriteQuorumSize();
-            if (!useBatchRead(entriesToRead, useBookkeeperV2WireProtocol, isStriped)) {
+            if (!useBatchRead) {
                 return handle.readAsync(firstEntry, lastEntry);
             }
             return handle.batchReadAsync(firstEntry, entriesToRead, 0);
@@ -60,10 +73,35 @@ public class ReadEntryUtils {
             return CompletableFuture.failedFuture(new ManagedLedgerException("LastConfirmedEntry is "
                     + lastConfirmedEntry + " when reading entry " + lastEntry));
         }
+
+        if (useBatchRead && handle instanceof LedgerHandle lh) {
+            return asyncBatchReadUnconfirmedEntries(lh, firstEntry, entriesToRead);
+        }
         return handle.readUnconfirmedAsync(firstEntry, lastEntry);
     }
 
     private static boolean useBatchRead(int entriesToRead, boolean useBookkeeperV2WireProtocol, boolean isStriped) {
         return entriesToRead > 1 && useBookkeeperV2WireProtocol && !isStriped;
+    }
+
+    private static CompletableFuture<LedgerEntries> asyncBatchReadUnconfirmedEntries(LedgerHandle lh, long firstEntry,
+            int numEntries) {
+        CompletableFuture<LedgerEntries> f = new CompletableFuture<>();
+        lh.asyncBatchReadUnconfirmedEntries(firstEntry, numEntries, 0, (rc, lh1, seq, ctx) -> {
+            if (rc != BKException.Code.OK) {
+                log.error("Failed to batch read entries from ledger {} : {}", lh1.getId(), BKException.getMessage(rc));
+                f.completeExceptionally(org.apache.bookkeeper.client.BKException.create(rc));
+                return;
+            }
+            List<org.apache.bookkeeper.client.api.LedgerEntry> entries = new ArrayList<>(numEntries);
+            while (seq.hasMoreElements()) {
+                LedgerEntry entry = seq.nextElement();
+                LedgerEntryImpl entryImpl = LedgerEntryImpl.create(entry.getLedgerId(), entry.getEntryId(),
+                        entry.getLength(), entry.getEntryBuffer());
+                entries.add(entryImpl);
+            }
+            f.complete(LedgerEntriesImpl.create(entries));
+        }, null);
+        return f;
     }
 }
