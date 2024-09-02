3b6eb0da6f0de7d2e6e6fd00c1f6c3ed5d7d0948
Lari Hotari <lhotari@apache.org>
2024-09-02 22:18:20 +0300

    Add utility methods for logging topic stats and internal stats in tests

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/BrokerTestUtil.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/BrokerTestUtil.java
@@ -18,8 +18,17 @@
  */
 package org.apache.pulsar.broker;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.UncheckedIOException;
 import java.util.UUID;
+import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.apache.pulsar.common.util.ObjectMapperFactory;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
 
 /**
  * Holds util methods used in test.
@@ -77,4 +86,37 @@ public class BrokerTestUtil {
                 .defaultAnswer(Mockito.CALLS_REAL_METHODS)
                 .stubOnly());
     }
+
+    /**
+     * Uses Jackson to create a JSON string for the given object
+     * @param object to convert to JSON
+     * @return JSON string
+     */
+    public static String toJson(Object object) {
+        ObjectWriter writer = ObjectMapperFactory.getMapper().writer();
+        StringWriter stringWriter = new StringWriter();
+        try (JsonGenerator generator = writer.createGenerator(stringWriter).useDefaultPrettyPrinter()) {
+            generator.writeObject(object);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        return stringWriter.toString();
+    }
+
+    /**
+     * Logs the topic stats and internal stats for the given topic
+     * @param logger logger to use
+     * @param pulsarAdmin PulsarAdmin client to use
+     * @param topic topic name
+     */
+    public static void logTopicStats(Logger logger, PulsarAdmin pulsarAdmin, String topic) {
+        try {
+            logger.info("[{}] stats: {}", topic, toJson(pulsarAdmin.topics().getStats(topic)));
+            logger.info("[{}] internalStats: {}", topic,
+                    toJson(pulsarAdmin.topics().getInternalStats(topic, true)));
+        } catch (PulsarAdminException e) {
+            logger.warn("Failed to get stats for topic {}", topic, e);
+        }
+    }
+
 }
