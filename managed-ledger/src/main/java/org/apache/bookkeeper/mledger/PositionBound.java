3d5de42c9d7c7326759b85008c5de8140a70b909
Lari Hotari <lhotari@users.noreply.github.com>
2024-09-17 00:21:45 -0700

    Adjust indent
    Co-authored-by: Yunze Xu <xyzinfernity@163.com>

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/PositionBound.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/PositionBound.java
@@ -20,5 +20,5 @@ package org.apache.bookkeeper.mledger;
 
 public enum PositionBound {
     // define boundaries for position based seeks and searches
-        startIncluded, startExcluded
+    startIncluded, startExcluded
 }
