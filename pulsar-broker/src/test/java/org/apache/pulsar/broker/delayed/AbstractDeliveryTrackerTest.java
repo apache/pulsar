c90e71c619724e180c77d5d26ab1bd6398d4a342
Lari Hotari <lhotari@apache.org>
2024-10-03 00:55:28 +0300

    Fix test related to readMoreEntriesAsync changes

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/delayed/AbstractDeliveryTrackerTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/delayed/AbstractDeliveryTrackerTest.java
@@ -126,10 +126,10 @@ public abstract class AbstractDeliveryTrackerTest {
         Timeout cancelledTimeout = mock(Timeout.class);
         when(cancelledTimeout.isCancelled()).thenReturn(true);
         task.run(cancelledTimeout);
-        verify(dispatcher, atMostOnce()).readMoreEntries();
+        verify(dispatcher, atMostOnce()).readMoreEntriesAsync();
 
         task.run(mock(Timeout.class));
-        verify(dispatcher).readMoreEntries();
+        verify(dispatcher).readMoreEntriesAsync();
 
         tracker.close();
     }
@@ -180,7 +180,7 @@ public abstract class AbstractDeliveryTrackerTest {
         Timeout timeout = mock(Timeout.class);
         when(timeout.isCancelled()).then(x -> false);
         ((AbstractDelayedDeliveryTracker) tracker).run(timeout);
-        verify(dispatcher, times(1)).readMoreEntries();
+        verify(dispatcher, times(1)).readMoreEntriesAsync();
 
         // Add a message that has a delivery time just after the previous run. It will get delivered based on the
         // tick delay plus the last tick run.
@@ -189,11 +189,11 @@ public abstract class AbstractDeliveryTrackerTest {
         // Wait longer than the tick time plus the HashedWheelTimer's tick time to ensure that enough time has
         // passed where it would have been triggered if the tick time was doing the triggering.
         Thread.sleep(600);
-        verify(dispatcher, times(1)).readMoreEntries();
+        verify(dispatcher, times(1)).readMoreEntriesAsync();
 
         // Not wait for the message delivery to get triggered.
         Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> verify(dispatcher).readMoreEntries());
+                .untilAsserted(() -> verify(dispatcher).readMoreEntriesAsync());
 
         tracker.close();
     }
@@ -212,7 +212,7 @@ public abstract class AbstractDeliveryTrackerTest {
         // Wait long enough for the runnable to run, but not longer than the tick time. The point is that the delivery
         // should get scheduled early when the tick duration has passed since the last tick.
         Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> verify(dispatcher).readMoreEntries());
+                .untilAsserted(() -> verify(dispatcher).readMoreEntriesAsync());
 
         tracker.close();
     }
@@ -233,7 +233,7 @@ public abstract class AbstractDeliveryTrackerTest {
 
         // Not wait for the message delivery to get triggered.
         Awaitility.await().atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> verify(dispatcher).readMoreEntries());
+                .untilAsserted(() -> verify(dispatcher).readMoreEntriesAsync());
 
         tracker.close();
     }
