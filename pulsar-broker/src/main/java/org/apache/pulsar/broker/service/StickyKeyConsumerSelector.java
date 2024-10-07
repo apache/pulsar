2e4f379b1ba6e36faceae6b58f38ccae64c3b31a
Lari Hotari <lhotari@apache.org>
2024-10-07 10:34:50 +0300

    Improve javadoc of StickyKeyConsumerSelector

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/StickyKeyConsumerSelector.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/StickyKeyConsumerSelector.java
@@ -23,72 +23,91 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import org.apache.pulsar.client.api.Range;
 
+/**
+ * Abstraction for selecting the same consumer based on a key.
+ * This interface provides methods to add and remove consumers,
+ * select a consumer based on a sticky key or hash, and retrieve
+ * the hash range assignments for consumers. This is used by the Key_Shared implementation.
+ */
 public interface StickyKeyConsumerSelector {
+    /**
+     * The default range size used for hashing.
+     * This should be a power of 2 so that it's compatible with all implementations.
+     */
     int DEFAULT_RANGE_SIZE =  2 << 15;
+
     /**
      * The value used to indicate that sticky key hash is not set.
      * This value cannot be -1 since some of the data structures require non-negative values.
-      */
+     */
     int STICKY_KEY_HASH_NOT_SET = 0;
 
     /**
      * Add a new consumer.
      *
-     * @param consumer new consumer
+     * @param consumer the new consumer to be added
+     * @return a CompletableFuture that completes with the result of impacted consumers.
+     *         The result contains information about the existing consumers whose hash ranges were affected
+     *         by the addition of the new consumer.
      */
     CompletableFuture<ImpactedConsumersResult> addConsumer(Consumer consumer);
 
     /**
      * Remove the consumer.
      *
-     * @param consumer consumer to be removed
-     * @return
+     * @param consumer the consumer to be removed
+     * @return the result of impacted consumers. The result contains information about the existing consumers
+     *         whose hash ranges were affected by the removal of the consumer.
      */
     ImpactedConsumersResult removeConsumer(Consumer consumer);
 
     /**
      * Select a consumer by sticky key.
      *
-     * @param stickyKey sticky key
-     * @return consumer
+     * @param stickyKey the sticky key to select the consumer
+     * @return the selected consumer
      */
     default Consumer select(byte[] stickyKey) {
         return select(makeStickyKeyHash(stickyKey));
     }
 
     /**
-     * Make a hash from sticky key.
+     * Make a hash from the sticky key. The hash value is in the range returned by the {@link #getKeyHashRange()}
+     * method instead of in the full range of integers. In other words, this returns the "slot".
      *
-     * @param stickyKey sticky key
-     * @return hash the hash value
+     * @param stickyKey the sticky key to hash
+     * @return the generated hash value
      */
     int makeStickyKeyHash(byte[] stickyKey);
 
     /**
      * Select a consumer by hash.
      *
-     * @param hash hash corresponding to sticky key
-     * @return consumer
+     * @param hash the hash corresponding to the sticky key
+     * @return the selected consumer
      */
     Consumer select(int hash);
 
     /**
-     * Get the full range of hash.
-     * @return the full range of hash
+     * Get the full range of hash values used by this selector. The upper bound is exclusive.
+     *
+     * @return the full range of hash values
      */
     Range getKeyHashRange();
 
     /**
      * Get key hash ranges handled by each consumer.
-     * @return A map where key is a consumer name and value is list of hash range it receiving message for.
+     *
+     * @return a map where the key is a consumer and the value is a list of hash ranges it is receiving messages for
      */
     default Map<Consumer, List<Range>> getConsumerKeyHashRanges() {
         return getConsumerHashAssignmentsSnapshot().getRangesByConsumer();
     }
 
     /**
-     * Get the mapping of hash range to consumer.
-     * @return A map where key is a hash range and value is the consumer that is responsible for the range.
+     * Get the current mappings of hash range to consumer.
+     *
+     * @return a snapshot of the consumer hash assignments
      */
     ConsumerHashAssignmentsSnapshot getConsumerHashAssignmentsSnapshot();
 }
