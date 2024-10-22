b592a6505da4a7e204b68365ad8c76ca394cf30c
fengyubiao <yubiao.feng@streamnative.io>
2024-10-23 01:01:50 +0800

    address comment

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConnectionPool.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConnectionPool.java
@@ -276,8 +276,7 @@ public class ConnectionPool implements AutoCloseable {
             }
             // Try use exists connection.
             if (clientCnx.getIdleState().tryMarkUsingAndClearIdleTime()) {
-                return CompletableFuture.completedFuture(clientCnx).supplyAsync(
-                        () -> clientCnx, clientCnx.ctx().executor());
+                return CompletableFuture.supplyAsync(() -> clientCnx, clientCnx.ctx().executor());
             } else {
                 // If connection already release, create a new one.
                 pool.remove(key, completableFuture);
