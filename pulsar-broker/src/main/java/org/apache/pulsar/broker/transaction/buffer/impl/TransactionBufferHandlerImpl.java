e5428b9c761d392add7f26544783fca61284ca4b
fanjianye <fanjianye@bigo.sg>
2024-11-06 17:38:20 +0800

    fix concurrent error cause stuck in TransactionBufferHandlerImpl#endTxn

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/transaction/buffer/impl/TransactionBufferHandlerImpl.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/transaction/buffer/impl/TransactionBufferHandlerImpl.java
@@ -128,6 +128,9 @@ public class TransactionBufferHandlerImpl implements TransactionBufferHandler {
             }
         } else {
             pendingRequests.add(op);
+            if (currentPermits != REQUEST_CREDITS_UPDATER.get(this)) {
+                checkPendingRequests();
+            }
             return false;
         }
     }
@@ -251,6 +254,7 @@ public class TransactionBufferHandlerImpl implements TransactionBufferHandler {
                             invalid.recycle();
                         }
                         endTxn(polled);
+                        break;
                     } else {
                         REQUEST_CREDITS_UPDATER.incrementAndGet(this);
                     }
