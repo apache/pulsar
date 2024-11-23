6693382ca10014b4722472e260f827663eb56111
Lari Hotari <lhotari@apache.org>
2024-11-24 00:02:21 +0200

    [fix][test][branch-3.0] Fix PrecisTopicPublishRateThrottleTest that broke after #23589 changes
    - commit aa4dbf3 broke this test

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/PrecisTopicPublishRateThrottleTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/PrecisTopicPublishRateThrottleTest.java
@@ -35,6 +35,7 @@ import org.apache.pulsar.common.util.RateLimiter;
 import org.awaitility.Awaitility;
 import org.mockito.Mockito;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Test(groups = "broker")
@@ -46,9 +47,10 @@ public class PrecisTopicPublishRateThrottleTest extends BrokerTestBase{
         //No-op
     }
 
+    @AfterMethod(alwaysRun = true)
     @Override
     protected void cleanup() throws Exception {
-        //No-op
+        super.internalCleanup();
     }
 
     @Test
@@ -85,7 +87,6 @@ public class PrecisTopicPublishRateThrottleTest extends BrokerTestBase{
             // No-op
         }
         Assert.assertNotNull(messageId);
-        super.internalCleanup();
     }
 
     @Test
@@ -114,7 +115,6 @@ public class PrecisTopicPublishRateThrottleTest extends BrokerTestBase{
         } catch (TimeoutException e) {
             // No-op
         }
-        super.internalCleanup();
     }
 
     @Test
@@ -166,7 +166,6 @@ public class PrecisTopicPublishRateThrottleTest extends BrokerTestBase{
             // No-op
         }
         Assert.assertNotNull(messageId);
-        super.internalCleanup();
     }
 
     @Test
@@ -205,7 +204,6 @@ public class PrecisTopicPublishRateThrottleTest extends BrokerTestBase{
         Assert.assertEquals(limiter.publishMaxMessageRate, rateInMsg);
 
         producer.close();
-        super.internalCleanup();
     }
 
     @Test
@@ -264,7 +262,5 @@ public class PrecisTopicPublishRateThrottleTest extends BrokerTestBase{
         blocking.set(false);
 
         sendFuture.join();
-
-        super.internalCleanup();
     }
 }
