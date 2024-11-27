bfcf3d418b2b7adebfe7edfac098703c94fa6f4f
fengyubiao <yubiao.feng@streamnative.io>
2024-11-27 18:18:51 +0800

    improve logs

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/lookup/TopicLookupBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/lookup/TopicLookupBase.java
@@ -353,10 +353,10 @@ public class TopicLookupBase extends PulsarWebResource {
         }
         boolean tlsEnabled = pulsar.getConfig().isBrokerClientTlsEnabled();
         if (!tlsEnabled && StringUtils.isBlank(lookupData.getBrokerUrl())) {
-            log.warn("[{}] Internal lookup without TLS will get a null brokerUrl, which is unexpected. options: {},"
+            log.warn("[{}] Unexpected lookup resul: brokerUrlTls is required when TLS isn't enabled. options: {},"
                 + " result {}", topic, options, lookupData);
         } else if (tlsEnabled && StringUtils.isBlank(lookupData.getBrokerUrlTls())) {
-            log.warn("[{}] Internal lookup with TLS will get a null brokerUrl, which is unexpected. options: {},"
+            log.warn("[{}] Unexpected lookup resul: brokerUrlTls is required when TLS is enabled. options: {},"
                     + " result {}", topic, options, lookupData);
         }
     }
