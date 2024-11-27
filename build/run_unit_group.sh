aee413b7176f65f91b8928b7a7bb4442b04d7f06
Yong Zhang <zhangyong1025.zy@gmail.com>
2024-11-28 05:55:10 +0800

    [improve] Use single buffer for metrics when noUnsafe use (#23612)
    Co-authored-by: Lari Hotari <lhotari@apache.org>
    (cherry picked from commit 9b79d701a2c1067693508515915b5a4504f60916)

--- build/run_unit_group.sh
+++ build/run_unit_group.sh
@@ -77,6 +77,8 @@ alias echo='{ [[ $- =~ .*x.* ]] && trace_enabled=1 || trace_enabled=0; set +x; }
 # Test Groups  -- start --
 function test_group_broker_group_1() {
   mvn_test -pl pulsar-broker -Dgroups='broker' -DtestReuseFork=true
+  # run tests in broker-isolated group individually (instead of with -Dgroups=broker-isolated) to avoid scanning all test classes
+  mvn_test -pl pulsar-broker -Dtest=org.apache.pulsar.broker.stats.prometheus.PrometheusMetricsGeneratorWithNoUnsafeTest -DtestForkCount=1 -DtestReuseFork=false
 }
 
 function test_group_broker_group_2() {
