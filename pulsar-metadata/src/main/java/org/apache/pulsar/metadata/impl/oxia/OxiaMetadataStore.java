2c05137289af91f4b748953ebb0aa80f9b077443
Lari Hotari <lhotari@users.noreply.github.com>
2024-11-28 18:01:17 +0200

    [improve] Upgrade oxia-java to 0.4.10 and fix closing of OxiaMetadataStore (#23653)
    (cherry picked from commit 7e6fa554012b237308db1bebf3c625e103bf0db5)

--- pulsar-metadata/src/main/java/org/apache/pulsar/metadata/impl/oxia/OxiaMetadataStore.java
+++ pulsar-metadata/src/main/java/org/apache/pulsar/metadata/impl/oxia/OxiaMetadataStore.java
@@ -297,10 +297,12 @@ public class OxiaMetadataStore extends AbstractMetadataStore {
 
     @Override
     public void close() throws Exception {
-        if (client != null) {
-            client.close();
+        if (isClosed.compareAndSet(false, true)) {
+            if (client != null) {
+                client.close();
+            }
+            super.close();
         }
-        super.close();
     }
 
     public Optional<MetadataEventSynchronizer> getMetadataEventSynchronizer() {
