6a008dc85aa3b8123b3188b1431f0379e05a0829
fengyubiao <yubiao.feng@streamnative.io>
2024-12-12 18:25:59 +0800

    [improve] [test] Add more test for the case that client receives a SendError, which relates to the PR #23038

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ServerCnxNonInjectionTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ServerCnxNonInjectionTest.java
@@ -19,16 +19,11 @@
 
 package org.apache.pulsar.broker.service;
 
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.broker.BrokerTestUtil;
-import org.apache.pulsar.client.api.MessageId;
-import org.apache.pulsar.client.api.MessageIdAdv;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.ProducerConsumerBase;
-import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.Schema;
-import org.apache.pulsar.common.api.proto.ServerError;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -62,40 +57,6 @@ public class ServerCnxNonInjectionTest extends ProducerConsumerBase {
         serverCnx.close();
         Thread.sleep(1000);
         serverCnx.checkConnectionLiveness().join();
-    }
-
-    @Test
-    public void producerPersistErrorTest() throws Exception {
-        PulsarClient client = PulsarClient.builder().serviceUrl(lookupUrl.toString())
-                .maxNumberOfRejectedRequestPerConnection(1000).build();
-
-        // Create a producer with customized name.
-        final String tp = BrokerTestUtil.newUniqueName("public/default/tp");
-        admin.topics().createNonPartitionedTopic(tp);
-        Producer<String> p = client.newProducer(Schema.STRING).producerName("p1").topic(tp).create();
-
-        // Inject a persistence error.
-        org.apache.pulsar.broker.service.Producer serverProducer = pulsar.getBrokerService().getTopic(tp, false)
-                .join().get().getProducers().values().iterator().next();
-        ServerCnx serverCnx = (ServerCnx) serverProducer.getCnx();
-        serverCnx.getCommandSender().sendSendError(serverProducer.getProducerId(), 1/* sequenceId */,
-                ServerError.PersistenceError, "mocked error");
-        // Wait for the client receives the error.
-        Thread.sleep(1000);
-
-        try {
-            // Verify: the next publish will finish.
-            MessageId messageId = p.sendAsync("1").get(10, TimeUnit.SECONDS);
-            MessageIdAdv messageIdAdv = (MessageIdAdv) messageId;
-            log.info("sent {}:{}", messageIdAdv.getLedgerId(), messageIdAdv.getEntryId());
-        } finally {
-            // cleanup orphan producers.
-            serverCnx.ctx().close();
-            // cleanup
-            client.close();
-            p.close();
-            admin.topics().delete(tp);
-        }
     }
 
 }
