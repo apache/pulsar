603e559096d13006ec702409a685899bfb5a041b
fengyubiao <yubiao.feng@streamnative.io>
2024-12-19 01:21:08 +0800

    guarantee compatibility for non-persistent topic and marker messages

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/GeoReplicationProducerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/GeoReplicationProducerImpl.java
@@ -27,12 +27,20 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.impl.conf.ProducerConfigurationData;
 import org.apache.pulsar.common.api.proto.KeyValue;
+import org.apache.pulsar.common.naming.TopicName;
+import org.apache.pulsar.common.protocol.Markers;
 
 @Slf4j
 public class GeoReplicationProducerImpl extends ProducerImpl{
 
     public static final String MSG_PROP_REPL_SEQUENCE_LID = "__MSG_PROP_REPL_SEQUENCE_LID";
     public static final String MSG_PROP_REPL_SEQUENCE_EID = "__MSG_PROP_REPL_SEQUENCE_EID";
+    public static final String MSG_PROP_IS_REPL_MARKER = "__MSG_PROP_IS_REPL_MARKER";
+
+    private long lastPersistedSourceLedgerId;
+    private long lastPersistedSourceEntryId;
+
+    private final boolean isPersistentTopic;
 
     public GeoReplicationProducerImpl(PulsarClientImpl client, String topic,
                                       ProducerConfigurationData conf,
@@ -40,100 +48,194 @@ public class GeoReplicationProducerImpl extends ProducerImpl{
                                       Schema schema, ProducerInterceptors interceptors,
                                       Optional overrideProducerName) {
         super(client, topic, conf, producerCreatedFuture, partitionIndex, schema, interceptors, overrideProducerName);
+        isPersistentTopic = TopicName.get(topic).isPersistent();
+    }
+
+    private boolean isBrokerSupportsDedupReplV2(ClientCnx cnx) {
+        // Non-Persistent topic does not have ledger id or entry id, so it does not support.
+        return cnx.isBrokerSupportsDedupReplV2() && isPersistentTopic;
     }
 
     @Override
-    protected void ackReceived(ClientCnx cnx, long lIdSent, long eIdSent, long ledgerId, long entryId) {
-        if (!cnx.isBrokerSupportsDedupReplV2()) {
-            super.ackReceived(cnx, lIdSent, eIdSent, ledgerId, entryId);
+    protected void ackReceived(ClientCnx cnx, long seq, long highSeq, long ledgerId, long entryId) {
+        if (!isBrokerSupportsDedupReplV2(cnx)) {
+            // Repl V1 is the same as normal for this handling.
+            super.ackReceived(cnx, seq, highSeq, ledgerId, entryId);
+            return;
+        }
+        synchronized (this) {
+            OpSendMsg op = pendingMessages.peek();
+            if (op == null) {
+                if (log.isDebugEnabled()) {
+                    log.debug("[{}] [{}] Got ack for timed out msg {}:{}", topic, producerName, seq, highSeq);
+                }
+                return;
+            }
+            // Replicator send markers also, use sequenceId to check the marker send-receipt.
+            if (isReplicationMarker(entryId)) {
+                ackReceivedReplMarker(cnx, op, seq, highSeq, ledgerId, entryId);
+                return;
+            }
+            ackReceivedReplicatedMsg(cnx, op, seq, highSeq, ledgerId, entryId);
+        }
+    }
+    private void ackReceivedReplicatedMsg(ClientCnx cnx, OpSendMsg op, long sourceLId, long sourceEId,
+                                          long targetLId, long targetEid) {
+        // Case-1: repeatedly publish.
+        if (sourceLId < lastPersistedSourceLedgerId
+                || (sourceLId == lastPersistedSourceLedgerId  && sourceEId < lastPersistedSourceEntryId)) {
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] [{}] Dropped a repl marker SendReceipt. Got entry {}:{}, last persisted source entry:"
+                        + " {}:{}",
+                        topic, producerName, sourceLId, sourceEId,
+                        lastPersistedSourceLedgerId, lastPersistedSourceEntryId);
+            }
             return;
         }
 
-        OpSendMsg op = null;
-        synchronized (this) {
-            op = pendingMessages.peek();
-            if (op == null) {
-                if (log.isDebugEnabled()) {
-                    log.debug("[{}] [{}] Got ack for timed out msg {}:{}", topic, producerName, lIdSent, eIdSent);
-                }
-                return;
-            }
-            Long lIdPendingRes = null;
-            Long eIdPendingRes = null;
-            List<KeyValue> kvPairList =  op.msg.getMessageBuilder().getPropertiesList();
-            for (KeyValue kvPair : kvPairList) {
-                if (kvPair.getKey().equals(MSG_PROP_REPL_SEQUENCE_LID)) {
-                    if (StringUtils.isNumeric(kvPair.getValue())) {
-                        lIdPendingRes = Long.valueOf(kvPair.getValue());
-                    } else {
-                        break;
-                    }
-                }
-                if (kvPair.getKey().equals(MSG_PROP_REPL_SEQUENCE_EID)) {
-                    if (StringUtils.isNumeric(kvPair.getValue())) {
-                        eIdPendingRes = Long.valueOf(kvPair.getValue());
-                    } else {
-                        break;
-                    }
-                }
-                if (lIdPendingRes != null && eIdPendingRes != null) {
+        // Parse source cluster's entry position.
+        Long pendingLId = null;
+        Long pendingEId = null;
+        List<KeyValue> kvPairList =  op.msg.getMessageBuilder().getPropertiesList();
+        for (KeyValue kvPair : kvPairList) {
+            if (kvPair.getKey().equals(MSG_PROP_REPL_SEQUENCE_LID)) {
+                if (StringUtils.isNumeric(kvPair.getValue())) {
+                    pendingLId = Long.valueOf(kvPair.getValue());
+                } else {
                     break;
                 }
             }
-            if (lIdPendingRes == null || eIdPendingRes == null) {
-                // Rollback to the original implementation.
-                log.error("[{}] [{}] can not found v2 sequence-id {}:{}, ackReceived: {}:{} {}:{} - queue-size: {}",
-                        topic, producerName, lIdPendingRes, eIdPendingRes, lIdSent,
-                        eIdSent, ledgerId, entryId, pendingMessages.messagesCount());
-                cnx.channel().close();
-                return;
+            if (kvPair.getKey().equals(MSG_PROP_REPL_SEQUENCE_EID)) {
+                if (StringUtils.isNumeric(kvPair.getValue())) {
+                    pendingEId = Long.valueOf(kvPair.getValue());
+                } else {
+                    break;
+                }
             }
+            if (pendingLId != null && pendingEId != null) {
+                break;
+            }
+        }
 
-            if (lIdSent == lIdPendingRes && eIdSent == eIdPendingRes) {
-                // Q: After a reconnect, maybe we have lost the response of Send-Receipt, then how can we remove
-                //    pending messages from the queue?
-                // A: if both @param-ledgerId and @param-entry-id are "-1", it means the message has been sent
-                //    successfully.
-                //    PS: broker will respond "-1" only when it confirms the message has been persisted, broker will
-                //        respond a "MessageDeduplication.MessageDupUnknownException" if the message is sending
-                //        in-progress.
-                //    Notice: if send messages outs of oder, may lost messages.
-                // Conclusion: So whether @param-ledgerId and @param-entry-id are "-1" or not, we can remove pending
-                //    message.
-                if (log.isDebugEnabled()) {
-                    log.debug("Got receipt for producer: [{}] -- source-message: {}:{} -- target-msg: {}:{}",
-                            getProducerName(), lIdSent, eIdSent, ledgerId, entryId);
-                }
-                pendingMessages.remove();
-                releaseSemaphoreForSendOp(op);
-                // Since Geo-Replicator will not send batched message, skip to update the field
-                // "LAST_SEQ_ID_PUBLISHED_UPDATER".
-                op.setMessageId(ledgerId, entryId, partitionIndex);
-                try {
-                    // Need to protect ourselves from any exception being thrown in the future handler from the
-                    // application
-                    op.sendComplete(null);
-                } catch (Throwable t) {
-                    log.warn("[{}] [{}] Got exception while completing the callback for -- source-message: {}:{} --"
-                                    + " target-msg: {}:{}",
-                            topic, producerName, lIdSent, eIdSent, ledgerId, entryId, t);
-                }
-                ReferenceCountUtil.safeRelease(op.cmd);
-                op.recycle();
-            } else if (lIdSent < lIdPendingRes || (lIdSent == lIdPendingRes  && eIdSent < eIdPendingRes)) {
-                // Ignoring the ack since it's referring to a message that has already timed out.
-                if (log.isDebugEnabled()) {
-                    log.debug("[{}] [{}] Got ack for timed out msg. expecting less or equals {}:{}, but got: {}:{}",
-                            topic, producerName, lIdPendingRes, eIdPendingRes, lIdSent,
-                            eIdSent);
-                }
-            } else {
-                log.warn("[{}] [{}] Got ack for msg. expecting less or equals {}:{}, but got: {}:{} - queue-size: {}",
-                        topic, producerName, lIdPendingRes, eIdPendingRes, lIdSent,
-                        eIdSent, pendingMessages.messagesCount());
-                // Force connection closing so that messages can be re-transmitted in a new connection
-                cnx.channel().close();
+        // Case-2, which is expected.
+        if (pendingLId!= null && pendingEId != null && sourceLId == pendingLId && sourceEId == pendingEId) {
+            // Case-3, which is expected.
+            // Q: After a reconnect, maybe we have lost the response of Send-Receipt, then how can we remove
+            //    pending messages from the queue?
+            // A: if both @param-ledgerId and @param-entry-id are "-1", it means the message has been sent
+            //    successfully.
+            //    PS: broker will respond "-1" only when it confirms the message has been persisted, broker will
+            //        respond a "MessageDeduplication.MessageDupUnknownException" if the message is sending
+            //        in-progress.
+            //    Notice: if send messages outs of oder, may lost messages.
+            // Conclusion: So whether @param-ledgerId and @param-entry-id are "-1" or not, we can remove pending
+            //    message.
+            lastPersistedSourceLedgerId = sourceLId;
+            lastPersistedSourceEntryId = sourceEId;
+            removeAndApplyCallback(op, sourceLId, sourceEId, targetLId, targetEid, false);
+            return;
+        }
+
+        // Case-3: got null source cluster's entry position, which is unexpected.
+        if (pendingLId == null || pendingEId == null) {
+            log.error("[{}] [{}] can not found v2 sequence-id {}:{}, ackReceived: {}:{} {}:{} - queue-size: {}",
+                    topic, producerName, pendingLId, pendingEId, sourceLId,
+                    sourceEId, targetLId, targetEid, pendingMessages.messagesCount());
+            cnx.channel().close();
+            return;
+        }
+
+        // Case-4: unknown error, which is unexpected.
+        log.warn("[{}] [{}] Got ack for msg. expecting {}:{}, but got: {}:{} - queue-size: {}",
+                topic, producerName, pendingLId, pendingEId, sourceLId,
+                sourceEId, pendingMessages.messagesCount());
+        // Force connection closing so that messages can be re-transmitted in a new connection
+        cnx.channel().close();
+    }
+
+    protected void ackReceivedReplMarker(ClientCnx cnx, OpSendMsg op, long req, long highReq,
+                                         long ledgerId, long entryId) {
+        // Case-1: repeatedly publish repl marker.
+        long lastSeqReceivedAck = LAST_SEQ_ID_PUBLISHED_UPDATER.get(this);
+        if (req <= lastSeqReceivedAck) {
+            // Ignoring the ack since it's referring to a message that has already timed out.
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] [{}] Dropped a repl marker SendReceipt. sequenceId: {},"
+                                + " sequenceIdPersisted: {},"
+                                + " highReq: {}, position: {}:{}",
+                        topic, producerName, req, lastSeqReceivedAck, highReq, ledgerId, entryId);
             }
+            return;
+        }
+
+        // Case-2, which is expected:
+        // 1. Broker responds SendReceipt who is a repl marker.
+        // 2. The current pending msg is also a marker.
+        if (isReplicationMarker(op) && req == op.sequenceId) {
+            long calculatedSeq = getHighestSequenceId(op);
+            LAST_SEQ_ID_PUBLISHED_UPDATER.getAndUpdate(this, last -> Math.max(last, calculatedSeq));
+            removeAndApplyCallback(op, req, highReq, ledgerId, entryId, true);
+            return;
+        }
+
+        // Case-3, which is unexpected.
+        // Case-3-1: expected a SendError if "seq <= lastInProgressSend".
+        // Case-3-2: something went wrong.
+        long lastInProgressSend = LAST_SEQ_ID_PUSHED_UPDATER.get(this);
+        String logText = String.format("[%s] [%s] Got ack for a repl marker msg. expecting %s, but got: %s."
+                + " sequenceIdPersisted: %s, lastInProgressSend: %s,"
+                + " HighReq: %s, position: %s:%s, queue-size: %s",
+                topic, producerName, lastSeqReceivedAck - 1, req,
+                lastSeqReceivedAck, lastInProgressSend,
+                highReq, ledgerId, entryId, pendingMessages.messagesCount()
+        );
+        if (req < lastInProgressSend) {
+            log.warn(logText);
+        } else {
+            log.error(logText);
+        }
+        // Force connection closing so that messages can be re-transmitted in a new connection.
+        cnx.channel().close();
+    }
+
+    private void removeAndApplyCallback(OpSendMsg op, long lIdSent, long eIdSent, long ledgerId, long entryId,
+                                        boolean isMarker) {
+        if (log.isDebugEnabled()) {
+            log.debug("Got receipt for producer: [{}] -- source-message: {}:{} -- target-msg: {}:{} -- isMarker: {}",
+                    getProducerName(), lIdSent, eIdSent, ledgerId, entryId, isMarker);
+        }
+        pendingMessages.remove();
+        releaseSemaphoreForSendOp(op);
+        // Since Geo-Replicator will not send batched message, skip to update the field
+        // "LAST_SEQ_ID_PUBLISHED_UPDATER".
+        op.setMessageId(ledgerId, entryId, partitionIndex);
+        try {
+            // Need to protect ourselves from any exception being thrown in the future handler from the
+            // application
+            op.sendComplete(null);
+        } catch (Throwable t) {
+            log.warn("[{}] [{}] Got exception while completing the callback for -- source-message: {}:{} --"
+                            + " target-msg: {}:{} -- isMarker: {}",
+                    topic, producerName, lIdSent, eIdSent, ledgerId, entryId, isMarker, t);
+        }
+        ReferenceCountUtil.safeRelease(op.cmd);
+        op.recycle();
+    }
+
+    private boolean isReplicationMarker(OpSendMsg op) {
+        return op.msg != null && op.msg.getMessageBuilder().hasMarkerType()
+                && Markers.isReplicationMarker(op.msg.getMessageBuilder().getMarkerType());
+    }
+
+    private boolean isReplicationMarker(long highestSeq) {
+        return Long.MIN_VALUE == highestSeq;
+    }
+
+    @Override
+    protected void updateLastSeqPushed(OpSendMsg op) {
+        // Only update the value for repl marker.
+        if (isReplicationMarker(op)) {
+            super.updateLastSeqPushed(op);
         }
     }
 }
