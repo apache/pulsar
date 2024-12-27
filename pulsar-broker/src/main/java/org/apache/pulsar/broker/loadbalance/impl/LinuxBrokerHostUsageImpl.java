6eeb5663d7275df2436ec350292cf77813b3b342
wangda <38549158+daziz@users.noreply.github.com>
2024-12-27 11:21:11 +0800

    Update pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/impl/LinuxBrokerHostUsageImpl.java
    Co-authored-by: Lari Hotari <lhotari@users.noreply.github.com>

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/impl/LinuxBrokerHostUsageImpl.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/loadbalance/impl/LinuxBrokerHostUsageImpl.java
@@ -155,7 +155,7 @@ public class LinuxBrokerHostUsageImpl implements BrokerHostUsage {
      * </pre>
      *
      * Line is split in "words", filtering the first. The sum of all numbers give the amount of cpu cycles used this
-     * far. Real CPU usage should equal the sum subtracting the idle cycles(that is idle+iowait), this would include
+     * far. Real CPU usage should equal the sum after subtracting the idle cycles (that is idle+iowait), this would include
      * cpu, user, nice, system, irq, softirq, steal, guest and guest_nice.
      */
     private double getTotalCpuUsageForEntireHost() {
