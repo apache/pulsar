db733a1805605cef9e5560851a0f2852415301d4
Lari Hotari <lhotari@apache.org>
2025-01-17 18:10:15 +0200

    Improve quiet time implementation in BrokerTestUtil.receiveMessages

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/BrokerTestUtil.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/BrokerTestUtil.java
@@ -39,6 +39,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
@@ -239,33 +240,42 @@ public class BrokerTestUtil {
     public static <T> void receiveMessages(BiFunction<Consumer<T>, Message<T>, Boolean> messageHandler,
                                            Duration quietTimeout,
                                            Stream<Consumer<T>> consumers) {
+        long quietTimeoutNanos = quietTimeout.toNanos();
+        AtomicLong lastMessageReceivedNanos = new AtomicLong(System.nanoTime());
         FutureUtil.waitForAll(consumers
-                .map(consumer -> receiveMessagesAsync(consumer, quietTimeout, messageHandler)).toList()).join();
+                .map(consumer -> receiveMessagesAsync(consumer, quietTimeoutNanos, messageHandler,
+                        lastMessageReceivedNanos)).toList()).join();
     }
 
     // asynchronously receive messages from a consumer and handle them using the provided message handler
     // the benefit is that multiple consumers can be concurrently consumed without the need to have multiple threads
     // this is useful in tests where multiple consumers are needed to test the functionality
-    private static <T> CompletableFuture<Void> receiveMessagesAsync(Consumer<T> consumer, Duration quietTimeout,
-                                                             BiFunction<Consumer<T>, Message<T>, Boolean>
-                                                                     messageHandler) {
-        CompletableFuture<Message<T>> receiveFuture = consumer.receiveAsync();
-        return receiveFuture
-                .orTimeout(quietTimeout.toMillis(), TimeUnit.MILLISECONDS)
+    private static <T> CompletableFuture<Void> receiveMessagesAsync(Consumer<T> consumer,
+                                                                    long quietTimeoutNanos,
+                                                                    BiFunction<Consumer<T>, Message<T>, Boolean>
+                                                                            messageHandler,
+                                                                    AtomicLong lastMessageReceivedNanos) {
+        return consumer.receiveAsync()
+                .orTimeout(quietTimeoutNanos, TimeUnit.NANOSECONDS)
                 .handle((msg, t) -> {
+                    long currentNanos = System.nanoTime();
                     if (t != null) {
                         if (t instanceof TimeoutException) {
-                            // cancel the receive future so that Pulsar client can clean up the resources
-                            receiveFuture.cancel(false);
-                            return false;
+                            if (currentNanos - lastMessageReceivedNanos.get() > quietTimeoutNanos) {
+                                return false;
+                            } else {
+                                return true;
+                            }
                         } else {
                             throw FutureUtil.wrapToCompletionException(t);
                         }
                     }
+                    lastMessageReceivedNanos.set(currentNanos);
                     return messageHandler.apply(consumer, msg);
                 }).thenComposeAsync(receiveMore -> {
                     if (receiveMore) {
-                        return receiveMessagesAsync(consumer, quietTimeout, messageHandler);
+                        return receiveMessagesAsync(consumer, quietTimeoutNanos, messageHandler,
+                                lastMessageReceivedNanos);
                     } else {
                         return CompletableFuture.completedFuture(null);
                     }
