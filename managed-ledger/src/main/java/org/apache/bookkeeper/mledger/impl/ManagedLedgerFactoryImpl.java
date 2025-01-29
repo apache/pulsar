8d782690ee3748de8a3fed228343894af052251f
Lari Hotari <lhotari@users.noreply.github.com>
2025-01-29 22:11:56 +0200

    [fix][broker] Make InflightReadsLimiter asynchronous and apply it for replay queue reads (#23901)
    (cherry picked from commit c5173d5e15efade90afb9b0b1c19f3ba5b3aab37)
    (cherry picked from commit 37f0bc22dffe6468fc775d68ad1a9c7f9516cf3a)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerFactoryImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerFactoryImpl.java
@@ -199,7 +199,7 @@ public class ManagedLedgerFactoryImpl implements ManagedLedgerFactory {
                 config.getCompressionConfigForManagedCursorInfo());
         this.config = config;
         this.mbean = new ManagedLedgerFactoryMBeanImpl(this);
-        this.entryCacheManager = new RangeEntryCacheManagerImpl(this);
+        this.entryCacheManager = new RangeEntryCacheManagerImpl(this, scheduledExecutor);
         this.statsTask = scheduledExecutor.scheduleWithFixedDelay(catchingAndLoggingThrowables(this::refreshStats),
                 0, config.getStatsPeriodSeconds(), TimeUnit.SECONDS);
         this.flushCursorsTask = scheduledExecutor.scheduleAtFixedRate(catchingAndLoggingThrowables(this::flushCursors),
