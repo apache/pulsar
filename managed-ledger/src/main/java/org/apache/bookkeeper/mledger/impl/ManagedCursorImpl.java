10ae74e8171cadf259758386b40ec20e35170e92
dao-jun <daojun@apache.org>
2025-02-04 15:38:41 +0800

    fix checkstyle & tests

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedCursorImpl.java
@@ -1336,7 +1336,7 @@ public class ManagedCursorImpl implements ManagedCursor {
         }
 
         if (max <= 0) {
-            callback.findEntryFailed(new ManagedLedgerException("No entries available"), Optional.empty(), ctx);
+            callback.findEntryComplete(null, ctx);
             return;
         }
 
