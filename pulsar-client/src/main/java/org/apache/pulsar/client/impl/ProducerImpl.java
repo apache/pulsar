617cb7123ef7daade0492a0de3bcd1ae8a9759cd
Baodi Shi <baodi@apache.org>
2025-02-06 19:42:09 +0800

    Cherry-pick #23935
    (cherry picked from commit df737f7e882408148e55f5836b903639f99f962b)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ProducerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ProducerImpl.java
@@ -1836,14 +1836,6 @@ public class ProducerImpl<T> extends ProducerBase<T> implements TimerTask, Conne
                         cnx.sendRequestWithId(cmd, closeRequestId);
                     }
 
-                    if (cause instanceof PulsarClientException.ProducerFencedException) {
-                        if (log.isDebugEnabled()) {
-                            log.debug("[{}] [{}] Failed to create producer: {}",
-                                    topic, producerName, cause.getMessage());
-                        }
-                    } else {
-                        log.error("[{}] [{}] Failed to create producer: {}", topic, producerName, cause.getMessage());
-                    }
                     // Close the producer since topic does not exist.
                     if (cause instanceof PulsarClientException.TopicDoesNotExistException) {
                         closeAsync().whenComplete((v, ex) -> {
@@ -1873,6 +1865,11 @@ public class ProducerImpl<T> extends ProducerBase<T> implements TimerTask, Conne
                     } else if (cause instanceof PulsarClientException.ProducerBlockedQuotaExceededError) {
                         log.warn("[{}] [{}] Producer is blocked on creation because backlog exceeded on topic.",
                                 producerName, topic);
+                    } else if (PulsarClientException.isRetriableError(cause)) {
+                        log.info("[{}] [{}] Temporary error in creating producer: {}", topic,
+                                producerName, cause.getMessage());
+                    } else {
+                        log.error("[{}] [{}] Failed to create producer: {}", topic, producerName, cause.getMessage());
                     }
 
                     if (cause instanceof PulsarClientException.TopicTerminatedException) {
