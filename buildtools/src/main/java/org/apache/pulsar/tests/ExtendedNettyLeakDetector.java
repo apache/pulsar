841ca988f68f5aa589bb46215327a2b3f5fecb68
Lari Hotari <lhotari@apache.org>
2025-02-10 10:28:39 +0200

    [improve][test] Enable Netty leak detection in tests

---
+++ buildtools/src/main/java/org/apache/pulsar/tests/ExtendedNettyLeakDetector.java
@@ -0,0 +1,216 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.tests;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.util.ResourceLeakDetector;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A custom Netty leak detector used in Pulsar tests that dumps the detected leaks to a file in a directory that is
+ * configured with the NETTY_LEAK_DUMP_DIR environment variable. This directory defaults to java.io.tmpdir.
+ * The files will be named netty_leak_YYYYMMDD-HHMMSS.SSS.txt.
+ * By default, the JVM will exit when a leak is detected. This behavior can be disabled by setting the
+ * org.apache.pulsar.tests.ExtendedNettyLeakDetector.exitJvmOnLeak system property to false.
+ */
+public class ExtendedNettyLeakDetector<T> extends ResourceLeakDetector<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(ExtendedNettyLeakDetector.class);
+    private static final File DUMP_DIR =
+            new File(System.getenv().getOrDefault("NETTY_LEAK_DUMP_DIR", System.getProperty("java.io.tmpdir")));
+    public static final String EXIT_JVM_ON_LEAK_SYSTEM_PROPERTY_NAME =
+            ExtendedNettyLeakDetector.class.getName() + ".exitJvmOnLeak";
+    public static final String SLEEP_AFTER_GC_AND_FINALIZATION_MILLIS_SYSTEM_PROPERTY_NAME =
+            ExtendedNettyLeakDetector.class.getName() + ".sleepAfterGCAndFinalizationMillis";
+    private static final long SLEEP_AFTER_GC_AND_FINALIZATION_MILLIS = Long.parseLong(System.getProperty(
+            SLEEP_AFTER_GC_AND_FINALIZATION_MILLIS_SYSTEM_PROPERTY_NAME, "10"));
+    private static boolean exitJvmOnLeak = Boolean.valueOf(
+            System.getProperty(EXIT_JVM_ON_LEAK_SYSTEM_PROPERTY_NAME, "true"));
+    private static final boolean DEFAULT_EXIT_JVM_ON_LEAK = exitJvmOnLeak;
+    public static final String EXIT_JVM_DELAY_MILLIS_SYSTEM_PROPERTY_NAME =
+            ExtendedNettyLeakDetector.class.getName() + ".exitJvmDelayMillis";
+    private static final long EXIT_JVM_DELAY_MILLIS =
+            Long.parseLong(System.getProperty(EXIT_JVM_DELAY_MILLIS_SYSTEM_PROPERTY_NAME, "1000"));
+    private boolean exitThreadStarted;
+    private static volatile String initialHint;
+
+    /**
+     * Disable exit on leak.
+     */
+    public static void disableExitJVMOnLeak() {
+        exitJvmOnLeak = false;
+    }
+
+    /**
+     * Restore exit on leak to original value.
+     */
+    public static void restoreExitJVMOnLeak() {
+        triggerLeakDetection();
+        exitJvmOnLeak = DEFAULT_EXIT_JVM_ON_LEAK;
+    }
+
+    /**
+     * Triggers Netty leak detection.
+     * Passing "-XX:+UnlockExperimentalVMOptions -XX:ReferencesPerThread=0" to the JVM will help to detect leaks
+     * with a shorter latency. When ReferencesPerThread is set to 0, the JVM will use maximum parallelism for processing
+     * reference objects. Netty's leak detection relies on WeakReferences and this setting will help to process them
+     * faster.
+     */
+    public static void triggerLeakDetection() {
+        // run System.gc() to trigger finalization of objects and detection of possible Netty leaks
+        System.gc();
+        System.runFinalization();
+        try {
+            // wait for WeakReference collection to complete
+            Thread.sleep(SLEEP_AFTER_GC_AND_FINALIZATION_MILLIS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        triggerLeakReporting();
+    }
+
+    private static void triggerLeakReporting() {
+        // create a ByteBuf and release it to trigger leak detection
+        // this calls ResourceLeakDetector's reportLeak method when paranoid is enabled.
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.directBuffer();
+        // this triggers leak detection which forces tracking even when paranoid isn't enabled
+        // check the source code of ResourceLeakDetector to see how it works or step through it in a debugger
+        // This will call leak detector's trackForcibly method which will then call ResourceLeakDetector's reportLeak
+        // trackForcibly gets called in io.netty.buffer.SimpleLeakAwareByteBuf.unwrappedDerived
+        ByteBuf retainedSlice = buffer.retainedSlice();
+        retainedSlice.release();
+        buffer.release();
+    }
+
+    public ExtendedNettyLeakDetector(Class<?> resourceType, int samplingInterval, long maxActive) {
+        super(resourceType, samplingInterval, maxActive);
+    }
+
+    public ExtendedNettyLeakDetector(Class<?> resourceType, int samplingInterval) {
+        super(resourceType, samplingInterval);
+    }
+
+    public ExtendedNettyLeakDetector(String resourceType, int samplingInterval, long maxActive) {
+        super(resourceType, samplingInterval, maxActive);
+    }
+
+    /**
+     * Set the initial hint to be used when reporting leaks.
+     * This hint will be printed alongside the stack trace of the creation of the resource in the Netty leak report.
+     *
+     * @see ResourceLeakDetector#getInitialHint(String)
+     * @param initialHint the initial hint
+     */
+    public static void setInitialHint(String initialHint) {
+        ExtendedNettyLeakDetector.initialHint = initialHint;
+    }
+
+    @Override
+    protected boolean needReport() {
+        return true;
+    }
+
+    @Override
+    protected Object getInitialHint(String resourceType) {
+        String currentInitialHint = ExtendedNettyLeakDetector.initialHint;
+        if (currentInitialHint != null) {
+            return currentInitialHint;
+        } else {
+            return super.getInitialHint(resourceType);
+        }
+    }
+
+    @Override
+    protected void reportTracedLeak(String resourceType, String records) {
+        super.reportTracedLeak(resourceType, records);
+        dumpToFile(resourceType, records);
+        maybeExitJVM();
+    }
+
+    @Override
+    protected void reportUntracedLeak(String resourceType) {
+        super.reportUntracedLeak(resourceType);
+        dumpToFile(resourceType, null);
+        maybeExitJVM();
+    }
+
+    private synchronized void maybeExitJVM() {
+        if (exitThreadStarted) {
+            return;
+        }
+        if (exitJvmOnLeak) {
+            new Thread(() -> {
+                LOG.error("Exiting JVM due to Netty resource leak. Check logs for more details. Dumped to {}",
+                        DUMP_DIR.getAbsolutePath());
+                System.err.println("Exiting JVM due to Netty resource leak. Check logs for more details. Dumped to "
+                        + DUMP_DIR.getAbsolutePath());
+                triggerLeakDetection();
+                try {
+                    Thread.sleep(EXIT_JVM_DELAY_MILLIS);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+                triggerLeakDetection();
+                // shutdown log4j2 logging to prevent log truncation
+                LogManager.shutdown();
+                // flush log buffers
+                System.err.flush();
+                System.out.flush();
+                // exit JVM immediately
+                Runtime.getRuntime().halt(1);
+            }).start();
+            exitThreadStarted = true;
+        }
+    }
+
+    private void dumpToFile(String resourceType, String records) {
+        try {
+            if (!DUMP_DIR.exists()) {
+                DUMP_DIR.mkdirs();
+            }
+            String datetimePart =
+                    DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss.SSS").format(ZonedDateTime.now());
+            File nettyLeakDumpFile =
+                    new File(DUMP_DIR, "netty_leak_" + datetimePart + ".txt");
+            // Prefix the line to make it easier to show the errors in GitHub Actions as annotations
+            String linePrefix = exitJvmOnLeak ? "::error::" : "::warning::";
+            try (PrintWriter out = new PrintWriter(new FileWriter(nettyLeakDumpFile, true))) {
+                out.print(linePrefix);
+                if (records != null) {
+                    out.println("Traced leak detected " + resourceType);
+                    out.println(records);
+                } else {
+                    out.println("Untraced leak detected " + resourceType);
+                }
+                out.println();
+                out.flush();
+            }
+        } catch (IOException e) {
+            LOG.error("Cannot write thread leak dump", e);
+        }
+    }
+}
