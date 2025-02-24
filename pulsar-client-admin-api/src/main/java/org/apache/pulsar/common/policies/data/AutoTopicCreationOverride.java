3984650e99760ce434f1d1068a1e9d561f331280
道君 <daojun@apache.org>
2025-02-25 02:27:25 +0800

    fix checkstyle

--- pulsar-client-admin-api/src/main/java/org/apache/pulsar/common/policies/data/AutoTopicCreationOverride.java
+++ pulsar-client-admin-api/src/main/java/org/apache/pulsar/common/policies/data/AutoTopicCreationOverride.java
@@ -29,7 +29,7 @@ public interface AutoTopicCreationOverride {
     String getTopicType();
 
     Integer getDefaultNumPartitions();
-  
+
     Integer getSystemTopicDefaultNumPartitions();
 
     interface Builder {
