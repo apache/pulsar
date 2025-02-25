6f6af021932671ef9825b2b4bcb0bfc6fde205e5
walkinggo <40360529+walkinggo@users.noreply.github.com>
2025-02-25 22:05:37 +0800

    Illustrate more about conf

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/conf/ClientConfigurationData.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/conf/ClientConfigurationData.java
@@ -375,7 +375,8 @@ public class ClientConfigurationData implements Serializable, Cloneable {
 
     @ApiModelProperty(
             name = "maxRetryRequestTimes",
-            value = "The Pulsar admin client max retry request times, default value is 5"
+            value = "The Pulsar admin client max retry request times, default value is 5. "
+                    + "This value is system-managed and cannot be modified externally."
     )
     private int maxRetryRequestTimes = 5;
 
