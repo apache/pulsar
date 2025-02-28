13fb338ec52b3dec562bbdcb5aa7f22e6b778614
Zixuan Liu <nodeces@gmail.com>
2025-02-28 23:20:03 +0800

    [fix][broker] Add expire check for replicator (#23975)
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>
    (cherry picked from commit d0025e79e9b866b9d2b5024f16e4d631e744ecd9)
    (cherry picked from commit ea13c92c6de3bb8685d81c92bec4ba42e02f34b9)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentTopic.java
@@ -1918,6 +1918,10 @@ public class PersistentTopic extends AbstractTopic implements Topic, AddEntryCal
                    sub.expireMessages(messageTtlInSeconds);
                 }
             });
+            replicators.forEach((__, replicator)
+                    -> ((PersistentReplicator) replicator).expireMessages(messageTtlInSeconds));
+            shadowReplicators.forEach((__, replicator)
+                    -> ((PersistentReplicator) replicator).expireMessages(messageTtlInSeconds));
         }
     }
 
