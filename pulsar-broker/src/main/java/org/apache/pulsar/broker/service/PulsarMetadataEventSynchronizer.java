fdd54ad736e64f951fa1139a9e2b91ec78241677
Rajan Dhabalia <rdhabalia@apache.org>
2025-03-17 01:45:36 -0700

    [fix][broker] Fix NPE while publishing Metadata-Event with not init producer (#24079)
    (cherry picked from commit 37300ab156d58d6d5b4d98aaba003999a879e013)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/PulsarMetadataEventSynchronizer.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/PulsarMetadataEventSynchronizer.java
@@ -121,6 +121,7 @@ public class PulsarMetadataEventSynchronizer implements MetadataEventSynchronize
         if (!isProducerStarted()) {
             log.info("Producer is not started on {}, failed to publish {}", topicName, event);
             future.completeExceptionally(new IllegalStateException("producer is not started yet"));
+            return;
         }
         producer.newMessage().value(event).sendAsync().thenAccept(__ -> {
             log.info("successfully published metadata change event {}", event);
@@ -136,6 +137,7 @@ public class PulsarMetadataEventSynchronizer implements MetadataEventSynchronize
     protected void startProducer() {
         if (isClosingOrClosed()) {
             log.info("[{}] Skip to start new producer because the synchronizer is closed", topicName);
+            return;
         }
         if (producer != null) {
             log.error("[{}] Failed to start the producer because the producer has been set, state: {}",
