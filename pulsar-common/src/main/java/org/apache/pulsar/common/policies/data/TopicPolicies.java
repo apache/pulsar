bdf3df51c64d3018a0b4af26ed73839f6b9a3f35
道君 <daojun@apache.org>
2025-03-17 15:07:09 +0800

    [fix][broker] Fix UnsupportedOperationException while setting subscription level dispatch rate policy (#24048)
    (cherry picked from commit 9f38a5c9c2335c0d30d070b587ead57618aa1f78)
    (cherry picked from commit d323e8d07b690b74c3a9add766911581e9af4742)

--- pulsar-common/src/main/java/org/apache/pulsar/common/policies/data/TopicPolicies.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/policies/data/TopicPolicies.java
@@ -19,12 +19,13 @@
 package org.apache.pulsar.common.policies.data;
 
 import com.google.common.collect.Sets;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -42,6 +43,7 @@ import org.apache.pulsar.common.policies.data.impl.DispatchRateImpl;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@SuppressFBWarnings(value = "RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE")
 public class TopicPolicies {
 
     @Builder.Default
@@ -191,6 +193,9 @@ public class TopicPolicies {
     }
 
     public Map<String, SubscriptionPolicies> getSubscriptionPolicies() {
-        return subscriptionPolicies == null ? Collections.emptyMap() : subscriptionPolicies;
+        if (subscriptionPolicies == null) {
+            subscriptionPolicies = new ConcurrentHashMap<>();
+        }
+        return subscriptionPolicies;
     }
 }
