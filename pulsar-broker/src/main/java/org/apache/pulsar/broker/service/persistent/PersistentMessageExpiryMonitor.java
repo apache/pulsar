fef163cee16cb6f2c31f086ee019ba20e555b212
zhenJiangWang <zhenjiang427@gmail.com>
2025-03-20 02:15:18 +0800

    [improve][broker] Optimize message expiration rate repeated update issues (#24073)
    Co-authored-by: zjxxzjwang <zjxxzjwang@tencent.com>
    (cherry picked from commit b936f46d2dd2ade98521b7825ee9277549703ef1)
    (cherry picked from commit f3368b4b4984ed7d597c07623825e5d8ff7f538a)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentMessageExpiryMonitor.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentMessageExpiryMonitor.java
@@ -170,11 +170,12 @@ public class PersistentMessageExpiryMonitor implements FindEntryCallback, Messag
     }
 
 
-    public void updateRates() {
+    private void updateRates() {
         msgExpired.calculateRate();
     }
 
     public double getMessageExpiryRate() {
+        updateRates();
         return msgExpired.getRate();
     }
 
@@ -190,7 +191,6 @@ public class PersistentMessageExpiryMonitor implements FindEntryCallback, Messag
             long numMessagesExpired = (long) ctx - cursor.getNumberOfEntriesInBacklog(false);
             msgExpired.recordMultipleEvents(numMessagesExpired, 0 /* no value stats */);
             totalMsgExpired.add(numMessagesExpired);
-            updateRates();
             // If the subscription is a Key_Shared subscription, we should to trigger message dispatch.
             if (subscription != null && subscription.getType() == SubType.Key_Shared) {
                 subscription.getDispatcher().markDeletePositionMoveForward();
