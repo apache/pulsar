f8816e1d981695fd781a32a6e4b14cd4f2f4e8d3
Baodi Shi <baodi@apache.org>
2025-03-26 08:06:38 +0800

    [improve][io] Remove sleep when sourceTask.poll of kafka return null (#24124)
    (cherry picked from commit d0c996e9f80f7297ab7539b8bc09e7e6ec607651)
    (cherry picked from commit 0782b7d71502abac86d2059ca07c0e9e39c18a75)

--- pulsar-io/kafka-connect-adaptor/src/main/java/org/apache/pulsar/io/kafka/connect/AbstractKafkaConnectSource.java
+++ pulsar-io/kafka-connect-adaptor/src/main/java/org/apache/pulsar/io/kafka/connect/AbstractKafkaConnectSource.java
@@ -169,7 +169,6 @@ public abstract class AbstractKafkaConnectSource<T> implements Source<T> {
                 flushFuture = new CompletableFuture<>();
                 List<SourceRecord> recordList = sourceTask.poll();
                 if (recordList == null || recordList.isEmpty()) {
-                    Thread.sleep(1000);
                     continue;
                 }
                 outstandingRecords.addAndGet(recordList.size());
