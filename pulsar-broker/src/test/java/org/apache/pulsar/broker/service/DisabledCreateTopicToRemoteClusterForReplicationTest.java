969165714478c839f20a2dd983cea4d4a5c25cf1
3pacccccc <36988878+3pacccccc@users.noreply.github.com>
2025-03-28 21:39:47 +0800

    [fix][test] increase time to receive message

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/DisabledCreateTopicToRemoteClusterForReplicationTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/DisabledCreateTopicToRemoteClusterForReplicationTest.java
@@ -108,10 +108,7 @@ public class DisabledCreateTopicToRemoteClusterForReplicationTest extends OneWay
         admin2.topics().createPartitionedTopic(tp, 1);
         Consumer<String> consumer2 = client2.newConsumer(Schema.STRING).topic(tp).isAckReceiptEnabled(true)
                 .subscriptionName("s1").subscribe();
-        Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
-            assertEquals(consumer2.receive().getValue(), msgValue);
-        });
-
+        assertEquals(consumer2.receive(20, TimeUnit.SECONDS).getValue(), msgValue);
         consumer2.close();
 
         // cleanup.
