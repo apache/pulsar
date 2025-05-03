9a1124516ed46cf59cba39ffa7b12a0caf7b9d80
Lari Hotari <lhotari@users.noreply.github.com>
2025-05-03 14:47:06 +0300

    [improve][misc] Migrate from multiple nullness annotation libraries to JSpecify annotations (#24239)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
@@ -96,8 +96,8 @@ import org.apache.pulsar.common.policies.path.PolicyPath;
 import org.apache.pulsar.common.util.FutureUtil;
 import org.apache.pulsar.metadata.api.MetadataStoreException;
 import org.apache.pulsar.metadata.api.coordination.LockManager;
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
