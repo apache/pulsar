890e02a715b21abf8e1ffcb64cc9d2dcf022695e
Lari Hotari <lhotari@apache.org>
2025-05-05 15:22:01 +0300

    [fix][test] Fix TestNG BetweenTestClassesListenerAdapter listener
    The previous solution was causing issues since it got called after the next test class had been initialized.
    This caused issues with some tests which initialized test mocks in constructor.

--- buildtools/src/main/java/org/apache/pulsar/tests/BetweenTestClassesListenerAdapter.java
+++ buildtools/src/main/java/org/apache/pulsar/tests/BetweenTestClassesListenerAdapter.java
@@ -18,43 +18,89 @@
  */
 package org.apache.pulsar.tests;
 
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.IClass;
 import org.testng.IClassListener;
+import org.testng.IConfigurationListener;
 import org.testng.ITestClass;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
 
 /**
- * TestNG listener adapter for detecting when execution finishes in previous
- * test class and starts in a new class.
+ * TestNG listener adapter that detects when execution finishes in a test class, including AfterClass methods.
+ * TestNG's IClassListener.onAfterClass method is called before AfterClass methods are executed,
+ * which is why this solution is needed.
  */
-abstract class BetweenTestClassesListenerAdapter implements IClassListener, ITestListener {
-    Class<?> lastTestClass;
+abstract class BetweenTestClassesListenerAdapter implements IClassListener, IConfigurationListener {
+    private static final Logger log = LoggerFactory.getLogger(BetweenTestClassesListenerAdapter.class);
+    volatile Class<?> currentTestClass;
+    volatile int remainingAfterClassMethodCount;
 
     @Override
-    public void onBeforeClass(ITestClass testClass) {
-        checkIfTestClassChanged(testClass.getRealClass());
-    }
-
-    private void checkIfTestClassChanged(Class<?> testClazz) {
-        if (lastTestClass != testClazz) {
-            onBetweenTestClasses(lastTestClass, testClazz);
-            lastTestClass = testClazz;
+    public final void onBeforeClass(ITestClass testClass) {
+        // for parameterized tests for the same class, the onBeforeClass method is called for each instance
+        // so we need to check if the test class is the same as for the previous call before resetting the counter
+        if (testClass.getRealClass() != currentTestClass) {
+            // find out how many parameterized instances of the test class are expected
+            Object[] instances = testClass.getInstances(false);
+            int instanceCount = instances != null && instances.length != 0 ? instances.length : 1;
+            // expect invocations of all annotated and enabled after class methods
+            int annotatedAfterClassMethodCount = (int) Arrays.stream(testClass.getAfterClassMethods())
+                    .filter(ITestNGMethod::getEnabled)
+                    .count();
+            // additionally expect invocations of the "onAfterClass" listener method in this class
+            int expectedMethodCountForEachInstance = 1 + annotatedAfterClassMethodCount;
+            // multiple by the number of instances
+            remainingAfterClassMethodCount = instanceCount * expectedMethodCountForEachInstance;
+            currentTestClass = testClass.getRealClass();
         }
     }
 
     @Override
-    public void onFinish(ITestContext context) {
-        if (lastTestClass != null) {
-            onBetweenTestClasses(lastTestClass, null);
-            lastTestClass = null;
+    public final void onAfterClass(ITestClass testClass) {
+        handleAfterClassMethodCalled(testClass);
+    }
+
+    @Override
+    public final void onConfigurationSuccess(ITestResult tr) {
+        handleAfterClassConfigurationMethodCompletion(tr);
+    }
+
+    @Override
+    public final void onConfigurationSkip(ITestResult tr) {
+        handleAfterClassConfigurationMethodCompletion(tr);
+    }
+
+    @Override
+    public final void onConfigurationFailure(ITestResult tr) {
+        handleAfterClassConfigurationMethodCompletion(tr);
+    }
+
+    private void handleAfterClassConfigurationMethodCompletion(ITestResult tr) {
+        if (tr.getMethod().isAfterClassConfiguration() && !tr.wasRetried()) {
+            handleAfterClassMethodCalled(tr.getTestClass());
+        }
+    }
+
+    private void handleAfterClassMethodCalled(IClass testClass) {
+        if (currentTestClass != testClass.getRealClass()) {
+            log.error("Unexpected test class: {}. Expected: {}", testClass.getRealClass(), currentTestClass);
+            return;
+        }
+        remainingAfterClassMethodCount--;
+        if (remainingAfterClassMethodCount == 0) {
+            onBetweenTestClasses(testClass);
+        } else if (remainingAfterClassMethodCount < 0) {
+            // unexpected case, log it for easier debugging if this causes test failures
+            log.error("Remaining after class method count is negative: {} for test class: {}",
+                    remainingAfterClassMethodCount, testClass.getRealClass());
         }
     }
 
     /**
-     * Call back hook for adding logic when test execution moves from test class to another.
-     *
-     * @param endedTestClass the test class which has finished execution. null if the started test class is the first
-     * @param startedTestClass the test class which has started execution. null if the ended test class is the last
+     * Call back hook for adding logic when test execution has completely finished for a test class.
      */
-    protected abstract void onBetweenTestClasses(Class<?> endedTestClass, Class<?> startedTestClass);
+    protected abstract void onBetweenTestClasses(IClass testClass);
 }
