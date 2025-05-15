0fb354b164f39a2323d2fcd8eeb75758775271e8
Lari Hotari <lhotari@users.noreply.github.com>
2025-05-15 07:16:27 +0300

    [fix][test] Simplify BetweenTestClassesListenerAdapter and fix issue with BeforeTest/AfterTest annotations (#24304)
    ### Motivation
    
    It wasn't clear how test class changes should be detected with TestNG.
    The solution was recently revisited in #24258. However problems remain after adding that solution. BeforeTest/AfterTest annotation usage won't work and cause false reports for detected leaks.
    
    It turns out that it's possible to detect a test change with ITestListener's `onFinish` callback. In TestNG, it's possible to group multiple test classes into a single test, but this is only when using TestNG's XML configuration for test suites. maven-surefire-plugin will create a separate TestNG test for each test class, so that's why it's the correct solution for BetweenTestClassesListenerAdapter.
    
    ### Modifications
    
    - Remove previous logic to detect test class change and instead rely on ITestListener's `onFinish` callback
    - Adapt the current listener implementations to the new base class
    
    (cherry picked from commit 965ef5c14c93ca896ef4c8f34520066285fcf047)

--- buildtools/src/main/java/org/apache/pulsar/tests/BetweenTestClassesListenerAdapter.java
+++ buildtools/src/main/java/org/apache/pulsar/tests/BetweenTestClassesListenerAdapter.java
@@ -19,88 +19,33 @@
 package org.apache.pulsar.tests;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.IClass;
-import org.testng.IClassListener;
-import org.testng.IConfigurationListener;
 import org.testng.ITestClass;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
 import org.testng.ITestNGMethod;
-import org.testng.ITestResult;
 
 /**
- * TestNG listener adapter that detects when execution finishes in a test class, including AfterClass methods.
- * TestNG's IClassListener.onAfterClass method is called before AfterClass methods are executed,
- * which is why this solution is needed.
+ * TestNG listener adapter that detects when execution finishes for a test class,
+ * assuming that a single test class is run in each context.
+ * This is the case when running tests with maven-surefire-plugin.
  */
-abstract class BetweenTestClassesListenerAdapter implements IClassListener, IConfigurationListener {
+abstract class BetweenTestClassesListenerAdapter implements ITestListener {
     private static final Logger log = LoggerFactory.getLogger(BetweenTestClassesListenerAdapter.class);
-    volatile Class<?> currentTestClass;
-    volatile int remainingAfterClassMethodCount;
 
     @Override
-    public final void onBeforeClass(ITestClass testClass) {
-        // for parameterized tests for the same class, the onBeforeClass method is called for each instance
-        // so we need to check if the test class is the same as for the previous call before resetting the counter
-        if (testClass.getRealClass() != currentTestClass) {
-            // find out how many parameterized instances of the test class are expected
-            Object[] instances = testClass.getInstances(false);
-            int instanceCount = instances != null && instances.length != 0 ? instances.length : 1;
-            // expect invocations of all annotated and enabled after class methods
-            int annotatedAfterClassMethodCount = (int) Arrays.stream(testClass.getAfterClassMethods())
-                    .filter(ITestNGMethod::getEnabled)
-                    .count();
-            // additionally expect invocations of the "onAfterClass" listener method in this class
-            int expectedMethodCountForEachInstance = 1 + annotatedAfterClassMethodCount;
-            // multiple by the number of instances
-            remainingAfterClassMethodCount = instanceCount * expectedMethodCountForEachInstance;
-            currentTestClass = testClass.getRealClass();
-        }
-    }
-
-    @Override
-    public final void onAfterClass(ITestClass testClass) {
-        handleAfterClassMethodCalled(testClass);
-    }
-
-    @Override
-    public final void onConfigurationSuccess(ITestResult tr) {
-        handleAfterClassConfigurationMethodCompletion(tr);
-    }
-
-    @Override
-    public final void onConfigurationSkip(ITestResult tr) {
-        handleAfterClassConfigurationMethodCompletion(tr);
-    }
-
-    @Override
-    public final void onConfigurationFailure(ITestResult tr) {
-        handleAfterClassConfigurationMethodCompletion(tr);
-    }
-
-    private void handleAfterClassConfigurationMethodCompletion(ITestResult tr) {
-        if (tr.getMethod().isAfterClassConfiguration() && !tr.wasRetried()) {
-            handleAfterClassMethodCalled(tr.getTestClass());
-        }
-    }
-
-    private void handleAfterClassMethodCalled(IClass testClass) {
-        if (currentTestClass != testClass.getRealClass()) {
-            log.error("Unexpected test class: {}. Expected: {}", testClass.getRealClass(), currentTestClass);
-            return;
-        }
-        remainingAfterClassMethodCount--;
-        if (remainingAfterClassMethodCount == 0) {
-            onBetweenTestClasses(testClass);
-        } else if (remainingAfterClassMethodCount < 0) {
-            // unexpected case, log it for easier debugging if this causes test failures
-            log.error("Remaining after class method count is negative: {} for test class: {}",
-                    remainingAfterClassMethodCount, testClass.getRealClass());
-        }
+    public final void onFinish(ITestContext context) {
+        List<ITestClass> testClasses =
+                Arrays.stream(context.getAllTestMethods()).map(ITestNGMethod::getTestClass).distinct()
+                        .collect(Collectors.toList());
+        onBetweenTestClasses(testClasses);
     }
 
     /**
-     * Call back hook for adding logic when test execution has completely finished for a test class.
+     * Call back hook for adding logic when test execution has completely finished for one or many test classes.
      */
-    protected abstract void onBetweenTestClasses(IClass testClass);
+    protected abstract void onBetweenTestClasses(List<ITestClass> testClasses);
 }
