a0078da9bb056182be218a4c1620adb2cef97ab6
Lari Hotari <lhotari@users.noreply.github.com>
2025-05-16 05:20:25 +0300

    [improve][cli] Make pulsar-perf termination more responsive by using Thread interrupt status (#24309)
    ### Motivation
    
    Terminating pulsar-perf with CTRL-C could take a while. There's also a similar problem in some test cases that I came across while fixing test resource leaks.
    
    ### Modifications
    
    - replace `while(true)` loops with `while(!Thread.currentThread().isInterrupted())`
    - properly propagate Thread interrupted information after catching exceptions
    - fix issue in tests related to runtime hook resource leaks
      - when the execution completes successfully, remove the shutdown hook and run it without waiting for JVM exit
    
    (cherry picked from commit 99ad0e01a92bff75865f15fe14f30f32b61373e3)
    (cherry picked from commit e3ed8fcf343eb595cb32de0620123d54cb6f8334)

--- pulsar-testclient/src/main/java/org/apache/pulsar/proxy/socket/client/PerformanceClient.java
+++ pulsar-testclient/src/main/java/org/apache/pulsar/proxy/socket/client/PerformanceClient.java
@@ -259,6 +259,9 @@ public class PerformanceClient extends CmdBase {
                     }
                 } catch (Exception e) {
                     log.error("Authentication plugin error: " + e.getMessage());
+                    if (PerfClientUtils.hasInterruptedException(e)) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
             }
 
@@ -272,6 +275,9 @@ public class PerformanceClient extends CmdBase {
                 return;
             } catch (Exception e1) {
                 log.error("Fail in starting client[{}]", e1.getMessage());
+                if (PerfClientUtils.hasInterruptedException(e1)) {
+                    Thread.currentThread().interrupt();
+                }
                 return;
             }
 
@@ -288,7 +294,7 @@ public class PerformanceClient extends CmdBase {
                 long testEndTime = startTime + (long) (this.testTime * 1e9);
                 // Send messages on all topics/producers
                 long totalSent = 0;
-                while (true) {
+                while (!Thread.currentThread().isInterrupted()) {
                     for (String topic : producersMap.keySet()) {
                         if (this.testTime > 0 && System.nanoTime() > testEndTime) {
                             log.info("------------- DONE (reached the maximum duration: [{} seconds] of production) "
@@ -352,10 +358,11 @@ public class PerformanceClient extends CmdBase {
         histogramLogWriter.outputLogFormatVersion();
         histogramLogWriter.outputLegend();
 
-        while (true) {
+        while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
                 break;
             }
 
@@ -399,6 +406,9 @@ public class PerformanceClient extends CmdBase {
             Class clz = classLoader.loadClass(formatterClass);
             return (IMessageFormatter) clz.getDeclaredConstructor().newInstance();
         } catch (Exception e) {
+            if (PerfClientUtils.hasInterruptedException(e)) {
+                Thread.currentThread().interrupt();
+            }
             return null;
         }
     }
@@ -408,11 +418,12 @@ public class PerformanceClient extends CmdBase {
         loadArguments();
         PerfClientUtils.printJVMInformation(log);
         long start = System.nanoTime();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        Thread shutdownHookThread = PerfClientUtils.addShutdownHook(() -> {
             printAggregatedThroughput(start);
             printAggregatedStats();
-        }));
+        });
         runPerformanceTest();
+        PerfClientUtils.removeAndRunShutdownHook(shutdownHookThread);
     }
 
     private class Tuple {
