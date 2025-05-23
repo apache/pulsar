3a2d674e0b31d5488a5c5183ca54c6701543741b
zhenJiangWang <zhenjiang427@gmail.com>
2025-05-23 17:38:34 +0800

    [fix][broker] Resolve the issue of frequent updates in message expiration deletion rate (#24190)
    Co-authored-by: zjxxzjwang <zjxxzjwang@tencent.com>
    (cherry picked from commit 81c94c8a912cf08ac3f561a2aab3fa910d781db8)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentMessageExpiryMonitor.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentMessageExpiryMonitor.java
@@ -173,12 +173,11 @@ public class PersistentMessageExpiryMonitor implements FindEntryCallback, Messag
     }
 
 
-    private void updateRates() {
+    public void updateRates() {
         msgExpired.calculateRate();
     }
 
     public double getMessageExpiryRate() {
-        updateRates();
         return msgExpired.getRate();
     }
 
