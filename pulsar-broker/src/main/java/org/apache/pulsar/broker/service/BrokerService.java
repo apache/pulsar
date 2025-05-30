3f00757539e4f3ced1a37dbdfa880781dd793195
fengyubiao <yubiao.feng@streamnative.io>
2025-05-30 23:46:48 +0800

    [fix][broker] Fix issue that topic policies was deleted after a sub topic deleted, even if the partitioned topic still exists (#24350)
    (cherry picked from commit 8db6a3b9b0d03bbaf10d825c82f3fa8f9bd6a5cb)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -1293,6 +1293,9 @@ public class BrokerService implements Closeable {
         deleteTopicAuthenticationFuture
         .thenCompose(__ -> deleteSchema(tn))
         .thenCompose(__ -> {
+            if (tn.isPartitioned()) {
+                return CompletableFuture.completedFuture(null);
+            }
             if (!SystemTopicNames.isTopicPoliciesSystemTopic(topic)
                     && getPulsar().getConfiguration().isSystemTopicEnabled()) {
                 return deleteTopicPolicies(tn);
