81978600c53dcfd3363731c6e35b18a3f7b4cb9e
xiaocairush <xiaocairush@gmail.com>
2025-06-09 00:43:44 +0800

    [fix][broker] add test about transaction

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/persistent/PersistentDispatcherSingleActiveConsumerTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/persistent/PersistentDispatcherSingleActiveConsumerTest.java
@@ -20,6 +20,7 @@ package org.apache.pulsar.broker.service.persistent;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -29,14 +30,21 @@ import org.apache.bookkeeper.mledger.ManagedCursor;
 import org.apache.bookkeeper.mledger.ManagedLedgerException;
 import org.apache.bookkeeper.mledger.impl.ManagedCursorImpl;
 import org.apache.pulsar.broker.BrokerTestUtil;
+import org.apache.pulsar.broker.ServiceConfiguration;
 import org.apache.pulsar.broker.service.Consumer;
 import org.apache.pulsar.broker.service.Subscription;
 import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.ProducerConsumerBase;
+import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.api.SubscriptionType;
+import org.apache.pulsar.client.api.transaction.Transaction;
 import org.apache.pulsar.common.api.proto.CommandSubscribe;
+import org.apache.pulsar.common.naming.NamespaceName;
+import org.apache.pulsar.common.naming.SystemTopicNames;
+import org.apache.pulsar.common.partition.PartitionedTopicMetadata;
+import org.apache.pulsar.common.policies.data.TenantInfoImpl;
 import org.apache.pulsar.common.policies.data.TopicStats;
 import org.awaitility.Awaitility;
 import org.mockito.Mockito;
@@ -51,8 +59,19 @@ public class PersistentDispatcherSingleActiveConsumerTest extends ProducerConsum
     @BeforeClass(alwaysRun = true)
     @Override
     protected void setup() throws Exception {
-        super.internalSetup();
+        ServiceConfiguration serviceConfiguration = getDefaultConf();
+//        serviceConfiguration.setTopicLevelPoliciesEnabled(false);
+        serviceConfiguration.setTransactionCoordinatorEnabled(true);
+        super.internalSetup(serviceConfiguration);
         super.producerBaseSetup();
+
+        // Setup namespaces
+        admin.tenants().createTenant(NamespaceName.SYSTEM_NAMESPACE.getTenant(),
+                new TenantInfoImpl(Sets.newHashSet("appid1"), Sets.newHashSet("test")));
+        admin.namespaces().createNamespace(NamespaceName.SYSTEM_NAMESPACE.toString());
+        pulsar.getPulsarResources().getNamespaceResources().getPartitionedTopicResources()
+                .createPartitionedTopic(SystemTopicNames.TRANSACTION_COORDINATOR_ASSIGN,
+                        new PartitionedTopicMetadata(1));
     }
 
     @AfterClass(alwaysRun = true)
@@ -135,8 +154,9 @@ public class PersistentDispatcherSingleActiveConsumerTest extends ProducerConsum
 
     @Test
     public void testUnackedMessages() throws Exception {
-        String topicNamePrefix = "testUnackedMessages-" + System.currentTimeMillis();
-        String subscriptionNamePrefix = "testUnackedMessages-sub-";
+        long timeMillis = System.currentTimeMillis();
+        String topicNamePrefix = "testUnackedMessages-" + timeMillis;
+        String subscriptionNamePrefix = "testUnackedMessages-sub-" + timeMillis;
         int totalProducedMessage = 100;
         // we will check unacked messages every 20 messages
         int checkStep = 20;
@@ -216,6 +236,109 @@ public class PersistentDispatcherSingleActiveConsumerTest extends ProducerConsum
             checkUnackedMessage(topicName, subscriptionName, totalProducedMessage);
         }
     }
+
+    @Test
+    public void testUnackedMessagesWithTransaction() throws Exception {
+        String topicNamePrefix = "testUnackedMessagesWithTrn-" + System.currentTimeMillis();
+        String subscriptionNamePrefix = "testUnackedMessagesWithTrn-sub-";
+        int totalProducedMessage = 100;
+
+        @Cleanup
+        PulsarClient pulsarClient = PulsarClient.builder()
+                .enableTransaction(true)
+                .serviceUrl(pulsar.getBrokerServiceUrl())
+                .connectionsPerBroker(100)
+                .statsInterval(0, TimeUnit.SECONDS)
+                .build();
+
+        for (boolean enableBatching: ImmutableList.of(false, true)) {
+            for (SubscriptionType subscription : ImmutableList.of(SubscriptionType.Exclusive,
+                    SubscriptionType.Failover)) {
+                String inputTopicName = topicNamePrefix + subscription.name() + "-input-" + enableBatching;
+                String outputTopicName = topicNamePrefix + subscription.name() + "-output-"+ enableBatching;
+                String subscriptionName = subscriptionNamePrefix + subscription.name();
+                @Cleanup
+                org.apache.pulsar.client.api.Consumer<String> inputConsumer = pulsarClient.newConsumer(Schema.STRING)
+                        .topic(inputTopicName).subscriptionName(subscriptionName)
+                        .subscriptionType(subscription)
+                        .receiverQueueSize(totalProducedMessage)
+                        .ackTimeout(1000, TimeUnit.MILLISECONDS)
+                        .negativeAckRedeliveryDelay(0, TimeUnit.SECONDS)
+                        .acknowledgmentGroupTime(0, TimeUnit.SECONDS)
+                        .maxAcknowledgmentGroupSize(1)
+                        .subscribe();
+                @Cleanup
+                org.apache.pulsar.client.api.Consumer<String> outputConsumer = pulsarClient.newConsumer(Schema.STRING)
+                        .topic(outputTopicName).subscriptionName(subscriptionName)
+                        .subscriptionType(subscription)
+                        .receiverQueueSize(totalProducedMessage)
+                        .ackTimeout(1000, TimeUnit.MILLISECONDS)
+                        .negativeAckRedeliveryDelay(0, TimeUnit.SECONDS)
+                        .acknowledgmentGroupTime(0, TimeUnit.SECONDS)
+                        .maxAcknowledgmentGroupSize(1)
+                        .subscribe();
+
+                @Cleanup
+                Producer<String> inputProducer =
+                        pulsarClient.newProducer(Schema.STRING).enableBatching(enableBatching).topic(inputTopicName).create();
+
+                @Cleanup
+                Producer<String> outputProducer =
+                        pulsarClient.newProducer(Schema.STRING).enableBatching(enableBatching).topic(outputTopicName).create();
+
+                for (int i = 0; i < totalProducedMessage; ++i) {
+                    inputProducer.sendAsync(Integer.toString(i));
+                }
+
+                // 1) check unacked message for inputTopic before transaction
+                checkUnackedMessage(inputTopicName, subscriptionName, totalProducedMessage);
+
+                Transaction trn =
+                        pulsarClient.newTransaction().withTransactionTimeout(10, TimeUnit.SECONDS).build().get();
+
+                // 2) check unacked message after transaction committed
+                for (int i = 0; i < totalProducedMessage; ++i) {
+                    Message<String> msg = inputConsumer.receive(1, TimeUnit.SECONDS);
+                    if (Objects.isNull(msg)) {
+                        Assert.fail("Expected message not received");
+                    }
+                    outputProducer.newMessage(trn).value(msg.getValue()).sendAsync();
+                    inputConsumer.acknowledgeAsync(msg.getMessageId(), trn);
+                }
+                trn.commit().get();
+
+                checkUnackedMessage(inputTopicName, subscriptionName, 0);
+                checkUnackedMessage(outputTopicName, subscriptionName, totalProducedMessage);
+
+                // 3) check unacked message after transaction aborted
+                trn = pulsarClient.newTransaction().withTransactionTimeout(10, TimeUnit.SECONDS).build().get();
+                for (int i = 0; i < totalProducedMessage; ++i) {
+                    Message<String> msg = outputConsumer.receive(1, TimeUnit.SECONDS);
+                    if (Objects.isNull(msg)) {
+                        Assert.fail("Expected message not received");
+                    }
+                    outputConsumer.acknowledgeAsync(msg.getMessageId(), trn);
+                }
+
+                trn.abort().get();
+                checkUnackedMessage(outputTopicName, subscriptionName, totalProducedMessage);
+
+                // 4) check unacked message after transaction commit
+                trn = pulsarClient.newTransaction().withTransactionTimeout(10, TimeUnit.SECONDS).build().get();
+                for (int i = 0; i < totalProducedMessage; ++i) {
+                    Message<String> msg = outputConsumer.receive(1, TimeUnit.SECONDS);
+                    if (Objects.isNull(msg)) {
+                        Assert.fail("Expected message not received");
+                    }
+                    outputConsumer.acknowledgeAsync(msg.getMessageId(), trn);
+                }
+
+                trn.commit().get();
+                checkUnackedMessage(outputTopicName, subscriptionName, 0);
+            }
+        }
+    }
+
 
     private void checkUnackedMessage(String topicName, String subscriptionName, int expectedUnackedMessage) {
         Awaitility.await().atMost(5, TimeUnit.SECONDS).pollInterval(500, TimeUnit.MILLISECONDS).untilAsserted(() -> {
