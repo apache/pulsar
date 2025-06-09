a0e08f6a5a218f80d439e9d4407dc56f48c4e1d1
Yunze Xu <xyzinfernity@163.com>
2025-06-09 21:55:01 +0800

    Make deadlock easy to happen

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/transaction/TransactionTest.java
@@ -178,7 +178,9 @@ public class TransactionTest extends TransactionTestBase {
 
     @BeforeClass
     protected void setup() throws Exception {
-       setUpBase(NUM_BROKERS, NUM_PARTITIONS, NAMESPACE1 + "/test", 0);
+        // Use a single transaction thread to reproduce possible deadlock easily
+        conf.setNumTransactionReplayThreadPoolSize(1);
+        setUpBase(NUM_BROKERS, NUM_PARTITIONS, NAMESPACE1 + "/test", 0);
     }
 
     @AfterClass(alwaysRun = true)
