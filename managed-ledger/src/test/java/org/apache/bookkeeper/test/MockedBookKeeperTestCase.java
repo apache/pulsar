c8872591e2e87cb7f537d9978905e1fc3ba1b618
Yunze Xu <xyzinfernity@163.com>
2025-07-19 12:26:22 +0800

    [fix][ml] Fix asyncReadEntries might never complete if empty entries are read from BK (#24515)
    (cherry picked from commit eea7c13fb75e7acea012fbe7b46be0f4f7558a90)

--- managed-ledger/src/test/java/org/apache/bookkeeper/test/MockedBookKeeperTestCase.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/test/MockedBookKeeperTestCase.java
@@ -23,12 +23,16 @@ import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import lombok.SneakyThrows;
 import org.apache.bookkeeper.client.PulsarMockBookKeeper;
 import org.apache.bookkeeper.common.util.OrderedScheduler;
 import org.apache.bookkeeper.mledger.ManagedLedgerException;
 import org.apache.bookkeeper.mledger.ManagedLedgerFactoryConfig;
 import org.apache.bookkeeper.mledger.impl.ManagedLedgerFactoryImpl;
+import org.apache.bookkeeper.mledger.impl.ManagedLedgerImpl;
+import org.apache.bookkeeper.mledger.impl.cache.EntryCache;
+import org.apache.bookkeeper.mledger.impl.cache.RangeEntryCacheManagerImpl;
 import org.apache.pulsar.metadata.api.MetadataStoreConfig;
 import org.apache.pulsar.metadata.api.MetadataStoreException;
 import org.apache.pulsar.metadata.api.extended.MetadataStoreExtended;
@@ -57,6 +61,7 @@ public abstract class MockedBookKeeperTestCase {
     protected ExecutorService cachedExecutor;
 
     protected FaultInjectionMetadataStore metadataStore;
+    private Function<ManagedLedgerImpl, EntryCache> entryCacheCreator = null;
 
     public MockedBookKeeperTestCase() {
         // By default start a 3 bookies cluster
@@ -84,7 +89,17 @@ public abstract class MockedBookKeeperTestCase {
 
         ManagedLedgerFactoryConfig managedLedgerFactoryConfig = new ManagedLedgerFactoryConfig();
         initManagedLedgerFactoryConfig(managedLedgerFactoryConfig);
-        factory = new ManagedLedgerFactoryImpl(metadataStore, bkc);
+        factory = new ManagedLedgerFactoryImpl(metadataStore, bkc, __ -> new RangeEntryCacheManagerImpl(__,
+                __.getScheduledExecutor()) {
+
+            @Override
+            public EntryCache getEntryCache(ManagedLedgerImpl ml) {
+                if (entryCacheCreator != null) {
+                    return entryCacheCreator.apply(ml);
+                }
+                return super.getEntryCache(ml);
+            }
+        });
 
         setUpTestCase();
     }
@@ -163,5 +178,9 @@ public abstract class MockedBookKeeperTestCase {
 
     protected void stopMetadataStore() {
         metadataStore.setAlwaysFail(new MetadataStoreException("failed"));
+    }
+
+    protected void setEntryCacheCreator(Function<ManagedLedgerImpl, EntryCache> entryCacheCreator) {
+        this.entryCacheCreator = entryCacheCreator;
     }
 }
