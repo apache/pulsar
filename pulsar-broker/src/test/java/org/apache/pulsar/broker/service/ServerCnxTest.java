apache/pulsar

commit e916457f9055cf3e292d98ffcb677518a27699f6

[fix][ws] Fix WebSocket authorization issue due to originalPrincipal must be provided (#24533)

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/authorization/AuthorizationService.java
@@ -35,6 +35,7 @@ import org.apache.pulsar.broker.authentication.AuthenticationParameters;
 import org.apache.pulsar.broker.resources.PulsarResources;
 import org.apache.pulsar.client.admin.GrantTopicPermissionOptions;
 import org.apache.pulsar.client.admin.RevokeTopicPermissionOptions;
+import org.apache.pulsar.common.naming.Constants;
 import org.apache.pulsar.common.naming.NamespaceName;
 import org.apache.pulsar.common.naming.TopicName;
 import org.apache.pulsar.common.policies.data.AuthAction;
@@ -87,7 +88,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(authParams)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(authParams.getClientRole())) {
+        if (isProxyRole(authParams.getClientRole()) && !isWebsocketPrinciple(authParams.getOriginalPrincipal())) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = isSuperUser(authParams.getClientRole(),
                     authParams.getClientAuthenticationDataSource());
             // The current paradigm is to pass the client auth data when we don't have access to the original auth data.
@@ -348,7 +349,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(authParams)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(authParams.getClientRole())) {
+        if (isProxyRole(authParams.getClientRole()) && !isWebsocketPrinciple(authParams.getOriginalPrincipal())) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowFunctionOpsAsync(namespaceName,
                     authParams.getClientRole(), authParams.getClientAuthenticationDataSource());
             // The current paradigm is to pass the client auth data when we don't have access to the original auth data.
@@ -376,7 +377,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(authParams)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(authParams.getClientRole())) {
+        if (isProxyRole(authParams.getClientRole()) && !isWebsocketPrinciple(authParams.getOriginalPrincipal())) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowSourceOpsAsync(namespaceName,
                     authParams.getClientRole(), authParams.getClientAuthenticationDataSource());
             // The current paradigm is to pass the client auth data when we don't have access to the original auth data.
@@ -404,7 +405,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(authParams)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(authParams.getClientRole())) {
+        if (isProxyRole(authParams.getClientRole()) && !isWebsocketPrinciple(authParams.getOriginalPrincipal())) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowSinkOpsAsync(namespaceName,
                     authParams.getClientRole(), authParams.getClientAuthenticationDataSource());
             // The current paradigm is to pass the client auth data when we don't have access to the original auth data.
@@ -504,6 +505,10 @@ public class AuthorizationService {
         return role != null && conf.getProxyRoles().contains(role);
     }
 
+    public boolean isWebsocketPrinciple(String role) {
+        return Constants.WEBSOCKET_DUMMY_ORIGINAL_PRINCIPLE.equals(role);
+    }
+
     /**
      * Grant authorization-action permission on a tenant to the given client.
      *
@@ -534,7 +539,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(role, originalRole, authData)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowTenantOperationAsync(
                     tenantName, operation, role, authData);
             CompletableFuture<Boolean> isOriginalAuthorizedFuture = allowTenantOperationAsync(
@@ -556,7 +561,7 @@ public class AuthorizationService {
             return CompletableFuture.completedFuture(false);
         }
 
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             final var isRoleAuthorizedFuture = provider.allowBrokerOperationAsync(clusterName, brokerId,
                     brokerOperation, role, authData);
             final var isOriginalAuthorizedFuture =  provider.allowBrokerOperationAsync(clusterName, brokerId,
@@ -577,7 +582,7 @@ public class AuthorizationService {
             return CompletableFuture.completedFuture(false);
         }
 
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             final var isRoleAuthorizedFuture = provider.allowClusterOperationAsync(clusterName,
                     clusterOperation, role, authData);
             final var isOriginalAuthorizedFuture =  provider.allowClusterOperationAsync(clusterName,
@@ -599,7 +604,7 @@ public class AuthorizationService {
             return CompletableFuture.completedFuture(false);
         }
 
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             final var isRoleAuthorizedFuture = provider.allowClusterPolicyOperationAsync(clusterName, role,
                     policy, operation, authData);
             final var isOriginalAuthorizedFuture =  provider.allowClusterPolicyOperationAsync(clusterName, originalRole,
@@ -662,7 +667,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(role, originalRole, authData)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowNamespaceOperationAsync(
                     namespaceName, operation, role, authData);
             CompletableFuture<Boolean> isOriginalAuthorizedFuture = allowNamespaceOperationAsync(
@@ -706,7 +711,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(role, originalRole, authData)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowNamespacePolicyOperationAsync(
                     namespaceName, policy, operation, role, authData);
             CompletableFuture<Boolean> isOriginalAuthorizedFuture = allowNamespacePolicyOperationAsync(
@@ -769,7 +774,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(role, originalRole, authData)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowTopicPolicyOperationAsync(
                     topicName, policy, operation, role, authData);
             CompletableFuture<Boolean> isOriginalAuthorizedFuture = allowTopicPolicyOperationAsync(
@@ -865,7 +870,7 @@ public class AuthorizationService {
         if (!isValidOriginalPrincipal(role, originalRole, authData)) {
             return CompletableFuture.completedFuture(false);
         }
-        if (isProxyRole(role)) {
+        if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowTopicOperationAsync(
                     topicName, operation, role, authData);
             CompletableFuture<Boolean> isOriginalAuthorizedFuture = allowTopicOperationAsync(

pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -4349,7 +4349,7 @@ public class PersistentTopicsBase extends AdminResource {
             AuthenticationDataSource authenticationData, TopicName topicName) {
         CompletableFuture<PartitionedTopicMetadata> metadataFuture = new CompletableFuture<>();
         CompletableFuture<Void> authorizationFuture = new CompletableFuture<>();
-        checkAuthorizationAsync(pulsar, topicName, clientAppId, authenticationData)
+        checkAuthorizationAsync(pulsar, topicName, clientAppId, originalPrincipal, authenticationData)
                 .thenRun(() -> authorizationFuture.complete(null))
                 .exceptionally(e -> {
                     Throwable throwable = FutureUtil.unwrapCompletionException(e);

pulsar-broker/src/main/java/org/apache/pulsar/broker/lookup/TopicLookupBase.java
@@ -24,7 +24,6 @@ import io.netty.buffer.ByteBuf;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -160,32 +159,6 @@ public class TopicLookupBase extends PulsarWebResource {
     }
 
     /**
-     * Lookup broker-service address for a given namespace-bundle which contains given topic.
-     *
-     * a. Returns broker-address if namespace-bundle is already owned by any broker
-     * b. If current-broker receives lookup-request and if it's not a leader then current broker redirects request
-     * to leader by returning leader-service address.
-     * c. If current-broker is leader then it finds out least-loaded broker to
-     * own namespace bundle and redirects request
-     * by returning least-loaded broker.
-     * d. If current-broker receives request to own the namespace-bundle then
-     * it owns a bundle and returns success(connect)
-     * response to client.
-     *
-     * @param pulsarService
-     * @param topicName
-     * @param authoritative
-     * @param clientAppId
-     * @param requestId
-     * @return
-     */
-    public static CompletableFuture<ByteBuf> lookupTopicAsync(PulsarService pulsarService, TopicName topicName,
-            boolean authoritative, String clientAppId, AuthenticationDataSource authenticationData, long requestId) {
-        return lookupTopicAsync(pulsarService, topicName, authoritative, clientAppId,
-                authenticationData, requestId, null, Collections.emptyMap());
-    }
-
-    /**
      *
      * Lookup broker-service address for a given namespace-bundle which contains given topic.
      *
@@ -209,6 +182,7 @@ public class TopicLookupBase extends PulsarWebResource {
      */
     public static CompletableFuture<ByteBuf> lookupTopicAsync(PulsarService pulsarService, TopicName topicName,
                                                               boolean authoritative, String clientAppId,
+                                                              String originalPrinciple,
                                                               AuthenticationDataSource authenticationData,
                                                               long requestId, final String advertisedListenerName,
                                                               Map<String, String> properties) {
@@ -231,7 +205,8 @@ public class TopicLookupBase extends PulsarWebResource {
                         requestId, false));
             } else {
                 // (2) authorize client
-                checkAuthorizationAsync(pulsarService, topicName, clientAppId, authenticationData).thenRun(() -> {
+                checkAuthorizationAsync(pulsarService, topicName, clientAppId, originalPrinciple,
+                        authenticationData).thenRun(() -> {
                         // (3) validate global namespace
                         // It is necessary for system topic operations because system topics are used to store metadata
                         // and other vital information. Even after namespace starting deletion,

pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
@@ -477,27 +477,16 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
         if (!service.isAuthorizationEnabled()) {
             return CompletableFuture.completedFuture(true);
         }
-        CompletableFuture<Boolean> isProxyAuthorizedFuture;
-        if (originalPrincipal != null) {
-            isProxyAuthorizedFuture = service.getAuthorizationService().allowTopicOperationAsync(
-                    topicName, operation, originalPrincipal,
-                    originalAuthDataSource != null ? originalAuthDataSource : authDataSource);
-        } else {
-            isProxyAuthorizedFuture = CompletableFuture.completedFuture(true);
-        }
-        CompletableFuture<Boolean> isAuthorizedFuture = service.getAuthorizationService().allowTopicOperationAsync(
-            topicName, operation, authRole, authDataSource);
-        return isProxyAuthorizedFuture.thenCombine(isAuthorizedFuture, (isProxyAuthorized, isAuthorized) -> {
-            if (!isProxyAuthorized) {
-                log.warn("OriginalRole {} is not authorized to perform operation {} on topic {}",
-                        originalPrincipal, operation, topicName);
-            }
+        CompletableFuture<Boolean> result = service.getAuthorizationService().allowTopicOperationAsync(
+                topicName, operation, originalPrincipal, authRole,
+                originalAuthDataSource != null ? originalAuthDataSource : authDataSource);
+        result.thenAccept(isAuthorized -> {
             if (!isAuthorized) {
-                log.warn("Role {} is not authorized to perform operation {} on topic {}",
-                        authRole, operation, topicName);
+                log.warn("Role {} and OriginalRole {} is not authorized to perform operation {} on topic {}",
+                        authRole, originalPrincipal, operation, topicName);
             }
-            return isProxyAuthorized && isAuthorized;
         });
+        return result;
     }
 
     private CompletableFuture<Boolean> isTopicOperationAllowed(TopicName topicName, String subscriptionName,
@@ -561,7 +550,7 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
                         properties = Collections.emptyMap();
                     }
                     lookupTopicAsync(getBrokerService().pulsar(), topicName, authoritative,
-                            getPrincipal(), getAuthenticationData(),
+                            authRole, originalPrincipal, getAuthenticationData(),
                             requestId, advertisedListenerName, properties).handle((lookupResponse, ex) -> {
                                 if (ex == null) {
                                     writeAndFlush(lookupResponse);

pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
@@ -987,14 +987,14 @@ public abstract class PulsarWebResource {
     }
 
     protected static CompletableFuture<Void> checkAuthorizationAsync(PulsarService pulsarService, TopicName topicName,
-                        String role, AuthenticationDataSource authenticationData) {
+                        String role, String originalPrinciple, AuthenticationDataSource authenticationData) {
         if (!pulsarService.getConfiguration().isAuthorizationEnabled()) {
             // No enforcing of authorization policies
             return CompletableFuture.completedFuture(null);
         }
         // get zk policy manager
         return pulsarService.getBrokerService().getAuthorizationService().allowTopicOperationAsync(topicName,
-                TopicOperation.LOOKUP, null, role, authenticationData).thenAccept(allow -> {
+                TopicOperation.LOOKUP, originalPrinciple, role, authenticationData).thenAccept(allow -> {
                     if (!allow) {
                         log.warn("[{}] Role {} is not allowed to lookup topic", topicName, role);
                         throw new RestException(Status.UNAUTHORIZED,

commit 1b43d1c0d6d73a5f60e3699972c4d932b063f3c7

Fix proxy authorization bug introduced by PR #24533
PR #24533 introduced incorrect AuthData handling in proxy scenarios, where
the AuthorizationService.allowTopicOperationAsync method was being called
with wrong parameters, losing the separation between proxy auth data and
original client auth data.

This fix:
- Adds a new 6-parameter allowTopicOperationAsync method that properly
  separates originalAuthData and authData validation
- Updates ServerCnx to use the new method with correct parameters
- Fixes authorization logic to validate both proxy and original client
  permissions independently
- Updates all affected test verification calls to match new method signatures

🤖 Generated with [Claude Code](https://claude.ai/code)

Co-Authored-By: Claude <noreply@anthropic.com>

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/authorization/AuthorizationService.java
@@ -868,15 +868,14 @@ public class AuthorizationService {
                                                                String role,
                                                                AuthenticationDataSource originalAuthData,
                                                                AuthenticationDataSource authData) {
-        if (!isValidOriginalPrincipal(role, originalRole, originalAuthData)
-                || !isValidOriginalPrincipal(role, originalRole, authData)) {
+        if (!isValidOriginalPrincipal(role, originalRole, originalAuthData)) {
             return CompletableFuture.completedFuture(false);
         }
         if (isProxyRole(role) && !isWebsocketPrinciple(originalRole)) {
             CompletableFuture<Boolean> isRoleAuthorizedFuture = allowTopicOperationAsync(
                     topicName, operation, role, authData);
             CompletableFuture<Boolean> isOriginalAuthorizedFuture = allowTopicOperationAsync(
-                    topicName, operation, originalRole, originalAuthData != null ? originalAuthData : authData);
+                    topicName, operation, originalRole, originalAuthData);
             return isRoleAuthorizedFuture.thenCombine(isOriginalAuthorizedFuture,
                     (isRoleAuthorized, isOriginalAuthorized) -> isRoleAuthorized && isOriginalAuthorized);
         } else {

pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -4346,10 +4346,12 @@ public class PersistentTopicsBase extends AdminResource {
 
     public CompletableFuture<PartitionedTopicMetadata> getPartitionedTopicMetadata(
             PulsarService pulsar, String clientAppId, String originalPrincipal,
-            AuthenticationDataSource authenticationData, TopicName topicName) {
+            AuthenticationDataSource authenticationData, AuthenticationDataSource originalAuthenticationData,
+            TopicName topicName) {
         CompletableFuture<PartitionedTopicMetadata> metadataFuture = new CompletableFuture<>();
         CompletableFuture<Void> authorizationFuture = new CompletableFuture<>();
-        checkAuthorizationAsync(pulsar, topicName, clientAppId, originalPrincipal, authenticationData)
+        checkAuthorizationAsync(pulsar, topicName, clientAppId, originalPrincipal, authenticationData,
+                originalAuthenticationData)
                 .thenRun(() -> authorizationFuture.complete(null))
                 .exceptionally(e -> {
                     Throwable throwable = FutureUtil.unwrapCompletionException(e);

pulsar-broker/src/main/java/org/apache/pulsar/broker/lookup/TopicLookupBase.java
@@ -184,6 +184,7 @@ public class TopicLookupBase extends PulsarWebResource {
                                                               boolean authoritative, String clientAppId,
                                                               String originalPrinciple,
                                                               AuthenticationDataSource authenticationData,
+                                                              AuthenticationDataSource originalAuthenticationData,
                                                               long requestId, final String advertisedListenerName,
                                                               Map<String, String> properties) {
 
@@ -206,7 +207,7 @@ public class TopicLookupBase extends PulsarWebResource {
             } else {
                 // (2) authorize client
                 checkAuthorizationAsync(pulsarService, topicName, clientAppId, originalPrinciple,
-                        authenticationData).thenRun(() -> {
+                        authenticationData, originalAuthenticationData).thenRun(() -> {
                         // (3) validate global namespace
                         // It is necessary for system topic operations because system topics are used to store metadata
                         // and other vital information. Even after namespace starting deletion,

pulsar-broker/src/main/java/org/apache/pulsar/broker/service/ServerCnx.java
@@ -552,7 +552,7 @@ public class ServerCnx extends PulsarHandler implements TransportCnx {
                         properties = Collections.emptyMap();
                     }
                     lookupTopicAsync(getBrokerService().pulsar(), topicName, authoritative,
-                            authRole, originalPrincipal, getAuthenticationData(),
+                            authRole, originalPrincipal, authenticationData, originalAuthData,
                             requestId, advertisedListenerName, properties).handle((lookupResponse, ex) -> {
                                 if (ex == null) {
                                     writeAndFlush(lookupResponse);

pulsar-broker/src/main/java/org/apache/pulsar/broker/web/PulsarWebResource.java
@@ -987,14 +987,16 @@ public abstract class PulsarWebResource {
     }
 
     protected static CompletableFuture<Void> checkAuthorizationAsync(PulsarService pulsarService, TopicName topicName,
-                        String role, String originalPrinciple, AuthenticationDataSource authenticationData) {
+                        String role, String originalPrinciple, AuthenticationDataSource authenticationData,
+                        AuthenticationDataSource originalAuthenticationData) {
         if (!pulsarService.getConfiguration().isAuthorizationEnabled()) {
             // No enforcing of authorization policies
             return CompletableFuture.completedFuture(null);
         }
         // get zk policy manager
         return pulsarService.getBrokerService().getAuthorizationService().allowTopicOperationAsync(topicName,
-                TopicOperation.LOOKUP, originalPrinciple, role, authenticationData).thenAccept(allow -> {
+                TopicOperation.LOOKUP, originalPrinciple, role, originalAuthenticationData, authenticationData)
+                .thenAccept(allow -> {
                     if (!allow) {
                         log.warn("[{}] Role {} is not allowed to lookup topic", topicName, role);
                         throw new RestException(Status.UNAUTHORIZED,

pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ServerCnxTest.java
@@ -29,6 +29,7 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
@@ -1349,11 +1350,13 @@ public class ServerCnxTest {
         assertEquals(((CommandLookupTopicResponse) lookupResponse).getError(), ServerError.AuthorizationError);
         assertEquals(((CommandLookupTopicResponse) lookupResponse).getRequestId(), 1);
         verify(authorizationService, times(1))
-                .allowTopicOperationAsync(topicName, TopicOperation.LOOKUP, proxyRole, serverCnx.getAuthData());
+                .allowTopicOperationAsync(topicName, TopicOperation.LOOKUP, clientRole, proxyRole,
+                        serverCnx.getOriginalAuthData(), serverCnx.getAuthData());
         // This test is an example of https://github.com/apache/pulsar/issues/19332. Essentially, we're passing
         // the proxy's auth data because it is all we have. This test should be updated when we resolve that issue.
         verify(authorizationService, times(1))
-                .allowTopicOperationAsync(topicName, TopicOperation.LOOKUP, clientRole, serverCnx.getAuthData());
+                .allowTopicOperationAsync(topicName, TopicOperation.LOOKUP, clientRole, proxyRole,
+                        serverCnx.getOriginalAuthData(), serverCnx.getAuthData());
 
         // producer
         ByteBuf producer = Commands.newProducer(topicName.toString(), 1, 2, "test-producer", new HashMap<>(), false);
@@ -1364,9 +1367,11 @@ public class ServerCnxTest {
         assertEquals(((CommandError) producerResponse).getRequestId(), 2);
         // See https://github.com/apache/pulsar/issues/19332 for justification of this assertion.
         verify(authorizationService, times(1))
-                .allowTopicOperationAsync(topicName, TopicOperation.PRODUCE, clientRole, serverCnx.getAuthData());
+                .allowTopicOperationAsync(topicName, TopicOperation.PRODUCE, clientRole, proxyRole,
+                        serverCnx.getOriginalAuthData(), serverCnx.getAuthData());
         verify(authorizationService, times(1))
-                .allowTopicOperationAsync(topicName, TopicOperation.LOOKUP, proxyRole, serverCnx.getAuthData());
+                .allowTopicOperationAsync(topicName, TopicOperation.LOOKUP, clientRole, proxyRole,
+                        serverCnx.getOriginalAuthData(), serverCnx.getAuthData());
 
         // consumer
         String subscriptionName = "test-subscribe";
@@ -1379,7 +1384,7 @@ public class ServerCnxTest {
         assertEquals(((CommandError) subscribeResponse).getRequestId(), 3);
         verify(authorizationService, times(1)).allowTopicOperationAsync(
                 eq(topicName), eq(TopicOperation.CONSUME),
-                eq(clientRole), argThat(arg -> {
+                eq(clientRole), eq(proxyRole), isNull(), argThat(arg -> {
                     assertTrue(arg instanceof AuthenticationDataSubscription);
                     // We assert that the role is clientRole and commandData is proxyRole due to
                     // https://github.com/apache/pulsar/issues/19332.
@@ -1389,7 +1394,7 @@ public class ServerCnxTest {
                 }));
         verify(authorizationService, times(1)).allowTopicOperationAsync(
                 eq(topicName), eq(TopicOperation.CONSUME),
-                eq(proxyRole), argThat(arg -> {
+                eq(clientRole), eq(proxyRole), isNull(), argThat(arg -> {
                     assertTrue(arg instanceof AuthenticationDataSubscription);
                     assertEquals(arg.getCommandData(), proxyRole);
                     assertEquals(arg.getSubscription(), subscriptionName);