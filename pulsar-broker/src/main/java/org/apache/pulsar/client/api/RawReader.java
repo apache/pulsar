7274ee14eb09fab38d289f81b633342ccf826d34
Hang Chen <chenhang@apache.org>
2025-08-04 17:29:18 -0700

    [improve][client] RawReader support pause and resume (#24597)

--- pulsar-broker/src/main/java/org/apache/pulsar/client/api/RawReader.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/client/api/RawReader.java
@@ -101,4 +101,19 @@ public interface RawReader {
      * Close the raw reader.
      */
     CompletableFuture<Void> closeAsync();
+
+    /**
+     * Stop requesting new messages from the broker until {@link #resume()} is called. Note that this might cause
+     * {@link #readNextAsync()} to block until {@link #resume()} is called and new messages are pushed by the broker.
+     */
+    default void pause() {
+
+    }
+
+    /**
+     * Resume requesting messages from the broker.
+     */
+    default void resume() {
+
+    }
 }
