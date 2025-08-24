02343bb05f3718cc356b43e1a79ab853da70307a
3pacccccc <maruimin666@gmail.com>
2025-08-25 00:02:51 +0800

    update doc

--- pulsar-client-api/src/main/java/org/apache/pulsar/client/api/DecryptFailListener.java
+++ pulsar-client-api/src/main/java/org/apache/pulsar/client/api/DecryptFailListener.java
@@ -24,26 +24,32 @@ import org.apache.pulsar.common.classification.InterfaceStability;
 
 
 /**
- * A listener that will be called in order for every message received.
+ * <p>This listener is invoked when receives an encrypted message and cannot be decrypted successfully,
+ * either because no {@link CryptoKeyReader} is configured or the configured {@link CryptoKeyReader}
+ * cannot decrypt the message. This allows applications to handle decryption failures separately
+ * from normal message processing.
+ *
+ * <p>This listener must be used together with a {@link MessageListener} and cannot be used
+ * with {@link ConsumerCryptoFailureAction}.
  */
 @InterfaceAudience.Public
 @InterfaceStability.Stable
 public interface DecryptFailListener<T> extends Serializable {
     /**
-     * This method is called whenever a new message is received.
-     *
+     * This method is called whenever a new encrypted message is received and cannot be decrypted successfully
+     * by {@link CryptoKeyReader}
      * <p>Messages are guaranteed to be delivered in order and from the same thread for a single consumer
      *
-     * <p>This method will only be called once for each message, unless either application or broker crashes.
+     * <p>This method will only be called once for each encrypted message.
      *
-     * <p>Application is responsible for acking message by calling any of consumer acknowledgement methods.
+     * <p>Application is responsible for acknowledging the message by calling any of the consumer
+     * acknowledgement methods if needed.
      *
-     * <p>Application is responsible of handling any exception that could be thrown while processing the message.
+     * <p>Application is responsible for handling any exception that could be thrown while
+     * processing the undecryptable message.
      *
-     * @param consumer
-     *            the consumer that received the message
-     * @param msg
-     *            the message object
+     * @param consumer the consumer that received the undecryptable message
+     * @param msg the encrypted message object that failed decryption
      */
     void received(Consumer<T> consumer, Message<T> msg);
 }
