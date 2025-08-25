2ae5618d4aca8857edc83a1157d4af83f3cc6987
dao-jun <daojun@apache.org>
2025-08-25 19:25:54 +0800

    Fix tests

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/TopicPoliciesTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/admin/TopicPoliciesTest.java
@@ -65,6 +65,7 @@ import org.apache.pulsar.broker.service.persistent.DispatchRateLimiter;
 import org.apache.pulsar.broker.service.persistent.PersistentSubscription;
 import org.apache.pulsar.broker.service.persistent.PersistentTopic;
 import org.apache.pulsar.broker.service.persistent.SubscribeRateLimiter;
+import org.apache.pulsar.broker.systopic.SystemTopicClient;
 import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.client.api.Consumer;
 import org.apache.pulsar.client.api.ConsumerBuilder;
@@ -79,6 +80,7 @@ import org.apache.pulsar.client.api.SubscriptionInitialPosition;
 import org.apache.pulsar.client.api.SubscriptionMode;
 import org.apache.pulsar.client.api.SubscriptionType;
 import org.apache.pulsar.common.api.proto.CommandSubscribe;
+import org.apache.pulsar.common.events.PulsarEvent;
 import org.apache.pulsar.common.naming.NamespaceName;
 import org.apache.pulsar.common.naming.SystemTopicNames;
 import org.apache.pulsar.common.naming.TopicDomain;
@@ -3577,9 +3579,13 @@ public class TopicPoliciesTest extends MockedPulsarServiceBaseTest {
         Map<TopicName, TopicPolicies> globalPoliciesCache =
                 WhiteboxImpl.getInternalState(topicPoliciesService, "globalPoliciesCache");
 
+        Map<NamespaceName, CompletableFuture<SystemTopicClient.Reader<PulsarEvent>>> readerCaches = WhiteboxImpl
+                .getInternalState(topicPoliciesService, "readerCaches");
         policyCacheInitMap.clear();
         policiesCache.clear();
         globalPoliciesCache.clear();
+
+        readerCaches.clear();
     }
 
     @DataProvider(name = "reloadPolicyTypes")
