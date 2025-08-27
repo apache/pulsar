dd42978954d7ef95ad813e3230573b4b5d483b9e
Zixuan Liu <nodeces@gmail.com>
2025-08-27 15:41:08 +0800

    [fix][build] Import isNotBlank

--- pulsar-io/kinesis/src/main/java/org/apache/pulsar/io/kinesis/KinesisSinkConfig.java
+++ pulsar-io/kinesis/src/main/java/org/apache/pulsar/io/kinesis/KinesisSinkConfig.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.io.kinesis;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
