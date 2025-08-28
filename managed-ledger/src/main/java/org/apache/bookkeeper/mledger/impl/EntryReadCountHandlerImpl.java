bbca447b761441ccbfe905052b5449952054ffe2
Lari Hotari <lhotari@apache.org>
2025-08-28 10:24:02 +0300

    Address review comments in EntryReadCountHandlerImpl

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/EntryReadCountHandlerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/EntryReadCountHandlerImpl.java
@@ -20,10 +20,12 @@ package org.apache.bookkeeper.mledger.impl;
 
 import com.google.common.annotations.VisibleForTesting;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import lombok.ToString;
 import org.apache.bookkeeper.mledger.EntryReadCountHandler;
 
+@ToString
 public class EntryReadCountHandlerImpl implements EntryReadCountHandler {
-    private static final AtomicIntegerFieldUpdater<EntryReadCountHandlerImpl> expectedReadCountUpdater =
+    private static final AtomicIntegerFieldUpdater<EntryReadCountHandlerImpl> EXPECTED_READ_COUNT_UPDATER =
             AtomicIntegerFieldUpdater.newUpdater(EntryReadCountHandlerImpl.class, "expectedReadCount");
 
     private volatile int expectedReadCount;
@@ -38,17 +40,18 @@ public class EntryReadCountHandlerImpl implements EntryReadCountHandler {
 
     @Override
     public void incrementExpectedReadCount() {
-        expectedReadCountUpdater.incrementAndGet(this);
+        EXPECTED_READ_COUNT_UPDATER.incrementAndGet(this);
     }
 
     @Override
     public void markRead() {
-        expectedReadCountUpdater.decrementAndGet(this);
+        EXPECTED_READ_COUNT_UPDATER.decrementAndGet(this);
     }
 
     /**
      * Creates an instance of EntryReadCountHandlerImpl if the expected read count is greater than 0.
      * If the expected read count is 0 or less, it returns null.
+     *
      * @param expectedReadCount the expected read count for the entry
      * @return an instance of EntryReadCountHandlerImpl or null
      */
@@ -59,12 +62,5 @@ public class EntryReadCountHandlerImpl implements EntryReadCountHandler {
     @VisibleForTesting
     public void setExpectedReadCount(int expectedReadCount) {
         this.expectedReadCount = expectedReadCount;
-    }
-
-    @Override
-    public String toString() {
-        return "EntryReadCountHandlerImpl{"
-                + "expectedReadCount=" + expectedReadCount
-                + '}';
     }
 }
