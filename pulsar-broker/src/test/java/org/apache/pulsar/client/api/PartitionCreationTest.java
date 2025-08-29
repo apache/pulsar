03e0787cc445ecab5207769e0859c64fb589a0f3
fengyubiao <yubiao.feng@streamnative.io>
2025-08-29 09:39:31 +0800

    [improve][test]Add new test PartitionCreationTest.testGetPoliciesIfPartitionsNotCreated (#24681)

--- pulsar-broker/src/test/java/org/apache/pulsar/client/api/PartitionCreationTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/client/api/PartitionCreationTest.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pulsar.client.api;
 
+import org.apache.pulsar.broker.BrokerTestUtil;
 import org.apache.pulsar.client.admin.PulsarAdminException;
 import org.apache.pulsar.client.impl.MultiTopicsConsumerImpl;
 import org.apache.pulsar.common.naming.TopicDomain;
@@ -118,6 +119,20 @@ public class PartitionCreationTest extends ProducerConsumerBase {
                 .topic(topic).subscriptionName("sub-1").subscribe();
         Assert.assertNotNull(consumer);
         Assert.assertEquals(consumer.getConsumers().size(), 5);
+    }
+
+    @Test
+    public void testGetPoliciesIfPartitionsNotCreated() throws Exception {
+        final String topic = BrokerTestUtil.newUniqueName("persistent://public/default/tp");
+        int numPartitions = 3;
+        // simulate partitioned topic without partitions
+        pulsar.getPulsarResources().getNamespaceResources().getPartitionedTopicResources()
+                .createPartitionedTopicAsync(TopicName.get(topic),
+                        new PartitionedTopicMetadata(numPartitions)).join();
+        // Verify: the command will not get an topic not found error.
+        admin.topics().getReplicationClusters(topic, true);
+        // cleanup.
+        admin.topics().deletePartitionedTopic(topic);
     }
 
     @DataProvider(name = "restCreateMissedPartitions")
