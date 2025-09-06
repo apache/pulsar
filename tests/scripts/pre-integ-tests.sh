c0a130e61a6c60ad391d763b5e4b755b43cb6125
Enrique Fernandez <enrique@bluelabs.eu>
2025-09-06 16:31:40 +0200

    3.2.2 images do not exist

--- tests/scripts/pre-integ-tests.sh
+++ tests/scripts/pre-integ-tests.sh
@@ -30,5 +30,5 @@ docker pull apachepulsar/s3mock:latest
 docker pull alpine/socat:latest
 docker pull cassandra:3
 docker pull confluentinc/cp-kafka:4.0.0
-docker pull debezium/example-mysql:3.2.2.Final
+docker pull debezium/example-mysql:3.0.0.Final
 docker pull mysql:9.1.0
