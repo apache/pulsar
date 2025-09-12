80e9fef353d60e3805b5bf79a2eab22d6e7b7bda
Zixuan Liu <nodeces@gmail.com>
2025-09-12 19:36:48 +0800

    [fix][client] Fix PulsarAdmin description check and add test (#24734)
    (cherry picked from commit 8d7a28c145485265942e6dbe6b0b59a8ce8cb1ec)

--- pulsar-client-admin/src/test/java/org/apache/pulsar/client/admin/internal/PulsarAdminBuilderImplTest.java
+++ pulsar-client-admin/src/test/java/org/apache/pulsar/client/admin/internal/PulsarAdminBuilderImplTest.java
@@ -20,6 +20,7 @@ package org.apache.pulsar.client.admin.internal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.io.IOException;
@@ -179,6 +180,21 @@ public class PulsarAdminBuilderImplTest {
                 .loadConf(confProps).build()) {
             return ((PulsarAdminImpl) admin).auth;
         }
+    }
+
+    @Test
+    public void testClientDescription() throws PulsarClientException {
+        @Cleanup PulsarAdmin ignored =
+                PulsarAdmin.builder().serviceHttpUrl("http://localhost:8080").description("forked").build();
+    }
+
+    @Test
+    public void testClientDescriptionLengthExceed64() {
+        String longDescription = "a".repeat(65);
+        assertThatThrownBy(() -> {
+            @Cleanup PulsarAdmin ignored =
+                    PulsarAdmin.builder().serviceHttpUrl("http://localhost:8080").description(longDescription).build();
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     private String secretAuthParams(String secret) {
