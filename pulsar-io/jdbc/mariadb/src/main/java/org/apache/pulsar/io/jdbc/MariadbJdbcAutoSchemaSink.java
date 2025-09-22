166c8e87dd3c5781726606653800ce99b7161825
Omri Fried <omrifried@gmail.com>
2025-09-21 18:24:00 -0700

    Remaining dbs

--- pulsar-io/jdbc/mariadb/src/main/java/org/apache/pulsar/io/jdbc/MariadbJdbcAutoSchemaSink.java
+++ pulsar-io/jdbc/mariadb/src/main/java/org/apache/pulsar/io/jdbc/MariadbJdbcAutoSchemaSink.java
@@ -21,6 +21,9 @@ package org.apache.pulsar.io.jdbc;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.sql.Time;
 import org.apache.pulsar.io.core.annotations.Connector;
 import org.apache.pulsar.io.core.annotations.IOType;
 
@@ -75,5 +78,44 @@ public class MariadbJdbcAutoSchemaSink extends BaseJdbcAutoSchemaSink {
             throws Exception {
         throw new UnsupportedOperationException("Array types are not supported by MariaDB JDBC sink. "
                 + "Consider using PostgreSQL JDBC sink for array support.");
+    }
+
+    /**
+     * MariaDB supports Date, Timestamp, and Time types.
+     * <p>
+     * Inputs are converted to the appropriate datetime type.
+     * </p>
+     * <p>
+     * <strong>Implementation Guidelines:</strong>
+     * <ul>
+     * <li>If the value is a Timestamp, it is converted to a Timestamp</li>
+     * <li>If the value is a Date, it is converted to a Date</li>
+     * <li>If the value is a Time, it is converted to a Time</li>
+     * </ul>
+     * </p>
+     *
+     * @param statement     the PreparedStatement (not used)
+     * @param index         the parameter index (not used)
+     * @param value         the value (not used)
+     * @param targetSqlType the target SQL type (not used)
+     * @return false as SQLite doesn't support datetime
+     * @throws Exception if conversion or binding fails
+     */
+    @Override
+    protected boolean handleDateTime(PreparedStatement statement, int index, Object value, String targetSqlType)
+            throws Exception {
+        switch (targetSqlType) {
+            case "Timestamp":
+                statement.setTimestamp(index, (Timestamp) value);
+                return true;
+            case "Date":
+                statement.setDate(index, (Date) value);
+                return true;
+            case "Time":
+                statement.setTime(index, (Time) value);
+                return true;
+            default:
+                return false;
+        }
     }
 }
