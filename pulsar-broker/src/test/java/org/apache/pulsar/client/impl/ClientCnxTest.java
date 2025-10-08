708fefff345118d5e5857ba5bc52596a297fa905
Yunze Xu <xyzinfernity@163.com>
2025-10-08 17:56:40 +0800

    [fix][client] Make auto partitions update work for old brokers without PIP-344 (#24822)
    (cherry picked from commit 5e59d0e164d5c255021302394d9ad70583fa565a)

--- pulsar-broker/src/test/java/org/apache/pulsar/client/impl/ClientCnxTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/client/impl/ClientCnxTest.java
@@ -156,7 +156,7 @@ public class ClientCnxTest extends MockedPulsarServiceBaseTest {
             field.set(clientCnxFuture.get(), false);
         }
         try {
-            clientWitBinaryLookup.getPartitionsForTopic(topic, false).join();
+            clientWitBinaryLookup.getPartitionedTopicMetadata(topic, false, false).join();
             Assert.fail("Expected an error that the broker version is too old.");
         } catch (Exception ex) {
             Assert.assertTrue(ex.getMessage().contains("without auto-creation is not supported by the broker"));
