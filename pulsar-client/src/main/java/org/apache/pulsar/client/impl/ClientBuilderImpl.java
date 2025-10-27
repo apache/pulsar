79784c00154a33b9d01e8c28c8860918fedf977f
Ruimin MA <maruimin666@gmail.com>
2025-10-27 22:37:40 +0800

    [improve][client]Add null check for Pulsar client clock configuration (#24848)
    (cherry picked from commit 54db7f2828fbc38a9e341b11a263716bf52fb55a)

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientBuilderImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ClientBuilderImpl.java
@@ -365,6 +365,7 @@ public class ClientBuilderImpl implements ClientBuilder {
 
     @Override
     public ClientBuilder clock(Clock clock) {
+        checkArgument(clock != null, "clock must not be null");
         conf.setClock(clock);
         return this;
     }
