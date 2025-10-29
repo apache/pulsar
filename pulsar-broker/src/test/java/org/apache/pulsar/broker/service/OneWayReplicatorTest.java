79b916b61fa8283fd4f2b207c5c5299d7ec85b5b
Lari Hotari <lhotari@apache.org>
2025-10-29 13:44:26 +0200

    [improve][ci] Move replication tests to broker group 5 in Pulsar CI

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/OneWayReplicatorTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/OneWayReplicatorTest.java
@@ -124,7 +124,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Slf4j
-@Test(groups = "broker")
+@Test(groups = "broker-replication")
 public class OneWayReplicatorTest extends OneWayReplicatorTestBase {
 
     @Override
