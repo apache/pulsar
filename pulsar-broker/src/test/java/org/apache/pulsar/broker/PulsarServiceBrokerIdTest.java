a97a9dd9aa0bf63de9e239650643ecca00f1304e
Lari Hotari <lhotari@users.noreply.github.com>
2025-11-04 20:33:27 +0200

    [improve][broker] Add tests for using absolute FQDN for advertisedAddress and remove extra dot from brokerId (#24787)
    (cherry picked from commit a1ed5c4a26029a6c6d6cb455ee653f133d651ce7)
    (cherry picked from commit a3d9c531bc174692481997f96e9239f37a562f18)

---
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/PulsarServiceBrokerIdTest.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.broker;
+
+import java.net.InetAddress;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.broker.auth.MockedPulsarServiceBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Slf4j
+public class PulsarServiceBrokerIdTest extends MockedPulsarServiceBaseTest {
+    private String hostAddress;
+
+    @BeforeMethod
+    @Override
+    protected void setup() throws Exception {
+        super.internalSetup();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    @Override
+    protected void cleanup() throws Exception {
+        super.internalCleanup();
+    }
+
+    @Override
+    protected void doInitConf() throws Exception {
+        super.doInitConf();
+        hostAddress = InetAddress.getLocalHost().getCanonicalHostName();
+        // make it an absolute FQDN by adding a trailing dot
+        conf.setAdvertisedAddress(hostAddress + ".");
+    }
+
+    @Test
+    public void testBrokerIdDoesntContainTrailingDot() throws Exception {
+        Assert.assertEquals(pulsar.getBrokerId(), hostAddress + ":" + conf.getWebServicePort().get());
+    }
+}
