6c17641d91a78cec3e24a6a5f94f6eb164a2e536
Dream95 <864197662@qq.com>
2025-11-10 20:10:11 +0800

    [fix][test] Fix invalid test NonPersistentTopicTest.testProducerRateLimit (#24957)
    Signed-off-by: Dream95 <zhou_8621@163.com>
    (cherry picked from commit 0896c0a5fe23c57179faf8752b416bee5e43b6a2)
    (cherry picked from commit 66b7f7c3b0c9e9dcf50fad0f9685ca4d281d0b9f)

--- pulsar-broker/src/test/java/org/apache/pulsar/client/api/NonPersistentTopicTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/client/api/NonPersistentTopicTest.java
@@ -380,13 +380,14 @@ public class NonPersistentTopicTest extends ProducerConsumerBase {
             var producerName = BrokerTestUtil.newUniqueName("testProducerRateLimit");
             @Cleanup
             Producer<byte[]> producer = pulsarClient.newProducer().topic(topic).producerName(producerName).create();
-            byte[] msgData = "testData".getBytes();
             final int totalProduceMessages = 500;
             CountDownLatch latch = new CountDownLatch(totalProduceMessages);
             for (int i = 0; i < totalProduceMessages; i++) {
+                final int messageIndex = i;
                 executor.submit(() -> {
                     try {
-                        producer.send(msgData);
+                        String message = "my-message-" + messageIndex;
+                        producer.send(message.getBytes());
                     } catch (Exception e) {
                         log.error("Failed to send message", e);
                         failed.set(true);
