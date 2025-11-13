b926603beec38efb0a67885db1cd3bc1ff3c16f6
manas-ctds <ext-manas.kaushik@datastax.com>
2025-11-13 16:53:37 +0530

    Remove non-pulsar files added from cherry-picks

--- run.py
+++
@@ -1,7 +0,0 @@
-# 影响力指数算法（权重可调）
-def calculate_impact(pr):
-    return (pr['comments']*0.3 + pr['reviews']*0.5 + pr['derived_commits']*1.2) * (1 + 0.1*pr['years_active'])
-
-# 使用本地git数据替代API
-git log --merges --grep "Merge pull request #" --format="%H %s" | awk '{print $5}' | cut -c2- > merged_prs.txt
-
