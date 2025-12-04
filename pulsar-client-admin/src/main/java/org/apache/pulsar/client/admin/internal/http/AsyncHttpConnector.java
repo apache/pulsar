734a42cc5c35c39908cbc4586b56ae40814a5b01
oneby-wang <onebywang@qq.com>
2025-12-04 22:10:35 +0800

    [fix][admin] remove jersey client param in AsyncHttpConnector constructor

--- pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/http/AsyncHttpConnector.java
+++ pulsar-client-admin/src/main/java/org/apache/pulsar/client/admin/internal/http/AsyncHttpConnector.java
@@ -53,7 +53,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
 import javax.ws.rs.ProcessingException;
-import javax.ws.rs.client.Client;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response.Status;
 import lombok.Data;
@@ -116,9 +115,8 @@ public class AsyncHttpConnector implements Connector, AsyncHttpRequestExecutor {
     private final Map<String, ConcurrencyReducer<Response>> concurrencyReducers = new ConcurrentHashMap<>();
     private PulsarSslFactory sslFactory;
 
-    public AsyncHttpConnector(Client client, ClientConfigurationData conf, int autoCertRefreshTimeSeconds,
+    public AsyncHttpConnector(ClientConfigurationData conf, int autoCertRefreshTimeSeconds,
                               boolean acceptGzipCompression) {
-        // client is not used, but we need to keep it for compatibility with old code
         this(conf.getConnectionTimeoutMs(), conf.getReadTimeoutMs(), conf.getRequestTimeoutMs(),
                 autoCertRefreshTimeSeconds, conf, acceptGzipCompression, null);
     }
