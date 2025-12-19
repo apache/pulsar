9f6a4ede8142afdecd971a4db2398ebf7430ca24
Lari Hotari <lhotari@apache.org>
2025-12-19 11:08:15 +0200

    [fix][ci] Move flaky asyncMarkDeleteBlocking tests to flaky group

--- managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/ManagedCursorTest.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/ManagedCursorTest.java
@@ -1350,7 +1350,7 @@ public class ManagedCursorTest extends MockedBookKeeperTestCase {
         assertEquals(c4.getMarkDeletedPosition(), p1);
     }
 
-    @Test
+    @Test(groups = "flaky")
     public void asyncMarkDeleteBlocking() throws Exception {
         ManagedLedgerConfig config = new ManagedLedgerConfig();
         config.setMaxEntriesPerLedger(10);
