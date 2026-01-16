0eec79fc9586b77390f2dc30919c44e20a55eb22
Lari Hotari <lhotari@users.noreply.github.com>
2026-01-16 15:54:59 +0200

    [fix][fn] Fix graceful Pulsar Function shutdown so that consumers and producers are closed (#25157)
    (cherry picked from commit 0bf560fc0cacf75a31723fe96ecd48f6c1e3383b)
    (cherry picked from commit dfb172e6b64da6c8094d3e54a181c6c9bc6930f3)

--- pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstanceRunnable.java
+++ pulsar-functions/instance/src/main/java/org/apache/pulsar/functions/instance/JavaInstanceRunnable.java
@@ -359,6 +359,8 @@ public class JavaInstanceRunnable implements AutoCloseable, Runnable {
             if (stats != null) {
                 stats.incrSysExceptions(t);
             }
+            // clear possible thread interrupted state so that closing can be handled gracefully
+            Thread.interrupted();
         } finally {
             log.info("Closing instance");
             close();
