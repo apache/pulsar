496cbecacc0afd9b2baebe8bdad08d3e1e1cf8b3
Penghui Li <penghui@apache.org>
2026-01-16 01:55:31 -0800

    [fix][ml] Retry offload reads when OffloadReadHandleClosedException is encountered (#25148)
    (cherry picked from commit 16bcec35cb99b8f9666e1517b1af1be1f5d40c57)
    (cherry picked from commit ccaa970361e448405f295c7e73895696bfdceed4)

--- managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
+++ managed-ledger/src/main/java/org/apache/bookkeeper/mledger/impl/ManagedLedgerImpl.java
@@ -2084,6 +2084,11 @@ public class ManagedLedgerImpl implements ManagedLedger, CreateCallback {
         return Optional.ofNullable(ledgers.get(ledgerId));
     }
 
+    public CompletableFuture<ReadHandle> reopenReadHandle(long ledgerId) {
+        invalidateReadHandle(ledgerId);
+        return getLedgerHandle(ledgerId);
+    }
+
     CompletableFuture<ReadHandle> getLedgerHandle(long ledgerId) {
         CompletableFuture<ReadHandle> ledgerHandle = ledgerCache.get(ledgerId);
         if (ledgerHandle != null) {
