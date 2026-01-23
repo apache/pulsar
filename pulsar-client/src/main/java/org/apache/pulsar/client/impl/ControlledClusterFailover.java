7a2c1ea980ed8b30c4c7661129fbec3321bae999
Wenzhi Feng <thetumbled@apache.org>
2026-01-23 17:42:32 +0800

    [fix][client] ControlledClusterFailover avoid unnecessary reconnection. (#25178)
    Co-authored-by: fengwenzhi <fengwenzhi.max@bigo.sg>

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ControlledClusterFailover.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ControlledClusterFailover.java
@@ -44,6 +44,7 @@ import org.apache.pulsar.client.api.AuthenticationFactory;
 import org.apache.pulsar.client.api.ControlledClusterFailoverBuilder;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.ServiceUrlProvider;
+import org.apache.pulsar.client.impl.conf.ClientConfigurationData;
 import org.apache.pulsar.client.util.ExecutorProvider;
 import org.apache.pulsar.common.util.ObjectMapperFactory;
 import org.asynchttpclient.AsyncHttpClient;
@@ -118,6 +119,15 @@ public class ControlledClusterFailover implements ServiceUrlProvider {
                 .setNameResolver(pulsarClient.getNameResolver())
                 .addHeader("Accept", "application/json");
         headers.forEach(requestBuilder::addHeader);
+
+        // Initialize currentControlledConfiguration from client's current configuration
+        // to avoid unnecessary reconnection on first scheduled check when the configuration hasn't changed
+        ClientConfigurationData conf = pulsarClient.getConfiguration();
+        this.currentControlledConfiguration = new ControlledConfiguration();
+        this.currentControlledConfiguration.setServiceUrl(currentPulsarServiceUrl);
+        this.currentControlledConfiguration.setTlsTrustCertsFilePath(conf.getTlsTrustCertsFilePath());
+        this.currentControlledConfiguration.setAuthPluginClassName(conf.getAuthPluginClassName());
+        this.currentControlledConfiguration.setAuthParamsString(conf.getAuthParams());
 
         // start to check service url every 30 seconds
         this.executor.scheduleAtFixedRate(catchingAndLoggingThrowables(() -> {
