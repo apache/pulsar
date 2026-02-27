9467a9f512d61fcca84acd0e0aad99be5ce0f532
Oneby Wang <891734032@qq.com>
2026-02-27 11:55:19 +0800

    add testAnalyzeBacklog() method

---
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/cli/topic/AnalyzeBacklogTest.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pulsar.tests.integration.cli.topic;
+
+import static org.testng.AssertJUnit.assertEquals;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import lombok.Cleanup;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.common.stats.AnalyzeSubscriptionBacklogResult;
+import org.apache.pulsar.common.util.FutureUtil;
+import org.apache.pulsar.tests.integration.docker.ContainerExecResult;
+import org.apache.pulsar.tests.integration.suites.PulsarTestSuite;
+import org.testng.annotations.Test;
+
+public class AnalyzeBacklogTest extends PulsarTestSuite {
+
+    private static final String PREFIX = "PULSAR_PREFIX_";
+    private static final String ANALYZE_BACKLOG_TOPIC_NAME = "tenant/ns/analyze-backlog-topic";
+    private static final String ANALYZE_BACKLOG_SUBSCRIPTION_NAME = "sub1";
+    private static final int SUBSCRIPTION_BACKLOG_SCAN_MAX_ENTRIES = 10;
+
+    @Override
+    public void setupCluster() throws Exception {
+        brokerEnvs.put(PREFIX + "subscriptionBacklogScanMaxEntries",
+                String.valueOf(SUBSCRIPTION_BACKLOG_SCAN_MAX_ENTRIES));
+        super.setupCluster();
+    }
+
+    @Test
+    public void testAnalyzeBacklog() throws Exception {
+        prepareSubscriptionBacklog(SUBSCRIPTION_BACKLOG_SCAN_MAX_ENTRIES + 1);
+
+        ContainerExecResult result =
+                pulsarCluster.runAdminCommandOnAnyBroker("analyze-backlog", ANALYZE_BACKLOG_TOPIC_NAME, "-s",
+                        ANALYZE_BACKLOG_SUBSCRIPTION_NAME);
+        AnalyzeSubscriptionBacklogResult backlogResult =
+                jsonMapper().readValue(result.getStdout(), AnalyzeSubscriptionBacklogResult.class);
+
+        assertEquals(SUBSCRIPTION_BACKLOG_SCAN_MAX_ENTRIES, backlogResult.getEntries());
+    }
+
+    private void prepareSubscriptionBacklog(int backlogNum) throws Exception {
+        @Cleanup
+        PulsarClient client = PulsarClient.builder().serviceUrl(pulsarCluster.getPlainTextServiceUrl()).build();
+        @Cleanup
+        Producer<byte[]> producer = client.newProducer().topic(ANALYZE_BACKLOG_TOPIC_NAME).create();
+        @Cleanup
+        Consumer<byte[]> consumer = client.newConsumer().topic(ANALYZE_BACKLOG_TOPIC_NAME)
+                .subscriptionName(ANALYZE_BACKLOG_SUBSCRIPTION_NAME).subscribe();
+
+        List<CompletableFuture<MessageId>> futures = new ArrayList<>(backlogNum);
+        for (int i = 0; i < backlogNum; i++) {
+            byte[] msgBytes = ("test" + i).getBytes(StandardCharsets.UTF_8);
+            CompletableFuture<MessageId> future = producer.sendAsync(msgBytes);
+            futures.add(future);
+        }
+        FutureUtil.waitForAll(futures).get();
+    }
+
+}
