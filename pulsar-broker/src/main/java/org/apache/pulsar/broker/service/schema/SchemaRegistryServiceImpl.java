0b10b08809547534e1b237e9536a3744cd63e811
Jiwei Guo <technoboy@apache.org>
2026-04-08 16:34:47 +0800

    [fix][broker] Change the schema incompatible log from ERROR to WARN level (#25483)
    (cherry picked from commit 0b73b3a346c3f6c66107123f73bc098b53b0113d)
    (cherry picked from commit 43505863ebdda07c729195d596fe25a53ebe18d3)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/schema/SchemaRegistryServiceImpl.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/schema/SchemaRegistryServiceImpl.java
@@ -233,7 +233,11 @@ public class SchemaRegistryServiceImpl implements SchemaRegistryService {
                 }))).whenComplete((v, ex) -> {
                     var latencyMs = this.clock.millis() - start.getValue();
                     if (ex != null) {
-                        log.error("[{}] Put schema failed", schemaId, ex);
+                        if (ex instanceof IncompatibleSchemaException) {
+                            log.warn("[{}] Put schema failed due to incompatible schema", schemaId, ex);
+                        } else {
+                            log.error("[{}] Put schema failed", schemaId, ex);
+                        }
                         if (start.getValue() != 0) {
                             this.stats.recordPutFailed(schemaId, latencyMs);
                         }
@@ -461,7 +465,7 @@ public class SchemaRegistryServiceImpl implements SchemaRegistryService {
                 CompletableFuture<Void> result = new CompletableFuture<>();
                 result.whenComplete((__, t) -> {
                     if (t != null) {
-                        log.error("[{}] Schema is incompatible", schemaId);
+                        log.warn("[{}] Schema is incompatible", schemaId);
                         this.stats.recordSchemaIncompatible(schemaId);
                     } else {
                         if (log.isDebugEnabled()) {
@@ -498,7 +502,7 @@ public class SchemaRegistryServiceImpl implements SchemaRegistryService {
         result.whenComplete((v, t) -> {
             if (t != null) {
                 this.stats.recordSchemaIncompatible(schemaId);
-                log.error("[{}] Schema is incompatible, schema type {}", schemaId, schema.getType());
+                log.warn("[{}] Schema is incompatible, schema type {}", schemaId, schema.getType());
             } else {
                 this.stats.recordSchemaCompatible(schemaId);
                 if (log.isDebugEnabled()) {
