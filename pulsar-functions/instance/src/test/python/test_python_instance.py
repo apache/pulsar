c0d6a5cea1bb35f3f1f9a47fdf58f5560c5ce258
Rui Fu <rfu@apache.org>
2026-08-20 22:37:48 +0800

    [fix][fn] Honor producer batching configuration in Python runtime

--- pulsar-functions/instance/src/test/python/test_python_instance.py
+++ pulsar-functions/instance/src/test/python/test_python_instance.py
@@ -37,7 +37,9 @@ from pulsar import Message
 import Function_pb2
 import log
 import os
+import pulsar
 import unittest
+import util
 
 class TestContextImpl(unittest.TestCase):
 
@@ -149,3 +151,208 @@ class TestPropertiesForwarding(unittest.TestCase):
     self.assertNotIn("custom-key", kwargs['properties'])
     self.assertIn("__pfn_input_topic__", kwargs['properties'])
 
+
+class TestProducerConfigFromSpec(unittest.TestCase):
+
+  def test_defaults_when_no_producer_spec(self):
+    function_details = Function_pb2.FunctionDetails()
+    self.assertEqual(util.producer_config_from_function_details(function_details), {
+      "batching_enabled": True,
+      "batching_max_publish_delay_ms": 10,
+    })
+
+  def test_empty_batching_spec_keeps_defaults(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchingSpec.SetInParent()
+    self.assertTrue(function_details.sink.producerSpec.HasField("batchingSpec"))
+    config = util.producer_config_from_function_details(function_details)
+    self.assertTrue(config["batching_enabled"])
+    self.assertEqual(config["batching_max_publish_delay_ms"], 10)
+
+  def test_batching_can_be_disabled(self):
+    function_details = Function_pb2.FunctionDetails()
+    batching_spec = function_details.sink.producerSpec.batchingSpec
+    batching_spec.enabled = False
+    batching_spec.batchingMaxPublishDelayMs = 10
+    config = util.producer_config_from_function_details(function_details)
+    self.assertFalse(config["batching_enabled"])
+    self.assertEqual(config["batching_max_publish_delay_ms"], 10)
+
+  def test_full_batching_spec_is_translated(self):
+    function_details = Function_pb2.FunctionDetails()
+    batching_spec = function_details.sink.producerSpec.batchingSpec
+    batching_spec.enabled = True
+    batching_spec.batchingMaxPublishDelayMs = 1
+    batching_spec.batchingMaxMessages = 500
+    batching_spec.batchingMaxBytes = 65536
+    batching_spec.batchBuilder = "KEY_BASED"
+    self.assertEqual(util.producer_config_from_function_details(function_details), {
+      "batching_enabled": True,
+      "batching_max_publish_delay_ms": 1,
+      "batching_max_messages": 500,
+      "batching_max_allowed_size_in_bytes": 65536,
+      "batching_type": pulsar.BatchingType.KeyBased,
+    })
+
+  def test_non_positive_values_keep_defaults(self):
+    function_details = Function_pb2.FunctionDetails()
+    batching_spec = function_details.sink.producerSpec.batchingSpec
+    batching_spec.enabled = True
+    batching_spec.batchingMaxPublishDelayMs = 0
+    batching_spec.batchingMaxMessages = 0
+    batching_spec.batchingMaxBytes = 0
+    config = util.producer_config_from_function_details(function_details)
+    self.assertEqual(config["batching_max_publish_delay_ms"], 10)
+    self.assertNotIn("batching_max_messages", config)
+    self.assertNotIn("batching_max_allowed_size_in_bytes", config)
+
+  def test_pending_message_limits_are_translated(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.maxPendingMessages = 2000
+    function_details.sink.producerSpec.maxPendingMessagesAcrossPartitions = 8000
+    config = util.producer_config_from_function_details(function_details)
+    self.assertEqual(config["max_pending_messages"], 2000)
+    self.assertEqual(config["max_pending_messages_across_partitions"], 8000)
+
+  def test_pending_message_limits_are_omitted_when_unset(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchingSpec.enabled = True
+    config = util.producer_config_from_function_details(function_details)
+    self.assertNotIn("max_pending_messages", config)
+    self.assertNotIn("max_pending_messages_across_partitions", config)
+
+  def test_producer_spec_batch_builder_is_honoured(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchBuilder = "KEY_BASED"
+    config = util.producer_config_from_function_details(function_details)
+    self.assertEqual(config["batching_type"], pulsar.BatchingType.KeyBased)
+
+  def test_batching_spec_batch_builder_overrides_producer_spec(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchBuilder = "KEY_BASED"
+    function_details.sink.producerSpec.batchingSpec.enabled = True
+    function_details.sink.producerSpec.batchingSpec.batchBuilder = "DEFAULT"
+    config = util.producer_config_from_function_details(function_details)
+    self.assertEqual(config["batching_type"], pulsar.BatchingType.Default)
+
+  def test_unknown_batch_builder_falls_back_to_default(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchBuilder = "UNKNOWN"
+    config = util.producer_config_from_function_details(function_details)
+    self.assertEqual(config["batching_type"], pulsar.BatchingType.Default)
+
+  def test_unsupported_round_robin_frequency_is_ignored(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchingSpec.enabled = True
+    function_details.sink.producerSpec.batchingSpec.roundRobinRouterBatchingPartitionSwitchFrequency = 7
+    config = util.producer_config_from_function_details(function_details)
+    self.assertNotIn("round_robin_router_batching_partition_switch_frequency", config)
+
+  def test_none_function_details_keeps_defaults(self):
+    self.assertEqual(util.producer_config_from_function_details(None), {
+      "batching_enabled": True,
+      "batching_max_publish_delay_ms": 10,
+    })
+
+
+class TestSinkProducerBatchingConfig(unittest.TestCase):
+
+  def _create_producer_kwargs(self, function_details):
+    pulsar_client = Mock()
+    pulsar_client.create_producer.return_value = Mock()
+    instance = PythonInstance('test_instance', 'test_func', '1.0', function_details, 100, 30,
+                              'user_code', pulsar_client, Mock(), 'test_cluster', 'test_url', None)
+    instance.get_schema = Mock(return_value="DEFAULT_SCHEMA")
+    instance.get_crypto_reader = Mock(return_value=None)
+    instance.setup_producer()
+    _, kwargs = pulsar_client.create_producer.call_args
+    return kwargs
+
+  def test_defaults_are_unchanged_without_a_producer_spec(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.topic = "test_sink_topic"
+    kwargs = self._create_producer_kwargs(function_details)
+    self.assertTrue(kwargs["batching_enabled"])
+    self.assertEqual(kwargs["batching_max_publish_delay_ms"], 10)
+    self.assertTrue(kwargs["block_if_queue_full"])
+
+  def test_batching_disabled_reaches_the_producer(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.topic = "test_sink_topic"
+    function_details.sink.producerSpec.batchingSpec.enabled = False
+    function_details.sink.producerSpec.batchingSpec.batchingMaxPublishDelayMs = 10
+    kwargs = self._create_producer_kwargs(function_details)
+    self.assertFalse(kwargs["batching_enabled"])
+
+  def test_batching_settings_reach_the_producer(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.topic = "test_sink_topic"
+    batching_spec = function_details.sink.producerSpec.batchingSpec
+    batching_spec.enabled = True
+    batching_spec.batchingMaxPublishDelayMs = 2
+    batching_spec.batchingMaxMessages = 100
+    batching_spec.batchingMaxBytes = 4096
+    function_details.sink.producerSpec.maxPendingMessages = 500
+    kwargs = self._create_producer_kwargs(function_details)
+    self.assertTrue(kwargs["batching_enabled"])
+    self.assertEqual(kwargs["batching_max_publish_delay_ms"], 2)
+    self.assertEqual(kwargs["batching_max_messages"], 100)
+    self.assertEqual(kwargs["batching_max_allowed_size_in_bytes"], 4096)
+    self.assertEqual(kwargs["max_pending_messages"], 500)
+
+  def test_key_based_batch_builder_reaches_the_producer(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.topic = "test_sink_topic"
+    function_details.sink.producerSpec.batchBuilder = "KEY_BASED"
+    kwargs = self._create_producer_kwargs(function_details)
+    self.assertEqual(kwargs["batching_type"], pulsar.BatchingType.KeyBased)
+
+
+class TestContextPublishBatchingConfig(unittest.TestCase):
+
+  def _create_producer_kwargs(self, function_details):
+    instance_config = InstanceConfig('test_instance_id', 'test_function_id', 'test_function_version',
+                                     function_details, 100)
+    pulsar_client = Mock()
+    producer = Mock()
+    producer.send_async = Mock(return_value=None)
+    pulsar_client.create_producer = Mock(return_value=producer)
+    context_impl = ContextImpl(instance_config, log.Log, pulsar_client, __file__, None, None, None, None, None)
+
+    msg = Message()
+    msg.message_id = Mock(return_value="test_message_id")
+    msg.partition_key = Mock(return_value="test_key")
+    context_impl.set_current_message_context(msg, "test_topic_name")
+    context_impl.publish("test_topic_name", "test_message")
+
+    _, kwargs = pulsar_client.create_producer.call_args
+    return kwargs
+
+  def test_defaults_are_unchanged_without_a_producer_spec(self):
+    kwargs = self._create_producer_kwargs(Function_pb2.FunctionDetails())
+    self.assertTrue(kwargs["batching_enabled"])
+    self.assertEqual(kwargs["batching_max_publish_delay_ms"], 10)
+    self.assertTrue(kwargs["block_if_queue_full"])
+
+  def test_batching_disabled_reaches_the_producer(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchingSpec.enabled = False
+    function_details.sink.producerSpec.batchingSpec.batchingMaxPublishDelayMs = 10
+    kwargs = self._create_producer_kwargs(function_details)
+    self.assertFalse(kwargs["batching_enabled"])
+
+  def test_batching_settings_reach_the_producer(self):
+    function_details = Function_pb2.FunctionDetails()
+    batching_spec = function_details.sink.producerSpec.batchingSpec
+    batching_spec.enabled = True
+    batching_spec.batchingMaxPublishDelayMs = 5
+    batching_spec.batchingMaxMessages = 250
+    kwargs = self._create_producer_kwargs(function_details)
+    self.assertEqual(kwargs["batching_max_publish_delay_ms"], 5)
+    self.assertEqual(kwargs["batching_max_messages"], 250)
+
+  def test_batch_builder_reaches_the_producer(self):
+    function_details = Function_pb2.FunctionDetails()
+    function_details.sink.producerSpec.batchBuilder = "KEY_BASED"
+    kwargs = self._create_producer_kwargs(function_details)
+    self.assertEqual(kwargs["batching_type"], pulsar.BatchingType.KeyBased)
