c67607ee22caa8e7f97c0a56b7b258886418dccd
Lari Hotari <lhotari@users.noreply.github.com>
2026-09-14 22:59:57 +0300

    [improve][broker] Disable subscription-thread dispatch by default (#26578)
    (cherry picked from commit c613d254c271154530a3fb5ebe4fdecf540fbef9)

--- pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
+++ pulsar-broker-common/src/main/java/org/apache/pulsar/broker/ServiceConfiguration.java
@@ -1308,7 +1308,7 @@ public class ServiceConfiguration implements PulsarConfiguration {
             category = CATEGORY_SERVER,
             doc = "Dispatch messages and execute broker side filters in a per-subscription thread"
     )
-    private boolean dispatcherDispatchMessagesInSubscriptionThread = true;
+    private boolean dispatcherDispatchMessagesInSubscriptionThread = false;
 
     @FieldContext(
         dynamic = false,
